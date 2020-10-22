becdcf837eead39feed8703664b59ec65050e81f
Rich Harris <richard.a.harris@gmail.com>
2020-10-22 18:12:42 -0400

    prettier

--- packages/app-utils/src/files/index.spec.ts
+++ packages/app-utils/src/files/index.spec.ts
@@ -11,117 +11,76 @@ let source_dir;
 let dest_dir;
 
 suite_copy.before.each(() => {
-  const temp_dir = mkdtempSync(join(tmpdir(), 'app-utils-'));
-  source_dir = join(temp_dir, 'source');
-  dest_dir = join(temp_dir, 'dest');
-  mkdirSync(source_dir);
-  mkdirSync(dest_dir);
+	const temp_dir = mkdtempSync(join(tmpdir(), 'app-utils-'));
+	source_dir = join(temp_dir, 'source');
+	dest_dir = join(temp_dir, 'dest');
+	mkdirSync(source_dir);
+	mkdirSync(dest_dir);
 });
 
 suite_copy('without filter', () => {
-  writeFileSync(join(source_dir, 'file-one.js'), '');
-  writeFileSync(join(source_dir, 'file-two.css'), '');
-  writeFileSync(join(source_dir, 'file-three'), '');
+	writeFileSync(join(source_dir, 'file-one.js'), '');
+	writeFileSync(join(source_dir, 'file-two.css'), '');
+	writeFileSync(join(source_dir, 'file-three'), '');
 
-  copy(
-    source_dir,
-    dest_dir
-  );
+	copy(source_dir, dest_dir);
 
-  const copied = readdirSync(dest_dir);
+	const copied = readdirSync(dest_dir);
 
-  assert.equal(
-    copied.sort(),
-    [
-      'file-one.js',
-      'file-two.css',
-      'file-three'
-    ].sort()
-  );
+	assert.equal(copied.sort(), ['file-one.js', 'file-two.css', 'file-three'].sort());
 });
 
 suite_copy('filters out subdirectory contents', () => {
-  writeFileSync(join(source_dir, 'file-one.js'), '');
-  writeFileSync(join(source_dir, 'file-two.css'), '');
-  mkdirSync(join(source_dir, 'no-copy'));
-  writeFileSync(join(source_dir, 'no-copy', 'do-not-copy.js'), '');
+	writeFileSync(join(source_dir, 'file-one.js'), '');
+	writeFileSync(join(source_dir, 'file-two.css'), '');
+	mkdirSync(join(source_dir, 'no-copy'));
+	writeFileSync(join(source_dir, 'no-copy', 'do-not-copy.js'), '');
 
-  copy(
-    source_dir,
-    dest_dir,
-    f => f === 'source'
-  );
+	copy(source_dir, dest_dir, f => f === 'source');
 
-  const copied = readdirSync(dest_dir);
+	const copied = readdirSync(dest_dir);
 
-  assert.equal(
-    copied.sort(),
-    [
-      'file-one.js',
-      'file-two.css',
-      'no-copy'
-    ].sort()
-  );
+	assert.equal(copied.sort(), ['file-one.js', 'file-two.css', 'no-copy'].sort());
 });
 
 suite_copy('copies recursively', () => {
-  writeFileSync(join(source_dir, 'file-one.js'), '');
-  writeFileSync(join(source_dir, 'file-two.css'), '');
-  const sub_dir = join(source_dir, 'deep');
-  mkdirSync(sub_dir);
-  writeFileSync(join(sub_dir, 'a.js'), '');
-  writeFileSync(join(sub_dir, 'b.js'), '');
+	writeFileSync(join(source_dir, 'file-one.js'), '');
+	writeFileSync(join(source_dir, 'file-two.css'), '');
+	const sub_dir = join(source_dir, 'deep');
+	mkdirSync(sub_dir);
+	writeFileSync(join(sub_dir, 'a.js'), '');
+	writeFileSync(join(sub_dir, 'b.js'), '');
 
-  copy(
-    source_dir,
-    dest_dir
-  );
+	copy(source_dir, dest_dir);
 
-  const root = readdirSync(dest_dir);
+	const root = readdirSync(dest_dir);
 
-  assert.equal(
-    root.sort(),
-    [
-      'file-one.js',
-      'file-two.css',
-      'deep'
-    ].sort()
-  );
+	assert.equal(root.sort(), ['file-one.js', 'file-two.css', 'deep'].sort());
 
-  const subdir = readdirSync(sub_dir);
+	const subdir = readdirSync(sub_dir);
 
-  assert.equal(
-    subdir.sort(),
-    [
-      'a.js',
-      'b.js'
-    ].sort()
-  )
+	assert.equal(subdir.sort(), ['a.js', 'b.js'].sort());
 });
 
-
 suite_copy('returns a list of copied files', () => {
-  writeFileSync(join(source_dir, 'file-one.js'), '');
-  writeFileSync(join(source_dir, 'file-two.css'), '');
-  const sub_dir = join(source_dir, 'deep');
-  mkdirSync(sub_dir);
-  writeFileSync(join(sub_dir, 'a.js'), '');
-  writeFileSync(join(sub_dir, 'b.js'), '');
+	writeFileSync(join(source_dir, 'file-one.js'), '');
+	writeFileSync(join(source_dir, 'file-two.css'), '');
+	const sub_dir = join(source_dir, 'deep');
+	mkdirSync(sub_dir);
+	writeFileSync(join(sub_dir, 'a.js'), '');
+	writeFileSync(join(sub_dir, 'b.js'), '');
 
-  const file_list = copy(
-    source_dir,
-    dest_dir
-  );
+	const file_list = copy(source_dir, dest_dir);
 
-  assert.equal(
-    file_list.sort(),
-    [
-      join(dest_dir, 'file-one.js'),
-      join(dest_dir, 'file-two.css'),
-      join(dest_dir, 'deep/a.js'),
-      join(dest_dir, 'deep/b.js')
-    ].sort()
-  );
+	assert.equal(
+		file_list.sort(),
+		[
+			join(dest_dir, 'file-one.js'),
+			join(dest_dir, 'file-two.css'),
+			join(dest_dir, 'deep/a.js'),
+			join(dest_dir, 'deep/b.js')
+		].sort()
+	);
 });
 
 suite_copy.run();
