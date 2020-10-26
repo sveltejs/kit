b44537682498ed7a21fbf0b7274588e69c022480
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2020-10-26 18:58:04 +0200

    build types on prepublish

--- packages/kit/src/ambient.d.ts
+++ packages/kit/src/ambient.d.ts
@@ -2,6 +2,7 @@ declare module "MANIFEST" {
   export const components: any[];
   export const routes: any[];
   export const Layout: any;
+  export const ErrorComponent: any;
 }
 
 declare module "ROOT" {
