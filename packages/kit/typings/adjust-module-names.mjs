54b8512a89e8a1f508d24ff20572fc934c935ed0
Rich Harris <richard.a.harris@gmail.com>
2020-10-29 21:26:11 -0400

    use sync methods

--- packages/kit/typings/adjust-module-names.mjs
+++ packages/kit/typings/adjust-module-names.mjs
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,13 +7,6 @@ const module_name = '$app';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const typings_file = join(__dirname, '..', 'index.d.ts');
 
-function handle_error(e) {
-	if (e) {
-		console.error(e.message);
-		process.exit(1);
-	}
-}
-
 function add_module_name_after(str, in_str) {
 	return in_str.replace(
 		new RegExp(`${str}(?!${module_name.replace('$', '\\$')})`, 'g'),
@@ -21,15 +14,9 @@ function add_module_name_after(str, in_str) {
 	);
 }
 
-readFile(typings_file, 'utf8', (err, data) => {
-	if (err) {
-		handle_error(err);
-	} else {
-		writeFile(
-			join(__dirname, '..', 'index.d.ts'),
-			add_module_name_after(' module "', add_module_name_after(' from "', data)),
-			'utf8',
-			handle_error
-		);
-	}
-});
+const data = readFileSync(typings_file, 'utf8');
+writeFileSync(
+	typings_file,
+	add_module_name_after(' module "', add_module_name_after(' from "', data)),
+	'utf8'
+);
