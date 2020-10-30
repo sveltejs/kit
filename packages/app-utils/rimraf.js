9dd7875e151c9feb09467600b82999a4939a853b
Antony Jones <antony@beyonk.com>
2020-10-30 18:19:21 +0000

    semis

--- packages/app-utils/rimraf.js
+++ packages/app-utils/rimraf.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const rm = fs.rm || fs.rmdir
-const [ , , ...filenames ] = process.argv
+const rm = fs.rm || fs.rmdir;
+const [ , , ...filenames ] = process.argv;
 filenames.forEach(filename => rm(filename, { recursive: true, force: true }, err => {
     if (err) {
       throw err;
