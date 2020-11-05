1d2b89e2583f0c4cabbef7c894badee5e2af3a32
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2020-11-05 09:17:10 +0200

    tsdoc comment

--- packages/app-utils/src/types.d.ts
+++ packages/app-utils/src/types.d.ts
@@ -71,8 +71,9 @@ export interface PageManifestPart {
 export interface PageManifest {
 	pattern: RegExp;
 	path: string;
-	// each part contains the parameters for the page (last part) or layout (earlier parts) corresponding to a URL segment
-	// any part except the last may be null if there is no layout for that segment.
+	/** Each part contains the parameters for the page (last part) or layout (earlier parts)
+	 * corresponding to a URL segment any part except the last may be null
+	 * if there is no layout for that segment. */
 	parts: Array<PageManifestPart | null>;
 }
 
