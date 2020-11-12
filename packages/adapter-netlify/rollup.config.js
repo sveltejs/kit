cf05d4731df0a3fb23f4dc40aa01210a2692ab11
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-11-12 15:09:44 -0800

    Fix TypeScript TODOs

--- packages/adapter-netlify/rollup.config.js
+++ packages/adapter-netlify/rollup.config.js
@@ -1,7 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-// TODO: see if we can get rollup-plugin-typescript2 working to standardize on Rollup TypeScript plugin used
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
 export default {
 	input: 'src/index.ts',
