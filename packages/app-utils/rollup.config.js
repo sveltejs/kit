3342312cfe26b9eb41f5a175936ad22aacc5f09b
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2020-11-14 15:19:05 +0200

    building types as part of rollup build

--- packages/app-utils/rollup.config.js
+++ packages/app-utils/rollup.config.js
@@ -1,6 +1,8 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
+const fs = require('fs');
+const path = require('path');
 
 const input = {};
 Object.keys(pkg.exports).forEach(key => {
@@ -27,10 +29,27 @@ export default {
 	],
 	plugins: [
 		nodeResolve(),
-		typescript()
+		typescript({ useTsconfigDeclarationDir: true }),
+		{
+			name: 'copy-types',
+			resolveId: () => null,
+			load: () => null,
+			writeBundle: (...args) => {
+				copyRecursiveSync('build/types', '.');
+				copyRecursiveSync('src/types', 'types');
+			}
+		}
 	],
-	external: [
-		...require('module').builtinModules,
-		...Object.keys(pkg.dependencies)
-	]
+	external: [...require('module').builtinModules, ...Object.keys(pkg.dependencies)]
 };
+
+function copyRecursiveSync(src, dest) {
+	if (fs.existsSync(src) && fs.statSync(src).isDirectory()) {
+		fs.mkdirSync(dest, { recursive: true });
+		fs.readdirSync(src).forEach(file =>
+			copyRecursiveSync(path.join(src, file), path.join(dest, file))
+		);
+	} else {
+		fs.copyFileSync(src, dest);
+	}
+}
