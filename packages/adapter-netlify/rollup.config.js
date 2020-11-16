9b0e15fa0774a84ede36ed313c84b5233946f0f7
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-11-16 12:01:43 -0800

    Convert more adapter files to TypeScript

--- packages/adapter-netlify/rollup.config.js
+++ packages/adapter-netlify/rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
-	input: 'src/index.ts',
+	input: 'src/render.ts',
 	output: {
 		file: 'render.js',
 		format: 'cjs'
