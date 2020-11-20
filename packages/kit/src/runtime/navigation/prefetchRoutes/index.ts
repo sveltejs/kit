c31462aff8218bcbe38210e678605f1973019ac0
Rich Harris <richard.a.harris@gmail.com>
2020-11-20 09:12:53 -0500

    use Promise.all instead of reduce

--- packages/kit/src/runtime/navigation/prefetchRoutes/index.ts
+++ packages/kit/src/runtime/navigation/prefetchRoutes/index.ts
@@ -1,12 +1,11 @@
 import { components, routes } from 'MANIFEST';
 
-export default function prefetchRoutes(pathnames: string[]): Promise<void> {
-	const path_routes = 
-		pathnames ? routes.filter(route => pathnames.some(pathname => route.pattern.test(pathname))) : routes;
+export default async function prefetchRoutes(pathnames: string[]): Promise<void> {
+	const path_routes = pathnames
+		? routes.filter(route => pathnames.some(pathname => route.pattern.test(pathname)))
+		: routes;
 
-	return path_routes.reduce<Promise<any>>(
-		(promise, route) =>
-			promise.then(() => Promise.all(route.parts.map(part => part && components[part.i]()))),
-		Promise.resolve()
-	);
+	const promises = path_routes.map(r => Promise.all(r.parts.map(p => p && components[p.i]())));
+
+	await Promise.all(promises);
 }
