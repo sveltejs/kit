f32f59978c7b10dbe30355da1e4b4d917c7cd1d4
Rich Harris <richard.a.harris@gmail.com>
2020-12-03 12:12:08 -0500

    transform exported functions correctly - fixes #224

--- packages/kit/src/api/dev/loader.js
+++ packages/kit/src/api/dev/loader.js
@@ -1,9 +1,6 @@
 import { URL } from 'url';
 import { resolve, relative } from 'path';
-import * as meriyah from 'meriyah';
-import MagicString from 'magic-string';
-import { extract_names } from 'periscopic';
-import { walk } from 'estree-walker';
+import { transform } from './transform';
 
 // This function makes it possible to load modules from the 'server'
 // snowpack server, for the sake of SSR
@@ -47,7 +44,6 @@ export default function loader(snowpack, config) {
 	});
 
 	async function load(url, url_stack) {
-		// TODO: meriyah (JS parser) doesn't support `import.meta.hot = ...` used in HMR setup code.
 		if (url.endsWith('.css.proxy.js')) {
 			return null;
 		}
@@ -75,133 +71,7 @@ export default function loader(snowpack, config) {
 	}
 
 	async function initialize_module(url, data, url_stack) {
-		const code = new MagicString(data);
-		const ast = meriyah.parseModule(data, {
-			ranges: true,
-			next: true
-		});
-
-		const imports = [];
-
-		const export_from_identifiers = new Map();
-		let uid = 1;
-
-		ast.body.forEach((node) => {
-			if (node.type === 'ImportDeclaration') {
-				imports.push(node);
-				code.remove(node.start, node.end);
-			}
-
-			if (node.type === 'ExportAllDeclaration') {
-				if (!export_from_identifiers.has(node.source)) {
-					export_from_identifiers.set(node.source, `__import${uid++}`);
-				}
-
-				code.overwrite(
-					node.start,
-					node.end,
-					`Object.assign(exports, ${export_from_identifiers.get(node.source)})`
-				);
-				imports.push(node);
-			}
-
-			if (node.type === 'ExportDefaultDeclaration') {
-				code.overwrite(node.start, node.declaration.start, 'exports.default = ');
-			}
-
-			if (node.type === 'ExportNamedDeclaration') {
-				if (node.source) {
-					imports.push(node);
-
-					if (!export_from_identifiers.has(node.source)) {
-						export_from_identifiers.set(node.source, `__import${uid++}`);
-					}
-				}
-
-				if (node.specifiers && node.specifiers.length > 0) {
-					code.remove(node.start, node.specifiers[0].start);
-
-					node.specifiers.forEach((specifier) => {
-						const lhs = `exports.${specifier.exported.name}`;
-						const rhs = node.source
-							? `${export_from_identifiers.get(node.source)}.${specifier.local.name}`
-							: specifier.local.name;
-
-						code.overwrite(specifier.start, specifier.end, `${lhs} = ${rhs}`);
-					});
-
-					code.remove(node.specifiers[node.specifiers.length - 1].end, node.end);
-				} else {
-					// `export const foo = ...` or `export function foo() {...}`
-					if (node.declaration.type === 'VariableDeclaration') {
-						code.remove(node.start, node.declaration.start);
-
-						const names = [];
-						node.declaration.declarations.forEach((declarator) => {
-							names.push(...extract_names(declarator.id));
-						});
-
-						code.appendLeft(node.end, names.map((name) => ` exports.${name} = ${name};`).join(''));
-					} else {
-						code.overwrite(
-							node.start,
-							node.declaration.start,
-							`exports.${node.declaration.id.name} = `
-						);
-					}
-				}
-			}
-		});
-
-		// replace import.meta and import(dynamic)
-		if (/import\s*\.\s*meta/.test(data) || /import\s*\(/.test(data)) {
-			walk(ast.body, {
-				enter(node) {
-					if (node.type === 'MetaProperty' && node.meta.name === 'import') {
-						code.overwrite(node.start, node.end, '__importmeta__');
-					} else if (node.type === 'ImportExpression') {
-						code.overwrite(node.start, node.start + 6, '__import__');
-					}
-				}
-			});
-		}
-
-		const deps = [];
-		imports.forEach((node) => {
-			const promise = get_module(url, node.source.value, url_stack);
-
-			if (node.type === 'ExportAllDeclaration' || node.type === 'ExportNamedDeclaration') {
-				// `export * from './other.js'` or `export { foo } from './other.js'`
-				deps.push({
-					name: export_from_identifiers.get(node.source),
-					promise
-				});
-			} else if (node.specifiers.length === 0) {
-				// bare import
-				deps.push({
-					name: null,
-					promise
-				});
-			} else if (node.specifiers[0].type === 'ImportNamespaceSpecifier') {
-				deps.push({
-					name: node.specifiers[0].local.name,
-					promise
-				});
-			} else {
-				deps.push(
-					...node.specifiers.map((specifier) => ({
-						name: specifier.local.name,
-						promise: promise.then(
-							(exports) => exports[specifier.imported ? specifier.imported.name : 'default']
-						)
-					}))
-				);
-			}
-		});
-
-		deps.sort((a, b) => (!!a.name !== !!b.name ? (a.name ? -1 : 1) : 0));
-
-		code.append(`\n//# sourceURL=${url}`);
+		const { code, deps } = transform(url, data);
 
 		const fn = new Function(
 			'exports',
@@ -210,9 +80,15 @@ export default function loader(snowpack, config) {
 			'__import__',
 			'__importmeta__',
 			...deps.map((d) => d.name).filter(Boolean),
-			code.toString()
+			`${code}\n//# sourceURL=${url}`
 		);
-		const values = await Promise.all(deps.map((d) => d.promise));
+
+		const promises = deps.map(async (dep) => {
+			const mod = await get_module(url, dep.source);
+			return dep.prop ? mod[dep.prop] : dep;
+		});
+
+		const values = await Promise.all(promises);
 
 		const exports = {};
 
