18b5c3477f9ef8c3ce4bf00d8648de0b78774684
Antony Jones <antony@beyonk.com>
2020-12-07 23:24:38 +0000

    prettier

--- packages/adapter-vercel/files/index.js
+++ packages/adapter-vercel/files/index.js
@@ -5,20 +5,20 @@ const { get_body } = require('@sveltejs/app-utils/http');
 const app = require('./server/app.js');
 
 module.exports = async (req, res) => {
-  const { pathname, query = '' } = parse(req.url || '');
-  
+	const { pathname, query = '' } = parse(req.url || '');
+
 	const rendered = await app.render({
 		host: null, // TODO
 		method: req.method,
 		headers: req.headers,
 		path: pathname,
-    query: new URLSearchParams(query),
-    body: await get_body(req)
+		query: new URLSearchParams(query),
+		body: await get_body(req)
 	});
 
 	if (rendered) {
-    const { status, headers, body } = rendered;
-    return res.writeHead(status, headers).end(body);
+		const { status, headers, body } = rendered;
+		return res.writeHead(status, headers).end(body);
 	}
 
 	return res.writeHead(404).end();
--- packages/adapter-vercel/src/index.js
+++ packages/adapter-vercel/src/index.js
@@ -1,41 +1,43 @@
 const { copyFileSync, writeFileSync, mkdirSync } = require('fs');
 const { resolve, join } = require('path');
-const { exec } = require('child_process')
+const { exec } = require('child_process');
 
-module.exports = async function adapter (builder) {
-  const vercel_output_directory = resolve('.vercel_build_output');
-  const config_directory = join(vercel_output_directory, 'config');
-  const static_directory = join(vercel_output_directory, 'static');
-  const lambda_directory = join(vercel_output_directory, 'functions', 'node', 'render');
-  const server_directory = join(lambda_directory, 'server');
-  
-  builder.log.info('Writing client application...');
-  builder.copy_static_files(static_directory);
-  builder.copy_client_files(static_directory);
+module.exports = async function adapter(builder) {
+	const vercel_output_directory = resolve('.vercel_build_output');
+	const config_directory = join(vercel_output_directory, 'config');
+	const static_directory = join(vercel_output_directory, 'static');
+	const lambda_directory = join(vercel_output_directory, 'functions', 'node', 'render');
+	const server_directory = join(lambda_directory, 'server');
 
-  builder.log.info('Building lambda...');
-  builder.copy_server_files(server_directory);
-  const fileList = [ 'index.js', 'package.json' ];
-  fileList.forEach(f => copyFileSync(resolve(join(__dirname, '..', 'files', f)), join(lambda_directory, f)));
-  exec('npm install', {
-    cwd: lambda_directory,
-    stdio: [ 1, 2, 3 ]
-  });
+	builder.log.info('Writing client application...');
+	builder.copy_static_files(static_directory);
+	builder.copy_client_files(static_directory);
 
-  builder.log.info('Prerendering static pages...');
+	builder.log.info('Building lambda...');
+	builder.copy_server_files(server_directory);
+	const fileList = ['index.js', 'package.json'];
+	fileList.forEach((f) =>
+		copyFileSync(resolve(join(__dirname, '..', 'files', f)), join(lambda_directory, f))
+	);
+	exec('npm install', {
+		cwd: lambda_directory,
+		stdio: [1, 2, 3]
+	});
+
+	builder.log.info('Prerendering static pages...');
 	await builder.prerender({
 		dest: static_directory
-  });
+	});
 
-  builder.log.info('Writing routes...');
-  mkdirSync(config_directory);
-  writeFileSync(
-    join(config_directory, 'routes.json'),
-    JSON.stringify([
-      {
-        src: '/.*',
-        dest: '.vercel/functions/render'
-      }
-    ])
-  );
+	builder.log.info('Writing routes...');
+	mkdirSync(config_directory);
+	writeFileSync(
+		join(config_directory, 'routes.json'),
+		JSON.stringify([
+			{
+				src: '/.*',
+				dest: '.vercel/functions/render'
+			}
+		])
+	);
 };
