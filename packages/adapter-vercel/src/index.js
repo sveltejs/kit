5aaa6c5c48883983e804b0c6fa9c8f1f03980be9
Antony Jones <antony@beyonk.com>
2020-12-07 12:18:37 +0000

    use client-facing path for handler

--- packages/adapter-vercel/src/index.js
+++ packages/adapter-vercel/src/index.js
@@ -29,7 +29,7 @@ module.exports = async function adapter (builder) {
       routes: [
         {
           src: '/.*',
-          dest: '/functions/node/render'
+          dest: '/functions/render'
         }
       ]
     })
