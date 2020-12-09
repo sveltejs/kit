9ef9932e769e2bdacac63d8668ef765265ef7bde
Rich Harris <richard.a.harris@gmail.com>
2020-12-09 15:49:52 -0500

    remove obsolete TODO

--- packages/adapter-static/index.js
+++ packages/adapter-static/index.js
@@ -1,8 +1,6 @@
 'use strict';
 
 module.exports = async function adapter(builder, { pages = 'build', assets = 'build' } = {}) {
-	// TODO implement adapter options, allow 'build' to be specified
-
 	builder.copy_static_files(assets);
 	builder.copy_client_files(assets);
 
