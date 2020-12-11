c06bee6010f501ee4b48828d96d22d1a3a906f45
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-12-11 08:36:23 -0800

    Fix css for dynamic imports in entry chunk

--- packages/kit/src/api/build/css_injection.js
+++ packages/kit/src/api/build/css_injection.js
@@ -111,7 +111,7 @@ export const css_injection = {
 
 				if (chunk_has_css) {
 					has_css = true;
-					chunk.code += `\nimport __inject_styles from './${inject_styles_file}';`;
+					chunk.code = `import __inject_styles from './${inject_styles_file}';\n` + chunk.code;
 				}
 			}
 		}
