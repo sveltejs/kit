965b1bafc7202952a4767b8c2c89d36d160721fb
Rich Harris <richard.a.harris@gmail.com>
2020-12-11 13:58:50 -0500

    render to <div id=svelte> by default (#263)
    * render to <div id=svelte> by default - closes #262
    
    * update examples

--- examples/hn.svelte.dev/svelte.config.js
+++ examples/hn.svelte.dev/svelte.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: '@sveltejs/adapter-netlify'
+		adapter: '@sveltejs/adapter-netlify',
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	}
 };
