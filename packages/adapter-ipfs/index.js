028a06d2f41f4ab4c10a4b496ef38554b0ffe742
Antony Jones <antony@beyonk.com>
2020-12-11 18:10:51 +0000

    prettier

--- packages/adapter-ipfs/index.js
+++ packages/adapter-ipfs/index.js
@@ -2,20 +2,23 @@
 
 const IpfsHttpClient = require('ipfs-http-client');
 
-module.exports = async function adapter(builder, { pages = 'build', assets = 'build', node = 'http://localhost:5001'} = {}) {
+module.exports = async function adapter(
+	builder,
+	{ pages = 'build', assets = 'build', node = 'http://localhost:5001' } = {}
+) {
 	builder.copy_static_files(assets);
 	builder.copy_client_files(assets);
 
 	await builder.prerender({
 		force: true,
 		dest: pages
-  });
-  
-  builder.log.minor('Connecting to IPFS...');
-  const ipfs = IpfsHttpClient(node);
+	});
 
-  for (const dir of new Set([ pages, assets ])) {
-    const { path, size, cid } = await ipfs.add(IpfsHttpClient.globSource(dir, { recursive: true }));
-    builder.log.minor(`Uploaded ${path}... ${size}b @ ${cid}`);
-  }
+	builder.log.minor('Connecting to IPFS...');
+	const ipfs = IpfsHttpClient(node);
+
+	for (const dir of new Set([pages, assets])) {
+		const { path, size, cid } = await ipfs.add(IpfsHttpClient.globSource(dir, { recursive: true }));
+		builder.log.minor(`Uploaded ${path}... ${size}b @ ${cid}`);
+	}
 };
