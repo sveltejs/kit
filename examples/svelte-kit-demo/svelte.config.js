528beee1e0d2a7586df35be7c8ce67a63b442c95
Rich Harris <richard.a.harris@gmail.com>
2020-12-11 13:49:53 -0500

    update examples

--- examples/svelte-kit-demo/svelte.config.js
+++ examples/svelte-kit-demo/svelte.config.js
@@ -3,6 +3,9 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: '@sveltejs/adapter-node'
+		adapter: '@sveltejs/adapter-node',
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	}
 };
