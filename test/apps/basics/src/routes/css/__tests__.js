913a13c326f458581244a26ef2571dc275df3b6f
Rich Harris <richard.a.harris@gmail.com>
2021-01-04 12:13:02 -0500

    unskip tests

--- test/apps/basics/src/routes/css/__tests__.js
+++ test/apps/basics/src/routes/css/__tests__.js
@@ -1,7 +1,7 @@
 import * as assert from 'uvu/assert';
 
 export default function (test) {
-	test.only('applies imported styles', async ({ visit, evaluate }) => {
+	test('applies imported styles', async ({ visit, evaluate }) => {
 		await visit('/css');
 
 		assert.equal(
