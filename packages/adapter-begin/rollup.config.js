93231a8e6775ffacb638f302ed2351dab3b5af10
Antony Jones <antony@beyonk.com>
2021-01-07 07:57:42 +0000

    fix fallout from rebasing

--- packages/adapter-begin/rollup.config.js
+++ packages/adapter-begin/rollup.config.js
@@ -2,13 +2,13 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-	input: 'src/index.js',
-	output: {
-		file: 'files/index.js',
-		format: 'cjs',
-		sourcemap: true,
-		exports: 'default'
-	},
-	plugins: [nodeResolve(), commonjs()],
-	external: require('module').builtinModules
+  input: 'src/index.js',
+  output: {
+    file: 'files/index.js',
+    format: 'cjs',
+    sourcemap: true,
+    exports: 'named'
+  },
+  plugins: [nodeResolve(), commonjs()],
+  external: [...require('module').builtinModules, '@architect/shared/app.js']
 };
--- packages/adapter-begin/src/index.js
+++ packages/adapter-begin/src/index.js
@@ -1,93 +1,46 @@
 'use strict';
 
-<<<<<<< HEAD
-const { render } = require('@sveltejs/app-utils/renderer');
-const client = require('@architect/shared/client.json');
-const manifest = require('@architect/shared/manifest.js');
-const App = require('@architect/shared/root.js');
-const template = require('@architect/shared/template.js');
-const static_assets = require('@architect/shared/static_assets.js');
-const setup = require('@architect/shared/setup.js');
-
-exports.handler = async function http(req) {
-	const { rawPath: path, requestContext, headers, queryStringParameters: query } = req;
-
-	const { method, domainName } = requestContext.http;
-
-	// TODO: We should handle static assets better than this
-	if (static_assets.includes(path)) {
-		return {
-			statusCode: 308,
-			headers: { location: `/_static${path}` }
-		};
-	}
-
-	const rendered = await render(
-		{
-			host: domainName,
-			method,
-			headers,
-			path,
-			// body, TODO: convert this file to typescript and utilize the body once we know what type it is and whether it first needs some conversion
-			query
-		},
-		{
-			template,
-			manifest,
-			client,
-			root: App,
-			setup,
-			load: (route) => require(`@architect/shared/routes/${route.name}.js`),
-			dev: false
-		}
-	);
-=======
 const url = require('url');
 const app = require('@architect/shared/app.js');
 
 exports.handler = async (event) => {
+  const {
+    host,
+    rawPath: path,
+    httpMethod,
+    headers,
+    queryStringParameters,
+    body
+  } = event;
 
-	const {
-		rawPath: path,
-		httpMethod,
-		headers,
-		queryStringParameters
-		// body, // TODO pass this to renderer
-		// isBase64Encoded // TODO is this useful?
-	} = event;
+  const query = new url.URLSearchParams();
+  for (const k in queryStringParameters) {
+    const value = queryStringParameters[k];
+    value.split(', ').forEach((v) => {
+      query.append(k, v);
+    });
+  }
 
-	const query = new url.URLSearchParams();
-	for (const k in queryStringParameters) {
-		const value = queryStringParameters[k];
-		value.split(', ').forEach((v) => {
-			query.append(k, v);
-		});
-	}
+  const rendered = await app.render({
+    host,
+    method: httpMethod,
+    headers,
+    path,
+    body,
+    query
+  });
 
-	const rendered = await app.render({
-		host: null, // TODO
-		method: httpMethod,
-		headers,
-		path,
-		query
-	});
->>>>>>> build the lambda
+  if (rendered) {
+    return {
+      isBase64Encoded: false,
+      statusCode: rendered.status,
+      headers: rendered.headers,
+      body: rendered.body
+    };
+  }
 
-	if (rendered) {
-		return {
-			isBase64Encoded: false,
-			statusCode: rendered.status,
-			headers: rendered.headers,
-			body: rendered.body
-		};
-	}
-
-	return {
-		statusCode: 404,
-<<<<<<< HEAD
-		body: 'Not Found'
-=======
-		body: 'Not found'
->>>>>>> build the lambda
-	};
+  return {
+    statusCode: 404,
+    body: 'Not Found'
+  };
 };
