feefd123b20a8cd0a736b992d16a325c680d0136
Antony Jones <antony@beyonk.com>
2021-01-13 23:39:00 +0000

    prettier

--- packages/adapter-begin/rollup.config.js
+++ packages/adapter-begin/rollup.config.js
@@ -2,13 +2,13 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/index.js',
-  output: {
-    file: 'files/index.js',
-    format: 'cjs',
-    sourcemap: true,
-    exports: 'named'
-  },
-  plugins: [nodeResolve(), commonjs()],
-  external: [...require('module').builtinModules, '@architect/shared/app.js']
+	input: 'src/index.js',
+	output: {
+		file: 'files/index.js',
+		format: 'cjs',
+		sourcemap: true,
+		exports: 'named'
+	},
+	plugins: [nodeResolve(), commonjs()],
+	external: [...require('module').builtinModules, '@architect/shared/app.js']
 };
--- packages/adapter-begin/src/index.js
+++ packages/adapter-begin/src/index.js
@@ -4,43 +4,36 @@ const url = require('url');
 const app = require('@architect/shared/app.js');
 
 exports.handler = async (event) => {
-  const {
-    host,
-    rawPath: path,
-    httpMethod,
-    headers,
-    queryStringParameters,
-    body
-  } = event;
+	const { host, rawPath: path, httpMethod, headers, queryStringParameters, body } = event;
 
-  const query = new url.URLSearchParams();
-  for (const k in queryStringParameters) {
-    const value = queryStringParameters[k];
-    value.split(', ').forEach((v) => {
-      query.append(k, v);
-    });
-  }
+	const query = new url.URLSearchParams();
+	for (const k in queryStringParameters) {
+		const value = queryStringParameters[k];
+		value.split(', ').forEach((v) => {
+			query.append(k, v);
+		});
+	}
 
-  const rendered = await app.render({
-    host,
-    method: httpMethod,
-    headers,
-    path,
-    body,
-    query
-  });
+	const rendered = await app.render({
+		host,
+		method: httpMethod,
+		headers,
+		path,
+		body,
+		query
+	});
 
-  if (rendered) {
-    return {
-      isBase64Encoded: false,
-      statusCode: rendered.status,
-      headers: rendered.headers,
-      body: rendered.body
-    };
-  }
+	if (rendered) {
+		return {
+			isBase64Encoded: false,
+			statusCode: rendered.status,
+			headers: rendered.headers,
+			body: rendered.body
+		};
+	}
 
-  return {
-    statusCode: 404,
-    body: 'Not Found'
-  };
+	return {
+		statusCode: 404,
+		body: 'Not Found'
+	};
 };
