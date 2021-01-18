0570c8c41112fa9e3ae3703a545065be5c1363b7
Rich Harris <richard.a.harris@gmail.com>
2021-01-18 15:07:26 -0500

    prevent svelte-announcer causing css glitch

--- packages/kit/src/core/create_app.js
+++ packages/kit/src/core/create_app.js
@@ -189,13 +189,15 @@ function generate_app(manifest_data) {
 
 		<style>
 			#svelte-announcer {
+				position: absolute;
+				left: 0;
+				top: 0;
 				clip: rect(0 0 0 0);
 				clip-path: inset(50%);
-				height: 1px;
 				overflow: hidden;
-				position: absolute;
 				white-space: nowrap;
 				width: 1px;
+				height: 1px;
 			}
 		</style>
 	`);
