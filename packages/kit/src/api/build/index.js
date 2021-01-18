550f93cd356116281566da91c0f44ef9ee5f90da
Rich Harris <richard.a.harris@gmail.com>
2021-01-18 15:20:35 -0500

    dont add redundant .js to urls

--- packages/kit/src/api/build/index.js
+++ packages/kit/src/api/build/index.js
@@ -222,7 +222,7 @@ export async function build(config) {
 		component_indexes.set(c.file, i);
 	});
 
-	const stringify_component = (c) => `() => import(${s(`.${c.url + '.js'}`)})`;
+	const stringify_component = (c) => `() => import(${s(`.${c.url}`)})`;
 
 	// TODO ideally we wouldn't embed the css_lookup, but this is the easiest
 	// way to be able to inline CSS into AMP documents. if we come up with
@@ -311,10 +311,7 @@ export async function build(config) {
 					${manifest.endpoints
 						.map((data) => {
 							const params = get_params(data.params);
-
-							// TODO clarify file renaming rules (i.e. why does foo.json.ts become foo.json.js and not foo.json.ts.js?)
-							// https://github.com/snowpackjs/snowpack/discussions/2260
-							const load = `() => import(${s(`.${data.url.replace(/\.\w+$/, '.js')}`)})`;
+							const load = `() => import(${s(`.${data.url}`)})`;
 
 							return `{ pattern: ${data.pattern}, params: ${params}, load: ${load} }`;
 						})
