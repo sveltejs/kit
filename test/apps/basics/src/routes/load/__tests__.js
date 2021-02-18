c7b1d525d7bccb2e929fdbd2329907ea88276a29
Tan Li Hau <lhtan93@gmail.com>
2021-02-18 18:17:45 +0800

    preserve context for navigation

--- test/apps/basics/src/routes/load/__tests__.js
+++ test/apps/basics/src/routes/load/__tests__.js
@@ -1,6 +1,6 @@
 import * as assert from 'uvu/assert';
 
-export default function(test, is_dev) {
+export default function (test, is_dev) {
 	test('loads', async ({ visit, contains }) => {
 		await visit('/load');
 		assert.ok(await contains('bar == bar'));
@@ -11,19 +11,20 @@ export default function(test, is_dev) {
 			await visit('/load/serialization');
 		});
 
-		const payload = '{"status":200,"statusText":"OK","headers":{"content-type":"application/json"},"body":"{\\"answer\\":42}"}';
+		const payload =
+			'{"status":200,"statusText":"OK","headers":{"content-type":"application/json"},"body":"{\\"answer\\":42}"}';
 
-		if (!js) { // by the time JS has run, hydration will have nuked these scripts
+		if (!js) {
+			// by the time JS has run, hydration will have nuked these scripts
 			const script_contents = await html('script[type="svelte-data"]');
 
-			assert.equal(
-				script_contents,
-				payload,
-				'Page should contain serialized data'
-			);
+			assert.equal(script_contents, payload, 'Page should contain serialized data');
 		}
 
-		assert.ok(!requests.some(r => r.endsWith('/load/serialization.json')), 'Should not load JSON over the wire');
+		assert.ok(
+			!requests.some((r) => r.endsWith('/load/serialization.json')),
+			'Should not load JSON over the wire'
+		);
 	});
 
 	test('prefers static data over endpoint', async ({ visit, text }) => {
@@ -31,14 +32,30 @@ export default function(test, is_dev) {
 		assert.equal(await text('h1'), 'static file');
 	});
 
-	test('context is inherited', async ({ visit, text }) => {
+	test('context is inherited', async ({ visit, text, html, js, goto }) => {
 		await visit('/load/context/a/b/c');
 		assert.equal(await text('h1'), 'message: original + new');
-		assert.equal(await text('pre'), JSON.stringify({
-			x: 'a',
-			y: 'b',
-			z: 'c'
-		}));
+		assert.equal(
+			await text('pre'),
+			JSON.stringify({
+				x: 'a',
+				y: 'b',
+				z: 'c'
+			})
+		);
+		if (js) {
+			await goto('/load/context/d/e/f');
+
+			assert.equal(await text('h1'), 'message: original + new');
+			assert.equal(
+				await text('pre'),
+				JSON.stringify({
+					x: 'd',
+					y: 'e',
+					z: 'f'
+				})
+			);
+		}
 	});
 
 	test('load function is only called when necessary', async ({ visit, goto, text, js }) => {
