5475c6cacf7eba14f5479e41999d6f76dc07b9f8
GrygrFlzr <grygrflzr@hotmail.com>
2021-02-21 13:10:25 +0700

    Fix prerender glob routes

--- packages/kit/src/api/adapt/prerender.js
+++ packages/kit/src/api/adapt/prerender.js
@@ -195,7 +195,8 @@ export async function prerender({ dir, out, log, config, force }) {
 			// TODO support other extensions, e.g. .svelte.md?
 			const entries = glob('**/*.svelte', { cwd: config.files.routes })
 				.map((file) => {
-					const parts = file.split('/'); // TODO is this true for glob results on windows?
+					// support both windows and unix glob results
+					const parts = file.split(/[/\\]/);
 
 					if (parts.some((part) => part[0] === '_' || /\[/.test(part))) {
 						return null;
