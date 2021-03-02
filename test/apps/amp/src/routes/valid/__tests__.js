8fef0c84f62396458fc679c489e7e382d333a977
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-03-01 19:54:31 -0800

    Add explanatory message for flaky test (#369)
    Co-authored-by: Rich Harris <richard.a.harris@gmail.com>

--- test/apps/amp/src/routes/valid/__tests__.js
+++ test/apps/amp/src/routes/valid/__tests__.js
@@ -1,14 +1,18 @@
 import * as assert from 'uvu/assert';
 
 export default function (test, is_dev) {
-	test('amp is true', async ({ visit, contains, $ }) => {
+	test('amp is true', async ({ $, contains, page, visit }) => {
 		await visit('/valid');
 
-		assert.ok(await contains(`Hello from the server in ${is_dev ? 'dev' : 'prod'} mode!`));
-		assert.ok(await contains('The answer is 42'));
-		assert.ok(await contains('amp is true'));
+		await assertContains(`Hello from the server in ${is_dev ? 'dev' : 'prod'} mode!`);
+		await assertContains('The answer is 42');
+		await assertContains('amp is true');
 
 		const script = await $('script[type="svelte-data"]');
 		assert.ok(!script, 'Should not include serialized data');
+
+		async function assertContains(message) {
+			assert.ok(await contains(message), `Did not find "${message}" in ${await page.innerHTML('body')}`);
+		}
 	});
 }
