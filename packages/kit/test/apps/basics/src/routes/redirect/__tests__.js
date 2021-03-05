53abe53951a8babc6abf5677e0d584130e16622c
Rich Harris <richard.a.harris@gmail.com>
2021-03-05 18:38:45 -0500

    add failing test for redirect loops

--- packages/kit/test/apps/basics/src/routes/redirect/__tests__.js
+++ packages/kit/test/apps/basics/src/routes/redirect/__tests__.js
@@ -10,4 +10,18 @@ export default function (test, is_dev) {
 		assert.equal(await page.url(), `${base}/redirect/b`);
 		assert.equal(await page.textContent('h1'), 'b');
 	});
+
+	test('prevents redirect loops', '/redirect', async ({ base, page, js }) => {
+		await page.click('[href="/redirect/loopy/a"]');
+
+		if (js) {
+			await page.waitForTimeout(50);
+
+			assert.equal(await page.url(), `${base}/redirect/loopy/a`);
+			assert.equal(await page.textContent('h1'), '500');
+			assert.equal(await page.textContent('#message'), 'Redirect loop');
+		} else {
+			assert.equal(await page.url(), 'chrome-error://chromewebdata/');
+		}
+	});
 }
--- packages/kit/test/apps/basics/src/routes/redirect/index.svelte
+++ packages/kit/test/apps/basics/src/routes/redirect/index.svelte
@@ -1,3 +1,6 @@
 <h1>redirects</h1>
 <a href="/redirect/a">a</a>
 <a href="/redirect/b">b</a>
+
+<a href="/redirect/loopy/a">a (loopy)</a>
+<a href="/redirect/loopy/b">b (loopy)</a>
