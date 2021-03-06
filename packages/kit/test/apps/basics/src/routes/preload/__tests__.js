07a18986943d6d11ee0b7fcdeabed25e0545a1a3
Rich Harris <richard.a.harris@gmail.com>
2021-03-05 22:59:48 -0500

    error on preload - closes #273

---
+++ packages/kit/test/apps/basics/src/routes/preload/__tests__.js
@@ -0,0 +1,15 @@
+import * as assert from 'uvu/assert';
+
+/** @type {import('../../../../../types').TestMaker} */
+export default function test(test) {
+	test.only('errors on preload', '/preload', async ({ page }) => {
+		await page.click('[href="/preload/uses-preload"]');
+
+		assert.equal(await page.textContent('h1'), '500');
+		assert.equal(await page.textContent('footer'), 'Custom layout');
+		assert.equal(
+			await page.textContent('#message'),
+			'This is your custom error page saying: "preload has been deprecated in favour of load. Please consult the documentation: https://kit.svelte.dev/docs#load"'
+		);
+	});
+}
---
+++ packages/kit/test/apps/basics/src/routes/preload/uses-preload.svelte
@@ -0,0 +1,5 @@
+<script context="module">
+	export function preload(){}
+</script>
+
+<h1>this should not be visible</h1>
