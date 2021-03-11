e158b9665739232a7ecc3971ed5e84315036b094
GrygrFlzr <grygrflzr@hotmail.com>
2021-03-11 21:16:00 +0700

    adapter-netlify: Use CJS entrypoint to load ESM files

--- packages/adapter-netlify/index.js
+++ packages/adapter-netlify/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { existsSync, readFileSync, copyFileSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, copyFileSync, writeFileSync, renameSync } from 'fs';
 import { dirname, resolve } from 'path';
 import toml from 'toml';
 import { fileURLToPath } from 'url';
@@ -37,8 +37,14 @@ export default async function adapter(builder) {
 	builder.copy_client_files(publish);
 	builder.copy_server_files(`${functions}/render`);
 
+	// rename app to .mjs
+	renameSync(`${functions}/render/app.js`, `${functions}/render/app.mjs`);
+
 	// copy the renderer
-	copyFileSync(resolve(__dirname, 'files/render.js'), `${functions}/render/index.js`);
+	copyFileSync(resolve(__dirname, 'files/render.js'), `${functions}/render/handler.mjs`);
+
+	// copy the entry point
+	copyFileSync(resolve(__dirname, 'files/index.cjs'), `${functions}/render/index.js`);
 
 	// create _redirects
 	writeFileSync(`${publish}/_redirects`, '/* /.netlify/functions/render 200');
