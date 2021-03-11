b4ebb60588439447bcecdb4e100fb7952b173491
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-03-11 08:12:58 -0800

    Update examples to use new redirect syntax (#482)

--- examples/realworld.svelte.dev/src/routes/profile/index.svelte
+++ examples/realworld.svelte.dev/src/routes/profile/index.svelte
@@ -1,10 +1,5 @@
 <script context="module">
 	export function load({ session }) {
-		return {
-			redirect: {
-				to: session.user ? `/profile/@${user.username}` : '/login',
-				status: 302
-			}
-		};
+		return { redirect: session.user ? `/profile/@${user.username}` : '/login', status: 302 };
 	}
 </script>
