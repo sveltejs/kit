8d39b31daa0aeb7afcdd8a3e0c6fb75131f25bd1
Shriji <shrijike@gmail.com>
2021-03-12 11:36:01 +0530

    titles on pages
    show the title instead of localhost:3000

--- examples/svelte-kit-demo/src/routes/index.svelte
+++ examples/svelte-kit-demo/src/routes/index.svelte
@@ -2,11 +2,19 @@
 	export const prerender = true;
 </script>
 
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
 <h1>Home</h1>
 
-<p>This is an example of using Snowpack for SSR. See <a href="https://gist.github.com/Rich-Harris/3b6f5f7d5dbfb9779a79f77352be6415">this gist</a> for more information</p>
+<p>
+	This is an example of using Snowpack for SSR. See <a
+		href="https://gist.github.com/Rich-Harris/3b6f5f7d5dbfb9779a79f77352be6415">this gist</a
+	> for more information
+</p>
 
-<img alt="Svelte logo" src="/logo.svg">
+<img alt="Svelte logo" src="/logo.svg" />
 
 <style>
 	h1 {
