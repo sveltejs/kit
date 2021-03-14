528548fbb138bc557706b0b1443c01712e4035d5
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-03-13 21:26:55 -0800

    Add ts-ignore to lines that should be ignored

--- packages/kit/src/core/build/index.js
+++ packages/kit/src/core/build/index.js
@@ -389,6 +389,7 @@ async function build_server(
 		// this API is marked as @alpha https://github.com/vitejs/vite/blob/27785f7fcc5b45987b5f0bf308137ddbdd9f79ea/packages/vite/src/node/config.ts#L129
 		// it's not exposed in the typescript definitions as a result
 		// so we need to ignore the fact that it's missing
+		// @ts-ignore
 		ssr: {
 			noExternal: ['svelte', '@sveltejs/kit']
 		},
