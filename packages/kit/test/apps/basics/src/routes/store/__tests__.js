42e88921c3a77678cae5463302fbbf93451040c9
Rich Harris <richard.a.harris@gmail.com>
2021-03-16 21:57:29 -0400

    remove a page.click

--- packages/kit/test/apps/basics/src/routes/store/__tests__.js
+++ packages/kit/test/apps/basics/src/routes/store/__tests__.js
@@ -2,12 +2,11 @@ import * as assert from 'uvu/assert';
 
 /** @type {import('../../../../../types').TestMaker} */
 export default function (test) {
-	test('page store functions as expected', '/store', async ({ page, js }) => {
-		await page.waitForLoadState();
+	test('page store functions as expected', '/store', async ({ page, click, js }) => {
 		assert.equal(await page.textContent('h1'), 'Test');
 		assert.equal(await page.textContent('h2'), 'Calls: 1');
 
-		await page.click('a[href="/store/result"]');
+		await click('a[href="/store/result"]');
 		assert.equal(await page.textContent('h1'), 'Result');
 		assert.equal(await page.textContent('h2'), js ? 'Calls: 1' : 'Calls: 0');
 
