911c15b24ad6d1a7521ca6dd4ee174e2066638dd
Rich Harris <richard.a.harris@gmail.com>
2021-03-23 20:39:20 -0400

    changeset, remove test.only

--- packages/kit/test/apps/basics/src/routes/__tests__.js
+++ packages/kit/test/apps/basics/src/routes/__tests__.js
@@ -2,7 +2,7 @@ import * as assert from 'uvu/assert';
 
 /** @type {import('../../../../types').TestMaker} */
 export default function (test) {
-	test.only('fetch in root index.svelte works', '/', async ({ page }) => {
+	test('fetch in root index.svelte works', '/', async ({ page }) => {
 		assert.equal(await page.textContent('h1'), 'the answer is 42');
 	});
 }
