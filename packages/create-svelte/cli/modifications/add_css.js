7d42f72ee89d83fec35a086ce9df7969c6bef3ce
Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-03-28 21:38:33 -0400

    Global stylesheets (during `create-svelte` depending on the chosen CSS preprocessor) (#726)
    * Add a global stylesheet during create-svelte depending on the chosen CSS preprocessor
    
    * Changeset for "Add a global stylesheet during `create-svelte` depending on the chosen CSS preprocessor"
    
    * Indent `$layout.svelte` using tabs instead of spaces
    
    * Switch `global.css` to `app.css`
    
    * Make examples use `app.css`

--- packages/create-svelte/cli/modifications/add_css.js
+++ packages/create-svelte/cli/modifications/add_css.js
@@ -1,6 +1,7 @@
 import { bold, green } from 'kleur/colors';
 import {
 	add_svelte_preprocess_to_config,
+	copy_from_template_additions,
 	update_component,
 	update_package_json_dev_deps
 } from './utils';
@@ -13,12 +14,15 @@ import {
  */
 export default async function add_css(cwd, which) {
 	if (which === 'css') {
+		copy_from_template_additions(cwd, ['src', 'app.css']);
 		console.log('You can add support for CSS preprocessors like SCSS/Less/PostCSS later.');
 	} else if (which === 'less') {
 		update_package_json_dev_deps(cwd, {
 			less: '^3.0.0',
 			'svelte-preprocess': '^4.0.0'
 		});
+		copy_from_template_additions(cwd, ['src', 'app.less']);
+		update_component(cwd, 'src/routes/$layout.svelte', [['../app.css', '../app.less']]);
 		update_component(cwd, 'src/lib/Counter.svelte', [['<style>', '<style lang="less">']]);
 		update_component(cwd, 'src/routes/index.svelte', [['<style>', '<style lang="less">']]);
 		add_svelte_preprocess_to_config(cwd);
@@ -35,6 +39,8 @@ export default async function add_css(cwd, which) {
 			sass: '^1.0.0',
 			'svelte-preprocess': '^4.0.0'
 		});
+		copy_from_template_additions(cwd, ['src', 'app.scss']);
+		update_component(cwd, 'src/routes/$layout.svelte', [['../app.css', '../app.scss']]);
 		update_component(cwd, 'src/lib/Counter.svelte', [['<style>', '<style lang="scss">']]);
 		update_component(cwd, 'src/routes/index.svelte', [['<style>', '<style lang="scss">']]);
 		add_svelte_preprocess_to_config(cwd);
