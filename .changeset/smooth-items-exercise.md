4543d6811e4be62717c0155f1e2a767655b3cc8e
Rich Harris <richard.a.harris@gmail.com>
2021-03-30 00:25:38 -0400

    prevent XSS when serializing fetch results - fixes #721

---
+++ .changeset/smooth-items-exercise.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/kit': patch
+---
+
+Prevent XSS when serializing fetch results
--- packages/kit/src/runtime/server/page.js
+++ packages/kit/src/runtime/server/page.js
@@ -179,7 +179,7 @@ async function get_response({ request, options, $session, route, status = 200, e
 					if (key === 'text') {
 						return async () => {
 							const text = await response.text();
-							inline.payload.body = text;
+							inline.payload.body = escape(text);
 							serialized_data.push(inline);
 							return text;
 						};
@@ -187,10 +187,7 @@ async function get_response({ request, options, $session, route, status = 200, e
 
 					if (key === 'json') {
 						return async () => {
-							const json = await response.json();
-							inline.payload.body = s(json);
-							serialized_data.push(inline);
-							return json;
+							return JSON.parse(await response.text());
 						};
 					}
 
@@ -544,4 +541,51 @@ function serialize_error(error) {
 		serialized = '{}';
 	}
 	return serialized;
+}
+
+/** @type {Record<string, string>} */
+const escaped = {
+	'<': '\\u003C',
+	'>': '\\u003E',
+	'/': '\\u002F',
+	'\\': '\\\\',
+	'\b': '\\b',
+	'\f': '\\f',
+	'\n': '\\n',
+	'\r': '\\r',
+	'\t': '\\t',
+	'\0': '\\0',
+	'\u2028': '\\u2028',
+	'\u2029': '\\u2029'
+};
+
+/** @param {string} str */
+function escape(str) {
+	let result = '"';
+
+	for (let i = 0; i < str.length; i += 1) {
+		const char = str.charAt(i);
+		const code = char.charCodeAt(0);
+
+		if (char === '"') {
+			result += '\\"';
+		} else if (char in escaped) {
+			result += escaped[char];
+		} else if (code >= 0xd800 && code <= 0xdfff) {
+			const next = str.charCodeAt(i + 1);
+
+			// If this is the beginning of a [high, low] surrogate pair,
+			// add the next two characters, otherwise escape
+			if (code <= 0xdbff && next >= 0xdc00 && next <= 0xdfff) {
+				result += char + str[++i];
+			} else {
+				result += `\\u${code.toString(16).toUpperCase()}`;
+			}
+		} else {
+			result += char;
+		}
+	}
+
+	result += '"';
+	return result;
 }
