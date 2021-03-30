36d9f2e6368e45a12afa21284b7fcf890922eb97
Rich Harris <richard.a.harris@gmail.com>
2021-03-30 00:10:57 -0400

    hmmm

--- packages/kit/test/apps/basics/src/routes/routing/disabled/__tests__.js
+++ packages/kit/test/apps/basics/src/routes/routing/disabled/__tests__.js
@@ -5,14 +5,14 @@ export default function (test, is_dev) {
 	test('disables router if router=false', '/routing/disabled/a', async ({ page, clicknav, js }) => {
 		if (js) {
 			await page.click('button');
-			await page.waitForTimeout(1);
+			await page.waitForTimeout(50);
 			assert.equal(await page.textContent('button'), 'clicks: 1');
 
 			await clicknav('[href="/routing/disabled/b"]');
 			assert.equal(await page.textContent('button'), 'clicks: 0');
 
 			await page.click('button');
-			await page.waitForTimeout(1);
+			await page.waitForTimeout(50);
 			assert.equal(await page.textContent('button'), 'clicks: 1');
 
 			await clicknav('[href="/routing/disabled/a"]');
