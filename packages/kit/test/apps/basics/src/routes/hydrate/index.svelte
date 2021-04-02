c8a4d9135c61f296598d69f9bbb5af13a729f177
Rich Harris <richard.a.harris@gmail.com>
2021-04-02 16:20:33 -0400

    only inline data if hydrate=true

--- packages/kit/test/apps/basics/src/routes/hydrate/index.svelte
+++ packages/kit/test/apps/basics/src/routes/hydrate/index.svelte
@@ -1,8 +1,23 @@
 <script context="module">
 	export const hydrate = false;
+
+	/** @type {import('../../../../../../types').Load} */
+	export async function load({ fetch }) {
+		const res = await fetch('/hydrate.json');
+
+		/** @type {any} */
+		const { type } = await res.json();
+
+		return {
+			props: { type }
+		}
+	}
 </script>
 
 <script>
+	/** @type {string} */
+	export let type;
+
 	let n = 0;
 </script>
 
@@ -10,4 +25,4 @@
 	clicks: {n}
 </button>
 
-<a href="/hydrate/other">other</a>
+<a href="/{type}/other">other</a>
