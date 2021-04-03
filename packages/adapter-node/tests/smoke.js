12d87985a31776d0a1f3076410f2d1c4b8d41d37
Sam Saccone <samccone@gmail.com>
2021-04-03 13:21:54 -0700

    Add test case for when a matching route is found.

--- packages/adapter-node/tests/smoke.js
+++ packages/adapter-node/tests/smoke.js
@@ -4,9 +4,10 @@ import * as assert from 'uvu/assert';
 import fetch from 'node-fetch';
 
 const { PORT = 3000 } = process.env;
+const DEFAULT_SERVER_OPTS = { render: () => {} };
 
-function startServer() {
-	const server = createServer({ render: () => {} });
+function startServer(opts = DEFAULT_SERVER_OPTS) {
+	const server = createServer(opts);
 	return new Promise((fulfil, reject) => {
 		server.listen(PORT, (err) => {
 			if (err) {
@@ -27,6 +28,21 @@ test('serves a 404', async () => {
 	const server = await startServer();
 	const res = await fetch(`http://localhost:${PORT}/nothing`);
 	assert.equal(res.status, 404);
+	server.server.close();
+});
+
+test('responses with the rendered status code', async () => {
+	const server = await startServer({
+		render: () => {
+			return {
+				headers: 'wow',
+				status: 203,
+				body: 'ok'
+			};
+		}
+	});
+	const res = await fetch(`http://localhost:${PORT}/wow`);
+	assert.equal(res.status, 203);
 	server.server.close();
 });
 
