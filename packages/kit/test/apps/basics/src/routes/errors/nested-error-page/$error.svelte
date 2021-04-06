210f1c7789de3d49bc001d9d7c756e5d939f4243
Rich Harris <richard.a.harris@gmail.com>
2021-04-05 21:08:06 -0400

    failing test for #626

---
+++ packages/kit/test/apps/basics/src/routes/errors/nested-error-page/$error.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+
+</script>
+
+<script>
+	/** @type {number} */
+	export let status;
+
+	/** @type {Error} */
+	export let error;
+</script>
+
+<h1>Nested error page</h1>
+<p id="nested-error-status">status: {status}</p>
+<p id="nested-error-message">error.message: {error.message}</p>
