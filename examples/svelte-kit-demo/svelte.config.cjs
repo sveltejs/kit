5f5523cb464f6c0231658b74676d334bfb1cfc27
Rafael Camara <rafaelcamaram@gmail.com>
2021-04-09 18:08:08 -0300

    Removing svelte-kit-demo in favor of new hello world page

--- examples/svelte-kit-demo/svelte.config.cjs
+++
@@ -1,9 +0,0 @@
-const adapter = require(process.env.ADAPTER || '@sveltejs/adapter-node');
-const options = JSON.stringify(process.env.OPTIONS || '{}');
-
-module.exports = {
-	kit: {
-		adapter: adapter(options),
-		target: '#svelte'
-	}
-};
