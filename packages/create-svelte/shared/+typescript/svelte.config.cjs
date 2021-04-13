3c41d0756678efe96c887df80205a00816845a0e
Rich Harris <richard.a.harris@gmail.com>
2021-04-13 10:51:47 -0400

    move preprocess option where it belongs

--- packages/create-svelte/shared/+typescript/svelte.config.cjs
+++ packages/create-svelte/shared/+typescript/svelte.config.cjs
@@ -4,15 +4,15 @@ const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: sveltePreprocess(),
+
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
 		adapter: node(),
-
-		// Consult https://github.com/sveltejs/svelte-preprocess
-		// for more information about preprocessors
-		preprocess: sveltePreprocess(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
