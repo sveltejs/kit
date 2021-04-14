614674cdaa21a362ff90db53ae4e6bd0f6bbb13e
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-04-14 09:59:47 +0700

    sync up handle hooks docs with types

--- packages/kit/types.d.ts
+++ packages/kit/types.d.ts
@@ -104,10 +104,7 @@ export type GetSession<Context = any, Session = any> = {
 	({ context }: { context: Context }): Session | Promise<Session>;
 };
 
-export type Handle<Context = any> = ({
-	request,
-	render
-}: {
+export type Handle<Context = any> = (handle: {
 	request: Request<Context>;
 	render: (request: Request<Context>) => Response | Promise<Response>;
 }) => Response | Promise<Response>;
