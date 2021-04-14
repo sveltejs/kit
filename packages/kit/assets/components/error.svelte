5ac4240ceff8cdc6d1bfa36d3037095483a4a8cc
GrygrFlzr <grygrflzr@hotmail.com>
2021-04-14 19:44:09 +0700

    Fix default

--- packages/kit/assets/components/error.svelte
+++ packages/kit/assets/components/error.svelte
@@ -1,3 +1,11 @@
+<script context="module">
+	export async function load({ error, status }) {
+		return {
+			props: { error, status }
+		};
+	}
+</script>
+
 <script>
 	export let status;
 	export let error;
