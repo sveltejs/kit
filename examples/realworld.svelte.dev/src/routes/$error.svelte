a0c02a4cdc9b46364ba8f1140fed3ed1b68d91f8
Snir David <snir@snir.dev>
2021-04-15 20:26:25 +0300

    Fix $error route in realworld example

--- examples/realworld.svelte.dev/src/routes/$error.svelte
+++ examples/realworld.svelte.dev/src/routes/$error.svelte
@@ -1,42 +1,21 @@
-<script>
-	import { dev } from '$app/env';
-
-	export let error, status;
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: { error, status }
+		};
+	}
 </script>
 
-<svelte:head>
-	<title>{status}</title>
-</svelte:head>
+<script>
+	export let status;
+	export let error;
+</script>
 
-<div class="col-md-9">
-	<h1>{status}</h1>
+<h1>{status}</h1>
 
-	<p>{error.message}</p>
+<p>{error.message}</p>
 
-	{#if dev && error.stack}
-		<pre>{error.stack}</pre>
-	{/if}
-</div>
-
-<style>
-	h1,
-	p {
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
+<!-- TODO figure out what to do with stacktraces in prod -->
+{#if error.stack}
+	<pre>{error.stack}</pre>
+{/if}
