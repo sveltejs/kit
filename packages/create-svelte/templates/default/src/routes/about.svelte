0688f8dce290d2a854b777f463d257dc85d44cd3
Rich Harris <richard.a.harris@gmail.com>
2021-04-15 08:45:04 -0400

    start fleshing out about page

--- packages/create-svelte/templates/default/src/routes/about.svelte
+++ packages/create-svelte/templates/default/src/routes/about.svelte
@@ -1,5 +1,23 @@
+<script context="module">
+	export const hydrate = false;
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<h1>About page</h1>
+<div class="content">
+	<h1>About this app</h1>
+
+	<p>This is a <a href="https://kit.svelte.dev">SvelteKit</a> app.</p>
+
+	<p>TODO add more more blurb</p>
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: 42rem;
+		margin: 4rem auto 0 auto;
+	}
+</style>
