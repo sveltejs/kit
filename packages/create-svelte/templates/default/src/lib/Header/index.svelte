20e9e68b59eb81195fb45db84f4a64cea4cce7d8
Rich Harris <richard.a.harris@gmail.com>
2021-04-15 08:42:10 -0400

    add sveltekit:prefetch to nav links

--- packages/create-svelte/templates/default/src/lib/Header/index.svelte
+++ packages/create-svelte/templates/default/src/lib/Header/index.svelte
@@ -15,9 +15,9 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z"/>
 		</svg>
 		<ul>
-			<li class:active={$page.path === '/'}><a href=".">Home</a></li>
-			<li class:active={$page.path === '/about'}><a href="about">About</a></li>
-			<li class:active={$page.path === '/blog'}><a href="blog">Blog</a></li>
+			<li class:active={$page.path === '/'}><a sveltekit:prefetch href=".">Home</a></li>
+			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="about">About</a></li>
+			<li class:active={$page.path === '/blog'}><a sveltekit:prefetch href="blog">Blog</a></li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z"/>
