e293cd4cb1bbf5be21010295d6af57a5b2ce74a8
Snir David <snir@snir.dev>
2021-04-16 18:15:01 +0300

    reinstate new lines

--- examples/realworld.svelte.dev/src/routes/$error.svelte
+++ examples/realworld.svelte.dev/src/routes/$error.svelte
@@ -8,6 +8,7 @@
 
 <script>
 	import { dev } from '$app/env';
+
 	export let error, status;
 </script>
 
@@ -30,14 +31,17 @@
 	p {
 		margin: 0 auto;
 	}
+
 	h1 {
 		font-size: 2.8em;
 		font-weight: 700;
 		margin: 0 0 0.5em 0;
 	}
+
 	p {
 		margin: 1em auto;
 	}
+
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 4em;
