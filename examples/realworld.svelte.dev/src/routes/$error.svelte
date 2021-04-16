d7636000a3c4259edd62583efbc326a956a33e88
Snir David <snir@snir.dev>
2021-04-16 14:04:52 +0300

    revert $error route, fix by adding the load function only, realworld example

--- examples/realworld.svelte.dev/src/routes/$error.svelte
+++ examples/realworld.svelte.dev/src/routes/$error.svelte
@@ -7,15 +7,40 @@
 </script>
 
 <script>
-	export let status;
-	export let error;
+	import { dev } from '$app/env';
+	export let error, status;
 </script>
 
-<h1>{status}</h1>
+<svelte:head>
+	<title>{status}</title>
+</svelte:head>
 
-<p>{error.message}</p>
+<div class="col-md-9">
+	<h1>{status}</h1>
 
-<!-- TODO figure out what to do with stacktraces in prod -->
-{#if error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+	<p>{error.message}</p>
+
+	{#if dev && error.stack}
+		<pre>{error.stack}</pre>
+	{/if}
+</div>
+
+<style>
+	h1,
+	p {
+		margin: 0 auto;
+	}
+	h1 {
+		font-size: 2.8em;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+	p {
+		margin: 1em auto;
+	}
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
