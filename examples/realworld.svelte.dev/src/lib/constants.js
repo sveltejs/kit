337a020bae5d7ff027d956f1594b995d2f7b3010
Rich Harris <richard.a.harris@gmail.com>
2021-04-17 19:17:23 -0400

    remove realworld example in favour of sveltejs/realworld — closes #703

--- examples/realworld.svelte.dev/src/lib/constants.js
+++
@@ -1,2 +0,0 @@
-export const page_size = 10;
-export const placeholder = 'https://static.productionready.io/images/smiley-cyrus.jpg';
--- examples/realworld.svelte.dev/src/routes/profile/@[user]/_load.js
+++
@@ -1,9 +0,0 @@
-export function create_load(endpoint) {
-	return async ({ page, fetch }) => {
-		const res = await fetch(`/profile/@${page.params.user}/${endpoint}.json`);
-
-		return {
-			props: await res.json()
-		};
-	};
-}
