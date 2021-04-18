daf69138e44d93c9d88fdb71d34c994c5dfb9df1
Conduitry <git@chor.date>
2021-04-18 12:09:18 -0400

    fix init command in starter app on about page (#1105)
    * fix init command in starter app on about page
    
    * add changeset

--- packages/create-svelte/templates/default/src/routes/about.svelte
+++ packages/create-svelte/templates/default/src/routes/about.svelte
@@ -21,14 +21,24 @@
 <div class="content">
 	<h1>About this app</h1>
 
-	<p>This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the following into your command line and following the prompts:</p>
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		following into your command line and following the prompts:
+	</p>
 
 	<!-- TODO lose the @next! -->
-	<pre>npx init svelte@next</pre>
+	<pre>npm init svelte@next</pre>
 
-	<p>The page you're looking at is purely static HTML, with no client-side interactivity needed. Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening the devtools network panel and reloading.</p>
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
 
-	<p>The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using it with JavaScript disabled!</p>
+	<p>
+		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
+		it with JavaScript disabled!
+	</p>
 </div>
 
 <style>
