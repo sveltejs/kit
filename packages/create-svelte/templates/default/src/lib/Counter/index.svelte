fb57addc26a445cbda3e5c20fac338e9d9e36b95
Geoff Rich <geoff.rich@alaskaair.com>
2021-04-20 08:35:56 -0700

    Hide out-of-view count from assistive tech

--- packages/create-svelte/templates/default/src/lib/Counter/index.svelte
+++ packages/create-svelte/templates/default/src/lib/Counter/index.svelte
@@ -15,21 +15,20 @@
 
 <div class="counter">
 	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
-		<svg role="img" aria-label="Minus" viewBox="0 0 1 1">
+		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5"></path>
 		</svg>
 	</button>
 
 	<div class="counter-viewport">
 		<div class="counter-digits" style="transform: translate(0, {100 * offset}%)">
-			<strong style="top: -100%">{Math.floor($displayed_count + 1)}</strong>
+			<strong style="top: -100%" aria-hidden="true">{Math.floor($displayed_count + 1)}</strong>
 			<strong>{Math.floor($displayed_count)}</strong>
 		</div>
 	</div>
 
 	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
-		<!-- <img src={plus} alt="Plus icon"> -->
-		<svg role="img" aria-label="Plus" viewBox="0 0 1 1">
+		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1"></path>
 		</svg>
 	</button>
