9c33c580b2b30b456d81aa1e3b30633adcd6e746
Conduitry <git@chor.date>
2021-04-23 17:53:43 -0400

    update ESLint plugin version in create-svelte template

--- packages/create-svelte/shared/+eslint+typescript/.eslintrc.cjs
+++ packages/create-svelte/shared/+eslint+typescript/.eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
-		'svelte3/typescript': require('typescript')
+		'svelte3/typescript': () => require('typescript')
 	},
 	parserOptions: {
 		sourceType: 'module',
