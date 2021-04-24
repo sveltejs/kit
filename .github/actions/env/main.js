eef759d2794167bdd50db9aa5bd90cba7c8de052
GrygrFlzr <grygrflzr@hotmail.com>
2021-04-24 15:51:00 +0700

    Attempt different path

--- .github/actions/env/main.js
+++ .github/actions/env/main.js
@@ -3,7 +3,7 @@ import { platform } from 'os';
 
 const WINDOWS_CACHE_PATH = 'D:\\.pnpm-store\\v3';
 const NIX_CACHE_PATH = '~/.pnpm-store/v3';
-const WINDOWS_BROWSER_PATH = '%USERPROFILE%\\AppData\\Local\\ms-playwright';
+const WINDOWS_BROWSER_PATH = '%LOCALAPPDATA%\\ms-playwright';
 const LINUX_BROWSER_PATH = '~/.cache/ms-playwright';
 const MACOS_BROWSER_PATH = '~/Library/Caches/ms-playwright';
 
