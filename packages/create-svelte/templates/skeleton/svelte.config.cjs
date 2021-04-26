7e74fa64b0e1673dba6d3da7f9b0e92cb0c9c487
ivoreis <reis.ivo@gmail.com>
2021-04-26 19:36:50 +0100

    chore: remove ssr noExternal to be match FAQs
    https://github.com/sveltejs/kit/commit/2e1b8c6fe8b96f56751b88d4374055d359788e03

--- packages/create-svelte/templates/skeleton/svelte.config.cjs
+++ packages/create-svelte/templates/skeleton/svelte.config.cjs
@@ -12,10 +12,6 @@ module.exports = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
+		vite: {}
 	}
 };
