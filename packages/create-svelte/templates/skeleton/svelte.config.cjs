c47b483313fcba7a0d4645195417d15732adcf09
Ivo Reis <1410207+ivoreis@users.noreply.github.com>
2021-04-26 20:10:31 +0100

    remove ssr.noExternal from skeleton template so it matches default template (#1234)
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- packages/create-svelte/templates/skeleton/svelte.config.cjs
+++ packages/create-svelte/templates/skeleton/svelte.config.cjs
@@ -10,12 +10,6 @@ module.exports = {
 		adapter: node(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
+		target: '#svelte'
 	}
 };
