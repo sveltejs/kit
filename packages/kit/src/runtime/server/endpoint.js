f5bd3b228b6fe2e92f77b12651851c74aec7cf7d
mrhumble <carlos@kwagei.com>
2021-04-28 10:12:28 +0000

    Return full obj if body exists

--- packages/kit/src/runtime/server/endpoint.js
+++ packages/kit/src/runtime/server/endpoint.js
@@ -37,9 +37,9 @@ export default async function render_route(request, route) {
 				body = JSON.stringify(body);
 			}
 
-			// (body ? { status, body, headers } : { status, headers})
+			const responseObj =  body ? { status, body, headers } : { status, headers};
 
-			return { status, body, headers };
+			return responseObj;
 		}
 	}
 }
