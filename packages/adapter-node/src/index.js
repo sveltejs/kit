e110fcaa6d952f405c370fe4056efd1b8d68d8ee
Stephen Anthony Uy <stephenu@alumni.uci.edu>
2021-05-06 13:35:05 -0700

    Makes Node adapter respect HOST (#1365)
    By default, the HOST variable is set to 127.0.0.1, for safety.

--- packages/adapter-node/src/index.js
+++ packages/adapter-node/src/index.js
@@ -2,9 +2,9 @@ import { createServer } from './server';
 /*eslint import/no-unresolved: [2, { ignore: ['\.\/app\.js$'] }]*/
 import * as app from './app.js';
 
-const { PORT = 3000 } = process.env; // TODO configure via svelte.config.js
+const { HOST='127.0.0.1', PORT = 3000 } = process.env; // TODO configure via svelte.config.js
 
-const instance = createServer({ render: app.render }).listen(PORT, (err) => {
+const instance = createServer({ render: app.render }).listen(PORT, HOST, (err) => {
 	if (err) {
 		console.log('error', err);
 	} else {
