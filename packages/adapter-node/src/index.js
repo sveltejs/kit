4fa5b418fcd6058d191e9d97d9d24b2f1501cd54
Stephen Anthony Uy <stephenu@alumni.uci.edu>
2021-05-06 15:16:41 -0700

    Fixes code formatting

--- packages/adapter-node/src/index.js
+++ packages/adapter-node/src/index.js
@@ -2,7 +2,7 @@ import { createServer } from './server';
 /*eslint import/no-unresolved: [2, { ignore: ['\.\/app\.js$'] }]*/
 import * as app from './app.js';
 
-const { HOST='0.0.0.0', PORT = 3000 } = process.env; // TODO configure via svelte.config.js
+const { HOST = '0.0.0.0', PORT = 3000 } = process.env; // TODO configure via svelte.config.js
 
 const instance = createServer({ render: app.render }).listen(PORT, HOST, (err) => {
 	if (err) {
