7fbc05de7a44f67a66af873c173e39b89b07ccc7
Rich Harris <richard.a.harris@gmail.com>
2021-05-09 12:44:02 -0400

    use shared hash helper

--- packages/kit/src/runtime/server/index.js
+++ packages/kit/src/runtime/server/index.js
@@ -3,6 +3,7 @@ import { render_response } from './page/render.js';
 import render_endpoint from './endpoint.js';
 import { parse_body } from './parse_body/index.js';
 import { lowercase_keys } from './utils.js';
+import { hash } from '../hash.js';
 
 /**
  * @param {import('types/hooks').Incoming} incoming
@@ -85,18 +86,4 @@ export async function respond(incoming, options, state = {}) {
 			body: options.dev ? e.stack : e.message
 		};
 	}
-}
-
-/** @param {string | Uint8Array} value */
-function hash(value) {
-	let hash = 5381;
-	let i = value.length;
-
-	if (typeof value === 'string') {
-		while (i) hash = (hash * 33) ^ value.charCodeAt(--i);
-	} else {
-		while (i) hash = (hash * 33) ^ value[--i];
-	}
-
-	return (hash >>> 0).toString(36);
 }
