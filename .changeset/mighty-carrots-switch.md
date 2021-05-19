5d318ec71ebbfe6248466c8a31f0341e90844570
Richard Burton <ryc76@users.noreply.github.com>
2021-05-19 04:23:24 +0100

    Allow passing HTTPS key pair in Vite section of config (#1456)
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

---
+++ .changeset/mighty-carrots-switch.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/kit': patch
+---
+
+Allow passing HTTPS key pair in Vite section of config
--- packages/kit/src/core/server/index.js
+++ packages/kit/src/core/server/index.js
@@ -1,20 +1,30 @@
 import http from 'http';
 import https from 'https';
-
 /**
  *
  * @param {number} port
  * @param {string} host
  * @param {boolean} use_https
+ * @param {any} user_config
  * @param {(req: http.IncomingMessage, res: http.ServerResponse) => void} handler
  * @returns {Promise<http.Server | https.Server>}
  */
-export async function get_server(port, host, use_https, handler) {
+export async function get_server(port, host, use_https, user_config, handler) {
 	/** @type {https.ServerOptions} */
 	const https_options = {};
 
 	if (use_https) {
-		https_options.key = https_options.cert = (await import('./cert')).createCertificate();
+		if (
+			user_config.server &&
+			user_config.server.https &&
+			user_config.server.https.key &&
+			user_config.server.https.cert
+		) {
+			https_options.key = user_config.server.https.key.toString();
+			https_options.cert = user_config.server.https.cert.toString();
+		} else {
+			https_options.key = https_options.cert = (await import('./cert')).createCertificate();
+		}
 	}
 
 	return new Promise((fulfil) => {
