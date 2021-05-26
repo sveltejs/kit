43615cefb763e42172702376277a5978177a9936
jthegedus <jthegedus@hey.com>
2021-05-27 08:55:27 +1000

    feat: expose ValidatedConfig for adapter consumption

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -3,7 +3,7 @@
 
 import './ambient-modules';
 
-export { Adapter, AdapterUtils, Config } from './config';
+export { Adapter, AdapterUtils, Config, ValidatedConfig } from './config';
 export { ErrorLoad, Load, Page } from './page';
 export { Incoming, GetSession, Handle, ServerResponse as Response } from './hooks';
 export { ServerRequest as Request, EndpointOutput, RequestHandler } from './endpoint';
