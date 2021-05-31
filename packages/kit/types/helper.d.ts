b0f61aa13d03eb3c68ad3a219be1dee9cb987776
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-31 10:40:38 +0700

    set Headers value to possibly be undefined

--- packages/kit/types/helper.d.ts
+++ packages/kit/types/helper.d.ts
@@ -16,7 +16,7 @@ export type ParameterizedBody<Body = unknown> = Body extends FormData
 // always follow this type, and response headers, in which
 // 'set-cookie' is a `string[]` (or at least `string | string[]`)
 // but this can't happen until TypeScript 4.3
-export type Headers = Record<string, string>;
+export type Headers = Record<string, string | undefined>;
 
 export type Location = {
 	host: string;
