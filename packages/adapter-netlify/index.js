3b988a49f970fd3fddbf8ff9ba0dce7c6bad37f6
Robert Schäfer <git@roschaefer.de>
2021-06-01 18:07:40 +0200

    Allow `_redirects` to be placed in root directory (#1586)

--- packages/adapter-netlify/index.js
+++ packages/adapter-netlify/index.js
@@ -39,6 +39,7 @@ export default function () {
 			utils.copy_client_files(publish);
 
 			utils.log.minor('Writing redirects...');
+			utils.copy('_redirects', `${publish}/_redirects`);
 			appendFileSync(`${publish}/_redirects`, '\n\n/* /.netlify/functions/render 200');
 		}
 	};
