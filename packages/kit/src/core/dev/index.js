45504ff258e00412962f890c886b3519e8012086
Joe Busillo <jbusillo@yahoo.com>
2021-06-02 23:32:06 -0500

    fix indents in literal

--- packages/kit/src/core/dev/index.js
+++ packages/kit/src/core/dev/index.js
@@ -273,36 +273,36 @@ class Watcher extends EventEmitter {
 											str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
 										rendered = `<!doctype html>
-										<head>
-											<meta charset="utf-8" />
-											<meta name="viewport" content="width=device-width, initial-scale=1" />
-											<style>
-												body {
-													font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-													color: #333;
-												}
+											<head>
+												<meta charset="utf-8" />
+												<meta name="viewport" content="width=device-width, initial-scale=1" />
+												<style>
+													body {
+														font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+														color: #333;
+													}
 
-												pre {
-													background: #f4f4f4;
-													padding: 1em;
-													overflow-x: auto;
-												}
-											</style>
-										</head>
-										<h1>AMP validation failed</h1>
+													pre {
+														background: #f4f4f4;
+														padding: 1em;
+														overflow-x: auto;
+													}
+												</style>
+											</head>
+											<h1>AMP validation failed</h1>
 
-										${result.errors
-											.map(
-												(error) => `
-											<h2>${error.severity}</h2>
-											<p>Line ${error.line}, column ${error.col}: ${error.message} (<a href="${error.specUrl}">${
-													error.code
-												}</a>)</p>
-											<pre>${escape(lines[error.line - 1])}</pre>
-										`
-											)
-											.join('\n\n')}
-									`;
+											${result.errors
+												.map(
+													(error) => `
+												<h2>${error.severity}</h2>
+												<p>Line ${error.line}, column ${error.col}: ${error.message} (<a href="${error.specUrl}">${
+														error.code
+													}</a>)</p>
+												<pre>${escape(lines[error.line - 1])}</pre>
+											`
+												)
+												.join('\n\n')}
+										`;
 									}
 								}
 
