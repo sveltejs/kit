b3bf2905a83d597782fdc1952a10da8bf8d0528e
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-06-11 06:01:41 +0700

    logical or instead of and

--- packages/kit/src/core/load_config/index.js
+++ packages/kit/src/core/load_config/index.js
@@ -135,7 +135,7 @@ export function validate_config(config) {
 	// resolve paths
 	const { paths } = validated.kit;
 
-	if (paths.base !== '' && paths.base.endsWith('/') && !paths.base.startsWith('/')) {
+	if (paths.base !== '' && (paths.base.endsWith('/') || !paths.base.startsWith('/'))) {
 		throw new Error(
 			"config.kit.paths.base must be a root-relative path and does not ends with '/'. See https://kit.svelte.dev/docs#configuration-paths"
 		);
