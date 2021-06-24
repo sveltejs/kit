0b428dc8adc38d37c9641f1522a497373a4f6220
Conduitry <git@chor.date>
2021-06-24 12:43:19 -0400

    allow customizing HOST/PORT env vars in adapter-node

--- packages/adapter-node/src/index.js
+++ packages/adapter-node/src/index.js
@@ -2,11 +2,10 @@ import './require_shim';
 import { createServer } from './server';
 // TODO hardcoding the relative location makes this brittle
 import { render } from '../output/server/app.js'; // eslint-disable-line import/no-unresolved
+import { host, port } from './env.js'; // eslint-disable-line import/no-unresolved
 
-const { HOST = '0.0.0.0', PORT = 3000 } = process.env;
-
-const instance = createServer({ render }).listen(PORT, HOST, () => {
-	console.log(`Listening on port ${PORT}`);
+const instance = createServer({ render }).listen(port, host, () => {
+	console.log(`Listening on port ${port}`);
 });
 
 export { instance };
