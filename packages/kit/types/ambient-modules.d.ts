a81460f0177114f78a636b36ad80808a08e5e34b
Rich Harris <richard.a.harris@gmail.com>
2021-06-28 18:03:28 -0400

    tidy up definition

--- packages/kit/types/ambient-modules.d.ts
+++ packages/kit/types/ambient-modules.d.ts
@@ -22,9 +22,11 @@ declare module '$app/navigation' {
 	 * Returns a Promise that resolves when SvelteKit navigates (or fails to navigate, in which case the promise rejects) to the specified href.
 	 *
 	 * @param href Where to navigate to
-	 * @param opts Optional. If `replaceState` is `true`, a new history entry won't be created. If `noscroll` is `true`, the browser won't scroll to the top of the page after navigation. If `keepfocus` is `true`, the browser won't focus the body element / unfocus the current element. If state is set, its value will be used to set the initial state of the new history entry, which defaults to `{}`
-	 */
-	export function goto(
+	 * @param opts.replaceState If `true`, will replace the current `history` entry rather than creating a new one with `pushState`
+	 * @param opts.noscroll If `true`, the browser will maintain its scroll position rather than scrolling to the top of the page after navigation
+	 * @param opts.keepfocus If `true`, the currently focused element will retain focus after navigation. Otherwise, focus will be reset to the body
+	 * @param opts.state The state of the new/updated history entry
+	 */export function goto(
 		href: string,
 		opts?: { replaceState?: boolean; noscroll?: boolean; keepfocus?: boolean; state?: any }
 	): Promise<any>;
