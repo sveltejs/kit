aeec616130a324bc872d7569106e92354273438e
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-07-02 22:49:32 +0700

    remove only flags

--- packages/kit/test/apps/basics/src/routes/endpoint-output/_tests.js
+++ packages/kit/test/apps/basics/src/routes/endpoint-output/_tests.js
@@ -2,23 +2,23 @@ import * as assert from 'uvu/assert';
 
 /** @type {import('test').TestMaker} */
 export default function (test) {
-	test.only('not ok on void endpoint', null, async ({ fetch }) => {
+	test('not ok on void endpoint', null, async ({ fetch }) => {
 		const res = await fetch('/endpoint-output/empty', { method: 'DELETE' });
 		assert.equal(res.ok, false);
 	});
-	test.only('200 status on empty endpoint', null, async ({ fetch }) => {
+	test('200 status on empty endpoint', null, async ({ fetch }) => {
 		const res = await fetch('/endpoint-output/empty');
 		assert.equal(res.status, 200);
 		assert.equal(await res.json(), {});
 	});
 
-	test.only('set-cookie without body', null, async ({ fetch }) => {
+	test('set-cookie without body', null, async ({ fetch }) => {
 		const res = await fetch('/endpoint-output/headers');
 		assert.equal(res.status, 200);
 		assert.equal(res.headers.has('set-cookie'), true);
 	});
 
-	test.only('200 status by default', null, async ({ fetch }) => {
+	test('200 status by default', null, async ({ fetch }) => {
 		const res = await fetch('/endpoint-output/body');
 		assert.equal(res.status, 200);
 		assert.equal(await res.text(), '{}');
