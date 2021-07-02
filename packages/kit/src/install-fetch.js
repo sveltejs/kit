d8bbad50f3de481148bdcc87d004e1b1af26ec61
GrygrFlzr <grygrflzr@hotmail.com>
2021-07-02 16:07:24 +0700

    fix(lint)

--- packages/kit/src/install-fetch.js
+++ packages/kit/src/install-fetch.js
@@ -1,20 +1,20 @@
 import fetch, { Response, Request, Headers } from 'node-fetch';
 
 Object.defineProperties(globalThis, {
-    fetch: {
-        enumerable: true,
-        value: fetch
-    },
-    Response: {
-        enumerable: true,
-        value: Response
-    },
-    Request: {
-        enumerable: true,
-        value: Request
-    },
-    Headers: {
-        enumerable: true,
-        value: Headers
-    }
+	fetch: {
+		enumerable: true,
+		value: fetch
+	},
+	Response: {
+		enumerable: true,
+		value: Response
+	},
+	Request: {
+		enumerable: true,
+		value: Request
+	},
+	Headers: {
+		enumerable: true,
+		value: Headers
+	}
 });
