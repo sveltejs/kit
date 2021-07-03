37453208c4fef15103dd350e91fe6b17053e4801
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-07-03 16:02:17 -0700

    Move adapter-begin to architect org

--- packages/adapter-begin/README.md
+++ packages/adapter-begin/README.md
@@ -1,37 +1,3 @@
-# adapter-begin
+# @sveltejs/adapter-begin
 
-Adapter for Svelte apps that creates a [Begin](https://begin.com/) app, using a function for dynamic server rendering.
-
-## Configuration
-
-Add `"@sveltejs/adapter-begin": "next"` to the `devDependencies` in your `package.json` and run `npm install`.
-
-Then add the adapter to your `svelte.config.js`:
-
-```js
-import begin from '@sveltejs/adapter-begin';
-
-export default {
-	kit: {
-		...
-		adapter: begin()
-	}
-};
-```
-
-This adapter expects to find an OpenJS Architect [app.arc](https://arc.codes/) file in the project root. It will use it to determine where to write static assets to based on the `@static` configuration. The default configuration for this file is as follows:
-
-```arc
-@app
-svelte-kit-app
-
-@http
-get /
-
-@static
-folder public
-```
-
-## Changelog
-
-[The Changelog for this package is available on GitHub](https://github.com/sveltejs/kit/blob/master/packages/adapter-begin/CHANGELOG.md).
+`@sveltejs/adapter-begin` can now be found on Github at https://github.com/architect/sveltekit-adapter and on npm at https://www.npmjs.com/package/@architect/sveltekit-adapter.
--- packages/adapter-begin/index.js
+++ packages/adapter-begin/index.js
@@ -1,53 +1,10 @@
-import { readFileSync, existsSync } from 'fs';
-import { resolve, join } from 'path';
-import parse from '@architect/parser';
-
-/** @param {string} file */
-function parse_arc(file) {
-	if (!existsSync(file)) {
-		throw new Error(`No ${file} found. See the documentation.`);
-	}
-
-	try {
-		const text = readFileSync(file).toString();
-		const arc = parse(text);
-
-		return {
-			static: arc.static[0][1]
-		};
-	} catch (e) {
-		throw new Error(`Error parsing ${file}. Please consult the documentation for correct syntax.`);
-	}
-}
-
 export default function () {
 	/** @type {import('@sveltejs/kit').Adapter} */
 	const adapter = {
 		name: '@sveltejs/adapter-begin',
 
 		async adapt({ utils }) {
-			utils.log.minor('Parsing app.arc file');
-			const { static: static_mount_point } = parse_arc('app.arc');
-
-			const lambda_directory = resolve(join('src', 'http', 'get-index'));
-			const static_directory = resolve(static_mount_point);
-			const server_directory = resolve(join('src', 'shared'));
-
-			utils.log.minor('Writing client application...');
-			utils.copy_static_files(static_directory);
-			utils.copy_client_files(static_directory);
-
-			utils.log.minor('Building lambda...');
-			const local_lambda_dir = join(__dirname, 'files');
-			utils.copy(local_lambda_dir, lambda_directory);
-
-			utils.log.minor('Writing server application...');
-			utils.copy_server_files(server_directory);
-
-			utils.log.minor('Prerendering static pages...');
-			await utils.prerender({
-				dest: static_directory
-			});
+			console.log('@sveltejs/adapter-begin can now be found at architect/sveltekit-adapter.');
 		}
 	};
 
