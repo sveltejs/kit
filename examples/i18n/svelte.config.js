1c05d3379e463eaeb51f621bc4d825c8362828d6
§Domenik Reitzner <d.reitzner@emakina.at>
2021-07-05 14:53:48 +0200

    lang should be string

--- examples/i18n/svelte.config.js
+++ examples/i18n/svelte.config.js
@@ -1,8 +1,11 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: adapter(),
 		i18n: {
 			defaultLocale: 'en',
 			locales: ['en', 'de']
