aeb5fe044b8abfb49e7d6df18f48ba63efd8c3b5
John Simons <coolcucumber+github@gmail.com>
2021-07-08 14:54:10 -0700

    Adding the ability to imperatively parse the path params, at clientside runtime.

--- packages/kit/src/runtime/app/navigation.js
+++ packages/kit/src/runtime/app/navigation.js
@@ -14,6 +14,7 @@ export const goto = import.meta.env.SSR ? guard('goto') : goto_;
 export const invalidate = import.meta.env.SSR ? guard('invalidate') : invalidate_;
 export const prefetch = import.meta.env.SSR ? guard('prefetch') : prefetch_;
 export const prefetchRoutes = import.meta.env.SSR ? guard('prefetchRoutes') : prefetchRoutes_;
+export const routeParams = import.meta.env.SSR ? guard('routeParams') : routeParams_;
 
 /**
  * @type {import('$app/navigation').goto}
@@ -48,4 +49,14 @@ async function prefetchRoutes_(pathnames) {
 	const promises = matching.map((r) => r.length !== 1 && Promise.all(r[1].map((load) => load())));
 
 	await Promise.all(promises);
+}
+
+/**
+ * @returns {Record<string, string>}
+ */
+function routeParams_() {
+	const { routes } = router.parse(new URL(location.href));
+	if (routes.length === 0) return {};
+	const match = routes[0][0].exec(location.pathname);
+	return routes[0][3](match);
 }
