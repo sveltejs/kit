0cfaf18b98c5c20060bd2c4717cdcede421725ab
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-07-09 15:17:40 -0700

    wait to create separate file for essential_files until there's more to put there

--- packages/kit/src/core/make_package/index.js
+++ packages/kit/src/core/make_package/index.js
@@ -4,7 +4,8 @@ import { createRequire } from 'module';
 import * as path from 'path';
 import { preprocess } from 'svelte/compiler';
 import { mkdirp, rimraf } from '../filesystem/index.js';
-import essential_files from './essential_files.js';
+
+const essential_files = ['README', 'LICENSE', 'CHANGELOG', '.gitignore', '.npmignore'];
 
 /**
  * @param {import('types/config').ValidatedConfig} config
