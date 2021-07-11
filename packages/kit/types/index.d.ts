fbda1495532ff9cb6b5a2e5abad25577ce625181
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-07-11 12:52:46 +0700

    sort exports alphabetically

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -5,11 +5,11 @@ import './ambient-modules';
 
 export { Adapter, AdapterUtils, Config, ValidatedConfig } from './config';
 export { EndpointOutput, RequestHandler } from './endpoint';
-export { ErrorLoad, Load, Page, LoadInput, LoadOutput, ErrorLoadInput } from './page';
+export { ErrorLoad, ErrorLoadInput, Load, LoadInput, LoadOutput, Page } from './page';
 export {
 	GetSession,
 	Handle,
+	ServerFetch,
 	ServerRequest as Request,
-	ServerResponse as Response,
-	ServerFetch
+	ServerResponse as Response
 } from './hooks';
