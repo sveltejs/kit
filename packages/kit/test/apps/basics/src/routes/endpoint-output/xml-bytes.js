8a2abc7ccba4c348f37dea5294975de0a002183b
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-07-13 10:59:43 -0700

    format

--- packages/kit/test/apps/basics/src/routes/endpoint-output/xml-bytes.js
+++ packages/kit/test/apps/basics/src/routes/endpoint-output/xml-bytes.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const get = () => {
-    const body = '<foo />';
-    return { status: 200, headers: { 'content-type': 'application/xml' }, body };
+	const body = '<foo />';
+	return { status: 200, headers: { 'content-type': 'application/xml' }, body };
 };
