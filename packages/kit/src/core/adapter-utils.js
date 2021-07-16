cb1d390dbefc296d195a20c50af5ca99abf217a3
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-07-16 14:09:01 -0300

    support updating multiple files

--- packages/kit/src/core/adapter-utils.js
+++ packages/kit/src/core/adapter-utils.js
@@ -18,49 +18,57 @@ export function isContentTypeTextual(content_type) {
 }
 
 /** Adds glob patterns to the project's `.gitignore`
+ *
+ * This function should be run at build time.
  * @param {object} options
  * @param {string[]} options.patterns An array of glob patterns to be inserted into the project's `.gitignore` file
  * @param {boolean} [options.generate] Whether the `.gitignore` file should be created if it doesn't exist
  */
-export async function updateGitIgnore({ patterns, generate = false }) {
+export async function updateIgnores({ patterns, generate = false }) {
 	const { existsSync } = await import('fs');
 	const { readFile, writeFile, appendFile } = await import('fs/promises');
 
-	const path = '.gitignore';
+	const targets = ['.gitignore', '.prettierignore', '.eslintignore'];
 	const title = '# Generated adapter build';
 
-	if (!existsSync(path)) {
-		if (!generate) return;
-		await writeFile(path, '');
-	}
-
-	const file = await readFile(path, { encoding: 'utf-8' });
-	const lines = file.split('\n');
-	const start_index = lines.indexOf(title);
-
-	if (start_index === -1) {
-		const last = lines[lines.length - 1];
-		if (last.trim().length !== 0) {
-			await appendFile(path, '\n');
+	for (const target of targets) {
+		if (!existsSync(target)) {
+			if (!generate) continue;
+			await writeFile(target, '');
 		}
-		await appendFile(path, ['', title, ...patterns].join('\n'));
-		return;
-	}
 
-	let insertion_index = lines.length - 1;
-	for (let i = start_index; i < lines.length; i++) {
-		const line = lines[i];
-		if (line.trim().length === 0) {
-			insertion_index = i;
-			break;
+		const file = await readFile(target, { encoding: 'utf-8' });
+		const lines = file.split('\n');
+		const start_index = lines.indexOf(title);
+
+		// append to file
+		if (start_index === -1) {
+			const last = lines[lines.length - 1];
+			if (last.trim().length !== 0) {
+				await appendFile(target, '\n');
+			}
+			await appendFile(target, ['', title, ...patterns].join('\n'));
+			continue;
 		}
-	}
-	const new_lines = new Set(patterns);
-	for (const line of lines) {
-		new_lines.delete(line);
-	}
-	if (new_lines.size === 0) return;
 
-	lines.splice(insertion_index, 0, ...new_lines);
-	await writeFile(path, lines.join('\n'));
+		let insertion_index = lines.length - 1;
+
+		// find last empty line
+		for (let i = start_index; i < lines.length; i++) {
+			const line = lines[i];
+			if (line.trim().length === 0) {
+				insertion_index = i;
+				break;
+			}
+		}
+		const new_lines = new Set(patterns);
+		// remove repeated lines
+		for (const line of lines) {
+			new_lines.delete(line);
+		}
+		if (new_lines.size === 0) return;
+
+		lines.splice(insertion_index, 0, ...new_lines);
+		await writeFile(target, lines.join('\n'));
+	}
 }
