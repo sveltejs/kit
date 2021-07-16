7d77989559ac0fa985722d2a389e6c6c7f6435bf
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-07-16 17:06:25 -0300

    update adapter-static

--- packages/adapter-static/index.js
+++ packages/adapter-static/index.js
@@ -11,6 +11,9 @@ export default function ({ pages = 'build', assets = pages, fallback } = {}) {
 		name: '@sveltejs/adapter-static',
 
 		async adapt({ utils }) {
+			if (utils.update_ignores({ patterns: [pages, assets] })) {
+				utils.log.minor('Ignore files updated');
+			}
 			utils.copy_static_files(assets);
 			utils.copy_client_files(assets);
 
