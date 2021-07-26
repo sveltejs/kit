f581003ce96765460ae8e7ffc18addcc310940a4
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-07-26 15:37:51 -0300

    undo adapter-begin update

--- packages/adapter-begin/index.js
+++ packages/adapter-begin/index.js
@@ -33,10 +33,6 @@ export default function () {
 			const static_directory = resolve(static_mount_point);
 			const server_directory = resolve(join('src', 'shared'));
 
-			utils.update_ignores({
-				patterns: [static_directory, lambda_directory, server_directory]
-			});
-
 			utils.log.minor('Writing client application...');
 			utils.copy_static_files(static_directory);
 			utils.copy_client_files(static_directory);
