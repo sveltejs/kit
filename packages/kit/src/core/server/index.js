1076db3f7910b0bd50e2bdab1fca76bf6cd177fe
Joe Busillo <jbusillo@yahoo.com>
2021-07-26 22:16:56 -0500

    Commit Staged Changes

--- packages/kit/src/core/server/index.js
+++ packages/kit/src/core/server/index.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import https from 'https';
-//import config from '../../../test/apps/amp/svelte.config';
+import fs from 'fs';
+import path from 'path';
 /**
  *
  * @param {boolean} use_https
@@ -10,24 +11,52 @@ import https from 'https';
  */
 export async function get_server(use_https, vite_config, handler) {
 	/** @type {https.ServerOptions} */
-	const https_options =
-		vite_config && vite_config.server && typeof vite_config.server.https === 'object'
-			? vite_config.server.https
-			: {};
-	const { key, cert } = https_options;
+	let https_options;
 
 	if (use_https) {
-		if (key && cert) {
-			https_options.key = key;
-			https_options.cert = cert;
-		} else {
-			https_options.key = https_options.cert = (await import('./cert')).createCertificate();
+		https_options =
+			vite_config && vite_config.server && typeof vite_config.server.https === 'object'
+				? vite_config.server.https
+				: {};
+
+		if (https_options.ca) https_options.ca = readFileIfExists(https_options.ca);
+		if (https_options.cert) https_options.cert = readFileIfExists(https_options.cert);
+		if (https_options.key) https_options.key = readFileIfExists(https_options.key);
+		if (https_options.pfx) {
+			https_options.pfx = readFileIfExists(https_options.pfx);
+			delete https_options.cert;
+			delete https_options.key;
+		}
+
+		if (!https_options.pfx && (!https_options.key || !https_options.cert)) {
+			https_options.cert = https_options.key = (await import('./cert')).createCertificate();
+		}
+	} else {
+		if (vite_config && vite_config.server) {
+			delete vite_config.server.https;
 		}
 	}
 
 	return Promise.resolve(
 		use_https
-			? https.createServer(/** @type {https.ServerOptions} */ (https_options), handler)
+			? https.createServer(/** @type {https.ServerOptions} */(https_options), handler)
 			: http.createServer(handler)
 	);
+}
+
+/**
+ *
+ * @param {string | Buffer | any[]} value
+ * @returns {string | Buffer | Buffer[]} value
+ */
+function readFileIfExists(value) {
+	if (typeof value === 'string') {
+		try {
+			return fs.readFileSync(path.resolve(value));
+			//			return fs.readFileSync(path.resolve(value), { encoding: 'utf-8' });
+		} catch (e) {
+			return value;
+		}
+	}
+	return value;
 }
