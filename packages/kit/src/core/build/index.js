b18a45c1a71f019e3ace43035fcc3d8561cd7ac9
Dominik G <dominik.goepel@gmx.de>
2021-07-28 01:37:51 +0200

    [feat] explicitly set compilerOptions.hydratable to config.kit.hydrate (#2024)

--- packages/kit/src/core/build/index.js
+++ packages/kit/src/core/build/index.js
@@ -174,7 +174,10 @@ async function build_client({
 		plugins: [
 			svelte({
 				extensions: config.extensions,
-				emitCss: !config.kit.amp
+				emitCss: !config.kit.amp,
+				compilerOptions: {
+					hydratable: !!config.kit.hydrate
+				}
 			})
 		]
 	});
@@ -463,7 +466,10 @@ async function build_server(
 		},
 		plugins: [
 			svelte({
-				extensions: config.extensions
+				extensions: config.extensions,
+				compilerOptions: {
+					hydratable: !!config.kit.hydrate
+				}
 			})
 		],
 		// this API is marked as @alpha https://github.com/vitejs/vite/blob/27785f7fcc5b45987b5f0bf308137ddbdd9f79ea/packages/vite/src/node/config.ts#L129
