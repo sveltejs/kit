4912c03755afa662ccb3d8fd3a9a6f6d462b8810
dominikg <dominik.goepel@gmx.de>
2021-08-06 15:39:38 +0200

    fix: use 302 for redirects during dev in hn.svelte.dev example (fixes #2118)

--- examples/hn.svelte.dev/src/routes/index.svelte
+++ examples/hn.svelte.dev/src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script context="module">
+	import {dev} from '$app/env';
 	export function load() {
-		return { redirect: '/top/1', status: 301 };
+		return { redirect: '/top/1', status: dev ? 302 : 301 };
 	}
 </script>
