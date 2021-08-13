3d13b8b417114214f544bdbfdc8ca5bc8748c398
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-08-13 08:37:43 -0300

    document json types

--- packages/kit/types/endpoint.d.ts
+++ packages/kit/types/endpoint.d.ts
@@ -1,8 +1,20 @@
 import { ServerRequest } from './hooks';
 import { Headers, MaybePromise } from './helper';
 
+/**
+ * Represents plain JSON, can be returned from an endpoint
+ * or from parsing a JSON string
+ */
 type PlainJSON = Exclude<JSONValue, ToJSON>;
+
+/**
+ * Type for objects that aren't plain JSON values, but can be converted to one
+ */
 type ToJSON = { toJSON(...args: unknown[]): PlainJSON };
+
+/**
+ * JSON output that can be retrieved from an endpoint
+ */
 type JSONValue =
 	| string
 	| number
