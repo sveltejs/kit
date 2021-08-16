2a1973a19e34f255cd29bee31c23248cef6d1522
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-08-15 22:12:19 -0300

    update raw body test

--- packages/kit/test/apps/basics/src/routes/load/raw-body.json.js
+++ packages/kit/test/apps/basics/src/routes/load/raw-body.json.js
@@ -1,9 +1,10 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export function post(request) {
+	console.log('raw body:', request.rawBody, TextDecoder);
 	return {
 		body: {
 			body: /** @type {string} */ (request.body),
-			rawBody: /** @type {string} */ (request.rawBody)
+			rawBody: new TextDecoder().decode(request.rawBody)
 		}
 	};
 }
