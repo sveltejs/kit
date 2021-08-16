82828cb50fa0e02686d90811ad0e6180cdbaf554
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-08-15 22:25:22 -0300

    remove debug log from taw body test

--- packages/kit/test/apps/basics/src/routes/load/raw-body.json.js
+++ packages/kit/test/apps/basics/src/routes/load/raw-body.json.js
@@ -1,6 +1,5 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export function post(request) {
-	console.log('raw body:', request.rawBody, TextDecoder);
 	return {
 		body: {
 			body: /** @type {string} */ (request.body),
