cefe824bac42be881f7bbdfad87b086cb55a9d69
Asendia Mayco <asendia@traveloka.com>
2021-08-22 15:03:57 +0700

    Fix and enable service-worker for AMP

--- packages/kit/src/runtime/server/page/render.js
+++ packages/kit/src/runtime/server/page/render.js
@@ -111,6 +111,7 @@ export async function render_response({
 		<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style>
 		<noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 		<script async src="https://cdn.ampproject.org/v0.js"></script>`;
+		init += options.service_worker ? '<script async custom-element="amp-install-serviceworker" src="https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js"></script>' : '';
 	} else if (include_js) {
 		// prettier-ignore
 		init = `<script type="module">
@@ -144,8 +145,11 @@ export async function render_response({
 		</script>`;
 	}
 
-	if (options.service_worker) {
-		init += `<script>
+	if (options.service_worker && !options.amp) {
+		init += options.amp ? `<amp-install-serviceworker
+			src="${options.service_worker}"
+			layout="nodisplay"></amp-install-serviceworker>`
+		: `<script>
 			if ('serviceWorker' in navigator) {
 				navigator.serviceWorker.register('${options.service_worker}');
 			}
