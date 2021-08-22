77b486543533250a616ce568e9980579cb03a9af
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-08-22 17:36:19 +0700

    remove eslint-disable-line comments

--- packages/adapter-netlify/files/shims.js
+++ packages/adapter-netlify/files/shims.js
@@ -1,1 +1,1 @@
-export { fetch, Response, Request, Headers } from '@sveltejs/kit/install-fetch'; // eslint-disable-line import/no-unresolved
+export { fetch, Response, Request, Headers } from '@sveltejs/kit/install-fetch';
