eae1e860d79eb26315671a810a426180f04ae27d
bluwy <bjornlu.dev@gmail.com>
2021-08-26 11:21:05 +0800

    refactor(config): use superstruct

--- packages/kit/src/core/config/options.js
+++ packages/kit/src/core/config/options.js
@@ -1,292 +1,167 @@
-const identity = (/** @type {any} */ id) => id;
+import {
+	any,
+	array,
+	boolean,
+	coerce,
+	defaulted as _,
+	define,
+	deprecated,
+	enums,
+	func,
+	object,
+	refine,
+	string,
+	type,
+	union
+} from 'superstruct';
 
-/** @typedef {import('./types').ConfigDefinition} ConfigDefinition */
+export const options_type = type({
+	// common svelte options
+	compilerOptions: _(any(), null),
+	preprocess: _(any(), null),
+	extensions: _(array(svelte_extension()), ['.svelte']),
 
-/** @type {Record<string, ConfigDefinition>} */
-const options = {
-	compilerOptions: {
-		type: 'leaf',
-		default: null,
-		validate: identity
-	},
-
-	extensions: {
-		type: 'leaf',
-		default: ['.svelte'],
-		validate: (option, keypath) => {
-			if (!Array.isArray(option) || !option.every((page) => typeof page === 'string')) {
-				throw new Error(`${keypath} must be an array of strings`);
-			}
-
-			option.forEach((extension) => {
-				if (extension[0] !== '.') {
-					throw new Error(`Each member of ${keypath} must start with '.' — saw '${extension}'`);
-				}
-
-				if (!/^(\.[a-z0-9]+)+$/i.test(extension)) {
-					throw new Error(`File extensions must be alphanumeric — saw '${extension}'`);
-				}
-			});
-
-			return option;
-		}
-	},
-
-	kit: {
-		type: 'branch',
-		children: {
-			adapter: {
-				type: 'leaf',
-				default: null,
-				validate: (option, keypath) => {
-					if (typeof option !== 'object' || !option.adapt) {
-						let message = `${keypath} should be an object with an "adapt" method`;
-
-						if (Array.isArray(option) || typeof option === 'string') {
-							// for the early adapter adopters
-							message += ', rather than the name of an adapter';
-						}
-
-						throw new Error(`${message}. See https://kit.svelte.dev/docs#adapters`);
-					}
-
-					return option;
-				}
-			},
-
-			amp: expect_boolean(false),
-
-			appDir: expect_string('_app', false),
-
-			files: {
-				type: 'branch',
-				children: {
-					assets: expect_string('static'),
-					hooks: expect_string('src/hooks'),
-					lib: expect_string('src/lib'),
-					routes: expect_string('src/routes'),
-					serviceWorker: expect_string('src/service-worker'),
-					template: expect_string('src/app.html')
-				}
-			},
-
-			floc: expect_boolean(false),
-
-			host: expect_string(null),
-
-			hostHeader: expect_string(null),
-
-			hydrate: expect_boolean(true),
-			serviceWorker: {
-				type: 'branch',
-				children: {
-					exclude: expect_array_of_strings([])
-				}
-			},
-
-			package: {
-				type: 'branch',
-				children: {
-					dir: expect_string('package'),
-					exports: {
-						type: 'branch',
-						children: {
-							include: expect_array_of_strings(['**']),
-							exclude: expect_array_of_strings(['_*', '**/_*'])
-						}
-					},
-					files: {
-						type: 'branch',
-						children: {
-							include: expect_array_of_strings(['**']),
-							exclude: expect_array_of_strings([])
-						}
-					},
-					emitTypes: expect_boolean(true)
-				}
-			},
-
-			paths: {
-				type: 'branch',
-				children: {
-					base: expect_string(''),
-					assets: expect_string('')
-				}
-			},
-
-			prerender: {
-				type: 'branch',
-				children: {
-					crawl: expect_boolean(true),
-					enabled: expect_boolean(true),
-					// TODO: remove this for the 1.0 release
-					force: {
-						type: 'leaf',
-						default: undefined,
-						validate: (option, keypath) => {
-							if (typeof option !== undefined) {
-								const newSetting = option ? 'continue' : 'fail';
-								const needsSetting = newSetting === 'continue';
-								throw new Error(
-									`${keypath} has been removed in favor of \`onError\`. In your case, set \`onError\` to "${newSetting}"${
-										needsSetting ? '' : ' (or leave it undefined)'
-									} to get the same behavior as you would with \`force: ${JSON.stringify(option)}\``
-								);
-							}
-						}
-					},
-					onError: {
-						type: 'leaf',
-						default: 'fail',
-						validate: (option, keypath) => {
-							if (typeof option === 'function') return option;
-							if (['continue', 'fail'].includes(option)) return option;
-							throw new Error(
-								`${keypath} should be either a custom function or one of "continue" or "fail"`
-							);
-						}
-					},
-					pages: {
-						type: 'leaf',
-						default: ['*'],
-						validate: (option, keypath) => {
-							if (!Array.isArray(option) || !option.every((page) => typeof page === 'string')) {
-								throw new Error(`${keypath} must be an array of strings`);
-							}
-
-							option.forEach((page) => {
-								if (page !== '*' && page[0] !== '/') {
-									throw new Error(
-										`Each member of ${keypath} must be either '*' or an absolute path beginning with '/' — saw '${page}'`
-									);
-								}
-							});
-
-							return option;
-						}
-					}
-				}
-			},
-
-			router: expect_boolean(true),
-
-			ssr: expect_boolean(true),
-
-			target: expect_string(null),
-
-			trailingSlash: expect_enum(['never', 'always', 'ignore']),
-
-			vite: {
-				type: 'leaf',
-				default: () => ({}),
-				validate: (option, keypath) => {
-					if (typeof option === 'object') {
-						const config = option;
-						option = () => config;
-					}
-
-					if (typeof option !== 'function') {
-						throw new Error(
-							`${keypath} must be a Vite config object (https://vitejs.dev/config) or a function that returns one`
-						);
-					}
-
-					return option;
-				}
-			}
-		}
-	},
-
-	preprocess: {
-		type: 'leaf',
-		default: null,
-		validate: identity
-	}
-};
+	// kit options
+	kit: obj({
+		adapter: _(adapter(), null),
+		amp: _(boolean(), false),
+		appDir: _(non_empty_string(), '_app'),
+		files: obj({
+			assets: _(string(), 'static'),
+			hooks: _(string(), 'src/hooks'),
+			lib: _(string(), 'src/lib'),
+			routes: _(string(), 'src/routes'),
+			serviceWorker: _(string(), 'src/service-worker'),
+			template: _(string(), 'src/app.html')
+		}),
+		floc: _(boolean(), false),
+		assets: _(string(), null),
+		hostHeader: _(string(), null),
+		hydrate: _(boolean(), true),
+		serviceWorker: obj({
+			exclude: array(string())
+		}),
+		package: obj({
+			dir: _(string(), 'package'),
+			exports: obj({
+				include: _(array(string()), ['**']),
+				exclude: _(array(string()), ['_*', '**/_*'])
+			}),
+			files: obj({
+				include: _(array(string()), ['**']),
+				exclude: array(string())
+			}),
+			emitTypes: _(boolean(), true)
+		}),
+		paths: obj({
+			base: _(string(), ''),
+			assets: _(string(), '')
+		}),
+		prerender: obj({
+			crawl: _(boolean(), true),
+			enabled: _(boolean(), true),
+			// TODO: remove this for the 1.0 release
+			force: prerender_force()
+		}),
+		onError: _(on_error(), 'fail'),
+		pages: _(page(), ['*']),
+		router: _(boolean(), true),
+		ssr: _(boolean(), true),
+		target: _(string(), null),
+		trailingSlash: _(enums(['never', 'always', 'ignore']), true),
+		vite: _(vite_config(), () => ({}))
+	})
+});
 
 /**
- * @param {string | null} string
- * @param {boolean} allow_empty
- * @returns {ConfigDefinition}
+ * @param {S} schema
+ * @template {Record<string, import('superstruct').Struct<any, any>>} S
  */
-function expect_string(string, allow_empty = true) {
-	return {
-		type: 'leaf',
-		default: string,
-		validate: (option, keypath) => {
-			assert_is_string(option, keypath);
-			if (!allow_empty && option === '') {
-				throw new Error(`${keypath} cannot be empty`);
-			}
-			return option;
+function obj(schema) {
+	return _(object(schema), {});
+}
+
+function non_empty_string() {
+	return refine(string(), 'non_empty_string', (value) => {
+		return value !== '' || `Expected a non-empty string`;
+	});
+}
+
+function svelte_extension() {
+	return refine(string(), 'svelte_extension', (value) => {
+		if (value[0] !== '.') {
+			return `Expected string to start with '.', but received: ${print(value)}`;
 		}
-	};
+		if (!/^(\.[a-z0-9]+)+$/i.test(value)) {
+			return `Expected string to be alphanumeric, but received: ${print(value)}`;
+		}
+		return true;
+	});
+}
+
+function adapter() {
+	return define('adapter', (value) => {
+		// @ts-expect-error - suppress error when checking value.adapt
+		if (value && typeof value === 'object' && typeof value.adapt === 'function') {
+			return true;
+		}
+
+		let message = `Expected an object with an "adapt" method`;
+
+		if (Array.isArray(value) || typeof value === 'string') {
+			// for the early adapter adopters
+			message += ', rather than the name of an adapter';
+		}
+
+		message += `. See https://kit.svelte.dev/docs#adapters`;
+
+		return message;
+	});
+}
+
+function on_error() {
+	return refine(union([func(), string()]), 'on_error', (value, ctx) => {
+		if (typeof value !== 'function' && !['continue', 'fail'].includes(value)) {
+			return `${ctx.path} should be either a custom function or one of "continue" or "fail"`;
+		}
+
+		return true;
+	});
+}
+
+function page() {
+	return refine(string(), 'page', (value) => {
+		if (value !== '*' && value[0] !== '/') {
+			return `Expected string be either '*' or an absolute path beginning with '/', but received ${print(
+				value
+			)}`;
+		}
+
+		return true;
+	});
+}
+
+function prerender_force() {
+	return deprecated(string(), (value, ctx) => {
+		const newSetting = value ? 'continue' : 'fail';
+		const needsSetting = newSetting === 'continue';
+		return `${
+			ctx.path
+		} has been removed in favor of \`onError\`. In your case, set \`onError\` to "${newSetting}"${
+			needsSetting ? '' : ' (or leave it undefined)'
+		} to get the same behavior as you would with \`force: ${print(value)}\``;
+	});
+}
+
+function vite_config() {
+	return coerce(func(), object(), (value) => () => value);
 }
 
 /**
- * @param {string[]} array
- * @returns {ConfigDefinition}
+ * utility function to print struct value
+ * https://github.com/ianstormtaylor/superstruct/blob/a44563d2784e6377e3e08330a5ec84b36d1b8933/src/utils.ts#L37-L39
+ * @param {any} value
  */
-function expect_array_of_strings(array) {
-	return {
-		type: 'leaf',
-		default: array,
-		validate: (option, keypath) => {
-			if (!Array.isArray(option) || !option.every((glob) => typeof glob === 'string')) {
-				throw new Error(`${keypath} must be an array of strings`);
-			}
-			return option;
-		}
-	};
+function print(value) {
+	return typeof value === 'string' ? JSON.stringify(value) : `${value}`;
 }
-
-/**
- * @param {boolean} boolean
- * @returns {ConfigDefinition}
- */
-function expect_boolean(boolean) {
-	return {
-		type: 'leaf',
-		default: boolean,
-		validate: (option, keypath) => {
-			if (typeof option !== 'boolean') {
-				throw new Error(`${keypath} should be true or false, if specified`);
-			}
-			return option;
-		}
-	};
-}
-
-/**
- * @param {string[]} options
- * @returns {ConfigDefinition}
- */
-function expect_enum(options, def = options[0]) {
-	return {
-		type: 'leaf',
-		default: def,
-		validate: (option, keypath) => {
-			if (!options.includes(option)) {
-				// prettier-ignore
-				const msg = options.length > 2
-					? `${keypath} should be one of ${options.slice(0, -1).map(option => `"${option}"`).join(', ')} or "${options[options.length - 1]}"`
-					: `${keypath} should be either "${options[0]}" or "${options[1]}"`;
-
-				throw new Error(msg);
-			}
-			return option;
-		}
-	};
-}
-
-/**
- * @param {any} option
- * @param {string} keypath
- */
-function assert_is_string(option, keypath) {
-	if (typeof option !== 'string') {
-		throw new Error(`${keypath} should be a string, if specified`);
-	}
-}
-
-export default options;
