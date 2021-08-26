8db56d394d56d8038cc2d8a4dff6a7cef23fcb2b
dishuostec <dishuostec@gmail.com>
2021-08-26 09:33:25 +0800

    revert JSDoc

--- packages/adapter-static/index.js
+++ packages/adapter-static/index.js
@@ -1,17 +1,8 @@
-/**
- * @param {{
- *   pages?: string;
- *   assets?: string;
- *   fallback?: string;
- *   outputFileName?: (opts: {path: string, is_html: boolean}) => string;
- * }} [opts]
- */
+/** @type {import('.')} */
 export default function({ pages = 'build', assets = pages, fallback, outputFileName } = {}) {
-	/** @type {import('@sveltejs/kit').Adapter} */
 	return {
 		name: '@sveltejs/adapter-static',
 
-		/** @type {import('@sveltejs/kit').Adapter['adapt']} */
 		async adapt({ utils }) {
 			utils.rimraf(assets);
 			utils.rimraf(pages);
