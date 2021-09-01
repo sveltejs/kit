c478b7b69dd0b3d16e9af947373e3922db9b05a6
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-09-01 06:38:53 -0700

    [fix] remove Vite workaround now that dev deps can be bundled (#2340)

--- packages/kit/src/core/build/index.js
+++ packages/kit/src/core/build/index.js
@@ -489,7 +489,6 @@ async function build_server(
 		},
 		ssr: {
 			noExternal: [
-				'@sveltejs/kit', // TODO: see https://github.com/vitejs/vite/issues/3953
 				// @ts-expect-error - ssr is considered in alpha, so not yet exposed by Vite
 				...((vite_config.ssr && vite_config.ssr.noExternal) || []),
 				...svelte_packages
--- packages/kit/src/core/dev/index.js
+++ packages/kit/src/core/dev/index.js
@@ -155,7 +155,6 @@ class Watcher extends EventEmitter {
 			},
 			ssr: {
 				noExternal: [
-					'@sveltejs/kit', // TODO: see https://github.com/vitejs/vite/issues/3953
 					// @ts-expect-error - ssr is considered in alpha, so not yet exposed by Vite
 					...((vite_config.ssr && vite_config.ssr.noExternal) || []),
 					...svelte_packages
