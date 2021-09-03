7bbc7e81bc2543c2820eed73e10b252beabe1f15
Robin Munn <rmunn@pobox.com>
2021-09-03 13:32:11 +0700

    [fix] load function should not leak props
    When navigating from a page with a load() function to a page without a
    load() function, props were not being reset, so if the second page had a
    prop with the same name as the prop returned by the first page's load()
    function, the second page would receive the same prop that the first
    page did.

---
+++ packages/kit/test/apps/basics/src/routes/load/props/about.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let data;
+</script>
+
+<h3>About</h3>
+
+<p>Data: {data}</p>
---
+++ packages/kit/test/apps/basics/src/routes/load/props/index.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export async function load() {
+		const data = 'Hello from Index!';
+		return { props: { data } };
+	}
+</script>
+
+<script>
+	export let data;
+</script>
+
+<h3>Index</h3>
+
+<p>Data: {data}</p>
