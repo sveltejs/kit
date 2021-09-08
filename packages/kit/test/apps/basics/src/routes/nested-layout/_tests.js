c0b30df3e3c59ebc41adab67d905e7a835dec83d
Robin Munn <rmunn@pobox.com>
2021-09-08 14:17:35 +0700

    [fix] deeply-nested error components render right
    Under some circumstances, a deeply-nested error component could render
    with a layout from a parent folder instead of the layout from the error
    component's own folder. This commit fixes that bug, so that error
    components are correctly matched up with layout components in the same
    folder even when deeply nested.

--- packages/kit/test/apps/basics/src/routes/nested-layout/_tests.js
+++ packages/kit/test/apps/basics/src/routes/nested-layout/_tests.js
@@ -32,4 +32,16 @@ export default function (test) {
 			assert.equal(await page.textContent('h1'), '500');
 		}
 	);
+
+	test(
+		'renders deeply-nested errors in the right layout',
+		'/nested-layout/foo/bar/nope',
+		async ({ page }) => {
+			assert.equal(await page.textContent('footer'), 'Custom layout');
+			assert.ok(await page.evaluate(() => document.querySelector('p#nested')));
+			assert.ok(await page.evaluate(() => document.querySelector('p#nested-foo')));
+			assert.ok(await page.evaluate(() => document.querySelector('p#nested-bar')));
+			assert.equal(await page.textContent('#nested-error-message'), 'error.message: nope');
+		}
+	);
 }
