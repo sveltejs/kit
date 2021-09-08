dbe410eb013090c17e598faf35d29b1eb14eaf73
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-09-08 08:14:01 +0700

    snake_case!

--- packages/adapter-node/index.js
+++ packages/adapter-node/index.js
@@ -25,7 +25,7 @@ export default function ({
 	out = 'build',
 	precompress,
 	env: { path: path_env = 'SOCKET_PATH', host: host_env = 'HOST', port: port_env = 'PORT' } = {},
-	esbuild: esbuildConfig
+	esbuild: esbuild_config
 } = {}) {
 	return {
 		name: '@sveltejs/adapter-node',
@@ -71,12 +71,12 @@ export default function ({
 					APP_DIR: `"/${config.kit.appDir}/"`
 				}
 			};
-			const buildOptions = esbuildConfig ? await esbuildConfig(defaultOptions) : defaultOptions;
-			await esbuild.build(buildOptions);
+			const build_options = esbuild_config ? await esbuild_config(defaultOptions) : defaultOptions;
+			await esbuild.build(build_options);
 
 			utils.log.minor('Building SvelteKit reference server');
 			/** @type {BuildOptions} */
-			const defaultOptionsRefServer = {
+			const default_options_ref_server = {
 				entryPoints: ['.svelte-kit/node/index.js'],
 				outfile: join(out, 'index.js'),
 				bundle: true,
@@ -95,10 +95,10 @@ export default function ({
 					}
 				]
 			};
-			const buildOptionsRefServer = esbuildConfig
-				? await esbuildConfig(defaultOptionsRefServer)
-				: defaultOptionsRefServer;
-			await esbuild.build(buildOptionsRefServer);
+			const build_options_ref_server = esbuild_config
+				? await esbuild_config(default_options_ref_server)
+				: default_options_ref_server;
+			await esbuild.build(build_options_ref_server);
 
 			utils.log.minor('Prerendering static pages');
 			await utils.prerender({
