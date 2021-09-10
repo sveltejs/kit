416d86ed642c31394db67cb135f2a08593697236
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2021-09-10 22:49:43 +0200

    Fix deendency error in adapters.

--- packages/adapter-vercel/files/shims.js
+++ packages/adapter-vercel/files/shims.js
@@ -1,4 +1,4 @@
-import { randomBytes } from 'node:crypto';
+import { randomBytes } from 'crypto';
 export { fetch, Response, Request, Headers } from '@sveltejs/kit/install-fetch';
 
 export const generateCspNonce = () => randomBytes(16).toString('base64');
