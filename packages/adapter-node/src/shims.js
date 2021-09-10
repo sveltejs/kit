9b2a3057b4d51daeba9c2cbf37806e7e9b311398
Karlinator <kehofset@live.no>
2021-09-10 18:13:04 +0200

    Add CspNonceGenerator shim to adapter-node.

--- packages/adapter-node/src/shims.js
+++ packages/adapter-node/src/shims.js
@@ -1,5 +1,8 @@
 import { createRequire } from 'module';
+import { randomBytes } from 'crypto';
 export { fetch, Response, Request, Headers } from '@sveltejs/kit/install-fetch';
+
+export const generateCspNonce = () => randomBytes(16).toString('base64');
 
 // esbuild automatically renames "require"
 // So we still have to use Object.defineProperty here
