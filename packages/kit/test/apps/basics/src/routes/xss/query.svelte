03fd667ea246e19c2a0958119af14cd0187af973
dominikg <dominik.goepel@gmx.de>
2021-10-13 21:33:19 +0200

    test(routing): add testcases for xss in dynamic route path and query params

---
+++ packages/kit/test/apps/basics/src/routes/xss/query.svelte
@@ -0,0 +1,36 @@
+<script context="module">
+	/** @typedef {Record<string, string[]>} Query */
+
+	/** @param {URLSearchParams} query */
+	function to_pojo(query) {
+		/** @type {Query}*/
+		const values = {};
+
+		query.forEach((value, key) => {
+			if (!(key in values)) values[key] = [];
+			values[key].push(value);
+		});
+
+		return values;
+	}
+
+	/** @type {import('@sveltejs/kit').Load} */
+	export function load({ page }) {
+		return {
+			props: {
+				values: to_pojo(page.query)
+			}
+		};
+	}
+</script>
+
+<script>
+	import { page } from '$app/stores';
+
+	/** @type {Query}*/
+	export let values;
+</script>
+
+<pre id="one">{JSON.stringify(values)}</pre>
+<pre id="two">{JSON.stringify(to_pojo($page.query))}</pre>
+
