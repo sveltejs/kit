8df532be6b7910605106f8f09c489fa8acdd2aec
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-10-20 13:33:53 -0700

    [fix] return 302 redirect on localhost (#2638)

--- examples/hn.svelte.dev/src/routes/index.svelte
+++ examples/hn.svelte.dev/src/routes/index.svelte
@@ -1,6 +1,13 @@
 <script context="module">
-	import {dev} from '$app/env';
-	export function load() {
-		return { redirect: '/top/1', status: dev ? 302 : 301 };
+	export function load({ page }) {
+		let host = page.host;
+		const i = host.indexOf(':');
+		if (i >= 0) {
+			host = host.substring(0, i);
+		}
+		return {
+			redirect: '/top/1',
+			status: host === 'localhost' || host === '127.0.0.1' ? 302 : 301
+		};
 	}
 </script>
