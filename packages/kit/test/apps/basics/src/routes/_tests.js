48d9bb11ac84bf327125926d62fae2a84e00bf76
gg187on <74201512+gg187on@users.noreply.github.com>
2021-10-25 15:37:38 +0200

    fix test

--- packages/kit/test/apps/basics/src/routes/_tests.js
+++ packages/kit/test/apps/basics/src/routes/_tests.js
@@ -8,13 +8,10 @@ export default function (test) {
 	test('prerender encoding data uri', '/', async ({ page }) => {
 		assert.equal(
 			await page.getAttribute('link', 'href'),
-			'<link rel="icon" \
-		href="data:image/svg+xml,\
-		<svg xmlns=%22http://www.w3.org/2000/svg%22 \
-		viewBox=%220 0 100 100%22>\
-		<text y=%22.9em%22 font-size=%2290%22>\
-		💥</text></svg>"\
-		>'
+			'data:image/svg+xml,\
+			<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>\
+			<text y=%22.9em%22 font-size=%2290%22>\
+			💥</text></svg>'
 		);
 	});
 }
