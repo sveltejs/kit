21c0ae3024268ab10a2395ccbe346d42a0938954
gg187on <74201512+gg187on@users.noreply.github.com>
2021-10-25 15:57:27 +0200

    formating

--- packages/kit/test/apps/basics/src/routes/_tests.js
+++ packages/kit/test/apps/basics/src/routes/_tests.js
@@ -9,9 +9,9 @@ export default function (test) {
 		assert.equal(
 			await page.getAttribute('link', 'href'),
 			'data:image/svg+xml,\
-			<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>\
-			<text y=%22.9em%22 font-size=%2290%22>\
-			💥</text></svg>'
+<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>\
+<text y=%22.9em%22 font-size=%2290%22>\
+💥</text></svg>'
 		);
 	});
 }
