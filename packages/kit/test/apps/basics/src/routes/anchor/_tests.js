455d88d93fd1c3988c394431b471b5d9e18c5b2f
bluwy <bjornlu.dev@gmail.com>
2021-11-10 01:20:27 +0800

    fix: tests

--- packages/kit/test/apps/basics/src/routes/anchor/_tests.js
+++ packages/kit/test/apps/basics/src/routes/anchor/_tests.js
@@ -11,7 +11,7 @@ export default function (test) {
 		'/anchor/anchor#go-to-element',
 		async ({ is_intersecting_viewport, js }) => {
 			if (js) {
-				assert.ok(is_intersecting_viewport('#go-to-element'));
+				assert.ok(await is_intersecting_viewport('#go-to-element'));
 			}
 		}
 	);
@@ -22,7 +22,7 @@ export default function (test) {
 		async ({ clicknav, is_intersecting_viewport, js }) => {
 			await clicknav('#first-anchor');
 			if (js) {
-				assert.ok(is_intersecting_viewport('#go-to-element'));
+				assert.ok(await is_intersecting_viewport('#go-to-element'));
 			}
 		}
 	);
@@ -33,7 +33,40 @@ export default function (test) {
 		async ({ clicknav, is_intersecting_viewport, js }) => {
 			await clicknav('#second-anchor');
 			if (js) {
-				assert.ok(is_intersecting_viewport('#go-to-element'));
+				assert.ok(await is_intersecting_viewport('#go-to-element'));
+			}
+		}
+	);
+
+	test(
+		'no-anchor url will scroll to top when navigated from scrolled page',
+		'/anchor',
+		async ({ clicknav, page, js }) => {
+			await clicknav('#third-anchor');
+			if (js) {
+				assert.ok(await page.evaluate(() => pageYOffset === 0));
+			}
+		}
+	);
+
+	test(
+		'url-supplied anchor works when navigated from bottom of page',
+		'/anchor',
+		async ({ clicknav, is_intersecting_viewport, js }) => {
+			await clicknav('#last-anchor');
+			if (js) {
+				assert.ok(await is_intersecting_viewport('#go-to-element'));
+			}
+		}
+	);
+
+	test(
+		'no-anchor url will scroll to top when navigated from bottom of page',
+		'/anchor',
+		async ({ clicknav, page, js }) => {
+			await clicknav('#last-anchor-2');
+			if (js) {
+				assert.ok(await page.evaluate(() => pageYOffset === 0));
 			}
 		}
 	);
