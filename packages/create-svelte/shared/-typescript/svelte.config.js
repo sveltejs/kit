475b798a1d342680647b1e0b7b91b415aa3d113b
Rich Harris <richard.a.harris@gmail.com>
2021-11-24 10:19:06 -0500

    add adapter-auto to template configs (#2885)
    * add adapter-auto to template configs
    
    * changeset
    
    * lint
    
    * jfc
    
    * fix versions
    
    * and again
    
    * maybe this will make netlify happy
    
    * update the goddamn lockfile
    
    * changeset for adapter-auto
    
    * these should point to the workspace

--- packages/create-svelte/shared/-typescript/svelte.config.js
+++ packages/create-svelte/shared/-typescript/svelte.config.js
@@ -1,6 +1,10 @@
+import adapter from '@sveltejs/adapter-auto';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter(),
+
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
