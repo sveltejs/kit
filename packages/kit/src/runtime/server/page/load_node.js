228f0e660a7f6713ff9eb9ac49244d70faa9b9df
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2021-11-24 21:28:38 -0800

    Use parent request headers in SS fetch

--- packages/kit/src/runtime/server/page/load_node.js
+++ packages/kit/src/runtime/server/page/load_node.js
@@ -131,6 +131,7 @@ export async function load_node({
 					const relative = resolved;
 
 					const headers = /** @type {import('types/helper').RequestHeaders} */ ({
+						...request.headers,
 						...opts.headers
 					});
 
