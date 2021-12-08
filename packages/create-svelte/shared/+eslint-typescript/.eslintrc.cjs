ae7f7904c4f3c111dbff1ad1209ad92fb6f057cf
josef <josef.aidt@gmail.com>
2021-12-07 23:06:06 -0600

    feat: bump eslint ecmaVersion to 2020 (#2985)

--- packages/create-svelte/shared/+eslint-typescript/.eslintrc.cjs
+++ packages/create-svelte/shared/+eslint-typescript/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2019
+		ecmaVersion: 2020
 	},
 	env: {
 		browser: true,
