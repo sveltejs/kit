f2d64256aecc7419ed6a8d424ef00686dbf8b33a
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-12-26 03:44:49 +0700

    [fix] comply demo app to TypeScript strict mode (#3095)

--- packages/create-svelte/templates/default/src/lib/form.ts
+++ packages/create-svelte/templates/default/src/lib/form.ts
@@ -8,7 +8,7 @@ export function enhance(
 		result
 	}: {
 		pending?: (data: FormData, form: HTMLFormElement) => void;
-		error?: (res: Response, error: Error, form: HTMLFormElement) => void;
+		error?: (res: Response | null, error: Error | null, form: HTMLFormElement) => void;
 		result: (res: Response, form: HTMLFormElement) => void;
 	}
 ): { destroy: () => void } {
@@ -41,7 +41,7 @@ export function enhance(
 			} else {
 				console.error(await res.text());
 			}
-		} catch (e) {
+		} catch (e: any) {
 			if (error) {
 				error(null, e, form);
 			} else {
