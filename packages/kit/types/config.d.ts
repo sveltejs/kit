cd60fae68d3c8224b38e4b7898fc06b288df2e62
Rich Harris <hello@rich-harris.dev>
2021-12-28 20:16:07 +0000

    document AdapterEntry

--- packages/kit/types/config.d.ts
+++ packages/kit/types/config.d.ts
@@ -10,8 +10,24 @@ export interface RouteDefinition {
 }
 
 export interface AdapterEntry {
+	/**
+	 * A string that uniquely identifies a function and is used for deduplication.
+	 * For example, `/foo/a-[b]` and `/foo/[c]` are different routes, but would both
+	 * be represented in a Netlify _redirects file as `/foo/:param`, so they share an ID
+	 */
 	id: string;
+
+	/**
+	 * A function that compares the candidate route with the current route to determine
+	 * if it should be treated as a fallback for the current route. For example, `/foo/[c]`
+	 * is a fallback for `/foo/a-[b]`, and `/[...catchall]` is a fallback for all routes
+	 */
 	filter: (route: RouteDefinition) => boolean;
+
+	/**
+	 * A function that is invoked once the entry has been created. This is where you
+	 * should write the function to the filesystem and generate redirect manifests.
+	 */
 	complete: (entry: {
 		generateManifest: (opts: { relativePath: string; format?: 'esm' | 'cjs' }) => string;
 	}) => void;
