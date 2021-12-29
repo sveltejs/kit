4511c61f3a6a560f260c2233012f7413b707aa54
Rich Harris <hello@rich-harris.dev>
2021-12-29 18:19:33 +0000

    all tests passing

--- packages/kit/test/apps/basics/src/routes/store/_tests.js
+++ packages/kit/test/apps/basics/src/routes/store/_tests.js
@@ -3,16 +3,6 @@ import * as assert from 'uvu/assert';
 /** @type {import('test').TestMaker} */
 export default function (test, is_dev) {
 	test('url store functions as expected', null, async ({ base, page, clicknav, js }) => {
-		if (!is_dev) {
-			// this prevents the URL from changing on hydration — we need to
-			// do this because these values are set in the config for a
-			// different test
-			page.setExtraHTTPHeaders({
-				'x-forwarded-host': 'localhost:3000',
-				'x-forwarded-proto': 'http'
-			});
-		}
-
 		await page.goto(`${base}/store`);
 
 		assert.equal(await page.textContent('h1'), 'Test');
@@ -24,8 +14,6 @@ export default function (test, is_dev) {
 
 		const oops = await page.evaluate(() => window.oops);
 		assert.ok(!oops, oops);
-
-		page.setExtraHTTPHeaders({});
 	});
 
 	test(
--- packages/kit/test/apps/basics/svelte.config.js
+++ packages/kit/test/apps/basics/svelte.config.js
@@ -1,10 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		headers: {
-			host: 'x-forwarded-host',
-			protocol: 'x-forwarded-proto'
-		},
 		vite: {
 			build: {
 				minify: false
