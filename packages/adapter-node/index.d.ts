c0f3c5c8b1127b3869b1cca9af2cbbfc4ca91868
Nate Stuyvesant <nstuyvesant@users.noreply.github.com>
2022-01-02 12:10:13 -0500

    fix(typings): remove esbuild and entryPoint properties per recent change
    https://github.com/sveltejs/kit/pull/2931

--- packages/adapter-node/index.d.ts
+++ packages/adapter-node/index.d.ts
@@ -1,8 +1,6 @@
 import { Adapter } from '@sveltejs/kit';
-import { BuildOptions } from 'esbuild';
 
 interface AdapterOptions {
-	entryPoint?: string;
 	out?: string;
 	precompress?: boolean;
 	env?: {
@@ -10,7 +8,6 @@ interface AdapterOptions {
 		host?: string;
 		port?: string;
 	};
-	esbuild?: (options: BuildOptions) => Promise<BuildOptions> | BuildOptions;
 }
 
 declare function plugin(options?: AdapterOptions): Adapter;
