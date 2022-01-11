1f72ea09f052540049f29583bd466e0974fac7fb
Rich Harris <hello@rich-harris.dev>
2022-01-10 20:23:13 -0500

    tweak test

--- packages/kit/test/apps/options/test/test.js
+++ packages/kit/test/apps/options/test/test.js
@@ -42,12 +42,17 @@ test.describe.parallel('base path', () => {
 		).toBe('rgb(255, 0, 0)');
 	});
 
-	test('inlines CSS', async ({ page }) => {
+	test('inlines CSS', async ({ page, javaScriptEnabled }) => {
 		await page.goto('/path-base/base/');
 		if (process.env.DEV) {
-			expect(
-				await page.evaluate(() => document.querySelector('style[data-svelte]'))
-			).not.toBeNull();
+			const ssr_style = await page.evaluate(() => document.querySelector('style[data-svelte]'));
+
+			if (javaScriptEnabled) {
+				expect(ssr_style).toBeNull();
+			} else {
+				expect(ssr_style).not.toBeNull();
+			}
+
 			expect(
 				await page.evaluate(() => document.querySelector('link[rel="stylesheet"]'))
 			).toBeNull();
