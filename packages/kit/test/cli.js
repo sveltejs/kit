11440b4e75d88d51b467a16a09bc318b44b0eb03
Rich Harris <hello@rich-harris.dev>
2022-01-14 08:59:28 -0500

    lint

--- packages/kit/test/cli.js
+++ packages/kit/test/cli.js
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 import { fileURLToPath } from 'url';
 
-const [node, _, ...args] = process.argv;
+const [node, , ...args] = process.argv;
 const mode = process.env.CI ? 'dist' : 'src';
 const bin = fileURLToPath(new URL(`../${mode}/cli.js`, import.meta.url));
 
