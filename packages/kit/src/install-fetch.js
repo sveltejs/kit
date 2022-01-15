f7abf1f5dda5287545badf0b26e762d89c093179
Gautier Ben Aïm <48261497+GauBen@users.noreply.github.com>
2022-01-15 16:08:14 +0100

    [fix] make properties overwritable (#3356)

--- packages/kit/src/install-fetch.js
+++ packages/kit/src/install-fetch.js
@@ -5,18 +5,22 @@ export function __fetch_polyfill() {
 	Object.defineProperties(globalThis, {
 		fetch: {
 			enumerable: true,
+			configurable: true,
 			value: fetch
 		},
 		Response: {
 			enumerable: true,
+			configurable: true,
 			value: Response
 		},
 		Request: {
 			enumerable: true,
+			configurable: true,
 			value: Request
 		},
 		Headers: {
 			enumerable: true,
+			configurable: true,
 			value: Headers
 		}
 	});
