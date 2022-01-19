453157b1ebb7283ccbf86588d5cb7b2ddd2d4575
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-01-19 09:43:03 -0800

    [fix] add methodOverride to default configs

--- packages/create-svelte/shared/+default+typescript/svelte.config.js
+++ packages/create-svelte/shared/+default+typescript/svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-// This config is ignored and replaced with one of the configs in the shared folder when a project is created.
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
--- packages/create-svelte/shared/+default-typescript/svelte.config.js
+++ packages/create-svelte/shared/+default-typescript/svelte.config.js
@@ -1,14 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
 
-// This config is ignored and replaced with one of the configs in the shared folder when a project is created.
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
 
