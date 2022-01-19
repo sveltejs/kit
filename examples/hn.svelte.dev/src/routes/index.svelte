f78c6203e6303e32eda6c2a9419de39d0fb2ba0a
Rich Harris <richard.a.harris@gmail.com>
2022-01-19 17:56:11 -0500

    remove examples (#3405)
    * remove examples
    
    * update lockfile and gitignore

--- examples/hn.svelte.dev/src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<script context="module">
-	/** @type {import('@sveltejs/kit').Load} */
-	export function load({ url }) {
-		return {
-			redirect: '/top/1',
-			status: url.hostname === 'localhost' || url.hostname === '127.0.0.1' ? 302 : 301
-		};
-	}
-</script>
