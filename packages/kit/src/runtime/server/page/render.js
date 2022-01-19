ceeadd602fe31edf2d2fc3c0cc4dbf466d2f30ec
Dave Smart <dave@tamethebots.com>
2022-01-19 20:25:49 +0000

    [fix] avoid downloading CSS with highest priority when `inlineStyleThreshold` is used (#3396)
    * add media to disabled CSS link to prevent browser downloading
    
    * change to media="print"
    
    * amend test for inline css
    
    * linted
    
    * revert to adding both disabled and media attributes
    
    Co-authored-by: Rich Harris <hello@rich-harris.dev>

--- packages/kit/src/runtime/server/page/render.js
+++ packages/kit/src/runtime/server/page/render.js
@@ -148,8 +148,8 @@ export async function render_response({
 		}
 		// prettier-ignore
 		head += Array.from(css)
-				.map((dep) => `\n\t<link${styles.has(dep) ? ' disabled' : ''} rel="stylesheet" href="${options.prefix + dep}">`)
-				.join('');
+			.map((dep) => `\n\t<link${styles.has(dep) ? ' disabled media="(max-width: 0)"' : ''} rel="stylesheet" href="${options.prefix + dep}">`)
+			.join('');
 
 		if (page_config.router || page_config.hydrate) {
 			head += Array.from(js)
