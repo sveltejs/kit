4b83cefd7a50649f11f4f9d5ff26a518875e68b2
Dave Smart <d.smart@smartinternetconsultants.co.uk>
2022-01-19 16:25:47 +0000

    change to media="print"

--- packages/kit/src/runtime/server/page/render.js
+++ packages/kit/src/runtime/server/page/render.js
@@ -24,139 +24,139 @@ import { create_prerendering_url_proxy } from './utils.js';
  * }} opts
  */
 export async function render_response({
-	branch,
-	options,
-	state,
-	$session,
-	page_config,
-	status,
-	error,
-	url,
-	params,
-	ssr,
-	stuff
+    branch,
+    options,
+    state,
+    $session,
+    page_config,
+    status,
+    error,
+    url,
+    params,
+    ssr,
+    stuff
 }) {
-	const css = new Set(options.manifest._.entry.css);
-	const js = new Set(options.manifest._.entry.js);
-	/** @type {Map<string, string>} */
-	const styles = new Map();
+    const css = new Set(options.manifest._.entry.css);
+    const js = new Set(options.manifest._.entry.js);
+    /** @type {Map<string, string>} */
+    const styles = new Map();
 
-	/** @type {Array<{ url: string, body: string, json: string }>} */
-	const serialized_data = [];
+    /** @type {Array<{ url: string, body: string, json: string }>} */
+    const serialized_data = [];
 
-	let rendered;
+    let rendered;
 
-	let is_private = false;
-	let maxage;
+    let is_private = false;
+    let maxage;
 
-	if (error) {
-		error.stack = options.get_stack(error);
-	}
+    if (error) {
+        error.stack = options.get_stack(error);
+    }
 
-	if (ssr) {
-		branch.forEach(({ node, loaded, fetched, uses_credentials }) => {
-			if (node.css) node.css.forEach((url) => css.add(url));
-			if (node.js) node.js.forEach((url) => js.add(url));
-			if (node.styles) Object.entries(node.styles).forEach(([k, v]) => styles.set(k, v));
+    if (ssr) {
+        branch.forEach(({ node, loaded, fetched, uses_credentials }) => {
+            if (node.css) node.css.forEach((url) => css.add(url));
+            if (node.js) node.js.forEach((url) => js.add(url));
+            if (node.styles) Object.entries(node.styles).forEach(([k, v]) => styles.set(k, v));
 
-			// TODO probably better if `fetched` wasn't populated unless `hydrate`
-			if (fetched && page_config.hydrate) serialized_data.push(...fetched);
+            // TODO probably better if `fetched` wasn't populated unless `hydrate`
+            if (fetched && page_config.hydrate) serialized_data.push(...fetched);
 
-			if (uses_credentials) is_private = true;
+            if (uses_credentials) is_private = true;
 
-			maxage = loaded.maxage;
-		});
+            maxage = loaded.maxage;
+        });
 
-		const session = writable($session);
+        const session = writable($session);
 
-		/** @type {Record<string, any>} */
-		const props = {
-			stores: {
-				page: writable(null),
-				navigating: writable(null),
-				session
-			},
-			page: {
-				url: state.prerender ? create_prerendering_url_proxy(url) : url,
-				params,
-				status,
-				error,
-				stuff
-			},
-			components: branch.map(({ node }) => node.module.default)
-		};
+        /** @type {Record<string, any>} */
+        const props = {
+            stores: {
+                page: writable(null),
+                navigating: writable(null),
+                session
+            },
+            page: {
+                url: state.prerender ? create_prerendering_url_proxy(url) : url,
+                params,
+                status,
+                error,
+                stuff
+            },
+            components: branch.map(({ node }) => node.module.default)
+        };
 
-		// TODO remove this for 1.0
-		/**
-		 * @param {string} property
-		 * @param {string} replacement
-		 */
-		const print_error = (property, replacement) => {
-			Object.defineProperty(props.page, property, {
-				get: () => {
-					throw new Error(`$page.${property} has been replaced by $page.url.${replacement}`);
-				}
-			});
-		};
+        // TODO remove this for 1.0
+        /**
+         * @param {string} property
+         * @param {string} replacement
+         */
+        const print_error = (property, replacement) => {
+            Object.defineProperty(props.page, property, {
+                get: () => {
+                    throw new Error(`$page.${property} has been replaced by $page.url.${replacement}`);
+                }
+            });
+        };
 
-		print_error('origin', 'origin');
-		print_error('path', 'pathname');
-		print_error('query', 'searchParams');
+        print_error('origin', 'origin');
+        print_error('path', 'pathname');
+        print_error('query', 'searchParams');
 
-		// props_n (instead of props[n]) makes it easy to avoid
-		// unnecessary updates for layout components
-		for (let i = 0; i < branch.length; i += 1) {
-			props[`props_${i}`] = await branch[i].loaded.props;
-		}
+        // props_n (instead of props[n]) makes it easy to avoid
+        // unnecessary updates for layout components
+        for (let i = 0; i < branch.length; i += 1) {
+            props[`props_${i}`] = await branch[i].loaded.props;
+        }
 
-		let session_tracking_active = false;
-		const unsubscribe = session.subscribe(() => {
-			if (session_tracking_active) is_private = true;
-		});
-		session_tracking_active = true;
+        let session_tracking_active = false;
+        const unsubscribe = session.subscribe(() => {
+            if (session_tracking_active) is_private = true;
+        });
+        session_tracking_active = true;
 
-		try {
-			rendered = options.root.render(props);
-		} finally {
-			unsubscribe();
-		}
-	} else {
-		rendered = { head: '', html: '', css: { code: '', map: null } };
-	}
+        try {
+            rendered = options.root.render(props);
+        } finally {
+            unsubscribe();
+        }
+    } else {
+        rendered = { head: '', html: '', css: { code: '', map: null } };
+    }
 
-	let { head, html: body } = rendered;
+    let { head, html: body } = rendered;
 
-	const inlined_style = Array.from(styles.values()).join('\n');
+    const inlined_style = Array.from(styles.values()).join('\n');
 
-	if (options.amp) {
-		head += `
+    if (options.amp) {
+        head += `
 		<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style>
 		<noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 		<script async src="https://cdn.ampproject.org/v0.js"></script>
 
 		<style amp-custom>${inlined_style}\n${rendered.css.code}</style>`;
 
-		if (options.service_worker) {
-			head +=
-				'<script async custom-element="amp-install-serviceworker" src="https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js"></script>';
+        if (options.service_worker) {
+            head +=
+                '<script async custom-element="amp-install-serviceworker" src="https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js"></script>';
 
-			body += `<amp-install-serviceworker src="${options.service_worker}" layout="nodisplay"></amp-install-serviceworker>`;
-		}
-	} else {
-		if (inlined_style) {
-			head += `\n\t<style${options.dev ? ' data-svelte' : ''}>${inlined_style}</style>`;
-		}
-		// prettier-ignore
-		head += Array.from(css)
-            .map((dep) => `\n\t<link${styles.has(dep) ? ' disabled media="screen and (max-width: 1px)"' : ''} rel="stylesheet" href="${options.prefix + dep}">`)
+            body += `<amp-install-serviceworker src="${options.service_worker}" layout="nodisplay"></amp-install-serviceworker>`;
+        }
+    } else {
+        if (inlined_style) {
+            head += `\n\t<style${options.dev ? ' data-svelte' : ''}>${inlined_style}</style>`;
+        }
+        // prettier-ignore
+        head += Array.from(css)
+            .map((dep) => `\n\t<link${styles.has(dep) ? ' media="print"' : ''} rel="stylesheet" href="${options.prefix + dep}">`)
             .join('');
 
-		if (page_config.router || page_config.hydrate) {
-			head += Array.from(js)
-				.map((dep) => `\n\t<link rel="modulepreload" href="${options.prefix + dep}">`)
-				.join('');
-			// prettier-ignore
-			head += `
+        if (page_config.router || page_config.hydrate) {
+            head += Array.from(js)
+                .map((dep) => `\n\t<link rel="modulepreload" href="${options.prefix + dep}">`)
+                .join('');
+            // prettier-ignore
+            head += `
 			<script type="module">
 				import { start } from ${s(options.prefix + options.manifest._.entry.file)};
 				start({
