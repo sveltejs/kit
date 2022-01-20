b29529065b9284e9ff051b68ea6c46f7f2b28e44
Rich Harris <hello@rich-harris.dev>
2022-01-20 10:39:52 -0500

    script appears to work

--- packages/create-svelte/scripts/update-template-repo-contents.js
+++ packages/create-svelte/scripts/update-template-repo-contents.js
@@ -1,1 +1,22 @@
-console.log('wheee!!!');
+import fs from 'fs';
+import path from 'path';
+import { create } from '../index.js';
+
+const tmp = process.argv[2];
+const repo = path.join(tmp, 'kit-template-default');
+
+fs.readdirSync(repo).forEach((file) => {
+	if (file !== '.git') {
+		fs.rmSync(path.join(repo, file), {
+			recursive: true,
+			force: true
+		});
+	}
+});
+
+await create(repo, {
+	template: 'default',
+	eslint: false,
+	typescript: false,
+	prettier: true
+});
--- packages/create-svelte/scripts/update-template-repo.sh
+++ packages/create-svelte/scripts/update-template-repo.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-DIR=$PWD
-TMP="$DIR/../node_modules/.tmp"
+
+get_abs_filename() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+DIR=$(get_abs_filename $(dirname "$0"))
+TMP=$(get_abs_filename "$DIR/../node_modules/.tmp")
 
 echo "dir: $DIR"
 echo "tmp: $TMP"
@@ -13,11 +18,20 @@ if [ "$CI" ]; then
 	(umask 0077; echo "$SSH_KEY" > ~/ssh_key;)
 	git config user.email 'noreply@svelte.dev'
 	git config user.name '[bot]'
+
+	export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key'
 fi
 
-export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key'
-
-rm -rf $DIR/kit-template-default
+# clone the template repo
+rm -rf kit-template-default
 git clone --depth 1 --single-branch --branch main git@github.com:sveltejs/kit-template-default.git
-node $DIR/update-template-repo-contents.js
-# git push git@github.com:sveltejs/kit-template-default.git main -f
+
+# empty out the repo
+cd kit-template-default
+node $DIR/update-template-repo-contents.js $TMP
+
+# commit the new files
+git add -A
+git commit -m "version $npm_package_version"
+
+git push git@github.com:sveltejs/kit-template-default.git main -f
