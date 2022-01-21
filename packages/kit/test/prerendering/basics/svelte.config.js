a0b3be9902a9d251f207b436214c8616015edf5f
Rich Harris <richard.a.harris@gmail.com>
2022-01-21 12:01:04 -0500

    escape redirect locations when prerendering, dont encode them (#3456)
    * escape redirect locations when prerendering, dont encode them
    
    * changeset
    
    * fix checking
    
    * huh
    
    * actually run the tests
    
    * ugh idk
    
    * oops

---
+++ packages/kit/test/prerendering/basics/svelte.config.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import adapter from '../../../../adapter-static/index.js';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+
+		vite: {
+			build: {
+				minify: false
+			},
+			clearScreen: false,
+			server: {
+				fs: {
+					allow: [path.resolve('../../../src')]
+				}
+			}
+		}
+	}
+};
+
+export default config;
---
+++ packages/kit/test/prerendering/basics/test/test.js
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+
+const build = fileURLToPath(new URL('../build', import.meta.url));
+
+/** @param {string} file */
+const read = (file) => fs.readFileSync(`${build}/${file}`, 'utf-8');
+
+test('prerenders /', () => {
+	const content = read('index.html');
+	assert.ok(content.includes('<h1>hello</h1>'));
+});
+
+test('renders a redirect', () => {
+	const content = read('redirect/index.html');
+	assert.equal(
+		content,
+		'<meta http-equiv="refresh" content="0;url=https://example.com/redirected">'
+	);
+});
+
+test('does not double-encode redirect locations', () => {
+	const content = read('redirect-encoded/index.html');
+	assert.equal(
+		content,
+		'<meta http-equiv="refresh" content="0;url=https://example.com/redirected?returnTo=%2Ffoo%3Fbar%3Dbaz">'
+	);
+});
+
+test('escapes characters in redirect', () => {
+	const content = read('redirect-malicious/index.html');
+	assert.equal(
+		content,
+		'<meta http-equiv="refresh" content="0;url=https://example.com/&lt;/script&gt;alert(&quot;pwned&quot;)">'
+	);
+});
+
+test.run();
