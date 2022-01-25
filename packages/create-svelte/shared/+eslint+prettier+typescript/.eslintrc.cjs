95c47bcdb884194359475968b1e0eeb342820611
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-01-25 21:57:53 +0700

    [fix] add configuration for mixed JS/TS codebase (#3536)
    Done as explained in https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/explicit-module-boundary-types.md#configuring-in-a-mixed-jsts-codebase
    Fixes #3514

--- packages/create-svelte/shared/+eslint+prettier+typescript/.eslintrc.cjs
+++ packages/create-svelte/shared/+eslint+prettier+typescript/.eslintrc.cjs
@@ -4,7 +4,15 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{ files: ['*.svelte'], processor: 'svelte3/svelte3' },
+		{
+			files: ['*.ts'],
+			rules: {
+				'@typescript-eslint/explicit-module-boundary-types': ['warn']
+			}
+		}
+	],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
@@ -16,5 +24,8 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		'@typescript-eslint/explicit-module-boundary-types': 'off'
 	}
 };
