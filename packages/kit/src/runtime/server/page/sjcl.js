cc9aca87a5332c6e5aabaf33028c2aa0d9b415cf
Rich Harris <hello@rich-harris.dev>
2022-01-25 10:27:05 -0500

    move some stuff out of the prototype

--- packages/kit/src/runtime/server/page/sjcl.js
+++ packages/kit/src/runtime/server/page/sjcl.js
@@ -175,6 +175,34 @@ const BitArray = {
 	}
 };
 
+/**
+ * The SHA-256 initialization vector, to be precomputed.
+ * @private
+ */
+const init = [];
+
+/*
+ _init:[0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19],
+ */
+
+/**
+ * The SHA-256 hash key, to be precomputed.
+ * @private
+ */
+const key = [];
+
+/*
+ _key:
+   [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2],
+ */
+
 /** @fileOverview Javascript SHA-256 implementation.
  *
  * An older version of this implementation is available in the public
@@ -195,7 +223,7 @@ const BitArray = {
  */
 
 sjcl.hash.sha256 = function (hash) {
-	if (!this._key[0]) {
+	if (!key[0]) {
 		this._precompute();
 	}
 
@@ -222,17 +250,11 @@ sjcl.hash.sha256.hash = function (data) {
 
 sjcl.hash.sha256.prototype = {
 	/**
-	 * The hash's block size, in bits.
-	 * @constant
-	 */
-	blockSize: 512,
-
-	/**
 	 * Reset the hash state.
 	 * @return this
 	 */
 	reset: function () {
-		this._h = this._init.slice(0);
+		this._h = init.slice(0);
 		this._buffer = [];
 		this._length = 0;
 		return this;
@@ -313,34 +335,6 @@ sjcl.hash.sha256.prototype = {
 	},
 
 	/**
-	 * The SHA-256 initialization vector, to be precomputed.
-	 * @private
-	 */
-	_init: [],
-
-	/*
-	_init:[0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19],
-	*/
-
-	/**
-	 * The SHA-256 hash key, to be precomputed.
-	 * @private
-	 */
-	_key: [],
-
-	/*
-	_key:
-	  [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-	   0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-	   0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-	   0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-	   0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-	   0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-	   0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-	   0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2],
-	*/
-
-	/**
 	 * Function to precompute _init and _key.
 	 * @private
 	 */
@@ -367,10 +361,10 @@ sjcl.hash.sha256.prototype = {
 
 			if (isPrime) {
 				if (i < 8) {
-					this._init[i] = frac(Math.pow(prime, 1 / 2));
+					init[i] = frac(Math.pow(prime, 1 / 2));
 				}
 
-				this._key[i] = frac(Math.pow(prime, 1 / 3));
+				key[i] = frac(Math.pow(prime, 1 / 3));
 
 				i++;
 			}
@@ -388,7 +382,7 @@ sjcl.hash.sha256.prototype = {
 			a,
 			b,
 			h = this._h,
-			k = this._key,
+			k = key,
 			h0 = h[0],
 			h1 = h[1],
 			h2 = h[2],
