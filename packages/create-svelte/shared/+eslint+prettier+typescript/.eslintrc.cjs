1474aee74338ef80cce355ae851377fa5a903f5c
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-01-26 10:54:48 +0700

    bump eslint plugin and parser (#3544)
    * bump ts eslint plugin and parser
    
    * revert additional configs to eslintrc
    
    * add changeset

--- packages/create-svelte/shared/+eslint+prettier+typescript/.eslintrc.cjs
+++ packages/create-svelte/shared/+eslint+prettier+typescript/.eslintrc.cjs
@@ -4,15 +4,7 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [
-		{ files: ['*.svelte'], processor: 'svelte3/svelte3' },
-		{
-			files: ['*.ts'],
-			rules: {
-				'@typescript-eslint/explicit-module-boundary-types': ['warn']
-			}
-		}
-	],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
@@ -24,8 +16,5 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
-	},
-	rules: {
-		'@typescript-eslint/explicit-module-boundary-types': 'off'
 	}
 };
