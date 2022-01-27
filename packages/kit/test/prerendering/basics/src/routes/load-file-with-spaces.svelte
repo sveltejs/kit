3bd82441c21eb5365ef3e6a4274fb8e6589e980b
Rich Harris <hello@rich-harris.dev>
2022-01-27 12:01:39 -0500

    decode URIs before matching against manifest

--- packages/kit/test/prerendering/basics/src/routes/load-file-with-spaces.svelte
+++ packages/kit/test/prerendering/basics/src/routes/load-file-with-spaces.svelte
@@ -1,9 +1,18 @@
 <script context="module">
 	/** @type {import('@sveltejs/kit').Load} */
 	export async function load({ fetch }) {
-		const res = await fetch('/file with spaces.json');
+		const r1 = await fetch('/file%20with%20spaces.json');
+		const p1 = await r1.json();
+
+		const r2 = await fetch('/file with spaces.json');
+		const p2 = await r2.json();
+
+		if (p1.answer !== p2.answer) {
+			throw new Error('oops');
+		}
+
 		return {
-			props: await res.json()
+			props: p1
 		};
 	}
 </script>
