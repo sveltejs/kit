bc6083adb5c3a05a2fcdcee1dc3f34fe16014478
Rich Harris <hello@rich-harris.dev>
2022-01-27 11:54:13 -0500

    test prerendering file with spaces (#1501)

---
+++ packages/kit/test/prerendering/basics/src/routes/load-file-with-spaces.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').Load} */
+	export async function load({ fetch }) {
+		const res = await fetch('/file with spaces.json');
+		return {
+			props: await res.json()
+		};
+	}
+</script>
+
+<script>
+	/** @type {number} */
+	export let answer;
+</script>
+
+<h1>answer: {answer}</h1>
