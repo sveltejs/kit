d7e60f515f5a9eb57d2735bdea2529e4341cfea4
Rich Harris <hello@rich-harris.dev>
2022-02-01 16:52:35 -0500

    add a SvelteKit namespace for app-level types (#3569)

---
+++ packages/kit/test/apps/basics/src/app.d.ts
@@ -0,0 +1,15 @@
+declare namespace SvelteKit {
+	interface Session {
+		answer: number;
+		calls: number;
+	}
+
+	interface Stuff {
+		message: string;
+		error: string;
+		page: string;
+		value: number;
+		x: string;
+		y: string;
+	}
+}
--- packages/kit/types/page.d.ts
+++ packages/kit/types/page.d.ts
@@ -1,85 +1,40 @@
 import { Fallthrough } from './endpoint';
-import { Either, InferValue, MaybePromise } from './helper';
+import { Either, MaybePromise } from './helper';
 
-export interface LoadInput<
-	PageParams extends Record<string, string> = Record<string, string>,
-	Stuff extends Record<string, any> = Record<string, any>,
-	Session = any
-> {
+export interface LoadInput<Params = Record<string, string>> {
 	url: URL;
-	params: PageParams;
+	params: Params;
 	fetch(info: RequestInfo, init?: RequestInit): Promise<Response>;
-	session: Session;
-	stuff: Stuff;
+	session: SvelteKit.Session;
+	stuff: Partial<SvelteKit.Stuff>;
 }
 
-export interface ErrorLoadInput<
-	PageParams extends Record<string, string> = Record<string, string>,
-	Stuff extends Record<string, any> = Record<string, any>,
-	Session = any
-> extends LoadInput<PageParams, Stuff, Session> {
+export interface ErrorLoadInput<Params = Record<string, string>> extends LoadInput<Params> {
 	status?: number;
 	error?: Error;
 }
 
-export interface LoadOutput<
-	Props extends Record<string, any> = Record<string, any>,
-	Stuff extends Record<string, any> = Record<string, any>
-> {
+export interface LoadOutput<Props = Record<string, any>> {
 	status?: number;
 	error?: string | Error;
 	redirect?: string;
 	props?: Props;
-	stuff?: Stuff;
+	stuff?: Partial<SvelteKit.Stuff>;
 	maxage?: number;
 }
 
 interface LoadInputExtends {
-	stuff?: Record<string, any>;
-	pageParams?: Record<string, string>;
-	session?: any;
+	params?: Record<string, string>;
 }
 
 interface LoadOutputExtends {
-	stuff?: Record<string, any>;
 	props?: Record<string, any>;
 }
 
-export interface Load<
-	Input extends LoadInputExtends = Required<LoadInputExtends>,
-	Output extends LoadOutputExtends = Required<LoadOutputExtends>
-> {
-	(
-		input: LoadInput<
-			InferValue<Input, 'pageParams', Record<string, string>>,
-			InferValue<Input, 'stuff', Record<string, any>>,
-			InferValue<Input, 'session', any>
-		>
-	): MaybePromise<
-		Either<
-			Fallthrough,
-			LoadOutput<
-				InferValue<Output, 'props', Record<string, any>>,
-				InferValue<Output, 'stuff', Record<string, any>>
-			>
-		>
-	>;
+export interface Load<Params = Record<string, string>, Props = Record<string, any>> {
+	(input: LoadInput<Params>): MaybePromise<Either<Fallthrough, LoadOutput<Props>>>;
 }
 
-export interface ErrorLoad<
-	Input extends LoadInputExtends = Required<LoadInputExtends>,
-	Output extends LoadOutputExtends = Required<LoadOutputExtends>
-> {
-	(
-		input: ErrorLoadInput<
-			InferValue<Input, 'pageParams', Record<string, string>>,
-			InferValue<Input, 'stuff', Record<string, any>>,
-			InferValue<Input, 'session', any>
-		>
-	): MaybePromise<
-		LoadOutput<
-			InferValue<Output, 'props', Record<string, any>>,
-			InferValue<Output, 'stuff', Record<string, any>>
-		>
-	>;
+export interface ErrorLoad<Params = Record<string, string>, Props = Record<string, any>> {
+	(input: ErrorLoadInput<Params>): MaybePromise<LoadOutput<Props>>;
 }
