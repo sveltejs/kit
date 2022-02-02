47195a5ebf6eba51d8e817de058224fb15cf3117
Rich Harris <hello@rich-harris.dev>
2022-02-02 17:03:41 -0500

    unfocus test

--- packages/kit/test/apps/basics/test/test.js
+++ packages/kit/test/apps/basics/test/test.js
@@ -346,8 +346,8 @@ test.describe.parallel('CSS', () => {
 	});
 });
 
-test.describe.parallel('Duplex routes', () => {
-	test.only('Loads props from an endpoint', async ({ page, clicknav }) => {
+test.describe.parallel('Shadowed pages', () => {
+	test('Loads props from an endpoint', async ({ page, clicknav }) => {
 		await page.goto('/shadowed');
 		await clicknav('[href="/shadowed/simple"]');
 		expect(await page.textContent('h1')).toBe('The answer is 42');
