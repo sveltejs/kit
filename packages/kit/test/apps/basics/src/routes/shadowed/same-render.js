35622aad863ef50fa072c9b82d36cd79f608f5a3
Mihael <mihaelmiki123@live.com>
2022-02-10 11:24:42 +0100

    .

--- packages/kit/test/apps/basics/src/routes/shadowed/same-render.js
+++ packages/kit/test/apps/basics/src/routes/shadowed/same-render.js
@@ -1,4 +1,4 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export function get({url}){
-    return {body:{url:url.toString()}};
+export function get({ url }) {
+	return { body: { url: url.toString() } };
 }
