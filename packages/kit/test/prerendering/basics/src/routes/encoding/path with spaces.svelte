fb24856c1905f869064f429d0eb57447246f93ee
Rich Harris <hello@rich-harris.dev>
2022-02-11 18:47:53 -0500

    i bet eslint is fun at parties

--- packages/kit/test/prerendering/basics/src/routes/encoding/path with spaces.svelte
+++ packages/kit/test/prerendering/basics/src/routes/encoding/path with spaces.svelte
@@ -1,8 +1,8 @@
 <script context="module">
 	/** @type {import('@sveltejs/kit').Load} */
 	export async function load({ fetch }) {
-		const a = await fetch(`/encoding/path with spaces.json`);
-		const b = await fetch(`/encoding/path%20with%20encoded%20spaces.json`);
+		const a = await fetch('/encoding/path with spaces.json');
+		const b = await fetch('/encoding/path%20with%20encoded%20spaces.json');
 
 		return {
 			props: {
