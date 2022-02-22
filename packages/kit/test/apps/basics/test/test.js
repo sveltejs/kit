239023446cf50e60e24359aac1f3149cf94fc401
Rich Harris <hello@rich-harris.dev>
2022-02-22 17:50:35 -0500

    skip test for now

--- packages/kit/test/apps/basics/test/test.js
+++ packages/kit/test/apps/basics/test/test.js
@@ -963,7 +963,8 @@ test.describe.parallel('Errors', () => {
 		expect(await page.innerHTML('h1')).toBe('500');
 	});
 
-	test('error evaluating module', async ({ request }) => {
+	// TODO re-enable this if https://github.com/vitejs/vite/issues/7046 is implemented
+	test.skip('error evaluating module', async ({ request }) => {
 		const response = await request.get('/errors/init-error-endpoint');
 
 		expect(response.status()).toBe(500);
