cd4a2ec6b2852c1f15fcceb304b099d5dffe5e1d
baseballyama <xydybaseball@gmail.com>
2022-02-23 11:55:11 +0900

    add tests

---
+++ packages/adapter-static/test/apps/shadowed/src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script context="module">
+  import { base } from '$app/paths'
+</script>
+
+<script>
+	/** @type {string} */
+  export let message
+</script>
+
+<h1>{message}</h1>
+<a href="{base}/link">go to link</a>
---
+++ packages/adapter-static/test/apps/shadowed/src/routes/link.js
@@ -0,0 +1,5 @@
+export async function get() {
+	return {
+		body: { message: 'This is link page!' }
+	};
+}
