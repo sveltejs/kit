76800c3d6652a79e245a0f075d5a8e747fd5146d
Rich Harris <hello@rich-harris.dev>
2022-02-22 22:51:22 -0500

    simplify test

--- packages/adapter-static/test/apps/shadowed/src/routes/link.js
+++
@@ -1,5 +0,0 @@
-export async function get() {
-	return {
-		body: { message: 'This is link page!' }
-	};
-}
---
+++ packages/kit/test/prerendering/basics/src/routes/index.js
@@ -0,0 +1,7 @@
+export function get() {
+	return {
+		body: {
+			message: 'hello'
+		}
+	};
+}
