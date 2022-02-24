34bd4f54b21cad6e3405489a186afe64487ca3a7
Timothy Foster <tf.auroratide@gmail.com>
2022-02-24 11:05:31 -0600

    Unit Tests for Netlify Adapter split_headers function (#4108)
    * Extract split_headers to its own file so it can be unit tested
    
    * Unit tests for split_headers
    
    * no need for a custom suite
    
    Co-authored-by: Rich Harris <hello@rich-harris.dev>

---
+++ packages/adapter-netlify/src/headers.spec.js
@@ -0,0 +1,54 @@
+import '../src/shims.js';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { split_headers } from '../src/headers.js';
+
+test('empty headers', () => {
+	const headers = new Headers();
+
+	const result = split_headers(headers);
+
+	assert.equal(result, {
+		headers: {},
+		multiValueHeaders: {}
+	});
+});
+
+test('single-value headers', () => {
+	const headers = new Headers();
+	headers.append('Location', '/apple');
+	headers.append('Content-Type', 'application/json');
+
+	const result = split_headers(headers);
+
+	assert.equal(result, {
+		headers: {
+			// Note: becomes lowercase even if specified as uppercase
+			location: '/apple',
+			'content-type': 'application/json'
+		},
+		multiValueHeaders: {}
+	});
+});
+
+test('multi-value headers', () => {
+	// https://httpwg.org/specs/rfc7231.html#http.date
+	const wednesday = 'Wed, 23 Feb 2022 21:01:48 GMT';
+	const thursday = 'Thu, 24 Feb 2022 21:01:48 GMT';
+
+	const headers = new Headers();
+	headers.append('Set-Cookie', `flavor=sugar; Expires=${wednesday}`);
+	headers.append('Set-Cookie', `diameter=6cm; Expires=${thursday}`);
+
+	const result = split_headers(headers);
+
+	// it splits at actual cookie boundaries, not the commas in the dates
+	assert.equal(result, {
+		headers: {},
+		multiValueHeaders: {
+			'set-cookie': [`flavor=sugar; Expires=${wednesday}`, `diameter=6cm; Expires=${thursday}`]
+		}
+	});
+});
+
+test.run();
