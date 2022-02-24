6b5c3ae17ef73030e94b3e5cef1b204db3a53d1e
Rich Harris <hello@rich-harris.dev>
2022-02-24 13:03:19 -0500

    temporary workaround

--- sites/kit.svelte.dev/src/routes/docs/[slug].js
+++ sites/kit.svelte.dev/src/routes/docs/[slug].js
@@ -1,7 +1,13 @@
+import { mode, prerendering } from '$app/env';
 import { read } from '$lib/docs/server';
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get({ params }) {
+	// TODO https://github.com/sveltejs/kit/issues/4093
+	if (!prerendering && mode === 'production') {
+		return { status: 404 };
+	}
+
 	const page = await read('docs', params.slug);
 
 	if (!page) {
