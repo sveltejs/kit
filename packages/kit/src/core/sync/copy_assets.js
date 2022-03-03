800476b83ead1bc866f8b0120231e6741eeb0be0
Rich Harris <richard.a.harris@gmail.com>
2022-03-03 14:10:27 -0500

    Update packages/kit/src/core/sync/copy_assets.js
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- packages/kit/src/core/sync/copy_assets.js
+++ packages/kit/src/core/sync/copy_assets.js
@@ -8,19 +8,18 @@ const __dirname = path.dirname(__filename);
 
 /** @param {string} dest */
 export function copy_assets(dest) {
-	if (process.env.BUNDLED) {
-		let prefix = '..';
-		do {
-			// we jump through these hoops so that this function
-			// works whether or not it's been bundled
-			const resolved = path.resolve(__dirname, `${prefix}/assets`);
+	if (!process.env.BUNDLED) return;
+	let prefix = '..';
+	do {
+		// we jump through these hoops so that this function
+		// works whether or not it's been bundled
+		const resolved = path.resolve(__dirname, `${prefix}/assets`);
 
-			if (fs.existsSync(resolved)) {
-				copy(resolved, dest);
-				return;
-			}
+		if (fs.existsSync(resolved)) {
+			copy(resolved, dest);
+			return;
+		}
 
-			prefix = `../${prefix}`;
-		} while (true); // eslint-disable-line
-	}
+		prefix = `../${prefix}`;
+	} while (true); // eslint-disable-line
 }
