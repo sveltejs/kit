a852a1f260726d36fdf1eb1db951b689c2e29c36
Rich Harris <hello@rich-harris.dev>
2022-03-14 16:18:37 -0400

    remove renamed test files

--- packages/kit/test/apps/basics/src/routes/shadowed/no-next-fallthrough/index.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import {page} from '$app/stores';
-</script>
-<h1>{$page.url.searchParams.get('a')}</h1>
-<a href="/shadowed/no-next-fallthrough?a=test">show</a>
-<a href="/shadowed/no-next-fallthrough">hide</a>
