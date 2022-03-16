f517ca9cde17953bacc3fc6fc8ef2efdf596cf6b
Rich Harris <hello@rich-harris.dev>
2022-03-16 09:50:12 -0400

    oops

--- packages/kit/test/apps/basics/src/routes/routing/route-id/[x].svelte
+++ packages/kit/test/apps/basics/src/routes/routing/route-id/[x].svelte
@@ -1,5 +1,5 @@
 <script context="module">
-	/** @type {import('./[x]').RequestHandler} */
+	/** @type {import('./[x]').Load} */
 	export function load({ routeId }) {
 		return {
 			props: { routeId }
