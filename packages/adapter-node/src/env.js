c4a411a22c75d4bb931d1220af802c595b21b6b7
Rich Harris <hello@rich-harris.dev>
2022-03-17 14:01:34 -0400

    error if env var namespace is already occupied

--- packages/adapter-node/src/env.js
+++ packages/adapter-node/src/env.js
@@ -1,5 +1,29 @@
 /* global ENV_PREFIX */
 
+const expected = new Set([
+	'SOCKET_PATH',
+	'HOST',
+	'PORT',
+	'ORIGIN',
+	'XFF_DEPTH',
+	'ADDRESS_HEADER',
+	'PROTOCOL_HEADER',
+	'HOST_HEADER'
+]);
+
+if (ENV_PREFIX) {
+	for (const name in process.env) {
+		if (name.startsWith(ENV_PREFIX)) {
+			const unprefixed = name.slice(ENV_PREFIX.length);
+			if (!expected.has(unprefixed)) {
+				throw new Error(
+					`You should change envPrefix (${ENV_PREFIX}) to avoid conflicts with existing environment variables — unexpectedly saw ${name}`
+				);
+			}
+		}
+	}
+}
+
 /**
  * @param {string} name
  * @param {any} fallback
