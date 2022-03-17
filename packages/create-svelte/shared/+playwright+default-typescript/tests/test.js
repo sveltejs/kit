29c0ce04bcb776ae778402725b5350a74a97e505
Rich Harris <hello@rich-harris.dev>
2022-03-17 09:07:33 -0400

    convert .ts files automatically

--- packages/create-svelte/shared/+playwright+default-typescript/tests/test.js
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('about page has expected h1', async ({ page }) => {
-	await page.goto('/about');
-	expect(await page.textContent('h1')).toBe('About this app');
-});
