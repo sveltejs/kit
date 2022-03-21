15bbd791f889c5c18529b9eefb548ec43f1289e6
John <john.walshuk@gmail.com>
2022-03-21 13:54:15 +0000

    Add cloudflare cache

--- packages/adapter-cloudflare/files/worker.js
+++ packages/adapter-cloudflare/files/worker.js
@@ -50,12 +50,25 @@ export default {
 
 		// dynamically-generated pages
 		try {
-			return await server.respond(req, {
-				platform: { env, context },
-				getClientAddress() {
-					return req.headers.get('cf-connecting-ip');
-				}
-			});
+			const cache = caches.default;
+			let response = await cache.match(req);
+
+			if (!response) {
+				response = await server.respond(req, {
+					platform: { env, context },
+					getClientAddress() {
+						return req.headers.get('cf-connecting-ip');
+					}
+				});
+
+				// Use context so you can return the response without blocking on
+				// writing to cache
+				try {
+					context(cache.put(req, response.clone()));
+				} catch {}
+			}
+
+			return response;
 		} catch (e) {
 			return new Response('Error rendering route: ' + (e.message || e.toString()), { status: 500 });
 		}
