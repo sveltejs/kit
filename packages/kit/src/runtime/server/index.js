c7956123a0d07cfaad0b06046d6c57ba67cc3e74
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-03-21 10:37:33 -0700

    [fix] don't redirect to external URLs

--- packages/kit/src/runtime/server/index.js
+++ packages/kit/src/runtime/server/index.js
@@ -18,7 +18,7 @@ export async function respond(request, options, state) {
 
 	const normalized = normalize_path(url.pathname, options.trailing_slash);
 
-	if (normalized !== url.pathname && !state.prerender?.fallback) {
+	if (normalized !== url.pathname && !state.prerender?.fallback && !/^(https?:)?\/\//.test(normalized)) {
 		return new Response(undefined, {
 			status: 301,
 			headers: {
