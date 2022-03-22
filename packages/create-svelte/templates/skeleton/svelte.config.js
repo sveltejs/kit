ca610237a60c9df43ebf760a2292e9e591695747
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2022-03-23 01:37:32 +0800

    Add svelte-preprocess as default and remove target: "#svelte" option

--- packages/create-svelte/templates/skeleton/svelte.config.js
+++ packages/create-svelte/templates/skeleton/svelte.config.js
@@ -1,9 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 // This config is ignored and replaced with one of the configs in the shared folder when a project is created.
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
 	kit: {
 		adapter: adapter()
 	}
