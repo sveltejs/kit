bb52cf3ab79e9d5592468919c4969bd21bfbef89
jycouet <jycouet@gmail.com>
2022-03-27 00:16:28 +0100

    :ok_hand: FIX: format

--- packages/create-svelte/templates/default/src/routes/todos/[uid].ts
+++ packages/create-svelte/templates/default/src/routes/todos/[uid].ts
@@ -1,41 +1,41 @@
-import type { RequestHandler } from "@sveltejs/kit";
-import { api } from "./_api";
+import type { RequestHandler } from '@sveltejs/kit';
+import { api } from './_api';
 
 export const get: RequestHandler = async ({ locals, params }) => {
 	const response = await api('get', `todos/${locals.userid}`);
-  
+
 	if (response.status === 404) {
 		return {
 			headers: { Location: '/todos' },
 			status: 302
-		}
+		};
 	}
 
-  const todos: any[] = await response.json();
-  const index = todos.findIndex(t=>t.uid === params.uid);
-	
-  let previousUid = null;
-  let nextUid = null;
-  if(index === -1){
-    return {
+	const todos: any[] = await response.json();
+	const index = todos.findIndex((t) => t.uid === params.uid);
+
+	let previousUid = null;
+	let nextUid = null;
+	if (index === -1) {
+		return {
 			headers: { Location: '/todos' },
 			status: 302
-		}
-  } else if(index === 0) {
-    nextUid = todos[index+1].uid;
-  }else if(index === todos.length-1) {
-    previousUid = todos[index-1].uid;
-  } else {
-    previousUid = todos[index-1].uid;
-    nextUid = todos[index+1].uid;
-  }
-	
+		};
+	} else if (index === 0) {
+		nextUid = todos[index + 1].uid;
+	} else if (index === todos.length - 1) {
+		previousUid = todos[index - 1].uid;
+	} else {
+		previousUid = todos[index - 1].uid;
+		nextUid = todos[index + 1].uid;
+	}
+
 	if (response.status === 200) {
 		return {
 			body: {
 				todo: todos[index],
-        previousUid,
-        nextUid,
+				previousUid,
+				nextUid
 			}
 		};
 	}
