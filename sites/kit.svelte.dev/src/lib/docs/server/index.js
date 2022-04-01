aa2e2c411d6541ad148c0325d01f08fad5a62a8d
Rich Harris <richard.a.harris@gmail.com>
2022-04-01 15:55:10 -0400

    apply linking logic to inline code spans (#4475)

--- sites/kit.svelte.dev/src/lib/docs/server/index.js
+++ sites/kit.svelte.dev/src/lib/docs/server/index.js
@@ -171,6 +171,16 @@ export async function read_file(dir, file) {
 							.join('');
 					}
 				);
+		},
+		codespan: (text) => {
+			return (
+				'<code>' +
+				text.replace(type_regex, (match, prefix, name) => {
+					const link = `<a href="${type_links.get(name)}">${name}</a>`;
+					return `${prefix || ''}${link}`;
+				}) +
+				'</code>'
+			);
 		}
 	});
 
@@ -241,7 +251,8 @@ export function read_headings(dir) {
 				file,
 				// gross hack to accommodate FAQ
 				slug: dir === 'faq' ? slug : undefined,
-				code: () => ''
+				code: () => '',
+				codespan: () => ''
 			});
 
 			return {
@@ -259,9 +270,10 @@ export function read_headings(dir) {
  *   file: string;
  *   slug: string;
  *   code: (source: string, language: string, current: string) => string;
+ *   codespan: (source: string) => string;
  * }} opts
  */
-function parse({ body, file, slug, code }) {
+function parse({ body, file, slug, code, codespan }) {
 	const headings = slug ? [slug] : [];
 	const sections = [];
 
@@ -307,7 +319,8 @@ function parse({ body, file, slug, code }) {
 
 			return `<h${level} id="${slug}">${html}<a href="#${slug}" class="anchor"><span class="visually-hidden">permalink</span></a></h${level}>`;
 		},
-		code: (source, language) => code(source, language, current)
+		code: (source, language) => code(source, language, current),
+		codespan
 	});
 
 	return {
