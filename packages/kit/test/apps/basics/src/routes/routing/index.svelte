62b3e8d0f7dedda1f03f196aa8e336b3833b2658
Rich Harris <hello@rich-harris.dev>
2022-04-06 15:06:52 -0400

    ok i guess we needed those

--- packages/kit/test/apps/basics/src/routes/routing/index.svelte
+++ packages/kit/test/apps/basics/src/routes/routing/index.svelte
@@ -5,7 +5,7 @@
 <h1>Great success!</h1>
 
 <a href="/routing/a">a</a>
-<a href="/routing/ambiguous/ok.json">ok</a>
+<a href="/routing/ambiguous/ok.json" rel="external">ok</a>
 <a href="http://localhost:{$page.url.searchParams.get('port')}">elsewhere</a>
 <a href="/static.json">static.json</a>
 
