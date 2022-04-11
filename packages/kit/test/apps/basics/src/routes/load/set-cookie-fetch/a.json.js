9e063c2e5b4f10dc82ddb1bb944e68056665f084
Rich Harris <hello@rich-harris.dev>
2022-04-11 17:07:50 -0400

    apply set-cookie headers from page dependencies - fixes #1198

---
+++ packages/kit/test/apps/basics/src/routes/load/set-cookie-fetch/a.json.js
@@ -0,0 +1,11 @@
+/** @type {import('./a.json').RequestHandler} */
+export function get({ url }) {
+	const answer = url.searchParams.get('answer') || '42';
+
+	return {
+		headers: {
+			'set-cookie': `answer=${answer}; HttpOnly; Path=/load/set-cookie-fetch`
+		},
+		body: {}
+	};
+}
