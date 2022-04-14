dbe65f0f1f1f450ce820e2050558d07fef756ab5
Rich Harris <hello@rich-harris.dev>
2022-04-14 15:52:40 -0400

    strip jsdoc from source

--- packages/create-svelte/scripts/build-templates.js
+++ packages/create-svelte/scripts/build-templates.js
@@ -6,9 +6,9 @@ import { transform } from 'sucrase';
 import glob from 'tiny-glob/sync.js';
 import { mkdirp, rimraf } from '../utils.js';
 
-/** @param {string} typescript */
-function convert_typescript(typescript) {
-	const transformed = transform(typescript, {
+/** @param {string} content */
+function convert_typescript(content) {
+	const transformed = transform(content, {
 		transforms: ['typescript']
 	});
 
@@ -19,6 +19,11 @@ function convert_typescript(typescript) {
 		trailingComma: 'none',
 		printWidth: 100
 	});
+}
+
+/** @param {string} content */
+function strip_jsdoc(content) {
+	return content.replace(/\/\*\*[\s\S]+?\*\/[\s\n]+/g, '');
 }
 
 /** @param {Set<string>} shared */
@@ -44,6 +49,9 @@ async function generate_templates(shared) {
 		if (!fs.existsSync(meta_file)) throw new Error('Template must have a .meta.json file');
 
 		/** @type {import('../types/internal.js').File[]} */
+		const js = [];
+
+		/** @type {import('../types/internal.js').File[]} */
 		const ts = [];
 
 		glob('**/*', { cwd, filesOnly: true, dot: true }).forEach((name) => {
@@ -64,84 +72,82 @@ async function generate_templates(shared) {
 			// ignore contents of .gitignore or .ignore
 			if (!gitignore.accepts(name) || !ignore.accepts(name) || name === '.ignore') return;
 
-			if (/\.(js|ts|svelte|svelte\.md)$/.test(name)) {
+			if (/\.(ts|svelte)$/.test(name)) {
 				const contents = fs.readFileSync(path.join(cwd, name), 'utf8');
-				ts.push({
-					name,
-					contents
-				});
+
+				if (name.endsWith('.d.ts')) {
+					if (name.endsWith('app.d.ts')) js.push({ name, contents });
+					ts.push({ name, contents });
+				} else if (name.endsWith('.ts')) {
+					ts.push({
+						name,
+						contents: strip_jsdoc(contents)
+					});
+
+					js.push({
+						name: name.replace(/\.ts$/, '.js'),
+						contents: convert_typescript(contents)
+					});
+				} else {
+					// we jump through some hoops, rather than just using svelte.preprocess,
+					// so that the output preserves the original formatting to the extent
+					// possible (e.g. preserving double line breaks). Sucrase is the best
+					// tool for the job because it just removes the types; Prettier then
+					// tidies up the end result
+					const js_contents = contents.replace(
+						/<script([^>]+)>([\s\S]+?)<\/script>/g,
+						(m, attrs, typescript) => {
+							// Sucrase assumes 'unused' imports (which _are_ used, but only
+							// in the markup) are type imports, and strips them. This step
+							// prevents it from drawing that conclusion
+							const imports = [];
+							const import_pattern = /import (.+?) from/g;
+							let import_match;
+							while ((import_match = import_pattern.exec(typescript))) {
+								const word_pattern = /[a-z_$][a-z0-9_$]*/gi;
+								let word_match;
+								while ((word_match = word_pattern.exec(import_match[1]))) {
+									imports.push(word_match[0]);
+								}
+							}
+
+							const suffix = `\n${imports.join(',')}`;
+
+							const transformed = transform(typescript + suffix, {
+								transforms: ['typescript']
+							}).code.slice(0, -suffix.length);
+
+							const contents = prettier
+								.format(transformed, {
+									parser: 'babel',
+									useTabs: true,
+									singleQuote: true,
+									trailingComma: 'none',
+									printWidth: 100
+								})
+								.trim()
+								.replace(/^(.)/gm, '\t$1');
+
+							return `<script${attrs.replace(' lang="ts"', '')}>\n${contents}\n</script>`;
+						}
+					);
+
+					js.push({
+						name,
+						contents: js_contents
+					});
+
+					ts.push({
+						name,
+						contents: strip_jsdoc(contents)
+					});
+				}
 			} else {
 				const dest = path.join(assets, name.replace(/^\./, 'DOT-'));
 				mkdirp(path.dirname(dest));
 				fs.copyFileSync(path.join(cwd, name), dest);
 			}
 		});
-
-		/** @type {import('../types/internal.js').File[]} */
-		const js = [];
-
-		for (const file of ts) {
-			// The app.d.ts file makes TS/JS aware of some ambient modules, which are
-			// also needed for JS projects if people turn on "checkJs" in their jsonfig
-			if (file.name.endsWith('.d.ts')) {
-				if (file.name.endsWith('app.d.ts')) js.push(file);
-			} else if (file.name.endsWith('.ts')) {
-				js.push({
-					name: file.name.replace(/\.ts$/, '.js'),
-					contents: convert_typescript(file.contents)
-				});
-			} else if (file.name.endsWith('.svelte')) {
-				// we jump through some hoops, rather than just using svelte.preprocess,
-				// so that the output preserves the original formatting to the extent
-				// possible (e.g. preserving double line breaks). Sucrase is the best
-				// tool for the job because it just removes the types; Prettier then
-				// tidies up the end result
-				const contents = file.contents.replace(
-					/<script([^>]+)>([\s\S]+?)<\/script>/g,
-					(m, attrs, typescript) => {
-						// Sucrase assumes 'unused' imports (which _are_ used, but only
-						// in the markup) are type imports, and strips them. This step
-						// prevents it from drawing that conclusion
-						const imports = [];
-						const import_pattern = /import (.+?) from/g;
-						let import_match;
-						while ((import_match = import_pattern.exec(typescript))) {
-							const word_pattern = /[a-z_$][a-z0-9_$]*/gi;
-							let word_match;
-							while ((word_match = word_pattern.exec(import_match[1]))) {
-								imports.push(word_match[0]);
-							}
-						}
-
-						const suffix = `\n${imports.join(',')}`;
-
-						const transformed = transform(typescript + suffix, {
-							transforms: ['typescript']
-						}).code.slice(0, -suffix.length);
-
-						const contents = prettier
-							.format(transformed, {
-								parser: 'babel',
-								useTabs: true,
-								singleQuote: true,
-								trailingComma: 'none',
-								printWidth: 100
-							})
-							.trim()
-							.replace(/^(.)/gm, '\t$1');
-
-						return `<script${attrs.replace(' lang="ts"', '')}>\n${contents}\n</script>`;
-					}
-				);
-
-				js.push({
-					name: file.name,
-					contents
-				});
-			} else {
-				js.push(file);
-			}
-		}
 
 		fs.copyFileSync(meta_file, `${dir}/meta.json`);
 		fs.writeFileSync(`${dir}/files.ts.json`, JSON.stringify(ts, null, '\t'));
