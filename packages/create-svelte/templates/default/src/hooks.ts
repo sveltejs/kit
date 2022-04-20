3b2bce2c8320a43ebf968d6deec190e556a045b1
Nicolas Le Cam <nicolas.lecam@digital4better.com>
2022-04-20 16:58:45 +0200

    create-svelte: Fix hooks tsc warnings on strictest typescript config
    Error: Module '@types/cookie/index' has no default export. ts(1192)
    Error: Property 'userid' comes from an index signature, so it must be accessed with ['userid']. ts(4111)
    
    Strictest tsconfig from https://github.com/tsconfig/bases/blob/27f9097a02f960e351d69bca8dd4c7581d70e1b6/bases/strictest.json

--- packages/create-svelte/templates/default/src/hooks.ts
+++ packages/create-svelte/templates/default/src/hooks.ts
@@ -1,15 +1,15 @@
-import cookie from 'cookie';
 import { v4 as uuid } from '@lukeed/uuid';
 import type { Handle } from '@sveltejs/kit';
+import * as cookie from 'cookie';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle: Handle = async ({ event, resolve }) => {
 	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-	event.locals.userid = cookies.userid || uuid();
+	event.locals.userid = cookies['userid'] || uuid();
 
 	const response = await resolve(event);
 
-	if (!cookies.userid) {
+	if (!cookies['userid']) {
 		// if this is the first time the user has visited this app,
 		// set a cookie so that we recognise them when they return
 		response.headers.set(
