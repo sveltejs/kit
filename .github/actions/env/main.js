e43f784d6e820f108b18dd9a51c958992ed47d61
Conduitry <git@chor.date>
2022-04-24 23:45:26 -0400

    tidy env action

--- .github/actions/env/main.js
+++ .github/actions/env/main.js
@@ -1,28 +1,12 @@
-const { homedir, platform, EOL } = require('os');
-const { join } = require('path');
-
-const WINDOWS_CACHE_PATH = 'D:\\.pnpm-store\\v3';
-const NIX_CACHE_PATH = '~/.pnpm-store/v3';
-const WINDOWS_BROWSER_PATH = join(homedir(), 'AppData', 'Local', 'ms-playwright');
-const LINUX_BROWSER_PATH = '~/.cache/ms-playwright';
-const MACOS_BROWSER_PATH = '~/Library/Caches/ms-playwright';
-
-const paths = {
-	win32: {
-		browser: WINDOWS_BROWSER_PATH,
-		cache: WINDOWS_CACHE_PATH
-	},
-	linux: {
-		browser: LINUX_BROWSER_PATH,
-		cache: NIX_CACHE_PATH
-	},
-	darwin: {
-		browser: MACOS_BROWSER_PATH,
-		cache: NIX_CACHE_PATH
-	}
-};
-
-const os = platform();
-
-process.stdout.write(EOL + '::set-output name=pnpm_cache_path::' + paths[os].cache + EOL);
-process.stdout.write(EOL + '::set-output name=browser_cache_path::' + paths[os].browser + EOL);
+console.log(
+	'::set-output name=pnpm_cache_path::' +
+		(process.platform === 'win32' ? 'D:\\.pnpm-store\\v3' : '~/.pnpm-store/v3')
+);
+console.log(
+	'::set-output name=browser_cache_path::' +
+		(process.platform === 'win32'
+			? require('path').join(require('os').homedir(), 'AppData', 'Local', 'ms-playwright')
+			: process.platform === 'linux'
+			? '~/.cache/ms-playwright'
+			: '~/Library/Caches/ms-playwright')
+);
