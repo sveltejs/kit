9e69ba7f35d0e531d1514f94aa7ebf96307a960e
Rich Harris <hello@rich-harris.dev>
2022-04-27 09:31:01 -0400

    Update packages/kit/src/core/sync/create_manifest_data/index.js

--- packages/kit/src/core/sync/create_manifest_data/index.js
+++ packages/kit/src/core/sync/create_manifest_data/index.js
@@ -306,7 +306,7 @@ function trace(file, path, tree, extensions) {
 	let layout_id = base.includes('@') ? base.split('@')[1] : DEFAULT;
 
 	if (parts.findIndex((part) => part.indexOf('@') > -1) > -1) {
-		throw new Error(`Invalid route ${file} - named layouts are not allowed in directory`);
+		throw new Error(`Invalid route ${file} - named layouts are not allowed in directories`);
 	}
 
 	// walk up the tree, find which __layout and __error components
