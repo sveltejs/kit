b796cb236a79cebde01f692ab711e496bda654a2
GHOST <ghostdevbusiness@gmail.com>
2022-05-04 18:19:49 +0100

    update generated tsconfig (#4791)
    * update generated tsconfig
    
    * update generated tsconfig
    
    * use esnext
    
    * Update .changeset/hip-ears-reflect.md
    
    Co-authored-by: Rich Harris <hello@rich-harris.dev>

--- packages/kit/src/core/sync/write_tsconfig.js
+++ packages/kit/src/core/sync/write_tsconfig.js
@@ -56,7 +56,14 @@ export function write_tsconfig(config) {
 					isolatedModules: true,
 					// TypeScript doesn't know about import usages in the template because it only sees the
 					// script of a Svelte file. Therefore preserve all value imports. Requires TS 4.5 or higher.
-					preserveValueImports: true
+					preserveValueImports: true,
+
+					// This is required for svelte-kit package to work as expected
+					// Can be overwritten
+					lib: ['esnext', 'DOM'],
+					moduleResolution: 'node',
+					module: 'esnext',
+					target: 'esnext'
 				},
 				include,
 				exclude: [config_relative('node_modules/**'), './**']
