cf8ab257587cafd71a78a2cdd03d94126905dc84
timothycohen <contact@timcohen.dev>
2022-05-07 16:37:03 -0400

    prevent scroll when resetting page focus

---
+++ packages/kit/test/apps/basics/src/routes/no-ssr/margin.svelte
@@ -0,0 +1,14 @@
+<div class="container">
+	<span> ^this is not the top of the screen</span>
+	<div class="spacer" />
+</div>
+
+<style>
+	.container {
+		margin-top: 300px;
+	}
+
+	.spacer {
+		height: 1000px;
+	}
+</style>
