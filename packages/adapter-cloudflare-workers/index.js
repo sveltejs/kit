f6f850578bb61171bf86f117b8dc3e45219e3722
Rich Harris <hello@rich-harris.dev>
2022-05-12 11:51:52 -0400

    wrangler 2 support

--- packages/adapter-cloudflare-workers/index.js
+++ packages/adapter-cloudflare-workers/index.js
@@ -1,9 +1,18 @@
 import { existsSync, readFileSync, writeFileSync } from 'fs';
-import { posix } from 'path';
+import { posix, dirname } from 'path';
 import { execSync } from 'child_process';
 import esbuild from 'esbuild';
 import toml from '@iarna/toml';
 import { fileURLToPath } from 'url';
+
+/**
+ * @typedef {{
+ *   main: string;
+ *   site: {
+ *     bucket: string;
+ *   }
+ * }} WranglerConfig
+ */
 
 /** @type {import('.')} */
 export default function (options = {}) {
@@ -11,22 +20,13 @@ export default function (options = {}) {
 		name: '@sveltejs/adapter-cloudflare-workers',
 
 		async adapt(builder) {
-			const { site, build } = validate_config(builder);
-
-			// @ts-ignore
-			const { bucket } = site;
-
-			// @ts-ignore
-			const entrypoint = site['entry-point'] || 'workers-site';
-
-			// @ts-ignore
-			const main_path = build.upload.main;
+			const { main, site } = validate_config(builder);
 
 			const files = fileURLToPath(new URL('./files', import.meta.url).href);
 			const tmp = builder.getBuildDirectory('cloudflare-workers-tmp');
 
-			builder.rimraf(bucket);
-			builder.rimraf(entrypoint);
+			builder.rimraf(site.bucket);
+			builder.rimraf(dirname(main));
 
 			builder.log.info('Installing worker dependencies...');
 			builder.copy(`${files}/_package.json`, `${tmp}/package.json`);
@@ -59,72 +59,48 @@ export default function (options = {}) {
 				platform: 'browser',
 				...options,
 				entryPoints: [`${tmp}/entry.js`],
-				outfile: `${entrypoint}/${main_path}`,
+				outfile: main,
 				bundle: true,
 				external: ['__STATIC_CONTENT_MANIFEST', ...(options?.external || [])],
 				format: 'esm'
 			});
 
-			writeFileSync(
-				`${entrypoint}/package.json`,
-				JSON.stringify({ main: main_path, type: 'module' })
-			);
-
 			builder.log.minor('Copying assets...');
-			builder.writeClient(bucket);
-			builder.writeStatic(bucket);
-			builder.writePrerendered(bucket);
+			builder.writeClient(site.bucket);
+			builder.writeStatic(site.bucket);
+			builder.writePrerendered(site.bucket);
 		}
 	};
 }
 
-/** @param {import('@sveltejs/kit').Builder} builder */
+/**
+ * @param {import('@sveltejs/kit').Builder} builder
+ * @returns {WranglerConfig}
+ */
 function validate_config(builder) {
 	if (existsSync('wrangler.toml')) {
+		/** @type {WranglerConfig} */
 		let wrangler_config;
 
 		try {
-			wrangler_config = toml.parse(readFileSync('wrangler.toml', 'utf-8'));
+			wrangler_config = /** @type {WranglerConfig} */ (
+				toml.parse(readFileSync('wrangler.toml', 'utf-8'))
+			);
 		} catch (err) {
 			err.message = `Error parsing wrangler.toml: ${err.message}`;
 			throw err;
 		}
 
-		// @ts-ignore
-		if (!wrangler_config.site || !wrangler_config.site.bucket) {
+		if (!wrangler_config.site?.bucket) {
 			throw new Error(
 				'You must specify site.bucket in wrangler.toml. Consult https://developers.cloudflare.com/workers/platform/sites/configuration'
 			);
 		}
 
-		// @ts-ignore
-		if (!wrangler_config.build || !wrangler_config.build.upload) {
+		if (!wrangler_config.main) {
 			throw new Error(
-				'You must specify build.upload options in wrangler.toml. Consult https://github.com/sveltejs/kit/tree/master/packages/adapter-cloudflare-workers'
+				'You must specify main option in wrangler.toml. Consult https://github.com/sveltejs/kit/tree/master/packages/adapter-cloudflare-workers'
 			);
-		}
-
-		// @ts-ignore
-		if (wrangler_config.build.upload.format !== 'modules') {
-			throw new Error('build.upload.format in wrangler.toml must be "modules"');
-		}
-
-		// @ts-ignore
-		const main_file = wrangler_config.build?.upload?.main;
-		const main_file_ext = main_file?.split('.').slice(-1)[0];
-		if (main_file_ext && main_file_ext !== 'mjs') {
-			// @ts-ignore
-			const upload_rules = wrangler_config.build?.upload?.rules;
-			// @ts-ignore
-			const matching_rule = upload_rules?.find(({ globs }) =>
-				// @ts-ignore
-				globs.find((glob) => glob.endsWith(`*.${main_file_ext}`))
-			);
-			if (!matching_rule) {
-				throw new Error(
-					'To support a build.upload.main value not ending in .mjs, an upload rule must be added to build.upload.rules. Consult https://developers.cloudflare.com/workers/cli-wrangler/configuration/#build'
-				);
-			}
 		}
 
 		return wrangler_config;
@@ -139,22 +115,16 @@ function validate_config(builder) {
 		Sample wrangler.toml:
 
 		name = "<your-site-name>"
-		type = "javascript"
 		account_id = "<your-account-id>"
-		workers_dev = true
-		route = ""
-		zone_id = ""
+		route = "<your-route>"
 
-		[build]
-		command = ""
+		main = "./.cloudflare/worker.js"
+		site.bucket = "./.cloudflare/public"
 
-		[build.upload]
-		format = "modules"
-		main = "./worker.mjs"
+		build.command = "npm run build"
 
-		[site]
-		bucket = "./.cloudflare/assets"
-		entry-point = "./.cloudflare/worker"`
+		compatibility_date = "2021-11-12"
+		workers_dev = true`
 			.replace(/^\t+/gm, '')
 			.trim()
 	);
