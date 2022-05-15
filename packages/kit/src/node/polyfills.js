3836e2e76e26dedb65416249ada236162f88622f
Rich Harris <hello@rich-harris.dev>
2022-05-15 15:24:13 -0400

    polyfill crypto

--- packages/kit/src/node/polyfills.js
+++ packages/kit/src/node/polyfills.js
@@ -1,8 +1,14 @@
 import fetch, { Response, Request, Headers } from 'node-fetch';
+import { webcrypto } from 'crypto';
 
 // exported for dev/preview and node environments
 export function installPolyfills() {
 	Object.defineProperties(globalThis, {
+		crypto: {
+			enumerable: true,
+			configurable: true,
+			value: webcrypto
+		},
 		fetch: {
 			enumerable: true,
 			configurable: true,
