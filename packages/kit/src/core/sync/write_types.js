ad66d264db203361c5daf7731b183844c7613542
Rich Harris <hello@rich-harris.dev>
2022-05-19 17:27:04 -0400

    remove rimraf for a second

--- packages/kit/src/core/sync/write_types.js
+++ packages/kit/src/core/sync/write_types.js
@@ -23,7 +23,7 @@ export type Load<
  * @param {import('types').ManifestData} manifest_data
  */
 export function write_types(config, manifest_data) {
-	rimraf(`${config.kit.outDir}/types`);
+	// rimraf(`${config.kit.outDir}/types`);
 
 	/** @type {Map<string, { params: string[], type: 'page' | 'endpoint' | 'both' }>} */
 	const shadow_types = new Map();
