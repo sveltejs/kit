a452bd83ac5feb27040a1cb364395c9ae5c04107
Rich Harris <richard.a.harris@gmail.com>
2022-05-21 08:34:20 -0400

    revert to using cjs for lambda (#4967)

--- packages/adapter-vercel/index.js
+++ packages/adapter-vercel/index.js
@@ -145,10 +145,10 @@ async function v1(builder, external) {
 		bundle: true,
 		platform: 'node',
 		external,
-		format: 'esm'
+		format: 'cjs'
 	});
 
-	writeFileSync(`${dirs.lambda}/package.json`, JSON.stringify({ type: 'module' }));
+	writeFileSync(`${dirs.lambda}/package.json`, JSON.stringify({ type: 'commonjs' }));
 
 	builder.log.minor('Copying assets...');
 
@@ -271,7 +271,7 @@ async function v3(builder, external, edge, split) {
 			target: `node${node_version.full}`,
 			bundle: true,
 			platform: 'node',
-			format: 'esm',
+			format: 'cjs',
 			external
 		});
 
@@ -284,7 +284,7 @@ async function v3(builder, external, edge, split) {
 			})
 		);
 
-		write(`${dirs.functions}/${name}.func/package.json`, JSON.stringify({ type: 'module' }));
+		write(`${dirs.functions}/${name}.func/package.json`, JSON.stringify({ type: 'commonjs' }));
 
 		routes.push({ src: pattern, dest: `/${name}` });
 	}
