d5cf5e042f5f2068d0ae99af491f8a244b1abb23
Rich Harris <richard.a.harris@gmail.com>
2022-05-23 19:54:05 -0400

    use `crypto.randomUUID()` (#5042)
    * use crypto.randomUUID() instead of @lukeed/uuid - closes #4929
    
    * changeset

--- packages/create-svelte/templates/default/src/hooks.ts
+++ packages/create-svelte/templates/default/src/hooks.ts
@@ -1,11 +1,10 @@
-import { v4 as uuid } from '@lukeed/uuid';
 import type { Handle } from '@sveltejs/kit';
 import * as cookie from 'cookie';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle: Handle = async ({ event, resolve }) => {
 	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-	event.locals.userid = cookies['userid'] || uuid();
+	event.locals.userid = cookies['userid'] || crypto.randomUUID();
 
 	const response = await resolve(event);
 
