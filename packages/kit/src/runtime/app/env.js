a56f13a4b064c38885ad3b3f0dfa215a818bdbe2
Rich Harris <hello@rich-harris.dev>
2022-06-23 22:07:31 -0400

    alphabetise within categories

--- packages/kit/src/runtime/app/env.js
+++ packages/kit/src/runtime/app/env.js
@@ -1,19 +1,23 @@
 /**
- * @type {import('$app/env').server}
- */
-export const server = !!import.meta.env.SSR;
-/**
  * @type {import('$app/env').browser}
  */
 export const browser = !import.meta.env.SSR;
+
 /**
- * @type {import('$app/env').prod}
+ * @type {import('$app/env').server}
  */
-export const prod = !import.meta.env.DEV;
+export const server = !!import.meta.env.SSR;
+
 /**
  * @type {import('$app/env').dev}
  */
 export const dev = !!import.meta.env.DEV;
+
+/**
+ * @type {import('$app/env').prod}
+ */
+export const prod = !import.meta.env.DEV;
+
 /**
  * @type {import('$app/env').mode}
  */
