e39c761592ed81263bc4df874ae94cbfff70a1eb
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-06-23 20:35:14 -0700

    Generate sourcemaps

--- packages/adapter-cloudflare-workers/index.js
+++ packages/adapter-cloudflare-workers/index.js
@@ -55,8 +55,9 @@ export default function (options = {}) {
 			);
 
 			await esbuild.build({
-				target: 'es2020',
 				platform: 'browser',
+				sourcemap: 'linked',
+				target: 'es2020',
 				...options,
 				entryPoints: [`${tmp}/entry.js`],
 				outfile: main,
