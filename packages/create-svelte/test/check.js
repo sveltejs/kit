cdf194e8b665c45469a2846897a82ed67735a919
Rich Harris <hello@rich-harris.dev>
2022-07-05 16:18:16 -0400

    neaten

--- packages/create-svelte/test/check.js
+++ packages/create-svelte/test/check.js
@@ -1,19 +1,19 @@
+import fs from 'fs';
+import { execSync } from 'child_process';
 import { test } from 'uvu';
 import { create } from '../index.js';
-import fs, { rmSync } from 'fs';
-import { execSync } from 'child_process';
 
 const dir = '.test-tmp';
 
 test.after(() => {
-	rmSync(dir, { recursive: true, force: true });
+	fs.rmSync(dir, { recursive: true, force: true });
 });
 
 for (const template of fs.readdirSync('templates')) {
 	for (const types of ['checkjs', 'typescript']) {
-		test(`${template}: JSDoc`, () => {
+		test(`${template}: ${types}`, () => {
 			const cwd = `${dir}/${template}-${types}`;
-			rmSync(cwd, { recursive: true, force: true });
+			fs.rmSync(cwd, { recursive: true, force: true });
 
 			create(cwd, {
 				name: 'test',
@@ -24,8 +24,7 @@ for (const template of fs.readdirSync('templates')) {
 				playwright: false
 			});
 
-			execSync('npm i', { cwd, stdio: 'inherit' });
-			execSync('npm run check', { cwd, stdio: 'inherit' });
+			execSync('npm i && npm run check', { cwd, stdio: 'inherit' });
 		});
 	}
 }
