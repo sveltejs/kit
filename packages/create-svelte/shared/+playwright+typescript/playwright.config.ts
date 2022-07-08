734f49bc3067fe2a11194a660219ce2882d4007f
Philip Weber <mail@philipweber.de>
2022-07-08 20:57:29 +0200

    [fix] Use vite preview port in playwright template

--- packages/create-svelte/shared/+playwright+typescript/playwright.config.ts
+++ packages/create-svelte/shared/+playwright+typescript/playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 3000
+		port: 4173
 	}
 };
 
