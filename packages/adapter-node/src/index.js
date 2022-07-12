f9832a44773a09936b0958e20e80a427ac57f5ed
Fernando López Guevara <fernando.lguevara@gmail.com>
2022-07-12 00:39:59 -0300

    feat(adapter-node): make compression optional

--- packages/adapter-node/src/index.js
+++ packages/adapter-node/src/index.js
@@ -6,13 +6,15 @@ import polka from 'polka';
 export const path = env('SOCKET_PATH', false);
 export const host = env('HOST', '0.0.0.0');
 export const port = env('PORT', !path && '3000');
+export const compression_enabled = env('COMPRESSION_ENABLED', 'true') === 'true';
 
-const server = polka().use(
-	// https://github.com/lukeed/polka/issues/173
-	// @ts-ignore - nothing we can do about so just ignore it
-	compression({ threshold: 0 }),
-	handler
-);
+const compression_middleware = compression_enabled ? compression({ threshold: 0 }) : undefined;
+
+const middlewares = [compression_middleware, handler].filter(Boolean);
+
+// https://github.com/lukeed/polka/issues/173
+// @ts-ignore - nothing we can do about so just ignore it
+const server = polka().use(...middlewares);
 
 server.listen({ path, host, port }, () => {
 	console.log(`Listening on ${path ? path : host + ':' + port}`);
