4f6c7597272463dfc88f1fb857ee7ebb3cbe08b0
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-07-12 09:00:56 -0700

    update default port

--- packages/kit/test/apps/basics/test/test.js
+++ packages/kit/test/apps/basics/test/test.js
@@ -1639,7 +1639,7 @@ test.describe('Load', () => {
 	});
 
 	test('using window.fetch causes a warning', async ({ page, javaScriptEnabled }) => {
-		const port = process.env.DEV ? 3000 : 4173;
+		const port = process.env.DEV ? 5173 : 4173;
 
 		if (javaScriptEnabled && process.env.DEV) {
 			const warnings = [];
