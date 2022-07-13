ace0b248f189ab48e7084dfc86eac97d1d017176
Fernando López Guevara <fernando.lguevara@gmail.com>
2022-07-13 16:47:09 -0300

    feat(compression): make threshold configurable

--- packages/adapter-node/src/index.js
+++ packages/adapter-node/src/index.js
@@ -1,3 +1,5 @@
+/* global ENV_PREFIX */
+
 import { handler } from './handler.js';
 import { env } from './env.js';
 import compression from 'compression';
@@ -7,8 +9,15 @@ export const path = env('SOCKET_PATH', false);
 export const host = env('HOST', '0.0.0.0');
 export const port = env('PORT', !path && '3000');
 export const compression_enabled = env('COMPRESSION_ENABLED', 'true') === 'true';
+export const compression_threshold = parseInt(env('COMPRESSION_THRESHOLD', '0'), 10);
 
-const compression_middleware = compression_enabled ? compression({ threshold: 0 }) : undefined;
+if (isNaN(compression_threshold) || compression_threshold < 0) {
+	throw Error(`${ENV_PREFIX}COMPRESSION_THRESHOLD should be a positve number`);
+}
+
+const compression_middleware = compression_enabled
+	? compression({ threshold: compression_threshold })
+	: undefined;
 
 const middlewares = [compression_middleware, handler].filter(Boolean);
 
