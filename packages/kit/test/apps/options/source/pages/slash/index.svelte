eccfdcbbf130bb3d0ab48f96d414f2d015493f23
Rich Harris <hello@rich-harris.dev>
2022-07-13 18:27:17 -0400

    add some logging that might help diagnose windows failure

--- packages/kit/test/apps/options/source/pages/slash/index.svelte
+++ packages/kit/test/apps/options/source/pages/slash/index.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
+
+	console.error(`\n======\nbase: ${base}\n$page.url.pathname: ${$page.url.pathname}\n======\n`);
 </script>
 
 <h2>{$page.url.pathname.replace(base, '')}</h2>
