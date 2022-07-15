68130ece3cd44532c8dac03eb840dbe91b030d41
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-07-14 22:05:51 -0700

    [fix] ensure completed_build is reset

--- packages/kit/src/vite/index.js
+++ packages/kit/src/vite/index.js
@@ -176,6 +176,7 @@ function kit() {
 			vite_config_env = config_env;
 			svelte_config = await load_config();
 			is_build = config_env.command === 'build';
+			completed_build = false;
 
 			paths = {
 				build_dir: `${svelte_config.kit.outDir}/build`,
