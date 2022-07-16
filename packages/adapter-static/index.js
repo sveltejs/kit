d2f1799c6383761385647256b21824bbf53d0348
Rich Harris <hello@rich-harris.dev>
2022-07-16 12:40:29 -0400

    Update packages/adapter-static/index.js

--- packages/adapter-static/index.js
+++ packages/adapter-static/index.js
@@ -15,7 +15,7 @@ export default function (options) {
 		async adapt(builder) {
 			if (!options?.fallback && !builder.config.kit.prerender.default) {
 				throw Error(
-					"It looks like you're using adapter-static, but `config.kit.prerender.default` is `false` and `fallback` has not been provided as a configuration argument to adapter-static. Either set `config.kit.prerender.default` to true (to statically generate your entire site) or set `config.kit.adapter.fallback` to your desired fallback page (to operate in SPA mode). For more information, check out https://github.com/sveltejs/kit/tree/master/packages/adapter-static#spa-mode"
+					'adapter-static requires `config.kit.prerender.default` to be `true` unless you set the `fallback: true` option to create a single-page app. See https://github.com/sveltejs/kit/tree/master/packages/adapter-static#spa-mode for more information'
 				);
 			}
 
