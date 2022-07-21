bfc86e6e944673c2046b5f5436eab3eadd83a46d
Rich Harris <hello@rich-harris.dev>
2022-07-21 15:03:44 -0400

    fix

--- packages/kit/src/runtime/server/page/render.js
+++ packages/kit/src/runtime/server/page/render.js
@@ -292,10 +292,11 @@ export async function render_response({
 		options.paths.assets || (segments.length > 0 ? segments.map(() => '..').join('/') : '.');
 
 	// TODO flush chunks as early as we can
-	const html = await resolve_opts.transformPageChunk({
-		html: options.template({ head, body, assets, nonce: /** @type {string} */ (csp.nonce) }),
-		done: true
-	});
+	const html =
+		(await resolve_opts.transformPageChunk({
+			html: options.template({ head, body, assets, nonce: /** @type {string} */ (csp.nonce) }),
+			done: true
+		})) || '';
 
 	const headers = new Headers({
 		'content-type': 'text/html',
