caf7cabe26057a74be75c5243bbf980694c2926d
Rich Harris <hello@rich-harris.dev>
2022-07-25 17:09:53 -0400

    Update packages/kit/src/runtime/env-private.js

--- packages/kit/src/runtime/env-private.js
+++ packages/kit/src/runtime/env-private.js
@@ -2,7 +2,6 @@
 /** @type {App.PrivateEnv} */
 export let env = {};
 
-// TODO put this in a separate module, so it's not user-accessible
 /** @type {(environment: Record<string, string>) => void} */
 export function set_private_env(environment) {
 	env = environment;
