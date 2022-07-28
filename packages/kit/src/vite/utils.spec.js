87c27afd3846c57f5af13b2ac9b988c1c509c653
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2022-07-27 23:26:24 -0600

    fix: utils tests

--- packages/kit/src/vite/utils.spec.js
+++ packages/kit/src/vite/utils.spec.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 import {
@@ -260,7 +261,7 @@ const rollup_node_getter = (id) => {
 	return nodes[id] ?? null;
 };
 
-const illegal_imports = new Set(['/illegal/boom.js']);
+const illegal_imports = new Set([path.normalize('/illegal/boom.js')]);
 const ok_rollup_node = rollup_node_getter('/test/path1.js');
 const bad_rollup_node_static = rollup_node_getter('/bad/static.js');
 const bad_rollup_node_dynamic = rollup_node_getter('/bad/dynamic.js');
