dc8661fa15701471c699cb3e82244980a20502fb
Rich Harris <hello@rich-harris.dev>
2022-07-30 16:08:32 -0400

    try this

--- packages/kit/svelte-kit.js
+++ packages/kit/svelte-kit.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-import './dist/cli.js';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+// in our own CI, and when deploying directly from this monorepo,
+// the `dist` directory will not exist yet
+if (fs.existsSync(fileURLToPath(new URL('./dist', import.meta.url)))) {
+	import('./dist/cli.js');
+} else {
+	console.error('Run "pnpm build" and try running this command again');
+}
