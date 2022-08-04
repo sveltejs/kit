246157eb160ba24f4bad736fd4fdcfc92c2e70be
Rich Harris <hello@rich-harris.dev>
2022-08-03 23:32:17 -0400

    simplify

--- packages/migrate/migrations/routes/migrate_page_server/index.js
+++ packages/migrate/migrations/routes/migrate_page_server/index.js
@@ -19,9 +19,13 @@ export function migrate_page_server(content) {
 	const file = parse(content);
 	if (!file) return give_up + content;
 
-	const unmigrated = new Set(
-		['GET', 'POST', 'PUT', 'PATCH', 'DELETE'].filter((name) => file.exports.map.has(name))
+	const methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'].filter((name) =>
+		file.exports.map.has(name)
 	);
+
+	const non_get_methods = methods.filter((name) => name !== 'GET');
+
+	const unmigrated = new Set(methods);
 
 	for (const statement of file.ast.statements) {
 		if (file.exports.map.has('GET')) {
@@ -44,16 +48,14 @@ export function migrate_page_server(content) {
 			}
 		}
 
-		for (const method of ['POST', 'PUT', 'PATCH', 'DELETE']) {
-			if (file.exports.map.has(method)) {
-				const fn = get_function_node(statement, file.exports.map.get(method));
-				if (fn) {
-					rewrite_returns(fn.body, (expr, node) => {
-						manual_return_migration(node || fn, file.code, TASKS.PAGE_ENDPOINT);
-					});
+		for (const method of non_get_methods) {
+			const fn = get_function_node(statement, file.exports.map.get(method));
+			if (fn) {
+				rewrite_returns(fn.body, (expr, node) => {
+					manual_return_migration(node || fn, file.code, TASKS.PAGE_ENDPOINT);
+				});
 
-					unmigrated.delete(method);
-				}
+				unmigrated.delete(method);
 			}
 		}
 	}
--- packages/migrate/migrations/routes/migrate_server/index.js
+++ packages/migrate/migrations/routes/migrate_server/index.js
@@ -21,97 +21,97 @@ export function migrate_server(content) {
 	const file = parse(content);
 	if (!file) return give_up + content;
 
-	const unmigrated = new Set(
-		['GET', 'POST', 'PUT', 'PATCH', 'DELETE'].filter((name) => file.exports.map.has(name))
+	const methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'].filter((name) =>
+		file.exports.map.has(name)
 	);
 
+	const unmigrated = new Set(methods);
+
 	for (const statement of file.ast.statements) {
-		for (const method of ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
-			if (file.exports.map.has(method)) {
-				const fn = get_function_node(statement, file.exports.map.get(method));
-				if (fn) {
-					rewrite_returns(fn.body, (expr, node) => {
-						if (
-							expr &&
-							ts.isObjectLiteralExpression(expr) &&
-							contains_only(expr, ['body', 'status', 'headers'], true)
-						) {
-							const props = expr.properties;
+		for (const method of methods) {
+			const fn = get_function_node(statement, file.exports.map.get(method));
+			if (fn) {
+				rewrite_returns(fn.body, (expr, node) => {
+					if (
+						expr &&
+						ts.isObjectLiteralExpression(expr) &&
+						contains_only(expr, ['body', 'status', 'headers'], true)
+					) {
+						const props = expr.properties;
 
-							const body = get_prop(props, 'body');
-							const headers = get_prop(props, 'headers');
-							const status = get_prop(props, 'status');
+						const body = get_prop(props, 'body');
+						const headers = get_prop(props, 'headers');
+						const status = get_prop(props, 'status');
 
-							const headers_has_multiple_cookies = /['"]set-cookie['"]:\s*\[/.test(
-								headers?.getText()?.toLowerCase()
-							);
-							const is_safe_transformation =
-								(!body ||
-									(!ts.isShorthandPropertyAssignment(body) &&
-										ts.isObjectLiteralExpression(body.initializer))) &&
-								(!headers ||
-									((!headers.getText().toLowerCase().includes('content-type') ||
-										headers.getText().includes('application/json')) &&
-										!headers_has_multiple_cookies));
+						const headers_has_multiple_cookies = /['"]set-cookie['"]:\s*\[/.test(
+							headers?.getText()?.toLowerCase()
+						);
+						const is_safe_transformation =
+							(!body ||
+								(!ts.isShorthandPropertyAssignment(body) &&
+									ts.isObjectLiteralExpression(body.initializer))) &&
+							(!headers ||
+								((!headers.getText().toLowerCase().includes('content-type') ||
+									headers.getText().includes('application/json')) &&
+									!headers_has_multiple_cookies));
 
-							const headers_str =
-								body &&
-								(!ts.isPropertyAssignment(body) || !is_string_like(body.initializer)) &&
-								(!headers || !headers.getText().toLowerCase().includes('content-type'))
-									? `headers: { 'content-type': 'application/json; charset=utf-8'${
-											headers
-												? ', ' +
-												  (ts.isPropertyAssignment(headers)
-														? remove_outer_braces(get_prop_initializer_text(props, 'headers'))
-														: '...headers')
-												: ''
-									  } }`
-									: headers
-									? headers.getText()
-									: undefined;
+						const headers_str =
+							body &&
+							(!ts.isPropertyAssignment(body) || !is_string_like(body.initializer)) &&
+							(!headers || !headers.getText().toLowerCase().includes('content-type'))
+								? `headers: { 'content-type': 'application/json; charset=utf-8'${
+										headers
+											? ', ' +
+											  (ts.isPropertyAssignment(headers)
+													? remove_outer_braces(get_prop_initializer_text(props, 'headers'))
+													: '...headers')
+											: ''
+								  } }`
+								: headers
+								? headers.getText()
+								: undefined;
 
-							const body_str = get_prop_initializer_text(props, 'body');
-							const response_body = body
-								? (!ts.isPropertyAssignment(body) ||
-										!is_string_like(body.initializer) ||
-										(headers && headers.getText().includes('application/json'))) &&
-								  (!headers ||
-										!headers.getText().toLowerCase().includes('content-type') ||
-										headers.getText().includes('application/json')) &&
-								  !body_str.startsWith('JSON.stringify')
-									? `JSON.stringify(${body_str})`
-									: body_str
-								: 'undefined';
+						const body_str = get_prop_initializer_text(props, 'body');
+						const response_body = body
+							? (!ts.isPropertyAssignment(body) ||
+									!is_string_like(body.initializer) ||
+									(headers && headers.getText().includes('application/json'))) &&
+							  (!headers ||
+									!headers.getText().toLowerCase().includes('content-type') ||
+									headers.getText().includes('application/json')) &&
+							  !body_str.startsWith('JSON.stringify')
+								? `JSON.stringify(${body_str})`
+								: body_str
+							: 'undefined';
 
-							const response_init =
-								headers_str || status
-									? // prettier-ignore
-									  ', ' +
+						const response_init =
+							headers_str || status
+								? // prettier-ignore
+								  ', ' +
 									(headers_has_multiple_cookies ? '\n// set-cookie with multiple values needs a different conversion, see the link at the top for more info\n' : '') +
 									`{ ${headers_str ? `${headers_str}${status ? ', ' : ''}` : ''}${status ? status.getText() : ''} }`
-									: '';
+								: '';
 
-							if (is_safe_transformation) {
-								automigration(
-									node,
-									file.code,
-									`return new Response(${response_body}${response_init});`
-								);
-							} else {
-								manual_return_migration(
-									node,
-									file.code,
-									TASKS.STANDALONE_ENDPOINT,
-									`return new Response(${response_body}${response_init});`
-								);
-							}
+						if (is_safe_transformation) {
+							automigration(
+								node,
+								file.code,
+								`return new Response(${response_body}${response_init});`
+							);
 						} else {
-							manual_return_migration(node, file.code, TASKS.STANDALONE_ENDPOINT);
+							manual_return_migration(
+								node,
+								file.code,
+								TASKS.STANDALONE_ENDPOINT,
+								`return new Response(${response_body}${response_init});`
+							);
 						}
-					});
+					} else {
+						manual_return_migration(node, file.code, TASKS.STANDALONE_ENDPOINT);
+					}
+				});
 
-					unmigrated.delete(method);
-				}
+				unmigrated.delete(method);
 			}
 		}
 	}
