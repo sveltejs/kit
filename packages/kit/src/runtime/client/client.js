d62d0419208580a6c14386c051957e38bb4635df
Rich Harris <hello@rich-harris.dev>
2022-08-08 20:12:38 -0400

    remove unused import

--- packages/kit/src/runtime/client/client.js
+++ packages/kit/src/runtime/client/client.js
@@ -1,6 +1,6 @@
 import { onMount, tick } from 'svelte';
 import { writable } from 'svelte/store';
-import { coalesce_to_error, normalize_error } from '../../utils/error.js';
+import { normalize_error } from '../../utils/error.js';
 import { LoadURL, decode_params, normalize_path } from '../../utils/url.js';
 import {
 	create_updated_store,
