e9342c70ecbf14444b071c0c1535f0748b619def
Rich Harris <hello@rich-harris.dev>
2022-08-15 18:15:08 -0400

    what fun

--- packages/migrate/migrations/routes/migrate_page_js/index.spec.js
+++ packages/migrate/migrations/routes/migrate_page_js/index.spec.js
@@ -5,7 +5,7 @@ import { migrate_page } from './index.js';
 
 for (const sample of read_samples(import.meta.url)) {
 	test(sample.description, () => {
-		const actual = migrate_page(sample.before);
+		const actual = migrate_page(sample.before, sample.filename ?? '+page.js');
 		assert.equal(actual, sample.after);
 	});
 }
