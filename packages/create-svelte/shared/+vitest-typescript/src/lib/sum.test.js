8c451fe4cd914cdca5332912ac00501432d0883b
Rich Harris <hello@rich-harris.dev>
2022-08-16 21:38:01 -0400

    create typescript/checkjs/no-typechecking variants of .ts files in shared

--- packages/create-svelte/shared/+vitest-typescript/src/lib/sum.test.js
+++
@@ -1,7 +0,0 @@
-import { sum } from './sum';
-
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(sum(1, 2)).toBe(3);
-	});
-});
--- packages/create-svelte/shared/+vitest/src/lib/sum.ts
+++ packages/create-svelte/shared/+vitest/src/lib/sum.ts
@@ -1,3 +1,8 @@
+/**
+ * Add two numbers
+ * @param {number} a
+ * @param {number} b
+ */
 export function sum(a: number, b: number): number {
 	return a + b;
 }
