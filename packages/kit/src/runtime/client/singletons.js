e6bf16e35fa01593a9c2ea5bcdfc7a5c7c0ab75d
Conduitry <git@chor.date>
2022-08-19 15:40:27 -0400

    keep browser stores in always-accessible singleton

--- packages/kit/src/runtime/client/singletons.js
+++ packages/kit/src/runtime/client/singletons.js
@@ -1,3 +1,6 @@
+import { writable } from 'svelte/store';
+import { create_updated_store, notifiable_store } from './utils.js';
+
 /** @type {import('./types').Client} */
 export let client;
 
@@ -9,3 +12,10 @@ export let client;
 export function init(opts) {
 	client = opts.client;
 }
+
+export const stores = {
+	url: notifiable_store({}),
+	page: notifiable_store({}),
+	navigating: writable(/** @type {import('types').Navigation | null} */ (null)),
+	updated: create_updated_store()
+};
