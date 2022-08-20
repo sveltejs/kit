dab5df115921f6c1a4038c53c1714d672b2ad5c9
Rich Harris <hello@rich-harris.dev>
2022-08-20 16:11:58 -0400

    use single-quote to avoid $ characters wreaking havoc - fixes #6113

--- packages/migrate/migrations/routes/utils.js
+++ packages/migrate/migrations/routes/utils.js
@@ -46,7 +46,7 @@ export function error(description, comment_id) {
  */
 export function move_file(file, renamed, content, use_git) {
 	if (use_git) {
-		execSync(`git mv ${JSON.stringify(file)} ${JSON.stringify(renamed)}`);
+		execSync(`git mv '${file.replace(/'/g, "\\'")}' '${renamed.replace(/'/g, "\\'")}'`);
 	} else {
 		fs.unlinkSync(file);
 	}
