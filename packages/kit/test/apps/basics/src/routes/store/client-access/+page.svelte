e0d1935f7a76c58e0f94a9443333b6e20aae4a20
Conduitry <git@chor.date>
2022-08-20 09:06:00 -0400

    add test

---
+++ packages/kit/test/apps/basics/src/routes/store/client-access/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { getStores } from '$app/stores';
+
+	let pathname;
+</script>
+
+<h1>{pathname}</h1>
+
+<button
+	on:click={() => {
+		getStores().page.subscribe(($page) => pathname = $page.url.pathname)();
+	}}>click</button
+>
