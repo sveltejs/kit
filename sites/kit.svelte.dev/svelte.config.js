e0e11fb22ccafc78dc84454ba0d9ea0565e65a35
Tal Hadad <tal_hd@hotmail.com>
2022-08-24 13:35:51 +0300

    remove the 'legacy' option from svelte.config.js

--- sites/kit.svelte.dev/svelte.config.js
+++ sites/kit.svelte.dev/svelte.config.js
@@ -8,16 +8,6 @@ const config = {
 		prerender: {
 			default: true,
 			entries: ['*', '/content.json']
-		},
-		
-		legacy: {
-			targets: ['ie >= 11'],
-			additionalLegacyPolyfills: [
-				'custom-event-polyfill',
-				'core-js/modules/es.promise.js',
-				'whatwg-fetch',
-				'regenerator-runtime/runtime'
-			]
 		}
 	}
 };
