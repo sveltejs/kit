8bba42db03a859ef73e0328982ccd27a2e4e272b
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-08-24 14:01:04 +0200

    Update packages/kit/src/utils/array.js
    Co-authored-by: Ignatius Bagus <ignatius.mbs@gmail.com>

--- packages/kit/src/utils/array.js
+++ packages/kit/src/utils/array.js
@@ -1,10 +1,8 @@
 /**
  * Removes nullish values from an array.
  *
- * @function
  * @template T
  * @param {Array<T>} arr
- * @returns {Array<NonNullable<T>>}
  */
 export function compact(arr) {
 	return arr.filter(/** @returns {val is NonNullable<T>} */ (val) => val != null);
