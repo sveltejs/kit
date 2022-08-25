128340cdc4a9307dfa59bbc378cf9c644310bdaa
Rich Harris <richard.a.harris@gmail.com>
2022-08-25 14:23:54 -0400

    set errors on all layout/leaf components - fixes #6264 (#6279)

--- packages/kit/src/core/sync/write_root.js
+++ packages/kit/src/core/sync/write_root.js
@@ -26,7 +26,7 @@ export function write_root(manifest_data, output) {
 	while (l--) {
 		pyramid = `
 			{#if components[${l + 1}]}
-				<svelte:component this={components[${l}]} data={data_${l}}>
+				<svelte:component this={components[${l}]} data={data_${l}} {errors}>
 					${pyramid.replace(/\n/g, '\n\t\t\t\t\t')}
 				</svelte:component>
 			{:else}
