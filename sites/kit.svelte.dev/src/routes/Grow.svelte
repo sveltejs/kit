abdf8809e8775f747a7f7ded53a31216500ef183
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-08-28 21:56:48 -0700

    map cleanup

--- sites/kit.svelte.dev/src/routes/Grow.svelte
+++ sites/kit.svelte.dev/src/routes/Grow.svelte
@@ -7,7 +7,7 @@
 	import Grow from './grow.svg';
 
 	onMount(() => {
-		const width = 1234, height = 500;
+		const width = 1104, height = 575;
 
 		// https://vercel.com/docs/concepts/edge-network/regions
 		const datacenters_csv = `lon,lat\n18.0686,59.3293\n72.8777,19.076\n2.3522,48.8566\n-81.6944,41.4993\n18.4241,-33.9249\n-6.2603,53.3498\n8.6821,50.1109\n-46.6396,-23.5558\n114.1694,22.3193\n139.6503,35.6762\n-77.0369,38.9072\n126.978,37.5665\n135.5023,34.6937\n-0.1276,51.5072\n-122.6784,45.5152\n-122.4367,37.7595\n103.8198,1.3521\n151.2093,-33.8688`;
@@ -17,8 +17,8 @@
 
 		const projection = d3.geoMercator()
 			.center([0, selected.lat])
-			.scale(150)
-			.rotate([-selected.lon, 0]);
+			.scale(200)
+			.rotate([-selected.lon + 15 /** why is this adjustment needed? */, 0]);
 
 		const svg = d3.select('#svg').append('svg')
 			.attr('width', width)
@@ -86,9 +86,9 @@
 			Only looks correct at arround 1500px viewport-width.
 			This should be done by someone who has a clue about what he's doing.
 
-			These 7em at bottom are to push "Starts fast" down.
+			These em at bottom are to push "Starts fast" down.
 		*/
-		margin: 0 0 7em 0;
+		margin: 0 0 12em 0;
 		width: 100%;
 		height: auto;
 		z-index: 1;
@@ -111,7 +111,7 @@
 	.box {
 		position: absolute;
 		max-width: 236px;
-		right: -2em;
+		right: var(--side-nav);
 		bottom: 0;
 		padding: 1.2em 1.6em 0.5em;
 		background-color: var(--prime);
