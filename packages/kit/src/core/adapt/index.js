16b29bc91fd0f4190e570b5caa0a51bb55c9ff25
Rich Harris <hello@rich-harris.dev>
2022-08-29 11:31:50 -0400

    move some logic around

--- packages/kit/src/core/adapt/index.js
+++ packages/kit/src/core/adapt/index.js
@@ -1,4 +1,5 @@
 import colors from 'kleur';
+import { get_path } from '../../utils/routing.js';
 import { create_builder } from './builder.js';
 
 /**
@@ -12,7 +13,24 @@ export async function adapt(config, build_data, prerendered, { log }) {
 
 	console.log(colors.bold().cyan(`\n> Using ${name}`));
 
-	const builder = create_builder({ config, build_data, prerendered, log });
+	/** @type {Set<string>} */
+	const prerendered_paths = new Set(prerendered.paths);
+
+	const builder = create_builder({
+		config,
+		build_data,
+		routes: build_data.manifest_data.routes.filter((route) => {
+			// TODO do this analysis during prerendering — figure out which routes were `prerender = false` or `prerender = 'auto'`
+			const path = route.page && get_path(route.id);
+			if (path) {
+				return !prerendered_paths.has(path) && !prerendered_paths.has(path + '/');
+			}
+
+			return true;
+		}),
+		prerendered,
+		log
+	});
 	await adapt(builder);
 
 	log.success('done');
