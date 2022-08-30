5d0b7987b50a9fc50c310b4df759054dab1bc9b3
Rich Harris <hello@rich-harris.dev>
2022-08-29 21:35:39 -0400

    lint

--- packages/create-svelte/templates/default/src/lib/header/Header.svelte
+++ packages/create-svelte/templates/default/src/lib/header/Header.svelte
@@ -15,7 +15,9 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li class:active={$page.url.pathname === '/'}><a data-sveltekit-prefetch href="/">Home</a></li>
+			<li class:active={$page.url.pathname === '/'}>
+				<a data-sveltekit-prefetch href="/">Home</a>
+			</li>
 			<li class:active={$page.url.pathname === '/about'}>
 				<a data-sveltekit-prefetch href="/about">About</a>
 			</li>
