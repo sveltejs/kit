48a5811970b3c693520a0851673b0819d148ca2a
Rich Harris <hello@rich-harris.dev>
2022-08-29 20:14:56 -0400

    fix test

--- packages/kit/src/core/config/index.spec.js
+++ packages/kit/src/core/config/index.spec.js
@@ -382,6 +382,7 @@ test('load default config (esm)', async () => {
 
 	const defaults = get_defaults(cwd + '/');
 	defaults.kit.version.name = config.kit.version.name;
+	defaults.kit.files.errorPage = fileURLToPath(new URL('./default-error.html', import.meta.url));
 
 	assert.equal(config, defaults);
 });
