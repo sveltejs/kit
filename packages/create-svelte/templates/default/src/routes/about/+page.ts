f1f8d3355add5fe1431d6f1cab9ab92992d35ea3
Rich Harris <richard.a.harris@gmail.com>
2022-08-30 18:28:30 -0400

    replace `router`/`hydrate` options with `csr` (#6446)
    * replace router/hydrate options with csr - closes #6436
    
    * update template
    
    * update docs
    
    * fail noisily

--- packages/create-svelte/templates/default/src/routes/about/+page.ts
+++ packages/create-svelte/templates/default/src/routes/about/+page.ts
@@ -1,12 +1,8 @@
-import { browser, dev } from '$app/environment';
+import { dev } from '$app/environment';
 
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement...
-export const hydrate = dev;
-
-// ...but if the client-side router is already loaded
-// (i.e. we came here from elsewhere in the app), use it
-export const router = browser;
+export const csr = dev;
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in prod
