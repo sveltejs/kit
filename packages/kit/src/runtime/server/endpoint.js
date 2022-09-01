62fa93cbec4d6ba7c5f1bf797bb1ab71beef21ca
Simon Holthausen <simon.holthausen@vercel.com>
2022-09-01 17:50:20 +0200

    support validation error thrown in endpoints

--- packages/kit/src/runtime/server/endpoint.js
+++ packages/kit/src/runtime/server/endpoint.js
@@ -1,4 +1,5 @@
-import { Redirect } from '../control.js';
+import { json } from '../../exports/index.js';
+import { Redirect, ValidationError } from '../control.js';
 import { check_method_names, method_not_allowed } from './utils.js';
 
 /**
@@ -56,6 +57,15 @@ export async function render_endpoint(event, mod, state) {
 				status: error.status,
 				headers: { location: error.location }
 			});
+		} else if (error instanceof ValidationError) {
+			return json(
+				{
+					status: error.status,
+					errors: error.errors,
+					values: error.values
+				},
+				{ status: error.status }
+			);
 		}
 
 		throw error;
