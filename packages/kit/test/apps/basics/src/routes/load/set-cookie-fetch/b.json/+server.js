ca248bc1c6bc87dcdc76ecb2fe7e257069a0f9c8
Rich Harris <hello@rich-harris.dev>
2022-09-05 19:11:35 -0400

    tidy up

--- packages/kit/test/apps/basics/src/routes/load/set-cookie-fetch/b.json/+server.js
+++ packages/kit/test/apps/basics/src/routes/load/set-cookie-fetch/b.json/+server.js
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
-export function GET({ cookies, request }) {
+export function GET({ cookies }) {
 	const answer = +cookies.get('answer');
 
 	return json(
