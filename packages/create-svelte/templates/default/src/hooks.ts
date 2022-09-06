1c6dc6bfb1da302d4b64cf57df1864f567aa3c08
Rich Harris <hello@rich-harris.dev>
2022-09-06 09:17:02 -0400

    make it more typescript friendly

--- packages/create-svelte/templates/default/src/hooks.ts
+++ packages/create-svelte/templates/default/src/hooks.ts
@@ -2,15 +2,16 @@ import type { Handle } from '@sveltejs/kit';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle: Handle = async ({ event, resolve }) => {
-	if (!event.cookies.get('userid')) {
+	let userid = event.cookies.get('userid');
+
+	if (!userid) {
 		// if this is the first time the user has visited this app,
 		// set a cookie so that we recognise them when they return
-		event.cookies.set('userid', crypto.randomUUID(), {
-			path: '/'
-		});
+		userid = crypto.randomUUID();
+		event.cookies.set('userid', userid, { path: '/' });
 	}
 
-	event.locals.userid = event.cookies.get('userid');
+	event.locals.userid = userid;
 
 	return resolve(event);
 };
