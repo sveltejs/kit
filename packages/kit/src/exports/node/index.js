a9bcd3584fd0271875d40f4ce65eb970be51aa76
repsac <repsac-by@users.noreply.github.com>
2022-09-13 17:23:57 +0300

    Allow streaming with body_size_limit (#6702)
    * Allow streaming with body_size_limit
    
    * More detailed error message
    
    * fix formatting
    
    * Clarify error message
    
    * Update packages/kit/src/exports/node/index.js
    
    * Create fair-rivers-repair.md
    
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>
    Co-authored-by: Rich Harris <hello@rich-harris.dev>

--- packages/kit/src/exports/node/index.js
+++ packages/kit/src/exports/node/index.js
@@ -11,23 +11,22 @@ function get_raw_body(req, body_size_limit) {
 		return null;
 	}
 
-	const length = Number(h['content-length']);
+	const content_length = Number(h['content-length']);
 
 	// check if no request body
 	if (
-		(req.httpVersionMajor === 1 && isNaN(length) && h['transfer-encoding'] == null) ||
-		length === 0
+		(req.httpVersionMajor === 1 && isNaN(content_length) && h['transfer-encoding'] == null) ||
+		content_length === 0
 	) {
 		return null;
 	}
 
+	let length = content_length;
+
 	if (body_size_limit) {
 		if (!length) {
-			throw new Error(
-				`Received content-length of ${length}. content-length must be provided when body size limit is specified.`
-			);
-		}
-		if (length > body_size_limit) {
+			length = body_size_limit;
+		} else if (length > body_size_limit) {
 			throw new Error(
 				`Received content-length of ${length}, but only accept up to ${body_size_limit} bytes.`
 			);
@@ -59,7 +58,13 @@ function get_raw_body(req, body_size_limit) {
 
 				size += chunk.length;
 				if (size > length) {
-					controller.error(new Error('content-length exceeded'));
+					req.destroy(
+						new Error(
+							`request body size exceeded ${
+								content_length ? "'content-length'" : 'BODY_SIZE_LIMIT'
+							} of ${length}`
+						)
+					);
 					return;
 				}
 
