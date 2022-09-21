7b4113138a82f27ab68fabe2fc10b52ee023b22b
Rich Harris <hello@rich-harris.dev>
2022-09-21 17:56:01 -0400

    use INIT_CWD

--- packages/kit/postinstall.js
+++ packages/kit/postinstall.js
@@ -8,15 +8,17 @@ const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 const directories = [];
 
+const cwd = process.env.INIT_CWD ?? process.cwd();
+
 if (pkg.workspaces) {
 	for (const directory of pkg.workspaces) {
-		directories.push(...glob(directory));
+		directories.push(...glob(directory, { cwd }).map((dir) => path.resolve(cwd, dir)));
 	}
 } else {
-	directories.push('.');
+	directories.push(cwd);
 }
 
-for (const cwd of directories.map((dir) => path.resolve(dir))) {
+for (const cwd of directories) {
 	if (fs.existsSync(path.join(cwd, 'svelte.config.js'))) {
 		process.chdir(cwd);
 		const config = await load_config();
