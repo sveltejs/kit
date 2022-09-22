1b394eda684d93fcc8c716fb8d20dc13c59c04f2
Rich Harris <hello@rich-harris.dev>
2022-09-22 11:26:31 -0400

    chdir before attempting postinstall

--- packages/kit/postinstall.js
+++ packages/kit/postinstall.js
@@ -4,11 +4,12 @@ import glob from 'tiny-glob/sync.js';
 import { load_config } from './src/core/config/index.js';
 import * as sync from './src/core/sync/sync.js';
 
+const cwd = process.env.INIT_CWD ?? process.cwd();
+process.chdir(cwd);
+
 const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 const directories = [];
-
-const cwd = process.env.INIT_CWD ?? process.cwd();
 
 if (pkg.workspaces) {
 	for (const directory of pkg.workspaces) {
