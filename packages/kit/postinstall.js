5aa97a01820111447a47d3d10a3d0307b19e291c
Rich Harris <hello@rich-harris.dev>
2022-09-23 14:22:47 -0400

    belt and braces

--- packages/kit/postinstall.js
+++ packages/kit/postinstall.js
@@ -4,37 +4,41 @@ import glob from 'tiny-glob/sync.js';
 import { load_config } from './src/core/config/index.js';
 import * as sync from './src/core/sync/sync.js';
 
-const cwd = process.env.INIT_CWD ?? process.cwd();
-process.chdir(cwd);
+try {
+	const cwd = process.env.INIT_CWD ?? process.cwd();
+	process.chdir(cwd);
 
-if (fs.existsSync('package.json')) {
-	const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-
-	const directories = [];
-
-	if (pkg.workspaces) {
-		for (const directory of pkg.workspaces) {
-			directories.push(...glob(directory, { cwd }).map((dir) => path.resolve(cwd, dir)));
-		}
-	} else {
-		directories.push(cwd);
-	}
-
-	for (const cwd of directories) {
-		process.chdir(cwd);
-
-		if (!fs.existsSync('package.json')) continue;
-		if (!fs.existsSync('svelte.config.js')) continue;
-
+	if (fs.existsSync('package.json')) {
 		const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-		if (!pkg.dependencies?.['@sveltejs/kit'] && !pkg.devDependencies?.['@sveltejs/kit']) continue;
 
-		try {
-			const config = await load_config();
-			await sync.all(config, 'development');
-		} catch (e) {
-			console.log('Error while trying to sync SvelteKit config');
-			console.log(e);
+		const directories = [];
+
+		if (pkg.workspaces) {
+			for (const directory of pkg.workspaces) {
+				directories.push(...glob(directory, { cwd }).map((dir) => path.resolve(cwd, dir)));
+			}
+		} else {
+			directories.push(cwd);
+		}
+
+		for (const cwd of directories) {
+			process.chdir(cwd);
+
+			if (!fs.existsSync('package.json')) continue;
+			if (!fs.existsSync('svelte.config.js')) continue;
+
+			const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+			if (!pkg.dependencies?.['@sveltejs/kit'] && !pkg.devDependencies?.['@sveltejs/kit']) continue;
+
+			try {
+				const config = await load_config();
+				await sync.all(config, 'development');
+			} catch (error) {
+				console.log('Error while trying to sync SvelteKit config');
+				console.log(error.stack);
+			}
 		}
 	}
+} catch (error) {
+	console.error(error.stack);
 }
