87a5e5e9812809cd3790a061f80423cc94063b4f
repsac <repsac-by@users.noreply.github.com>
2022-09-30 23:32:06 +0300

    Allow fetching ArrayBufferView request body types (#7065)
    * Allow all supported request body types
    
    * Calculating a hash from the body of a cloned request
    
    * Add Blob to globals
    
    * Hash FormData
    
    * Remove unnecessary await
    
    * Only a string and ArrayBufferView as request body
    
    * Create itchy-jobs-report.md
    
    * Create tricky-experts-tan.md
    
    Co-authored-by: Rich Harris <hello@rich-harris.dev>

--- packages/kit/types/internal.d.ts
+++ packages/kit/types/internal.d.ts
@@ -353,7 +353,7 @@ export interface SSRState {
 	prerender_default?: PrerenderOption;
 }
 
-export type StrictBody = string | Uint8Array;
+export type StrictBody = string | ArrayBufferView;
 
 export interface Uses {
 	dependencies: Set<string>;
