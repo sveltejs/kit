691e87476e9220133be6a0cfc97184468060fb6d
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-09-30 08:58:53 -0700

    Update packages/create-svelte/shared/+playwright+typescript/playwright.config.ts

--- packages/create-svelte/shared/+playwright+typescript/playwright.config.ts
+++ packages/create-svelte/shared/+playwright+typescript/playwright.config.ts
@@ -5,7 +5,7 @@ const config: PlaywrightTestConfig = {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testMatch: 'tests/**/*.{js,ts}'
+	testMatch: 'tests/**/.*(test|spec)\.(js|ts|mjs)'
 };
 
 export default config;
