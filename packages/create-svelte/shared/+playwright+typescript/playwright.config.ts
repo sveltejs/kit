78149b54dd76238c956a2d25d7cc1442457df455
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-09-30 09:08:18 -0700

    cleanup test patterns

--- packages/create-svelte/shared/+playwright+typescript/playwright.config.ts
+++ packages/create-svelte/shared/+playwright+typescript/playwright.config.ts
@@ -5,7 +5,7 @@ const config: PlaywrightTestConfig = {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testMatch: 'tests/**/.*(test|spec)\.(js|ts|mjs)'
+	testMatch: 'tests/**/.*(test|spec)\.(js|ts)'
 };
 
 export default config;
--- packages/create-svelte/shared/+vitest/vite.config.js
+++ packages/create-svelte/shared/+vitest/vite.config.js
@@ -4,7 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test}.{js,ts}'],
+		include: ['src/**/*.{test,spec}.{js,ts}'],
 		globals: true
 	}
 };
