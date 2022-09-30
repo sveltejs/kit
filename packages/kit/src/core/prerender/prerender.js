f1579de65f4ddb83fa2cdecacd1de222aab0d075
Simon Holthausen <simon.holthausen@vercel.com>
2022-09-30 17:33:56 +0200

    [fix] decode routeIds in headers for prerendering
    Fixes #7074

--- packages/kit/src/core/prerender/prerender.js
+++ packages/kit/src/core/prerender/prerender.js
@@ -244,10 +244,13 @@ export async function prerender() {
 
 			const prerender = headers['x-sveltekit-prerender'];
 			if (prerender) {
-				const route_id = headers['x-sveltekit-routeid'];
-				const existing_value = prerender_map.get(route_id);
-				if (existing_value !== 'auto') {
-					prerender_map.set(route_id, prerender === 'true' ? true : 'auto');
+				const encoded_route_id = headers['x-sveltekit-routeid'];
+				if (encoded_route_id != null) {
+					const route_id = decodeURI(encoded_route_id);
+					const existing_value = prerender_map.get(route_id);
+					if (existing_value !== 'auto') {
+						prerender_map.set(route_id, prerender === 'true' ? true : 'auto');
+					}
 				}
 			}
 
@@ -305,7 +308,8 @@ export async function prerender() {
 
 		if (written.has(file)) return;
 
-		const route_id = response.headers.get('x-sveltekit-routeid');
+		const encoded_route_id = response.headers.get('x-sveltekit-routeid');
+		const route_id = encoded_route_id != null ? decodeURI(encoded_route_id) : null;
 		if (route_id !== null) prerendered_routes.add(route_id);
 
 		if (response_type === REDIRECT) {
