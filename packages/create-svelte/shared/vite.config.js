8d3d11d26d8efd06df2c39d96814b33a46f11e39
Bert B <robert.bengtson@snc.edu>
2022-10-03 09:16:30 -0400

    Fixed types on default vite config

--- packages/create-svelte/shared/vite.config.js
+++ packages/create-svelte/shared/vite.config.js
@@ -1,8 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
 
 /** @type {import('vite').UserConfig} */
-const config: UserConfig = {
+const config = {
 	plugins: [sveltekit()]
 };
 
