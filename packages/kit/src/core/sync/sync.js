e276724481ed65589f19829b3e394cab8e1383f6
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-10-10 11:29:58 +0200

    add warning so people aren't confused where it's coming from

--- packages/kit/src/core/sync/sync.js
+++ packages/kit/src/core/sync/sync.js
@@ -20,6 +20,7 @@ export function init(config, mode) {
 		if (content.includes('PageError')) {
 			if (content.includes('// interface PageError')) {
 				fs.writeFileSync('src/app.d.ts', content.replace(/\/\/ interface PageError/g, '// interface Error'));
+				console.warn('App.PageError has been renamed to App.Error — we updated your src/app.d.ts');
 			} else {
 				throw new Error(
 					'App.PageError has been renamed to App.Error — please update your src/app.d.ts'
