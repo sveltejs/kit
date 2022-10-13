c7857324ded0c630b27faa9c49f70b925de6334d
Rich Harris <hello@rich-harris.dev>
2022-10-13 11:01:26 -0400

    prettier

--- packages/kit/test/apps/basics/src/routes/cookies/forwarded-in-etag/+page.svelte
+++ packages/kit/test/apps/basics/src/routes/cookies/forwarded-in-etag/+page.svelte
@@ -8,12 +8,13 @@
 	});
 </script>
 
-<button on:click={() => {
+<button
+	on:click={() => {
 		document.cookie = 'foo=bar;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        location.reload();
+		location.reload();
 	}}
 >
-    Delete cookies and reload the page
+	Delete cookies and reload the page
 </button>
 
 <p>{cookies}</p>
