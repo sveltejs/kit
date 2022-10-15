63b2794ac9a1d913191e00ee3136b2155c07ec8b
Mitchell Miller <95239376+untold-titan@users.noreply.github.com>
2022-10-15 08:45:07 -0600

    Update svelte.config.js

--- packages/create-svelte/templates/skeleton/svelte.config.js
+++ packages/create-svelte/templates/skeleton/svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapters from '@sveltejs/adapter-auto';
 
 // This config is ignored and replaced with one of the configs in the shared folder when a project is created.
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapters()
 	}
 };
 
