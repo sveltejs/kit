83e42709be4785dca2a06b54151e89b2d7735b7e
Rich Harris <hello@rich-harris.dev>
2022-10-16 13:02:58 -0400

    tidy up

--- packages/kit/src/core/sync/create_manifest_data/sort.js
+++ packages/kit/src/core/sync/create_manifest_data/sort.js
@@ -2,11 +2,9 @@ import { affects_path } from '../../../utils/routing.js';
 
 /**
  * @typedef {{
+ *   type: 'static' | 'required' | 'optional' | 'rest';
  *   content: string;
- *   dynamic: boolean;
- *   optional: boolean;
- *   rest: boolean;
- *   matcher: string | null;
+ *   matched: boolean;
  * }} Part
  */
 
@@ -16,74 +14,101 @@ import { affects_path } from '../../../utils/routing.js';
 
 /** @param {Map<string, import('types').RouteData>} route_map */
 export function sort_routes(route_map) {
-	/** @type {Map<string, string[]>} */
-	const segment_map = new Map();
+	/** @type {Map<string, Part[]>} */
+	const segment_cache = new Map();
 
 	/** @param {string} segment */
-	function split_segment(segment) {
-		if (!segment_map.has(segment)) {
-			segment_map.set(segment, split(segment));
+	function get_parts(segment) {
+		if (!segment_cache.has(segment)) {
+			segment_cache.set(segment, split(segment));
 		}
 
-		return segment_map.get(segment);
+		return segment_cache.get(segment);
+	}
+
+	/** @param {string} id */
+	function split(id) {
+		/** @type {Part[]} */
+		const parts = [];
+
+		let i = 0;
+		while (i <= id.length) {
+			const start = id.indexOf('[', i);
+			if (start === -1) {
+				parts.push({ type: 'static', content: id.slice(i), matched: false });
+				break;
+			}
+
+			parts.push({ type: 'static', content: id.slice(i, start), matched: false });
+
+			const type = id[start + 1] === '[' ? 'optional' : id[start + 1] === '.' ? 'rest' : 'required';
+			const delimiter = type === 'optional' ? ']]' : ']';
+			const end = id.indexOf(delimiter, start);
+
+			if (end === -1) {
+				throw new Error(`Invalid route ID ${id}`);
+			}
+
+			const content = id.slice(start, (i = end + delimiter.length));
+
+			parts.push({
+				type,
+				content,
+				matched: content.includes('=')
+			});
+		}
+
+		return parts;
 	}
 
 	return Array.from(route_map.values()).sort((route_a, route_b) => {
-		try {
-			const segments_a = split_route_id(route_a.id).map(split_segment);
-			const segments_b = split_route_id(route_b.id).map(split_segment);
+		const segments_a = split_route_id(route_a.id).map(get_parts);
+		const segments_b = split_route_id(route_b.id).map(get_parts);
 
-			for (let i = 0; i < Math.max(segments_a.length, segments_b.length); i += 1) {
-				const segment_a = segments_a[i];
-				const segment_b = segments_b[i];
+		for (let i = 0; i < Math.max(segments_a.length, segments_b.length); i += 1) {
+			const segment_a = segments_a[i];
+			const segment_b = segments_b[i];
 
-				// shallower path outranks deeper path
-				if (!segment_a) return -1;
-				if (!segment_b) return +1;
+			// shallower path outranks deeper path
+			if (!segment_a) return -1;
+			if (!segment_b) return +1;
 
-				// compare two segments
-				for (let j = 0; j < Math.max(segment_a.length, segment_b.length); j += 1) {
-					const a = segment_a[j];
-					const b = segment_b[j];
+			// compare two segments
+			for (let j = 0; j < Math.max(segment_a.length, segment_b.length); j += 1) {
+				const a = segment_a[j];
+				const b = segment_b[j];
 
-					// first part of each segment is always static
-					// (though it may be the empty string), then
-					// it alternates between dynamic and static
-					// (i.e. [foo][bar] is disallowed)
-					const dynamic = j % 2 === 1;
+				// first part of each segment is always static
+				// (though it may be the empty string), then
+				// it alternates between dynamic and static
+				// (i.e. [foo][bar] is disallowed)
+				const dynamic = j % 2 === 1;
 
-					if (dynamic) {
-						if (a === undefined) return -1;
-						if (b === undefined) return +1;
+				if (dynamic) {
+					if (a === undefined) return -1;
+					if (b === undefined) return +1;
 
-						// TODO parse in the first place
-						const parsed_a = parse(a);
-						const parsed_b = parse(b);
-
-						// part with matcher outranks one without
-						if (parsed_a.matched !== parsed_b.matched) {
-							return parsed_a.matched ? -1 : +1;
-						}
-
-						if (parsed_a.type !== parsed_b.type) {
-							// [required] outranks [[optional]] or [...rest]
-							if (parsed_a.type === 'required') return -1;
-							if (parsed_b.type === 'required') return +1;
-
-							// [[optional]] outranks [...rest]
-							if (parsed_a.type === 'optional') return -1;
-							if (parsed_b.type === 'optional') return +1;
-						}
-					} else if (a !== b) {
-						return sort_static(a, b);
+					// part with matcher outranks one without
+					if (a.matched !== b.matched) {
+						return a.matched ? -1 : +1;
 					}
+
+					if (a.type !== b.type) {
+						// [required] outranks [[optional]] or [...rest]
+						if (a.type === 'required') return -1;
+						if (b.type === 'required') return +1;
+
+						// [[optional]] outranks [...rest]
+						if (a.type === 'optional') return -1;
+						if (b.type === 'optional') return +1;
+					}
+				} else if (a.content !== b.content) {
+					return sort_static(a.content, b.content);
 				}
 			}
-
-			return route_a.id < route_b.id ? -1 : +1; // TODO error on conflicts?
-		} finally {
-			console.groupEnd();
 		}
+
+		return route_a.id < route_b.id ? -1 : +1; // TODO error on conflicts?
 	});
 }
 
@@ -96,32 +121,6 @@ function split_route_id(id) {
 			.split('/')
 			.filter((segment) => segment !== '' && affects_path(segment))
 	);
-}
-
-/** @param {string} id */
-function split(id) {
-	const parts = [];
-
-	let i = 0;
-	while (i <= id.length) {
-		const start = id.indexOf('[', i);
-		if (start === -1) {
-			parts.push(id.slice(i));
-			break;
-		}
-
-		parts.push(id.slice(i, start));
-		const delimiter = id[start + 1] === '[' ? ']]' : ']';
-		const end = id.indexOf(delimiter, start);
-
-		if (end === -1) {
-			throw new Error(`Invalid route ID ${id}`);
-		}
-
-		parts.push(id.slice(start, (i = end + delimiter.length)));
-	}
-
-	return parts;
 }
 
 /**
@@ -142,12 +141,4 @@ function sort_static(a, b) {
 			return char_a < char_b ? -1 : +1;
 		}
 	}
-}
-
-/** @param {string} part */
-function parse(part) {
-	return {
-		type: part.startsWith('[[') ? 'optional' : part.startsWith('[...') ? 'rest' : 'required',
-		matched: part.includes('=')
-	};
 }
