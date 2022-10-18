b86fc529e7d3c6b9f4d362a5bc7a198e3bc815dd
Rich Harris <richard.a.harris@gmail.com>
2022-10-18 11:51:16 -0400

    Update packages/kit/src/runtime/server/page/actions.js
    Co-authored-by: Geoff Rich <4992896+geoffrich@users.noreply.github.com>

--- packages/kit/src/runtime/server/page/actions.js
+++ packages/kit/src/runtime/server/page/actions.js
@@ -249,7 +249,7 @@ function check_serializability(value, id, path) {
 	}
 
 	throw new Error(
-		`${path} returned from action in ${id} cannot be serialized as JSON without loosing its original type` +
+		`${path} returned from action in ${id} cannot be serialized as JSON without losing its original type` +
 			// probably the most common case, so let's give a hint
 			(value instanceof Date ? ' (Date objects are serialized to strings)' : '')
 	);
