e435df40cc8ff04e45d83f37b1dcf47d8368795f
dominikg <dominik.goepel@gmx.de>
2022-10-19 16:39:35 +0200

    docs: improve error message for external prefetching

--- packages/kit/src/runtime/client/client.js
+++ packages/kit/src/runtime/client/client.js
@@ -199,7 +199,9 @@ export function create_client({ target, base, trailing_slash }) {
 		const intent = get_navigation_intent(url, false);
 
 		if (!intent) {
-			throw new Error('Attempted to prefetch a URL that does not belong to this app');
+			throw new Error(
+				`Attempted to prefetch "${url}", a URL that does not belong to this app. To disable prefetching add data-sveltekit-prefetch="off" to a parent. See https://kit.svelte.dev/docs/link-options#data-sveltekit-prefetch for more information.`
+			);
 		}
 
 		load_cache = { id: intent.id, promise: load_route(intent) };
