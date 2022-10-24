3d765b8aaa7b9a76bf62c042da3d797ddc9311c0
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-10-24 13:47:02 +0200

    [fix] get type gen working again (#7370)
    since the routeId has changed, the matching logic needs an update

--- packages/kit/src/core/sync/write_types/index.js
+++ packages/kit/src/core/sync/write_types/index.js
@@ -147,7 +147,7 @@ export async function write_types(config, manifest_data, file) {
 		return;
 	}
 
-	const id = posixify(path.relative(config.kit.files.routes, path.dirname(file)));
+	const id = '/' + posixify(path.relative(config.kit.files.routes, path.dirname(file)));
 
 	const route = manifest_data.routes.find((route) => route.id === id);
 	if (!route) return; // this shouldn't ever happen
