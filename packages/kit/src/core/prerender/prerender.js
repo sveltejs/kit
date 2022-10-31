ec601bcc86e855eb994bfa84aace8a302fbd6d7e
Rich Harris <richard.a.harris@gmail.com>
2022-10-31 17:28:47 -0400

    detect missing ids when prerendering (#7375)
    * detect missing ids (#3955)
    
    * fix name
    
    * dedupe
    
    * fix escaping
    
    * replace prerender.onError with prerender.handleHttpError, add prerender.handleMissingId
    
    * docs
    
    * default to fail
    
    * changeset
    
    * Create three-frogs-happen.md
    
    * handle foo#bar#baz
    
    * Update packages/kit/src/core/config/options.js
    
    Co-authored-by: Simon H <5968653+dummdidumm@users.noreply.github.com>
    
    * Update documentation/docs/50-api-reference/10-configuration.md
    
    Co-authored-by: Simon H <5968653+dummdidumm@users.noreply.github.com>
    
    * include message in error details
    
    * Update documentation/docs/50-api-reference/10-configuration.md
    
    * Update documentation/docs/50-api-reference/10-configuration.md
    
    * handle same-document fragment links
    
    * fix lockfile
    
    * fix logic
    
    * tweak naming
    
    Co-authored-by: Simon H <5968653+dummdidumm@users.noreply.github.com>

--- packages/kit/src/core/prerender/prerender.js
+++ packages/kit/src/core/prerender/prerender.js
@@ -12,45 +12,31 @@ import { load_config } from '../config/index.js';
 import { get_route_segments } from '../../utils/routing.js';
 import { get_option } from '../../runtime/server/utils.js';
 
-/**
- * @typedef {import('types').PrerenderErrorHandler} PrerenderErrorHandler
- * @typedef {import('types').Logger} Logger
- */
-
 const [, , client_out_dir, results_path, verbose, env] = process.argv;
 
 prerender();
 
 /**
- * @param {Parameters<PrerenderErrorHandler>[0]} details
- * @param {import('types').ValidatedKitConfig} config
+ * @template T
+ * @param {import('types').Logger} log
+ * @param {'fail' | 'warn' | 'ignore' | ((details: T) => void)} input
+ * @param {(details: T) => string} format
+ * @returns {(details: T) => void}
  */
-function format_error({ status, path, referrer, referenceType }, config) {
-	const message =
-		status === 404 && !path.startsWith(config.paths.base)
-			? `${path} does not begin with \`base\`, which is configured in \`paths.base\` and can be imported from \`$app/paths\` - see https://kit.svelte.dev/docs/configuration#paths for more info`
-			: path;
-
-	return `${status} ${message}${referrer ? ` (${referenceType} from ${referrer})` : ''}`;
-}
-
-/**
- * @param {Logger} log
- * @param {import('types').ValidatedKitConfig} config
- * @returns {PrerenderErrorHandler}
- */
-function normalise_error_handler(log, config) {
-	switch (config.prerender.onError) {
-		case 'continue':
-			return (details) => {
-				log.error(format_error(details, config));
-			};
+function normalise_error_handler(log, input, format) {
+	switch (input) {
 		case 'fail':
 			return (details) => {
-				throw new Error(format_error(details, config));
+				throw new Error(format(details));
 			};
+		case 'warn':
+			return (details) => {
+				log.error(format(details));
+			};
+		case 'ignore':
+			return () => {};
 		default:
-			return config.prerender.onError;
+			return (details) => input({ ...details, message: format(details) });
 	}
 }
 
@@ -133,7 +119,29 @@ export async function prerender() {
 	const server = new Server(manifest);
 	await server.init({ env: JSON.parse(env) });
 
-	const error = normalise_error_handler(log, config);
+	const handle_http_error = normalise_error_handler(
+		log,
+		config.prerender.handleHttpError,
+		({ status, path, referrer, referenceType }) => {
+			const message =
+				status === 404 && !path.startsWith(config.paths.base)
+					? `${path} does not begin with \`base\`, which is configured in \`paths.base\` and can be imported from \`$app/paths\` - see https://kit.svelte.dev/docs/configuration#paths for more info`
+					: path;
+
+			return `${status} ${message}${referrer ? ` (${referenceType} from ${referrer})` : ''}`;
+		}
+	);
+
+	const handle_missing_id = normalise_error_handler(
+		log,
+		config.prerender.handleMissingId,
+		({ path, id, referrers }) => {
+			return (
+				`The following pages contain links to ${path}#${id}, but no element with id="${id}" exists on ${path}:` +
+				referrers.map((l) => `\n  - ${l}`).join('')
+			);
+		}
+	);
 
 	const q = queue(config.prerender.concurrency);
 
@@ -154,6 +162,12 @@ export async function prerender() {
 	const files = new Set(walk(client_out_dir).map(posixify));
 	const seen = new Set();
 	const written = new Set();
+
+	/** @type {Map<string, Set<string>>} */
+	const expected_hashlinks = new Map();
+
+	/** @type {Map<string, string[]>} */
+	const actual_hashlinks = new Map();
 
 	/**
 	 * @param {string | null} referrer
@@ -177,7 +191,7 @@ export async function prerender() {
 	 */
 	async function visit(decoded, encoded, referrer) {
 		if (!decoded.startsWith(config.paths.base)) {
-			error({ status: 404, path: decoded, referrer, referenceType: 'linked' });
+			handle_http_error({ status: 404, path: decoded, referrer, referenceType: 'linked' });
 			return;
 		}
 
@@ -231,16 +245,28 @@ export async function prerender() {
 		const headers = Object.fromEntries(response.headers);
 
 		if (config.prerender.crawl && headers['content-type'] === 'text/html') {
-			for (const href of crawl(body.toString())) {
-				if (href.startsWith('data:') || href.startsWith('#')) continue;
+			const { ids, hrefs } = crawl(body.toString());
+
+			actual_hashlinks.set(decoded, ids);
+
+			for (const href of hrefs) {
+				if (href.startsWith('data:')) continue;
 
 				const resolved = resolve(encoded, href);
 				if (!is_root_relative(resolved)) continue;
 
-				const { pathname, search } = new URL(resolved, 'http://localhost');
+				const { pathname, search, hash } = new URL(resolved, 'http://localhost');
 
 				if (search) {
 					// TODO warn that query strings have no effect on statically-exported pages
+				}
+
+				if (hash) {
+					if (!expected_hashlinks.has(pathname + hash)) {
+						expected_hashlinks.set(pathname + hash, new Set());
+					}
+
+					/** @type {Set<string>} */ (expected_hashlinks.get(pathname + hash)).add(decoded);
 				}
 
 				enqueue(decoded, decodeURI(pathname), pathname);
@@ -329,7 +355,7 @@ export async function prerender() {
 
 			prerendered.paths.push(decoded);
 		} else if (response_type !== OK) {
-			error({ status: response.status, path: decoded, referrer, referenceType });
+			handle_http_error({ status: response.status, path: decoded, referrer, referenceType });
 		}
 	}
 
@@ -380,6 +406,20 @@ export async function prerender() {
 	}
 
 	await q.done();
+
+	// handle invalid fragment links
+	for (const [key, referrers] of expected_hashlinks) {
+		const index = key.indexOf('#');
+		const path = key.slice(0, index);
+		const id = key.slice(index + 1);
+
+		// ignore fragment links to pages that were not prerendered
+		if (!actual_hashlinks.has(path)) continue;
+
+		if (!actual_hashlinks.get(path).includes(id)) {
+			handle_missing_id({ id, path, referrers: Array.from(referrers) });
+		}
+	}
 
 	/** @type {string[]} */
 	const not_prerendered = [];
