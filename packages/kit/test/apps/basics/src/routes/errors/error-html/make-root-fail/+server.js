be89890f5d591eb2e7b47c7815a2a0a465daee6e
Rich Harris <richard.a.harris@gmail.com>
2022-11-14 15:34:18 -0500

    Update packages/kit/test/apps/basics/src/routes/errors/error-html/make-root-fail/+server.js
    Co-authored-by: Simon H <5968653+dummdidumm@users.noreply.github.com>

--- packages/kit/test/apps/basics/src/routes/errors/error-html/make-root-fail/+server.js
+++ packages/kit/test/apps/basics/src/routes/errors/error-html/make-root-fail/+server.js
@@ -2,6 +2,6 @@
  * @type {import('./$types').RequestHandler} param0
  */
 export function GET({ cookies, url }) {
-	cookies.set('fail-type', url.searchParams.get('type'));
+	cookies.set('fail-type', url.searchParams.get('type'), { path: '/' });
 	return new Response();
 }
