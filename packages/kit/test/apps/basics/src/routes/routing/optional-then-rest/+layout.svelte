b570d3ce943efaeebf7e8cc219e27a4815e880a6
Rich Harris <hello@rich-harris.dev>
2022-11-21 16:42:11 -0500

    add failing test for #7548

---
+++ packages/kit/test/apps/basics/src/routes/routing/optional-then-rest/+layout.svelte
@@ -0,0 +1,4 @@
+<a href="/routing/optional-then-rest/a/1">/routing/optional-then-rest/a/1</a>
+<a href="/routing/optional-then-rest/1/2">/routing/optional-then-rest/1/2</a>
+
+<slot />
---
+++ packages/kit/test/apps/basics/src/routes/routing/optional-then-rest/[[x=numeric]]/[...y]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<p data-testid="x">{$page.params.x}</p>
+<p data-testid="y">{$page.params.y}</p>
