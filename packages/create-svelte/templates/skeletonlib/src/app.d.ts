f501b45b8f690e83c8a9c326110793c4e553cbc1
Rich Harris <hello@rich-harris.dev>
2022-11-28 10:25:09 -0500

    missed a spot

--- packages/create-svelte/templates/skeletonlib/src/app.d.ts
+++ packages/create-svelte/templates/skeletonlib/src/app.d.ts
@@ -4,8 +4,8 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	interface Error {}
-	interface Locals {}
-	interface PageData {}
-	interface Platform {}
+	// interface Error {}
+	// interface Locals {}
+	// interface PageData {}
+	// interface Platform {}
 }
