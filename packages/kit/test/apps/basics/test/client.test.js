191583cd88f7bc119b23d0dc78bfff0b26c29683
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-11-28 10:02:36 -0800

    [chore] upgrade to playwright 1.28.1 (#7696)
    * [chore] upgrade to Playwright 1.28.1
    
    * wait for console event
    
    * better still
    
    * need to use a predicate since there are multiple console logs
    
    * stacktrace is unreliable
    
    * Revert "stacktrace is unreliable"
    
    This reverts commit 6c5e4d044f2fcf7e6ecda69857297b80708a787f.
    
    * solve stacktrace issue in another way
    
    * format
    
    * fix some turbo config
    
    Co-authored-by: Rich Harris <hello@rich-harris.dev>

--- packages/kit/test/apps/basics/test/client.test.js
+++ packages/kit/test/apps/basics/test/client.test.js
@@ -608,23 +608,27 @@ test.describe('Load', () => {
 
 	if (process.env.DEV) {
 		test('using window.fetch causes a warning', async ({ page, baseURL }) => {
+			await Promise.all([
+				page.goto('/load/window-fetch/incorrect'),
+				page.waitForEvent('console', {
+					predicate: (message) => {
+						return (
+							message.text() ===
+							`Loading ${baseURL}/load/window-fetch/data.json using \`window.fetch\`. For best results, use the \`fetch\` that is passed to your \`load\` function: https://kit.svelte.dev/docs/load#making-fetch-requests`
+						);
+					},
+					timeout: 3_000
+				})
+			]);
+			expect(await page.textContent('h1')).toBe('42');
+
 			/** @type {string[]} */
 			const warnings = [];
-
 			page.on('console', (msg) => {
 				if (msg.type() === 'warning') {
 					warnings.push(msg.text());
 				}
 			});
-
-			await page.goto('/load/window-fetch/incorrect');
-			expect(await page.textContent('h1')).toBe('42');
-
-			expect(warnings).toContain(
-				`Loading ${baseURL}/load/window-fetch/data.json using \`window.fetch\`. For best results, use the \`fetch\` that is passed to your \`load\` function: https://kit.svelte.dev/docs/load#making-fetch-requests`
-			);
-
-			warnings.length = 0;
 
 			await page.goto('/load/window-fetch/correct');
 			expect(await page.textContent('h1')).toBe('42');
