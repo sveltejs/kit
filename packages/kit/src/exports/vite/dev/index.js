78a23224b78d245ce04038dd83b7eeac80525cd5
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-12-02 15:36:41 +0100

    [fix] rebuild manifest when client hooks or param matcher file is added/removed (#7915)
    * [fix] rebuild manifest when client hook file is added/removed
    
    In contrast to server hooks, client hooks are written to the client manifest and therefore need rebuilding when they are added/removed
    
    * also applies to param matchers

--- packages/kit/src/exports/vite/dev/index.js
+++ packages/kit/src/exports/vite/dev/index.js
@@ -202,7 +202,13 @@ export async function dev(vite, vite_config, svelte_config) {
 	 */
 	const watch = (event, cb) => {
 		vite.watcher.on(event, (file) => {
-			if (file.startsWith(svelte_config.kit.files.routes + path.sep)) {
+			if (
+				file.startsWith(svelte_config.kit.files.routes + path.sep) ||
+				file.startsWith(svelte_config.kit.files.params + path.sep) ||
+				// in contrast to server hooks, client hooks are written to the client manifest
+				// and therefore need rebuilding when they are added/removed
+				file.startsWith(svelte_config.kit.files.hooks.client)
+			) {
 				cb(file);
 			}
 		});
