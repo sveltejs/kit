d6c69c63d35ab2d65e0bf519c48c261fd25b8b49
Rich Harris <hello@rich-harris.dev>
2022-12-02 17:34:21 -0500

    add a couple of showcase apps, to get us started

--- sites/kit.svelte.dev/src/routes/+page.svelte
+++ sites/kit.svelte.dev/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Section from '@sveltejs/site-kit/components/Section.svelte';
-	import Features from './Features.svelte';
-	import Hero from './Hero.svelte';
-	import Try from './Try.svelte';
-	import Video from './Video.svelte';
+	import Features from './home/Features.svelte';
+	import Hero from './home/Hero.svelte';
+	import Showcase from './home/Showcase.svelte';
+	import Try from './home/Try.svelte';
+	import Video from './home/Video.svelte';
+	import './home/common.css';
 </script>
 
 <svelte:head>
@@ -14,114 +16,95 @@
 	<meta name="description" content="SvelteKit is the official Svelte application framework" />
 </svelte:head>
 
-<h1 class="visually-hidden">SvelteKit</h1>
-<Hero />
+<div class="home">
+	<h1 class="visually-hidden">SvelteKit</h1>
 
-<Section>
-	<div class="grid" style="--columns: 3">
-		<div>
-			<h2>fast</h2>
-			<p>
-				Powered by <a href="https://svelte.dev">Svelte</a> and
-				<a href="https://vitejs.dev">Vite</a>, speed is baked into every aspect of SvelteKit's
-				design: fast installation, fast development, fast builds, fast page loads, fast navigations.
-				Did we mention it's fast?
-			</p>
+	<Hero />
+
+	<Section>
+		<div class="grid" style="--columns: 3">
+			<div>
+				<h2>fast</h2>
+				<p>
+					Powered by <a href="https://svelte.dev">Svelte</a> and
+					<a href="https://vitejs.dev">Vite</a>, speed is baked into every aspect of SvelteKit's
+					design: fast installation, fast development, fast builds, fast page loads, fast
+					navigations. Did we mention it's fast?
+				</p>
+			</div>
+
+			<div>
+				<h2>fun</h2>
+				<p>
+					No more wasted days figuring out bundler configuration and routing and server-side
+					rendering and progressive enhancement and CSP and environment variables and all the other
+					boring stuff. Code with joy.
+				</p>
+			</div>
+
+			<div>
+				<h2>flexible</h2>
+				<p>
+					SPA? MPA? SSR? SSG? WTF. SvelteKit doesn't force its opinions on you, but instead gives
+					you the tools to succeed whatever it is you're building. And it runs wherever JavaScript
+					does.
+				</p>
+			</div>
 		</div>
+	</Section>
 
-		<div>
-			<h2>fun</h2>
-			<p>
-				No more wasted days figuring out bundler configuration and routing and server-side rendering
-				and progressive enhancement and CSP and environment variables and all the other boring
-				stuff. Code with joy.
-			</p>
+	<Try />
+
+	<Section
+		--background="radial-gradient(circle at top right, rgb(230, 233, 236), rgb(244, 245, 247))"
+	>
+		<!-- <h2>svelte</h2> -->
+		<p class="definition">
+			<span>adjective</span> <em>/svelt/</em> attractively thin, graceful and stylish
+		</p>
+
+		<div class="grid" style="--columns: 3">
+			<div>
+				<p>
+					SvelteKit is built on Svelte, a different kind of UI framework. It uses a compiler to let
+					you write breathtakingly concise components that do minimal work in the browser, using
+					languages you already know — HTML, CSS and JavaScript. <strong
+						style="color: var(--sk-theme-2)">It's a love letter to web development.</strong
+					>
+				</p>
+
+				<p>
+					But don't take our word for it. Developers consistently rank Svelte as the framework
+					they're
+					<a
+						href="https://insights.stackoverflow.com/survey/2021#section-most-loved-dreaded-and-wanted-web-frameworks"
+						>most</a
+					>
+					<a
+						href="https://tsh.io/state-of-frontend/#which-of-the-following-frameworks-would-you-like-to-learn-in-the-future"
+						>excited</a
+					> <a href="https://2021.stateofjs.com/en-US/libraries/front-end-frameworks/">about</a>
+					<a href="https://twitter.com/Rich_Harris/status/1589675637195042817">using</a>.
+				</p>
+
+				<p>
+					Watch <a href="https://www.youtube.com/watch?v=kMlkCYL9qo0">Svelte Origins</a>
+					on YouTube.
+				</p>
+			</div>
+
+			<div class="video-container">
+				<Video />
+			</div>
 		</div>
+	</Section>
 
-		<div>
-			<h2>flexible</h2>
-			<p>
-				SPA? MPA? SSR? SSG? WTF. SvelteKit doesn't force its opinions on you, but instead gives you
-				the tools to succeed whatever it is you're building. And it runs wherever JavaScript does.
-			</p>
-		</div>
-	</div>
-</Section>
+	<Features />
 
-<Try />
-
-<Section
-	--background="radial-gradient(circle at top right, rgb(230, 233, 236), rgb(244, 245, 247))"
->
-	<!-- <h2>svelte</h2> -->
-	<p class="definition">
-		<span>adjective</span> <em>/svelt/</em> attractively thin, graceful and stylish
-	</p>
-
-	<div class="grid" style="--columns: 3">
-		<div>
-			<p>
-				SvelteKit is built on Svelte, a different kind of UI framework. It uses a compiler to let
-				you write breathtakingly concise components that do minimal work in the browser, using
-				languages you already know — HTML, CSS and JavaScript. <strong
-					style="color: var(--sk-theme-2)">It's a love letter to web development.</strong
-				>
-			</p>
-
-			<p>
-				But don't take our word for it. Developers consistently rank Svelte as the framework they're
-				<a
-					href="https://insights.stackoverflow.com/survey/2021#section-most-loved-dreaded-and-wanted-web-frameworks"
-					>most</a
-				>
-				<a
-					href="https://tsh.io/state-of-frontend/#which-of-the-following-frameworks-would-you-like-to-learn-in-the-future"
-					>excited</a
-				> <a href="https://2021.stateofjs.com/en-US/libraries/front-end-frameworks/">about</a>
-				<a href="https://twitter.com/Rich_Harris/status/1589675637195042817">using</a>.
-			</p>
-
-			<p>
-				Watch <a href="https://www.youtube.com/watch?v=kMlkCYL9qo0">Svelte Origins</a>
-				on YouTube.
-			</p>
-		</div>
-
-		<div class="video-container">
-			<Video />
-		</div>
-	</div>
-</Section>
-
-<Features />
-
-<Section --background="radial-gradient(circle at 40% 30%, rgb(110, 113, 118), rgb(81, 93, 106))">
-	<h2 style="color: white">TODO showcase/testimonials</h2>
-</Section>
+	<Showcase />
+</div>
 
 <style>
-	:global(.hero-container:dir(rtl)) {
-		max-width: 116rem;
-	}
-
-	.grid {
-		display: grid;
-		gap: 1em;
-		margin: 0 0 6rem 0;
-	}
-
-	.grid:last-child {
-		margin-bottom: 0;
-	}
-
-	h2 {
-		font-size: 2.8rem;
-	}
-
-	h2:not(.grid h2) {
-		text-align: center;
-	}
-
 	.definition {
 		font-size: var(--sk-text-l);
 	}
@@ -138,18 +121,16 @@
 		color: var(--sk-theme-1);
 	}
 
+	.home .video-container {
+		grid-column: 2 / 4;
+	}
+	h2 {
+		font-size: 2.8rem;
+	}
+
 	@media (min-width: 900px) {
 		h2 {
 			font-size: 4rem;
-		}
-
-		.grid {
-			grid-template-columns: repeat(var(--columns), 1fr);
-			gap: 7rem;
-		}
-
-		.video-container {
-			grid-column: 2 / 4;
 		}
 	}
 </style>
---
+++ sites/kit.svelte.dev/src/routes/home/Showcase.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Section from '@sveltejs/site-kit/components/Section.svelte';
+	import monogram from './showcase/monogram.png';
+	import raster from './showcase/raster.png';
+	import trading_strategy from './showcase/trading-strategy.png';
+
+	const showcase = [
+		{ url: 'monogram.io', image: monogram },
+		{ url: 'raster.app', image: raster },
+		{ url: 'trading-strategy.ai', image: trading_strategy }
+	];
+</script>
+
+<Section
+	--background="radial-gradient(circle at top right, rgb(230, 233, 236), rgb(244, 245, 247))"
+>
+	<h2>built with SvelteKit</h2>
+
+	<div class="grid" style="--columns: 3">
+		{#each showcase as { url, image }}
+			<a class="drop-shadow" href="https://{url}">
+				<img src={image} alt="Screenshot of {url}" />
+				<span>{url}</span>
+			</a>
+		{/each}
+	</div>
+</Section>
+
+<style>
+	div.grid {
+		/* column-gap: 6rem; */
+		row-gap: 0rem;
+	}
+
+	a {
+		overflow: hidden;
+		margin: 1em 0;
+	}
+
+	img {
+		border-radius: var(--sk-border-radius);
+		width: 100%;
+		margin: 0 0 0.5em 0;
+	}
+
+	a {
+		color: var(--sk-text);
+	}
+</style>
