dbedbe3e363df85810664d7fc2450cf882e33cd0
Rich Harris <hello@rich-harris.dev>
2022-12-07 18:28:58 -0500

    tweak copy

--- sites/kit.svelte.dev/src/routes/+page.svelte
+++ sites/kit.svelte.dev/src/routes/+page.svelte
@@ -7,6 +7,7 @@
 	import Video from './home/Video.svelte';
 	import './home/common.css';
 	import Deployment from './home/Deployment.svelte';
+	import Svelte from './home/Svelte.svelte';
 </script>
 
 <svelte:head>
@@ -53,72 +54,7 @@
 
 	<Try />
 
-	<Section --background="var(--background-1)">
-		<p class="definition">
-			<em>/ˈsvɛlt/</em> <span class="adjective">adjective</span>
-			<span class="description">attractively thin, graceful and stylish</span>
-		</p>
-
-		<div class="grid" style="--columns: 3">
-			<div class="video-container">
-				<Video />
-
-				<p>
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href="https://www.offerzen.com/community/svelte-origins-documentary"
-					>
-						Watch the full Svelte Origins documentary
-					</a>
-				</p>
-			</div>
-
-			<div>
-				<p>
-					SvelteKit is built on Svelte, a different kind of UI framework. It uses a compiler to let
-					you write breathtakingly concise components that do minimal work in the browser, using
-					languages you already know — HTML, CSS and JavaScript. <strong
-						style="color: var(--sk-theme-2)">It's a love letter to web development.</strong
-					>
-				</p>
-
-				<p>
-					But don't take our word for it. Developers consistently rank Svelte as the framework
-					they're
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href="https://insights.stackoverflow.com/survey/2021#section-most-loved-dreaded-and-wanted-web-frameworks"
-					>
-						most
-					</a>
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href="https://tsh.io/state-of-frontend/#which-of-the-following-frameworks-would-you-like-to-learn-in-the-future"
-					>
-						excited
-					</a>
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href="https://2021.stateofjs.com/en-US/libraries/front-end-frameworks/"
-					>
-						about
-					</a>
-
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href="https://twitter.com/Rich_Harris/status/1589675637195042817"
-					>
-						using</a
-					>.
-				</p>
-			</div>
-		</div>
-	</Section>
+	<Svelte />
 
 	<Features />
 
@@ -132,31 +68,6 @@
 </div>
 
 <style>
-	.definition {
-		font-size: var(--sk-text-l);
-	}
-
-	.definition em {
-		font-style: normal;
-		font-size: var(--sk-text-xxl);
-		color: var(--sk-theme-1);
-	}
-
-	.definition .adjective {
-		position: relative;
-		top: -0.2em;
-		background: var(--sk-theme-3);
-		padding: 0.2em 0.5em;
-		margin: 0 1em;
-		text-transform: uppercase;
-		font-size: var(--sk-text-s);
-		color: white;
-	}
-
-	.definition .description {
-		display: block;
-	}
-
 	footer {
 		padding: 1em var(--sk-page-padding-side);
 		text-align: center;
@@ -170,17 +81,6 @@
 	@media (min-width: 680px) {
 		footer p {
 			max-width: none;
-		}
-	}
-
-	@media (min-width: 900px) {
-		.definition .description {
-			display: inline;
-		}
-
-		.home .video-container {
-			order: 2;
-			grid-column: 2 / 4;
 		}
 	}
 </style>
