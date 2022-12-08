dc3e84609663269dc06513363dd999888378a4ec
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-12-08 15:22:35 -0800

    Update sites/kit.svelte.dev/vite.config.js
    Co-authored-by: Rich Harris <richard.a.harris@gmail.com>

--- sites/kit.svelte.dev/vite.config.js
+++ sites/kit.svelte.dev/vite.config.js
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import * as path from 'path';
 import { imagetools } from 'vite-imagetools';
 
-const supportedExtensions = ['png', 'jpg', 'jpeg'];
+const supportedExtensions = ['.png', '.jpg', '.jpeg'];
 
 /** @type {import('vite').UserConfig} */
 const config = {
