7969e6dcdaad2350492a8ca4fb06494066ffa996
Max Schmitt <max@schmitt.mx>
2022-12-14 18:54:37 +0100

    chore(create-svelte): use web-first assertion for assertion in PW

--- packages/create-svelte/shared/+playwright+default/tests/test.ts
+++ packages/create-svelte/shared/+playwright+default/tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('about page has expected h1', async ({ page }) => {
 	await page.goto('/about');
-	expect(await page.textContent('h1')).toBe('About this app');
+	await expect(page.locator('h1')).toHaveText('About this app');
 });
