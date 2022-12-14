b8371dd2360c51c77c290f33db0c1537cacb7171
Rich Harris <richard.a.harris@gmail.com>
2022-12-14 10:04:39 -0500

    Update sites/kit.svelte.dev/src/routes/Banner.svelte
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- sites/kit.svelte.dev/src/routes/Banner.svelte
+++ sites/kit.svelte.dev/src/routes/Banner.svelte
@@ -16,7 +16,7 @@
 <header class="banner" class:visible>
 	<p>
 		<a href="https://www.youtube.com/watch?v=N4BRVkQVoMc">
-			<strong>LIVE NOW:</strong> announcing SvelteKit 1.0
+			<strong>LIVE NOW:</strong> announcing SvelteKit 1.0!
 		</a>
 	</p>
 </header>
