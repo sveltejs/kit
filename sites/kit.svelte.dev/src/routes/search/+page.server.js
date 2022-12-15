c6cfc226257e00e813f85abd468fed9f7fce7c3a
gtmnayan <50981692+gtm-nayan@users.noreply.github.com>
2022-12-15 15:02:46 +0545

    use prerendered endpoint to get content for search

--- sites/kit.svelte.dev/src/routes/search/+page.server.js
+++ sites/kit.svelte.dev/src/routes/search/+page.server.js
@@ -1,11 +1,15 @@
-import { content } from '$lib/search/content.server.js';
 import { init, inited, search } from '$lib/search/search.js';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ url }) {
 	if (!inited) {
-		init(content());
+		const res = await fetch('/content.json');
+		if (!res.ok) throw new Error("Couldn't fetch content");
+
+		const blocks = (await res.json()).blocks;
+		init(blocks);
 	}
+
 
 	const query = url.searchParams.get('q');
 
