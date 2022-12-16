8ab1f5c51061b3a43b0a1e20e58f3e476ed471fb
depapp <depa.article@gmail.com>
2022-12-16 16:50:08 +0700

    add more e2e test

--- packages/create-svelte/shared/+playwright+default/tests/test.ts
+++ packages/create-svelte/shared/+playwright+default/tests/test.ts
@@ -1,6 +1,39 @@
 import { expect, test } from '@playwright/test';
 
-test('about page has expected h1', async ({ page }) => {
-	await page.goto('/about');
-	expect(await page.textContent('h1')).toBe('About this app');
+test.describe('home page e2e test', () => {
+    
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+        await expect(page).toHaveTitle(/Home/);
+    });
+
+    test('user should be able to see welcome page', async ({ page }) => {
+        const welcomeScreen = page.getByRole('heading', { name: 'Welcome to your new SvelteKit app' });
+		await expect(welcomeScreen).toBeVisible();
+    });
+
+    test('user should be able to use the counter', async ({ page }) => {
+        await expect(page.getByText('0')).toBeVisible();
+		const increaseButton = page.getByRole('button', { name: 'Increase the counter by one' });
+		await increaseButton.click({ clickCount: 3});
+		await expect(page.getByText('3')).toBeVisible();
+		const decreaseButton = page.getByRole('button', { name: 'Decrease the counter by one' });
+		await decreaseButton.click({ clickCount: 2});
+		await expect(page.getByText('1')).toBeVisible();
+    });
+
+});
+
+test.describe('about page e2e test', () => {
+    
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/about');
+        await expect(page).toHaveTitle(/About/);
+    });
+
+    test('user should be able to see about page', async ({ page }) => {
+        const headingText = page.textContent('h1');
+        expect(await headingText).toBe('About this app');
+    });
+
 });
