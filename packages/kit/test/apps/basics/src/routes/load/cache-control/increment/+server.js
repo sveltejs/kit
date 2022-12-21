8d5c3b33436cba33de8f7e0079e44fbbd4b92ffc
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-12-21 10:36:36 -0800

    server logging

--- packages/kit/test/apps/basics/src/routes/load/cache-control/increment/+server.js
+++ packages/kit/test/apps/basics/src/routes/load/cache-control/increment/+server.js
@@ -2,6 +2,7 @@ import { json } from '@sveltejs/kit';
 import { increment } from '../state.js';
 
 export function GET() {
+	console.warn('\n\nCALLED GET /load/cache-control/increment\n\n');
 	increment();
 	return json({});
 }
