cf85c74382f779eacf1f5f29efc7111b96a010e6
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-12-21 20:02:16 -0800

    trigger tests again

--- packages/kit/test/apps/basics/src/routes/load/cache-control/increment/+server.js
+++ packages/kit/test/apps/basics/src/routes/load/cache-control/increment/+server.js
@@ -3,8 +3,8 @@ import { increment } from '../state.js';
 
 export function GET() {
 	if (process.env.DEBUG) console.warn('\n\nCALLED GET /load/cache-control/increment');
-	increment();
-	if (process.env.DEBUG) console.warn('incremented');
+	const value = increment();
+	if (process.env.DEBUG) console.warn(`incremented to ${value}`);
 	const result = json({});
 	if (process.env.DEBUG) console.warn('RETURNING FROM GET /load/cache-control/increment\n\n');
 	return result;
