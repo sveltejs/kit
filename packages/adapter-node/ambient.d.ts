d671805bf6124bf81d718173434b79c81272e22c
Rich Harris <hello@rich-harris.dev>
2023-01-05 11:56:06 -0500

    add docs

--- packages/adapter-node/ambient.d.ts
+++ packages/adapter-node/ambient.d.ts
@@ -17,6 +17,9 @@ declare module 'SERVER' {
 
 declare namespace App {
 	export interface Platform {
+		/**
+		 * The original Node request object (https://nodejs.org/api/http.html#class-httpincomingmessage)
+		 */
 		req: import('http').IncomingMessage;
 	}
 }
