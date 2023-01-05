ee11d52a4dafed22076a951e9bd66b6b9dbb08b2
Rich Harris <hello@rich-harris.dev>
2023-01-05 10:40:31 -0500

    simplify

--- packages/create-svelte/shared/+vitest/vite.config.ts
+++ packages/create-svelte/shared/+vitest/vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
+/** @type {import('vite').UserConfig} */
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 };
 
 export default config;
