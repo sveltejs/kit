f3ff35f52349342864ffa621f3329a511b7acecf
Rich Harris <hello@rich-harris.dev>
2023-01-09 14:56:51 -0500

    temporarily add trailing slash

--- packages/create-svelte/templates/default/src/routes/about/+page.ts
+++ packages/create-svelte/templates/default/src/routes/about/+page.ts
@@ -1,5 +1,7 @@
 import { dev } from '$app/environment';
 
+export const trailingSlash = 'always';
+
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
 export const csr = dev;
