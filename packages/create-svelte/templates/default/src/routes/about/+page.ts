e9d1f4cf74cbec6707d62d34785c970ca937ed0d
Rich Harris <hello@rich-harris.dev>
2023-01-09 15:11:07 -0500

    Revert "temporarily add trailing slash"
    This reverts commit f3ff35f52349342864ffa621f3329a511b7acecf.

--- packages/create-svelte/templates/default/src/routes/about/+page.ts
+++ packages/create-svelte/templates/default/src/routes/about/+page.ts
@@ -1,7 +1,5 @@
 import { dev } from '$app/environment';
 
-export const trailingSlash = 'always';
-
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
 export const csr = dev;
