aa7f70063bfa849deb31b80d11685a857d573147
Algoinde <algoinde@gmail.com>
2023-01-09 15:07:44 +0500

    [fix] route invalidation comparison based on url and route, not on id (#8399)
    Fixes #8398

---
+++ .changeset/sweet-hairs-roll.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/kit': patch
+---
+
+fix route id comparison for load change detection
--- packages/kit/test/apps/basics/src/routes/load/invalidation/route/shared/+layout.svelte
+++ packages/kit/test/apps/basics/src/routes/load/invalidation/route/shared/+layout.svelte
@@ -4,7 +4,11 @@
 </script>
 
 <h1>route.id: {data.route.id}</h1>
+<h2>random id: {data.random}</h2>
 <slot />
 
 <a href="/load/invalidation/route/shared/a">/load/invalidation/route/shared/a</a>
 <a href="/load/invalidation/route/shared/b">/load/invalidation/route/shared/b</a>
+
+<a href="/load/invalidation/route/shared/unchanged-x">/load/invalidation/route/shared/unchanged-x</a>
+<a href="/load/invalidation/route/shared/unchanged-y">/load/invalidation/route/shared/unchanged-y</a>
