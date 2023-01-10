11e074418fff489ff9c38b8bb981bdbee0d0898c
Rich Harris <hello@rich-harris.dev>
2023-01-10 17:58:20 -0500

    add ambient declaration

---
+++ packages/kit/src/runtime/server/ambient.d.ts
@@ -0,0 +1,9 @@
+declare module '__GENERATED__/server-internal.js' {
+	export const options: import('types').SSROptions;
+	export const public_prefix: string;
+	export const get_hooks: () => Promise<{
+		handle?: import('types').Handle;
+		handleError?: import('types').HandleServerError;
+		handleFetch?: import('types').HandleFetch;
+	}>;
+}
