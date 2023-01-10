272e6c87385833e9add656070ddc87d3f46b0d2e
Rich Harris <richard.a.harris@gmail.com>
2023-01-10 04:05:25 -0500

    [fix] ignore `<a>` elements with no `href` (#8418)
    closes #8051

---
+++ .changeset/cyan-mirrors-cry.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/kit': patch
+---
+
+[fix] ignore `<a>` elements with no `href` attribute
---
+++ packages/kit/test/apps/basics/src/routes/routing/missing-href/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { afterNavigate } from '$app/navigation';
+
+	let count = 0;
+
+	afterNavigate(() => {
+		count += 1;
+	});
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<a data-testid="count">count: {count}</a>
