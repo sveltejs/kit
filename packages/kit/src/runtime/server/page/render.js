f09ad430ea298ef2e0b77b81c00205dc469ed1bc
Simon H <5968653+dummdidumm@users.noreply.github.com>
2023-01-13 21:07:07 +0100

    fix: provide better error message in case of missing `+page.svelte` (#8478)
    * fix: provide better error message in case of missing `+page.svelte`
    
    closes #8270
    
    * Update packages/kit/src/runtime/server/page/render.js
    
    Co-authored-by: Rich Harris <richard.a.harris@gmail.com>
    
    * oops
    
    Co-authored-by: Rich Harris <richard.a.harris@gmail.com>
    Co-authored-by: Rich Harris <hello@rich-harris.dev>

--- packages/kit/src/runtime/server/page/render.js
+++ packages/kit/src/runtime/server/page/render.js
@@ -77,6 +77,11 @@ export async function render_response({
 			: null;
 
 	if (page_config.ssr) {
+		if (!branch.at(-1)?.node.component) {
+			// Can only be the leaf, layouts have a fallback component generated
+			throw new Error(`Missing +page.svelte component for route ${event.route.id}`);
+		}
+
 		/** @type {Record<string, any>} */
 		const props = {
 			stores: {
