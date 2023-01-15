841f2116f6bad114e9a19f93838569d1ef5653e4
Tal Hadad <tal_hd@hotmail.com>
2023-01-15 20:29:09 +0200

    put `browserslist` in `package.json` for simplicity

--- sites/kit.svelte.dev/vite.config.js
+++ sites/kit.svelte.dev/vite.config.js
@@ -1,22 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { readFileSync } from 'fs';
 import * as path from 'path';
 import { imagetools } from 'vite-imagetools';
 import legacy from '@vitejs/plugin-legacy';
-
-// The recommended way to list the legacy browsers is by putting this on a file named '.browserslistrc'.
-// Sadly, babel preset plugin (the one that is being used by vite legacy plugin) doesn't read this file automatically,
-// and from the other hand, postcssPresetEnv can't read the value passed to vite legacy plugin.
-// This is why we don't specify the browser list explicitly in this vite config, but rather added this utility function
-//  to read the browser list from the file.
-const readBrowsersList = () => readFileSync("./.browserslistrc", { encoding: 'utf-8' })
-	.split(/\r?\n/) // Split it to lines
-	.map((line) => {
-		const trimmedLine = line.trim();
-		return (trimmedLine.length === 0 || trimmedLine[0] === "#") ? undefined : trimmedLine;
-	})
-	.filter((query) => query !== undefined)
-	.join(', ');
+import packageConfig from './package.json';
 
 const supportedExtensions = ['.png', '.jpg', '.jpeg'];
 
@@ -41,7 +27,7 @@ const config = {
 		}),
 		sveltekit(),
 		legacy({
-			targets: readBrowsersList(),
+			targets: packageConfig.browserslist,
 			additionalLegacyPolyfills: [
 				'custom-event-polyfill',
 				'core-js/modules/es.promise.js',
