65c7809b4652e6f347dde00cc8b29082b910c5e1
Simon Holthausen <simon.holthausen@vercel.com>
2023-01-16 22:11:31 +0100

    use *

--- packages/kit/src/runtime/client/client.js
+++ packages/kit/src/runtime/client/client.js
@@ -223,9 +223,10 @@ export function create_client({ target, base }) {
 
 	/** @param {...string} pathnames */
 	async function preload_code(...pathnames) {
-		const matching = routes.filter(
-			(route) => !pathnames.length || pathnames.some((pathname) => route.exec(pathname))
-		);
+		const matching =
+			pathnames[0] === '*'
+				? routes
+				: routes.filter((route) => pathnames.some((pathname) => route.exec(pathname)));
 
 		const promises = matching.map((r) => {
 			return Promise.all([...r.layouts, r.leaf].map((load) => load?.[1]()));
