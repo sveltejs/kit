f1ac8468b17692700b9f9feb4156edfef32c718f
Rich Harris <hello@rich-harris.dev>
2023-01-17 17:11:59 -0500

    remove unused deep_merge utility

--- packages/kit/src/exports/vite/utils.js
+++ packages/kit/src/exports/vite/utils.js
@@ -4,52 +4,6 @@ import { posixify } from '../../utils/filesystem.js';
 import { negotiate } from '../../utils/http.js';
 
 /**
- * Takes zero or more objects and returns a new object that has all the values
- * deeply merged together. None of the original objects will be mutated at any
- * level, and the returned object will have no references to the original
- * objects at any depth. If there's a conflict the last one wins, except for
- * arrays which will be combined.
- * @param {...Object} objects
- * @returns {Record<string, any>} the merged object
- */
-export function deep_merge(...objects) {
-	const result = {};
-	/** @type {string[]} */
-	objects.forEach((o) => merge_into(result, o));
-	return result;
-}
-
-/**
- * Merges b into a, recursively, mutating a.
- * @param {Record<string, any>} a
- * @param {Record<string, any>} b
- */
-function merge_into(a, b) {
-	/**
-	 * Checks for "plain old Javascript object", typically made as an object
-	 * literal. Excludes Arrays and built-in types like Buffer.
-	 * @param {any} x
-	 */
-	const is_plain_object = (x) => typeof x === 'object' && x.constructor === Object;
-
-	for (const prop in b) {
-		if (is_plain_object(b[prop])) {
-			if (!is_plain_object(a[prop])) {
-				a[prop] = {};
-			}
-			merge_into(a[prop], b[prop]);
-		} else if (Array.isArray(b[prop])) {
-			if (!Array.isArray(a[prop])) {
-				a[prop] = [];
-			}
-			a[prop].push(...b[prop]);
-		} else {
-			a[prop] = b[prop];
-		}
-	}
-}
-
-/**
  * Transforms kit.alias to a valid vite.resolve.alias array.
  *
  * Related to tsconfig path alias creation.
