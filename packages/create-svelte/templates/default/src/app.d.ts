8433999c28fc72ab775f878c5bfddc39469e248c
Rich Harris <richard.a.harris@gmail.com>
2023-01-20 07:01:58 -0500

    chore: remove obsolete comment from templates (#8620)

--- packages/create-svelte/templates/default/src/app.d.ts
+++ packages/create-svelte/templates/default/src/app.d.ts
@@ -1,6 +1,5 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-// and what to do when importing types
 declare global {
 	namespace App {
 		// interface Error {}
