8d89773799420c124c16051706d40f7cafda3fd1
Tee Ming <chewteeming01@gmail.com>
2023-01-24 21:24:46 +0800

    Revert "add append flag to _headers file writing"
    This reverts commit 4e27ca4108106763791ad202d04867d36847227c.

--- .changeset/fluffy-wombats-fetch.md
+++
@@ -1,5 +0,0 @@
----
-'@sveltejs/adapter-cloudflare': patch
----
-
-fix: prevent overwriting existing \_headers file
--- packages/adapter-cloudflare/src/worker.js
+++ packages/adapter-cloudflare/src/worker.js
@@ -1,119 +1,73 @@
-import { writeFileSync } from 'fs';
-import { posix } from 'path';
-import { fileURLToPath } from 'url';
-import * as esbuild from 'esbuild';
+import { Server } from 'SERVER';
+import { manifest, prerendered } from 'MANIFEST';
+import * as Cache from 'worktop/cfw.cache';
 
-/** @type {import('.').default} */
-export default function () {
-	return {
-		name: '@sveltejs/adapter-cloudflare',
-		async adapt(builder) {
-			const files = fileURLToPath(new URL('./files', import.meta.url).href);
-			const dest = builder.getBuildDirectory('cloudflare');
-			const tmp = builder.getBuildDirectory('cloudflare-tmp');
+const server = new Server(manifest);
 
-			builder.rimraf(dest);
-			builder.rimraf(tmp);
-			builder.mkdirp(tmp);
+const app_path = `/${manifest.appPath}/`;
 
-			const dest_dir = `${dest}${builder.config.kit.paths.base}`;
-			const written_files = builder.writeClient(dest_dir);
-			builder.writePrerendered(dest_dir);
+/** @type {import('worktop/cfw').Module.Worker<{ ASSETS: import('worktop/cfw.durable').Durable.Object }>} */
+const worker = {
+	async fetch(req, env, context) {
+		await server.init({ env });
+		// skip cache if "cache-control: no-cache" in request
+		let pragma = req.headers.get('cache-control') || '';
+		let res = !pragma.includes('no-cache') && (await Cache.lookup(req));
+		if (res) return res;
 
-			const relativePath = posix.relative(tmp, builder.getServerDirectory());
+		let { pathname } = new URL(req.url);
 
-			writeFileSync(
-				`${tmp}/manifest.js`,
-				`export const manifest = ${builder.generateManifest({ relativePath })};\n\n` +
-					`export const prerendered = new Set(${JSON.stringify(builder.prerendered.paths)});\n`
-			);
+		// static assets
+		if (pathname.startsWith(app_path)) {
+			res = await env.ASSETS.fetch(req);
+			if (!res.ok) return res;
 
-			writeFileSync(
-				`${dest}/_routes.json`,
-				JSON.stringify(get_routes_json(builder, written_files))
-			);
+			const cache_control = pathname.startsWith(app_path + 'immutable/')
+				? 'public, immutable, max-age=31536000'
+				: 'no-cache';
 
-			writeFileSync(`${dest}/_headers`, generate_headers(builder.config.kit.appDir), { flag: 'a' });
-
-			builder.copy(`${files}/worker.js`, `${tmp}/_worker.js`, {
-				replace: {
-					SERVER: `${relativePath}/index.js`,
-					MANIFEST: './manifest.js'
+			res = new Response(res.body, {
+				headers: {
+					// include original headers, minus cache-control which
+					// is overridden, and etag which is no longer useful
+					'cache-control': cache_control,
+					'content-type': res.headers.get('content-type'),
+					'x-robots-tag': 'noindex'
 				}
 			});
+		} else {
+			// prerendered pages and index.html files
+			pathname = pathname.replace(/\/$/, '') || '/';
 
-			await esbuild.build({
-				platform: 'browser',
-				conditions: ['worker', 'browser'],
-				sourcemap: 'linked',
-				target: 'es2020',
-				entryPoints: [`${tmp}/_worker.js`],
-				outfile: `${dest}/_worker.js`,
-				allowOverwrite: true,
-				format: 'esm',
-				bundle: true
-			});
+			let file = pathname.substring(1);
+
+			try {
+				file = decodeURIComponent(file);
+			} catch (err) {
+				// ignore
+			}
+
+			if (
+				manifest.assets.has(file) ||
+				manifest.assets.has(file + '/index.html') ||
+				prerendered.has(pathname)
+			) {
+				res = await env.ASSETS.fetch(req);
+			} else {
+				// dynamically-generated pages
+				res = await server.respond(req, {
+					platform: { env, context, caches },
+					getClientAddress() {
+						return req.headers.get('cf-connecting-ip');
+					}
+				});
+			}
 		}
-	};
-}
 
-/**
- * @param {import('@sveltejs/kit').Builder} builder
- * @param {string[]} assets
- * @returns {import('.').RoutesJSONSpec}
- */
-function get_routes_json(builder, assets) {
-	/**
-	 * The list of routes that will _not_ invoke functions (which cost money).
-	 * This is done on a best-effort basis, as there is a limit of 100 rules
-	 */
-	const exclude = [
-		`/${builder.config.kit.appDir}/*`,
-		...assets
-			.filter((file) => !file.startsWith(`${builder.config.kit.appDir}/`))
-			.map((file) => `/${file}`)
-	];
-
-	const MAX_EXCLUSIONS = 99; // 100 minus existing `include` rules
-	let excess;
-
-	if (exclude.length > MAX_EXCLUSIONS) {
-		excess = 'static assets';
-
-		if (builder.prerendered.paths.length > 0) {
-			excess += ' or prerendered routes';
-		}
-	} else if (exclude.length + builder.prerendered.paths.length > MAX_EXCLUSIONS) {
-		excess = 'prerendered routes';
+		// Writes to Cache only if allowed & specified
+		pragma = res.headers.get('cache-control');
+		return pragma && res.ok ? Cache.save(req, res, context) : res;
 	}
+};
 
-	for (const path of builder.prerendered.paths) {
-		if (!builder.prerendered.redirects.has(path)) {
-			exclude.push(path);
-		}
-	}
-
-	if (excess) {
-		const message = `Static file count exceeds _routes.json limits (see https://developers.cloudflare.com/pages/platform/functions/routing/#limits). Accessing some ${excess} will cause function invocations.`;
-		builder.log.warn(message);
-		exclude.length = 99;
-	}
-
-	return {
-		version: 1,
-		description: 'Generated by @sveltejs/adapter-cloudflare',
-		include: ['/*'],
-		exclude
-	};
-}
-
-/** @param {string} app_dir */
-function generate_headers(app_dir) {
-	return `
-# === START AUTOGENERATED SVELTE IMMUTABLE HEADERS ===
-/${app_dir}/immutable/*
-	Cache-Control: public, immutable, max-age=31536000
-	X-Robots-Tag: noindex
-# === END AUTOGENERATED SVELTE IMMUTABLE HEADERS ===
-`.trimEnd();
-}
+export default worker;
