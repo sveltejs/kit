fe62d2ba2a193cd27f4d00b1f8c9408be028944a
Rich Harris <hello@rich-harris.dev>
2023-01-26 15:31:49 -0500

    types

--- packages/kit/test/apps/basics/src/routes/snapshot/a/+page.svelte
+++ packages/kit/test/apps/basics/src/routes/snapshot/a/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	let message = '';
 
+	/** @type {import('./$types').Snapshot<string>} */
 	export const snapshot = {
 		capture: () => message,
 		restore: (snapshot) => (message = snapshot)
