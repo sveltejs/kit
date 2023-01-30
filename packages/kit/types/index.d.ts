33e7bc3062b964be1617928562cbb76dd32556e9
Rich Harris <hello@rich-harris.dev>
2023-01-30 12:36:38 -0500

    Update packages/kit/types/index.d.ts

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -506,7 +506,7 @@ export interface KitConfig {
 	typescript?: {
 		/**
 		 * A function that allows you to edit the generated `tsconfig.json`. You can mutate the config (recommended) or return a new one.
-		 * This is useful to — fo example — extend a shared `tsconfig.json` in a monorepo root.
+		 * This is useful for extending a shared `tsconfig.json` in a monorepo root, for example.
 		 * @default (config) => config
 		 */
 		config?: (config: Record<string, any>) => Record<string, any> | void;
