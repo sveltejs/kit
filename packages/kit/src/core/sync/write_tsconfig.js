ee53e8c704e5eaab0e8039edf19a64f760ee90ae
Simon Holthausen <simon.holthausen@vercel.com>
2023-02-06 17:04:09 +0100

    fix

--- packages/kit/src/core/sync/write_tsconfig.js
+++ packages/kit/src/core/sync/write_tsconfig.js
@@ -90,12 +90,12 @@ export function get_tsconfig(kit, include_base_url) {
 		'./types/**/$types.d.ts',
 		config_relative('vite.config.ts')
 	]);
-
-	const src_includes = [
-		kit.files.routes,
-		kit.files.lib,
-		path.resolve('src') // TODO(v2): find a better way to include all src files
-	].filter((dir) => !path.relative(dir, path.resolve('src')).startsWith('..'));
+	// TODO(v2): find a better way to include all src files. We can't just use routes/lib only because
+	// people might have other folders/files in src that they want included.
+	const src_includes = [kit.files.routes, kit.files.lib, path.resolve('src')].filter((dir) => {
+		const relative = path.relative(path.resolve('src'), dir);
+		return !relative || relative.startsWith('..');
+	});
 	for (const dir of src_includes) {
 		include.add(config_relative(`${dir}/**/*.js`));
 		include.add(config_relative(`${dir}/**/*.ts`));
