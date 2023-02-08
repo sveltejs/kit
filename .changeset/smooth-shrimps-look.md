c983428942f74eb9f4a35b763127bb4bd3d90f61
Jonathan Kula <jona.d.kula@gmail.com>
2023-02-08 13:59:56 -0800

    fix: always default `paths.assets` to `paths.base` (#8928)
    * Make sure that prerendering visitation code doesn't accidentally move outside the base path + add tests [1/2]
    
    * Make sure that prerendering visitation code doesn't accidentally move outside the base path + add tests [2/2]
    
    * Add changeset
    
    * Don’t specify default in test
    
    Co-authored-by: Simon H <5968653+dummdidumm@users.noreply.github.com>
    
    * fix
    
    * Update .changeset/smooth-shrimps-look.md
    
    ---------
    
    Co-authored-by: Simon H <5968653+dummdidumm@users.noreply.github.com>
    Co-authored-by: Rich Harris <hello@rich-harris.dev>

---
+++ .changeset/smooth-shrimps-look.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/kit': patch
+---
+
+fix: always default `paths.assets` to `paths.base`
---
+++ packages/kit/test/prerendering/paths-base/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { base } from '$app/paths'
+</script>
+
+<div>
+	<h1>Main Page</h1>
+	<a href="{base}/nested">Nested Link</a>
+</div>
