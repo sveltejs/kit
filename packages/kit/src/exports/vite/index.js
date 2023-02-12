74cfa8d5f1f13f81759e20e90f4ff86a4f96040d
gtmnayan <50981692+gtm-nayan@users.noreply.github.com>
2023-02-12 23:45:18 +0545

    fix: client build minification (#9012)
    * fix minification
    
    * changeset
    
    * fewer moving parts

--- packages/kit/src/exports/vite/index.js
+++ packages/kit/src/exports/vite/index.js
@@ -179,6 +179,9 @@ function kit({ svelte_config }) {
 	/** @type {() => Promise<void>} */
 	let finalise;
 
+	/** @type {import('vite').UserConfig} */
+	let initial_config;
+
 	const service_worker_entry_file = resolve_entry(kit.files.serviceWorker);
 
 	/** @type {import('vite').Plugin} */
@@ -190,6 +193,7 @@ function kit({ svelte_config }) {
 		 * @see https://vitejs.dev/guide/api-plugin.html#config
 		 */
 		async config(config, config_env) {
+			initial_config = config;
 			vite_config_env = config_env;
 			is_build = config_env.command === 'build';
 
@@ -634,7 +638,7 @@ export function set_assets(path) {
 						logLevel: vite_config.logLevel,
 						clearScreen: vite_config.clearScreen,
 						build: {
-							minify: vite_config.build.minify,
+							minify: initial_config.build?.minify,
 							assetsInlineLimit: vite_config.build.assetsInlineLimit,
 							sourcemap: vite_config.build.sourcemap
 						},
