fdcfb8766ff45fee3697c8913630799f14463d4f
Tee Ming <chewteeming01@gmail.com>
2023-02-15 04:13:14 +0800

    remove cloudflare worker asset check (#9040)

--- packages/adapter-cloudflare/src/worker.js
+++ packages/adapter-cloudflare/src/worker.js
@@ -4,8 +4,6 @@ import * as Cache from 'worktop/cfw.cache';
 
 const server = new Server(manifest);
 
-const app_path = `/${manifest.appPath}/`;
-
 /** @type {import('worktop/cfw').Module.Worker<{ ASSETS: import('worktop/cfw.durable').Durable.Object }>} */
 const worker = {
 	async fetch(req, env, context) {
@@ -17,64 +15,42 @@ const worker = {
 		if (res) return res;
 
 		let { pathname } = new URL(req.url);
+		try {
+			pathname = decodeURIComponent(pathname);
+		} catch {
+			// ignore invalid URI
+		}
 
-		// generated files
-		if (pathname.startsWith(app_path)) {
+		const stripped_pathname = pathname.replace(/\/$/, '');
+
+		// prerendered pages and /static files
+		let is_static_asset = false;
+		const filename = stripped_pathname.substring(1);
+		if (filename) {
+			is_static_asset =
+				manifest.assets.has(filename) || manifest.assets.has(filename + '/index.html');
+		}
+
+		const location = pathname.at(-1) === '/' ? stripped_pathname : pathname + '/';
+
+		if (is_static_asset || prerendered.has(pathname)) {
 			res = await env.ASSETS.fetch(req);
-			if (!res.ok) return res;
-
-			const cache_control = pathname.startsWith(app_path + 'immutable/')
-				? 'public, immutable, max-age=31536000'
-				: 'no-cache';
-
-			res = new Response(res.body, {
+		} else if (location && prerendered.has(location)) {
+			res = new Response('', {
+				status: 308,
 				headers: {
-					// include original headers, minus cache-control which
-					// is overridden, and etag which is no longer useful
-					'cache-control': cache_control,
-					'content-type': res.headers.get('content-type'),
-					'x-robots-tag': 'noindex'
+					location
 				}
 			});
 		} else {
-			// prerendered pages and /static files
-
-			try {
-				pathname = decodeURIComponent(pathname);
-			} catch {
-				// ignore invalid URI
-			}
-
-			const stripped_pathname = pathname.replace(/\/$/, '');
-
-			let is_static_asset = false;
-			const filename = stripped_pathname.substring(1);
-			if (filename) {
-				is_static_asset =
-					manifest.assets.has(filename) || manifest.assets.has(filename + '/index.html');
-			}
-
-			const counterpart_route = pathname.at(-1) === '/' ? stripped_pathname : pathname + '/';
-
-			if (is_static_asset || prerendered.has(pathname)) {
-				res = await env.ASSETS.fetch(req);
-			} else if (counterpart_route && prerendered.has(counterpart_route)) {
-				res = new Response('', {
-					status: 308,
-					headers: {
-						location: counterpart_route
-					}
-				});
-			} else {
-				// dynamically-generated pages
-				res = await server.respond(req, {
-					// @ts-ignore
-					platform: { env, context, caches },
-					getClientAddress() {
-						return req.headers.get('cf-connecting-ip');
-					}
-				});
-			}
+			// dynamically-generated pages
+			res = await server.respond(req, {
+				// @ts-ignore
+				platform: { env, context, caches },
+				getClientAddress() {
+					return req.headers.get('cf-connecting-ip');
+				}
+			});
 		}
 
 		// Writes to Cache only if allowed & specified
