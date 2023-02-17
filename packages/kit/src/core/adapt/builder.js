4626aec3bb84ee341a9f897fd0b1003ec4995129
Tee Ming <chewteeming01@gmail.com>
2023-02-18 03:07:55 +0800

    add asset path prefix to server assets

--- packages/kit/src/core/adapt/builder.js
+++ packages/kit/src/core/adapt/builder.js
@@ -185,7 +185,7 @@ export function create_builder({
 			const server_assets = copy(
 				`${config.kit.outDir}/output/server/${config.kit.appDir}/immutable/assets`,
 				join(dest, config.kit.appDir, 'immutable/assets')
-			);
+			).map((filename) => join(config.kit.appDir, 'immutable/assets', filename));
 			const client_assets = copy(`${config.kit.outDir}/output/client`, dest);
 			return Array.from(new Set([...server_assets, ...client_assets]));
 		},
