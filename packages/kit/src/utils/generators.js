386eaed7ee0c415421ac9db04d83183cd319c6d0
Rich Harris <hello@rich-harris.dev>
2023-02-17 22:45:07 -0500

    typo

--- packages/kit/src/utils/generators.js
+++ packages/kit/src/utils/generators.js
@@ -1,6 +1,6 @@
 /**
  * This method is necessary because we can't yield from inside a callback,
- * so we smooth other an internal less ergonomic callback API
+ * so we smooth over an internal less ergonomic callback API
  * @template {(...args: any) => void} Fn
  * @param {Fn} fn
  * @returns {Parameters<Fn> extends [...infer InitPs, (value: infer Value, done: boolean) => void] ? (...args: InitPs) => AsyncGenerator<Value, undefined, undefined> : never}
@@ -9,13 +9,16 @@ export function to_generator(fn) {
 	// @ts-ignore yeah TS this return type is fucked up, I know
 	return async function* (...args) {
 		/** @type {(v: any) => void} */
-		let fulfill;
+		let fulfil;
+
 		/** @type {(v: any) => void} */
 		let reject;
+
 		let promise = new Promise((f, r) => {
-			fulfill = f;
+			fulfil = f;
 			reject = r;
 		});
+
 		// Ensure it runs after we enter the loop to not swallow the first eager result
 		Promise.resolve()
 			.then(() =>
@@ -26,10 +29,10 @@ export function to_generator(fn) {
 					 * @param {boolean} done
 					 */
 					(result, done) => {
-						fulfill({ result, done });
+						fulfil({ result, done });
 						if (!done) {
 							promise = new Promise((r) => {
-								fulfill = r;
+								fulfil = r;
 							});
 						}
 					}
