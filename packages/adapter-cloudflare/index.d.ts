44802ad94f823d3e0631a6a8869fa071548e114a
ajgeiss0702 <ajgeiss72@gmail.com>
2023-02-18 08:54:00 -0700

    add options to cloudflare-adapter to allow custom _routes rules

--- packages/adapter-cloudflare/index.d.ts
+++ packages/adapter-cloudflare/index.d.ts
@@ -1,7 +1,40 @@
 import { Adapter } from '@sveltejs/kit';
 import './ambient.js';
 
-export default function plugin(): Adapter;
+export default function plugin(options?: AdapterOptions): Adapter;
+
+export interface AdapterOptions {
+	/**
+	 * Customize the automatically-generated _routes.json file.
+	 */
+	routes?: {
+		/**
+		 * Should we automatically generate excludes for _routes.json
+		 * based on prerendered pages and static assets?
+		 *
+		 * @default true
+		 */
+		autoGenerate?: boolean,
+
+		/**
+		 * Routes that will be invoked by Functions. Accepts wildcard behavior.
+		 *
+		 * If used with autoGenerate, these will be placed first.
+		 * If we reach the 100 limit, auto-generated includes will be removed first.
+		 */
+		include?: string[],
+
+		/**
+		 * Defines routes that will not be invoked by Functions. Accepts wildcard behavior.
+		 * Exclude always take priority over include.
+		 *
+		 * If used with autoGenerate, these will be placed first.
+		 * If we reach the 100 limit, auto-generated includes will be removed first.
+		 */
+		exclude?: string[]
+	}
+}
+
 
 export interface RoutesJSONSpec {
 	version: 1;
