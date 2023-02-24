c5d7f6b49e6df08ec69979cc19a3b573e98020be
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-24 19:30:03 +0100

    add tests

---
+++ packages/kit/src/core/sync/write_types/test/actions/+page.server.js
@@ -0,0 +1,35 @@
+import { fail } from '../../../../../../types/internal.js'
+
+let condition = false
+
+export const actions = {
+	default: () => {
+		if (condition) {
+			return fail(400, { fail: 'oops' })
+		}
+
+		return {
+			success: true,
+		}
+	}
+}
+
+/** @type {import('./.svelte-kit/types/src/core/sync/write_types/test/actions/$types').SubmitFunction} */
+const submit = () => {
+	return ({ result }) => {
+		if (result.type === 'success') {
+			result.data?.success === true
+			// @ts-expect-error should be of type `boolean`
+			result.data?.success === 'success'
+			// @ts-expect-error unknown property
+			result.data?.something
+		}
+		if (result.type === 'failure') {
+			result.data?.fail === ''
+			// @ts-expect-error should be of type `string`
+			result.data?.fail === 1
+			// @ts-expect-error unknown property
+			result.data?.unknown
+		}
+	}
+}
