2bb79de82786279ec525a7ace1787aaf8b029d43
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-02-26 21:16:34 -0800

    site: mention hackathon prize money

--- sites/kit.svelte.dev/src/routes/+layout.svelte
+++ sites/kit.svelte.dev/src/routes/+layout.svelte
@@ -70,7 +70,7 @@
 				<strong>Announcing SvelteHack</strong> Participate →
 			</span>
 			<span class="large">
-				<strong>Announcing SvelteHack</strong> Participate in our first hackathon and win →
+				<strong>Announcing SvelteHack</strong> Our first hackathon with over $12,000 in prizes →
 			</span>
 		</a>
 		<button on:click={() => (banner_height = '0px')}> ✕ </button>
