7a7b9f1783ee956c3e832ccfc65a28d6144d8e5a
Enrico Sacchetti <enrico@theetrain.ca>
2023-03-04 21:37:57 -0500

    chore: apply suggestions

--- packages/create-svelte/bin.js
+++ packages/create-svelte/bin.js
@@ -77,7 +77,7 @@ const options = await p.group(
 
 		features: () =>
 			p.multiselect({
-				message: 'Select additional options (use arrow keys to navigate and SPACE to toggle options)',
+				message: 'Select additional options (use arrow keys/space bar)',
 				required: false,
 				options: [
 					{
