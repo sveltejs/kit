4dff41b4f32298271797407de9335be3e44b2386
Richard Harris <richard.a.harris@gmail.com>
2023-03-06 15:40:02 -0500

    tidy up

--- packages/kit/src/runtime/server/page/render.js
+++ packages/kit/src/runtime/server/page/render.js
@@ -95,13 +95,9 @@ export async function render_response({
 
 	// if appropriate, use relative paths for greater portability
 	if (paths.relative !== false && !state.prerendering?.fallback) {
-		const segments = event.url.pathname.slice(paths.base.length).split('/');
+		const segments = event.url.pathname.slice(paths.base.length).split('/').slice(2);
 
-		base =
-			segments
-				.slice(2)
-				.map(() => '..')
-				.join('/') || '.';
+		base = segments.map(() => '..').join('/') || '.';
 
 		// resolve e.g. '../..' against current location, then remove trailing slash
 		base_expression = `new URL(${s(base)}, location).pathname.slice(0, -1)`;
