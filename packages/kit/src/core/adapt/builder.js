sveltejs/kit

commit c7648f6608a413e2654625cbd30f890d2148cf13

feat: route level config (#8740)
* route level config wip

* this seems to work

* proper config

* do shallow merge

* docs

* use config

* default config

* vercel docs

* fix tests

* appease TS

* oops

* more docs

* this feels clunky

* Route level config alt (#8863)

* Revert "this feels clunky"

This reverts commit b3b1b6ef89769d9d29369073b214b01ad6635a69.

* alternative approach to route-level config

* remove hasRouteLevelConfig

* handle data requests

* simplify

* fix types

* remove unused code

* fix site

* fix default runtime

* validate region, default to all for edge

* "all" not ["all"]

* changesets

* make defaultConfig a separate option key

* Update documentation/docs/25-build-and-deploy/90-adapter-vercel.md

* implement split: true

* tidy up

* fix site

* Update packages/adapter-vercel/index.d.ts

* tweaks

* get rid of top-level split option

* union type

* handle common case of one fn for everything separately to not pollute json for big apps

* tweak docs a little

* netlify

* make external a config option and simplify adapter options interface

* silence type union error

* use platform defaults

* include everything in builder.routes

* implement ISR

* fix some docs stuff

* clarify multi-region serverless is only for enterprise

* clarify memory stuff

* document ISR

* docs tweaks

* fix site

* add isr in config hash

* bump adapter-auto etc

* bump peerdeps

---------

Co-authored-by: Rich Harris <richard.a.harris@gmail.com>
Co-authored-by: Rich Harris <hello@rich-harris.dev>

packages/kit/src/core/adapt/builder.js
@@ -18,13 +18,54 @@ const pipe = promisify(pipeline);
  *   config: import('types').ValidatedConfig;
  *   build_data: import('types').BuildData;
  *   server_metadata: import('types').ServerMetadata;
- *   routes: import('types').RouteData[];
+ *   route_data: import('types').RouteData[];
  *   prerendered: import('types').Prerendered;
+ *   prerender_map: import('types').PrerenderMap;
  *   log: import('types').Logger;
  * }} opts
  * @returns {import('types').Builder}
  */
-export function create_builder({ config, build_data, server_metadata, routes, prerendered, log }) {
+export function create_builder({
+	config,
+	build_data,
+	server_metadata,
+	route_data,
+	prerendered,
+	prerender_map,
+	log
+}) {
+	/** @type {Map<import('types').RouteDefinition, import('types').RouteData>} */
+	const lookup = new Map();
+
+	/**
+	 * Rather than exposing the internal `RouteData` type, which is subject to change,
+	 * we expose a stable type that adapters can use to group/filter routes
+	 */
+	const routes = route_data.map((route) => {
+		const methods =
+			/** @type {import('types').HttpMethod[]} */
+			(server_metadata.routes.get(route.id)?.methods);
+		const config = server_metadata.routes.get(route.id)?.config;
+
+		/** @type {import('types').RouteDefinition} */
+		const facade = {
+			id: route.id,
+			segments: get_route_segments(route.id).map((segment) => ({
+				dynamic: segment.includes('['),
+				rest: segment.includes('[...'),
+				content: segment
+			})),
+			pattern: route.pattern,
+			prerender: prerender_map.get(route.id) ?? false,
+			methods,
+			config
+		};
+
+		lookup.set(facade, route);
+
+		return facade;
+	});
+
 	return {
 		log,
 		rimraf,
@@ -33,6 +74,7 @@ export function create_builder({ config, build_data, server_metadata, routes, pr
 
 		config,
 		prerendered,
+		routes,
 
 		async compress(directory) {
 			if (!existsSync(directory)) {
@@ -52,29 +94,12 @@ export function create_builder({ config, build_data, server_metadata, routes, pr
 		},
 
 		async createEntries(fn) {
-			/** @type {import('types').RouteDefinition[]} */
-			const facades = routes.map((route) => {
-				const methods =
-					/** @type {import('types').HttpMethod[]} */
-					(server_metadata.routes.get(route.id)?.methods);
-
-				return {
-					id: route.id,
-					segments: get_route_segments(route.id).map((segment) => ({
-						dynamic: segment.includes('['),
-						rest: segment.includes('[...'),
-						content: segment
-					})),
-					pattern: route.pattern,
-					methods
-				};
-			});
-
 			const seen = new Set();
 
-			for (let i = 0; i < routes.length; i += 1) {
-				const route = routes[i];
-				const { id, filter, complete } = fn(facades[i]);
+			for (let i = 0; i < route_data.length; i += 1) {
+				const route = route_data[i];
+				if (prerender_map.get(route.id) === true) continue;
+				const { id, filter, complete } = fn(routes[i]);
 
 				if (seen.has(id)) continue;
 				seen.add(id);
@@ -82,9 +107,10 @@ export function create_builder({ config, build_data, server_metadata, routes, pr
 				const group = [route];
 
 				// figure out which lower priority routes should be considered fallbacks
-				for (let j = i + 1; j < routes.length; j += 1) {
-					if (filter(facades[j])) {
-						group.push(routes[j]);
+				for (let j = i + 1; j < route_data.length; j += 1) {
+					if (prerender_map.get(routes[j].id) === true) continue;
+					if (filter(routes[j])) {
+						group.push(route_data[j]);
 					}
 				}
 
@@ -95,7 +121,7 @@ export function create_builder({ config, build_data, server_metadata, routes, pr
 				// TODO is this still necessary, given the new way of doing things?
 				filtered.forEach((route) => {
 					if (route.page) {
-						const endpoint = routes.find((candidate) => candidate.id === route.id + '.json');
+						const endpoint = route_data.find((candidate) => candidate.id === route.id + '.json');
 
 						if (endpoint) {
 							filtered.add(endpoint);
@@ -143,11 +169,13 @@ export function create_builder({ config, build_data, server_metadata, routes, pr
 			});
 		},
 
-		generateManifest: ({ relativePath }) => {
+		generateManifest: ({ relativePath, routes: subset }) => {
 			return generate_manifest({
 				build_data,
 				relative_path: relativePath,
-				routes
+				routes: subset
+					? subset.map((route) => /** @type {import('types').RouteData} */ (lookup.get(route)))
+					: route_data
 			});
 		},
 

packages/adapter-auto/adapters.js
@@ -5,7 +5,7 @@ export const adapters = [
 		name: 'Vercel',
 		test: () => !!process.env.VERCEL,
 		module: '@sveltejs/adapter-vercel',
-		version: '1'
+		version: '2'
 	},
 	{
 		name: 'Cloudflare Pages',
@@ -17,7 +17,7 @@ export const adapters = [
 		name: 'Netlify',
 		test: () => !!process.env.NETLIFY,
 		module: '@sveltejs/adapter-netlify',
-		version: '1'
+		version: '2'
 	},
 	{
 		name: 'Azure Static Web Apps',

packages/adapter-netlify/index.js
@@ -162,10 +162,17 @@ async function generate_lambda_functions({ builder, publish, split }) {
 	// Configuring the function to use ESM as the output format.
 	const fn_config = JSON.stringify({ config: { nodeModuleFormat: 'esm' }, version: 1 });
 
-	if (split) {
-		builder.log.minor('Generating serverless functions...');
+	builder.log.minor('Generating serverless functions...');
 
-		await builder.createEntries((route) => {
+	if (split) {
+		const seen = new Set();
+
+		for (let i = 0; i < builder.routes.length; i++) {
+			const route = builder.routes[i];
+			if (route.prerender === true) continue;
+
+			const routes = [route];
+
 			const parts = [];
 			// Netlify's syntax uses '*' and ':param' as "splats" and "placeholders"
 			// https://docs.netlify.com/routing/redirects/redirect-options/#splats
@@ -183,27 +190,33 @@ async function generate_lambda_functions({ builder, publish, split }) {
 			const pattern = `/${parts.join('/')}`;
 			const name = parts.join('-').replace(/[:.]/g, '_').replace('*', '__rest') || 'index';
 
-			return {
-				id: pattern,
-				filter: (other) => matches(route.segments, other.segments),
-				complete: (entry) => {
-					const manifest = entry.generateManifest({
-						relativePath: '../server'
-					});
+			// skip routes with identical patterns, they were already folded into another function
+			if (seen.has(pattern)) continue;
+			seen.add(pattern);
 
-					const fn = `import { init } from '../serverless.js';\n\nexport const handler = init(${manifest});\n`;
+			// figure out which lower priority routes should be considered fallbacks
+			for (let j = i + 1; j < builder.routes.length; j += 1) {
+				if (routes[j].prerender === true) continue;
 
-					writeFileSync(`.netlify/functions-internal/${name}.mjs`, fn);
-					writeFileSync(`.netlify/functions-internal/${name}.json`, fn_config);
-
-					redirects.push(`${pattern} /.netlify/functions/${name} 200`);
-					redirects.push(`${pattern}/__data.json /.netlify/functions/${name} 200`);
+				if (matches(route.segments, routes[j].segments)) {
+					routes.push(builder.routes[j]);
 				}
-			};
-		});
-	} else {
-		builder.log.minor('Generating serverless functions...');
+			}
 
+			const manifest = builder.generateManifest({
+				relativePath: '../server',
+				routes
+			});
+
+			const fn = `import { init } from '../serverless.js';\n\nexport const handler = init(${manifest});\n`;
+
+			writeFileSync(`.netlify/functions-internal/${name}.mjs`, fn);
+			writeFileSync(`.netlify/functions-internal/${name}.json`, fn_config);
+
+			redirects.push(`${pattern} /.netlify/functions/${name} 200`);
+			redirects.push(`${pattern}/__data.json /.netlify/functions/${name} 200`);
+		}
+	} else {
 		const manifest = builder.generateManifest({
 			relativePath: '../server'
 		});

packages/adapter-static/index.js
@@ -8,25 +8,9 @@ export default function (options) {
 
 		async adapt(builder) {
 			if (!options?.fallback) {
-				/** @type {string[]} */
-				const dynamic_routes = [];
-
-				// this is a bit of a hack — it allows us to know whether there are dynamic
-				// (i.e. prerender = false/'auto') routes without having dedicated API
-				// surface area for it
-				builder.createEntries((route) => {
-					dynamic_routes.push(route.id);
-
-					return {
-						id: '',
-						filter: () => false,
-						complete: () => {}
-					};
-				});
-
-				if (dynamic_routes.length > 0 && options?.strict !== false) {
+				if (builder.routes.some((route) => route.prerender !== true) && options?.strict !== false) {
 					const prefix = path.relative('.', builder.config.kit.files.routes);
-					const has_param_routes = dynamic_routes.some((route) => route.includes('['));
+					const has_param_routes = builder.routes.some((route) => route.id.includes('['));
 					const config_option =
 						has_param_routes || JSON.stringify(builder.config.kit.prerender.entries) !== '["*"]'
 							? `  - adjust the \`prerender.entries\` config option ${
@@ -38,7 +22,7 @@ export default function (options) {
 
 					builder.log.error(
 						`@sveltejs/adapter-static: all routes must be fully prerenderable, but found the following routes that are dynamic:
-${dynamic_routes.map((id) => `  - ${path.posix.join(prefix, id)}`).join('\n')}
+${builder.routes.map((route) => `  - ${path.posix.join(prefix, route.id)}`).join('\n')}
 
 You have the following options:
   - set the \`fallback\` option — see https://github.com/sveltejs/kit/tree/master/packages/adapter-static#spa-mode for more info.

packages/adapter-vercel/files/edge.js
@@ -3,7 +3,7 @@ import { manifest } from 'MANIFEST';
 
 const server = new Server(manifest);
 const initialized = server.init({
-	env: process.env
+	env: /** @type {Record<string, string>} */ (process.env)
 });
 
 /**
@@ -13,7 +13,7 @@ export default async (request) => {
 	await initialized;
 	return server.respond(request, {
 		getClientAddress() {
-			return request.headers.get('x-forwarded-for');
+			return /** @type {string} */ (request.headers.get('x-forwarded-for'));
 		}
 	});
 };

packages/adapter-vercel/files/serverless.js
@@ -8,7 +8,7 @@ installPolyfills();
 const server = new Server(manifest);
 
 await server.init({
-	env: process.env
+	env: /** @type {Record<string, string>} */ (process.env)
 });
 
 /**
@@ -22,7 +22,7 @@ export default async (req, res) => {
 	try {
 		request = await getRequest({ base: `https://${req.headers.host}`, request: req });
 	} catch (err) {
-		res.statusCode = err.status || 400;
+		res.statusCode = /** @type {any} */ (err).status || 400;
 		return res.end('Invalid request body');
 	}
 
@@ -30,7 +30,7 @@ export default async (req, res) => {
 		res,
 		await server.respond(request, {
 			getClientAddress() {
-				return request.headers.get('x-forwarded-for');
+				return /** @type {string} */ (request.headers.get('x-forwarded-for'));
 			}
 		})
 	);

packages/adapter-vercel/index.d.ts
@@ -1,9 +1,83 @@
 import { Adapter } from '@sveltejs/kit';
 
-type Options = {
-	edge?: boolean;
-	external?: string[];
-	split?: boolean;
-};
+export default function plugin(config?: Config): Adapter;
 
-export default function plugin(options?: Options): Adapter;
+export interface ServerlessConfig {
+	/**
+	 * Whether to use [Edge Functions](https://vercel.com/docs/concepts/functions/edge-functions) or [Serverless Functions](https://vercel.com/docs/concepts/functions/serverless-functions)
+	 * @default 'nodejs18.x'
+	 */
+	runtime?: 'nodejs16.x' | 'nodejs18.x';
+	/**
+	 * To which regions to deploy the app. A list of regions.
+	 * More info: https://vercel.com/docs/concepts/edge-network/regions
+	 */
+	regions?: string[];
+	/**
+	 * Maximum execution duration (in seconds) that will be allowed for the Serverless Function.
+	 * Serverless only.
+	 */
+	maxDuration?: number;
+	/**
+	 * Amount of memory (RAM in MB) that will be allocated to the Serverless Function.
+	 * Serverless only.
+	 */
+	memory?: number;
+	/**
+	 * If `true`, this route will always be deployed as its own separate function
+	 */
+	split?: boolean;
+	/**
+	 * [Incremental Static Regeneration](https://vercel.com/docs/concepts/incremental-static-regeneration/overview) configuration.
+	 * Serverless only.
+	 */
+	isr?: {
+		/**
+		 * Expiration time (in seconds) before the cached asset will be re-generated by invoking the Serverless Function. Setting the value to `false` means it will never expire.
+		 */
+		expiration?: number | false;
+		/**
+		 * Option group number of the asset. Assets with the same group number will all be re-validated at the same time.
+		 */
+		group?: number;
+		/**
+		 * Random token that can be provided in the URL to bypass the cached version of the asset, by requesting the asset
+		 * with a __prerender_bypass=<token> cookie.
+		 *
+		 * Making a `GET` or `HEAD` request with `x-prerender-revalidate: <token>` will force the asset to be re-validated.
+		 */
+		bypassToken?: string;
+		/**
+		 * List of query string parameter names that will be cached independently. If an empty array, query values are not considered for caching. If undefined each unique query value is cached independently
+		 */
+		allowQuery?: string[] | undefined;
+	};
+}
+
+export interface EdgeConfig {
+	/**
+	 * Whether to use [Edge Functions](https://vercel.com/docs/concepts/functions/edge-functions) or [Serverless Functions](https://vercel.com/docs/concepts/functions/serverless-functions)
+	 */
+	runtime?: 'edge';
+	/**
+	 * To which regions to deploy the app. A list of regions or `'all'`.
+	 * More info: https://vercel.com/docs/concepts/edge-network/regions
+	 */
+	regions?: string[] | 'all';
+	/**
+	 * List of environment variable names that will be available for the Edge Function to utilize.
+	 * Edge only.
+	 */
+	envVarsInUse?: string[];
+	/**
+	 * List of packages that should not be bundled into the Edge Function.
+	 * Edge only.
+	 */
+	external?: string[];
+	/**
+	 * If `true`, this route will always be deployed as its own separate function
+	 */
+	split?: boolean;
+}
+
+export type Config = EdgeConfig | ServerlessConfig;

packages/adapter-vercel/index.js
@@ -1,17 +1,31 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { nodeFileTrace } from '@vercel/nft';
 import esbuild from 'esbuild';
 
+const VALID_RUNTIMES = ['edge', 'nodejs16.x', 'nodejs18.x'];
+
+const get_default_runtime = () => {
+	const major = process.version.slice(1).split('.')[0];
+	if (major === '16') return 'nodejs16.x';
+	if (major === '18') return 'nodejs18.x';
+
+	throw new Error(
+		`Unsupported Node.js version: ${process.version}. Please use Node 16 or Node 18 to build your project, or explicitly specify a runtime in your adapter configuration.`
+	);
+};
+
 /** @type {import('.').default} **/
-const plugin = function ({ external = [], edge, split } = {}) {
+const plugin = function (defaults = {}) {
+	if ('edge' in defaults) {
+		throw new Error("{ edge: true } has been removed in favour of { runtime: 'edge' }");
+	}
+
 	return {
 		name: '@sveltejs/adapter-vercel',
 
 		async adapt(builder) {
-			const node_version = get_node_version();
-
 			const dir = '.vercel/output';
 			const tmp = builder.getBuildDirectory('vercel-tmp');
 
@@ -25,16 +39,16 @@ const plugin = function ({ external = [], edge, split } = {}) {
 				functions: `${dir}/functions`
 			};
 
-			const config = static_vercel_config(builder);
+			const static_config = static_vercel_config(builder);
 
 			builder.log.minor('Generating serverless function...');
 
 			/**
 			 * @param {string} name
-			 * @param {string} pattern
-			 * @param {(options: { relativePath: string }) => string} generate_manifest
+			 * @param {import('.').ServerlessConfig} config
+			 * @param {import('@sveltejs/kit').RouteDefinition<import('.').Config>[]} routes
 			 */
-			async function generate_serverless_function(name, pattern, generate_manifest) {
+			async function generate_serverless_function(name, config, routes) {
 				const relativePath = path.posix.relative(tmp, builder.getServerDirectory());
 
 				builder.copy(`${files}/serverless.js`, `${tmp}/index.js`, {
@@ -46,27 +60,48 @@ const plugin = function ({ external = [], edge, split } = {}) {
 
 				write(
 					`${tmp}/manifest.js`,
-					`export const manifest = ${generate_manifest({ relativePath })};\n`
+					`export const manifest = ${builder.generateManifest({ relativePath, routes })};\n`
 				);
 
 				await create_function_bundle(
 					builder,
 					`${tmp}/index.js`,
 					`${dirs.functions}/${name}.func`,
-					`nodejs${node_version.major}.x`
+					config
 				);
 
-				config.routes.push({ src: pattern, dest: `/${name}` });
+				if (config.isr) {
+					write(
+						`${dirs.functions}/${name}.prerender-config.json`,
+						JSON.stringify(
+							{
+								expiration: config.isr.expiration,
+								group: config.isr.group,
+								bypassToken: config.isr.bypassToken,
+								allowQuery: config.isr.allowQuery
+							},
+							null,
+							'\t'
+						)
+					);
+				}
 			}
 
 			/**
 			 * @param {string} name
-			 * @param {string} pattern
-			 * @param {(options: { relativePath: string }) => string} generate_manifest
+			 * @param {import('.').EdgeConfig} config
+			 * @param {import('@sveltejs/kit').RouteDefinition<import('.').EdgeConfig>[]} routes
 			 */
-			async function generate_edge_function(name, pattern, generate_manifest) {
+			async function generate_edge_function(name, config, routes) {
 				const tmp = builder.getBuildDirectory(`vercel-tmp/${name}`);
 				const relativePath = path.posix.relative(tmp, builder.getServerDirectory());
+
+				const envVarsInUse = new Set();
+				routes.forEach((route) => {
+					route.config?.envVarsInUse?.forEach((x) => {
+						envVarsInUse.add(x);
+					});
+				});
 
 				builder.copy(`${files}/edge.js`, `${tmp}/edge.js`, {
 					replace: {
@@ -77,7 +112,7 @@ const plugin = function ({ external = [], edge, split } = {}) {
 
 				write(
 					`${tmp}/manifest.js`,
-					`export const manifest = ${generate_manifest({ relativePath })};\n`
+					`export const manifest = ${builder.generateManifest({ relativePath, routes })};\n`
 				);
 
 				await esbuild.build({
@@ -87,51 +122,122 @@ const plugin = function ({ external = [], edge, split } = {}) {
 					bundle: true,
 					platform: 'browser',
 					format: 'esm',
-					external,
+					external: config.external,
 					sourcemap: 'linked',
 					banner: { js: 'globalThis.global = globalThis;' }
 				});
 
 				write(
 					`${dirs.functions}/${name}.func/.vc-config.json`,
-					JSON.stringify({
-						runtime: 'edge',
-						entrypoint: 'index.js'
-						// TODO expose envVarsInUse
-					})
+					JSON.stringify(
+						{
+							runtime: config.runtime,
+							regions: config.regions,
+							envVarsInUse: [...envVarsInUse],
+							entrypoint: 'index.js'
+						},
+						null,
+						'\t'
+					)
 				);
-
-				config.routes.push({ src: pattern, dest: `/${name}` });
 			}
 
-			const generate_function = edge ? generate_edge_function : generate_serverless_function;
+			/** @type {Map<string, { i: number, config: import('.').Config, routes: import('@sveltejs/kit').RouteDefinition<import('.').Config>[] }>} */
+			const groups = new Map();
 
-			if (split) {
-				await builder.createEntries((route) => {
-					return {
-						id: route.pattern.toString(), // TODO is `id` necessary?
-						filter: (other) => route.pattern.toString() === other.pattern.toString(),
-						complete: async (entry) => {
-							let sliced_pattern = route.pattern
-								.toString()
-								// remove leading / and trailing $/
-								.slice(1, -2)
-								// replace escaped \/ with /
-								.replace(/\\\//g, '/');
+			/** @type {Map<string, { hash: string, route_id: string }>} */
+			const conflicts = new Map();
 
-							// replace the root route "^/" with "^/?"
-							if (sliced_pattern === '^/') {
-								sliced_pattern = '^/?';
-							}
+			/** @type {Map<string, string>} */
+			const functions = new Map();
 
-							const src = `${sliced_pattern}(?:/__data.json)?$`; // TODO adding /__data.json is a temporary workaround — those endpoints should be treated as distinct routes
+			// group routes by config
+			for (const route of builder.routes) {
+				if (route.prerender === true) continue;
 
-							await generate_function(route.id.slice(1) || 'index', src, entry.generateManifest);
-						}
-					};
-				});
-			} else {
-				await generate_function('render', '/.*', builder.generateManifest);
+				const pattern = route.pattern.toString();
+
+				const runtime = route.config?.runtime ?? defaults?.runtime ?? get_default_runtime();
+				if (runtime && !VALID_RUNTIMES.includes(runtime)) {
+					throw new Error(
+						`Invalid runtime '${runtime}' for route ${
+							route.id
+						}. Valid runtimes are ${VALID_RUNTIMES.join(', ')}`
+					);
+				}
+
+				const config = { runtime, ...defaults, ...route.config };
+
+				const hash = hash_config(config);
+
+				// first, check there are no routes with incompatible configs that will be merged
+				const existing = conflicts.get(pattern);
+				if (existing) {
+					if (existing.hash !== hash) {
+						throw new Error(
+							`The ${route.id} and ${existing.route_id} routes must be merged into a single function that matches the ${route.pattern} regex, but they have incompatible configs. You must either rename one of the routes, or make their configs match.`
+						);
+					}
+				} else {
+					conflicts.set(pattern, { hash, route_id: route.id });
+				}
+
+				// then, create a group for each config
+				const id = config.split ? `${hash}-${groups.size}` : hash;
+				let group = groups.get(id);
+				if (!group) {
+					group = { i: groups.size, config, routes: [] };
+					groups.set(id, group);
+				}
+
+				group.routes.push(route);
+			}
+
+			for (const group of groups.values()) {
+				const generate_function =
+					group.config.runtime === 'edge' ? generate_edge_function : generate_serverless_function;
+
+				// generate one function for the group
+				const name = `fn-${group.i}`;
+				await generate_function(
+					name,
+					/** @type {any} */ (group.config),
+					/** @type {import('@sveltejs/kit').RouteDefinition<any>[]} */ (group.routes)
+				);
+
+				if (groups.size === 1) {
+					// Special case: One function for all routes
+					static_config.routes.push({ src: '/.*', dest: `/${name}` });
+				} else {
+					for (const route of group.routes) {
+						functions.set(route.pattern.toString(), name);
+					}
+				}
+			}
+
+			for (const route of builder.routes) {
+				if (route.prerender === true) continue;
+
+				const pattern = route.pattern.toString();
+
+				let src = pattern
+					// remove leading / and trailing $/
+					.slice(1, -2)
+					// replace escaped \/ with /
+					.replace(/\\\//g, '/');
+
+				// replace the root route "^/" with "^/?"
+				if (src === '^/') {
+					src = '^/?';
+				}
+
+				src += '(?:/__data.json)?$';
+
+				const name = functions.get(pattern);
+				if (name) {
+					static_config.routes.push({ src, dest: `/${name}` });
+					functions.delete(pattern);
+				}
 			}
 
 			builder.log.minor('Copying assets...');
@@ -141,10 +247,25 @@ const plugin = function ({ external = [], edge, split } = {}) {
 
 			builder.log.minor('Writing routes...');
 
-			write(`${dir}/config.json`, JSON.stringify(config, null, '  '));
+			write(`${dir}/config.json`, JSON.stringify(static_config, null, '\t'));
 		}
 	};
 };
+
+/** @param {import('.').EdgeConfig & import('.').ServerlessConfig} config */
+function hash_config(config) {
+	return [
+		config.runtime ?? '',
+		config.external ?? '',
+		config.regions ?? '',
+		config.memory ?? '',
+		config.maxDuration ?? '',
+		config.isr?.expiration ?? '',
+		config.isr?.group ?? '',
+		config.isr?.bypassToken ?? '',
+		config.isr?.allowQuery ?? ''
+	].join('/');
+}
 
 /**
  * @param {string} file
@@ -158,19 +279,6 @@ function write(file, data) {
 	}
 
 	fs.writeFileSync(file, data);
-}
-
-function get_node_version() {
-	const full = process.version.slice(1); // 'v16.5.0' --> '16.5.0'
-	const major = parseInt(full.split('.')[0]); // '16.5.0' --> 16
-
-	if (major < 16) {
-		throw new Error(
-			`SvelteKit only supports Node.js version 16 or greater (currently using v${full}). Consult the documentation: https://vercel.com/docs/runtimes#official-runtimes/node-js/node-js-version`
-		);
-	}
-
-	return { major, full };
 }
 
 // This function is duplicated in adapter-static
@@ -235,9 +343,9 @@ function static_vercel_config(builder) {
  * @param {import('@sveltejs/kit').Builder} builder
  * @param {string} entry
  * @param {string} dir
- * @param {string} runtime
+ * @param {import('.').ServerlessConfig} config
  */
-async function create_function_bundle(builder, entry, dir, runtime) {
+async function create_function_bundle(builder, entry, dir, config) {
 	fs.rmSync(dir, { force: true, recursive: true });
 
 	let base = entry;
@@ -265,7 +373,7 @@ async function create_function_bundle(builder, entry, dir, runtime) {
 				resolution_failures.set(importer, []);
 			}
 
-			resolution_failures.get(importer).push(module);
+			/** @type {string[]} */ (resolution_failures.get(importer)).push(module);
 		} else {
 			throw error;
 		}
@@ -286,7 +394,8 @@ async function create_function_bundle(builder, entry, dir, runtime) {
 	}
 
 	// find common ancestor directory
-	let common_parts;
+	/** @type {string[]} */
+	let common_parts = [];
 
 	for (const file of traced.fileList) {
 		if (common_parts) {
@@ -330,11 +439,18 @@ async function create_function_bundle(builder, entry, dir, runtime) {
 
 	write(
 		`${dir}/.vc-config.json`,
-		JSON.stringify({
-			runtime,
-			handler: path.relative(base + ancestor, entry),
-			launcherType: 'Nodejs'
-		})
+		JSON.stringify(
+			{
+				runtime: config.runtime,
+				regions: config.regions,
+				memory: config.memory,
+				maxDuration: config.maxDuration,
+				handler: path.relative(base + ancestor, entry),
+				launcherType: 'Nodejs'
+			},
+			null,
+			'\t'
+		)
 	);
 
 	write(`${dir}/package.json`, JSON.stringify({ type: 'module' }));

commit cc64efe077529094bc928fe3a34a934888493f94

fix: don't include prerendered routes in default generateManifest
fixes #9463
bug was introduced in #8740

packages/kit/src/core/adapt/builder.js
@@ -162,7 +162,7 @@ export function create_builder({
 				relative_path: relativePath,
 				routes: subset
 					? subset.map((route) => /** @type {import('types').RouteData} */ (lookup.get(route)))
-					: route_data
+					: route_data.filter((route) => prerender_map.get(route.id) !== true)
 			});
 		},
 