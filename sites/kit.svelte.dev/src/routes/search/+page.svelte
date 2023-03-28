0a86f2ad77fb4ef31f4c927761906441393befd8
Simon Holthausen <simon.holthausen@vercel.com>
2023-03-28 15:46:16 +0200

    fix type usage

--- sites/kit.svelte.dev/src/routes/search/+page.svelte
+++ sites/kit.svelte.dev/src/routes/search/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { SearchResults } from '@sveltejs/site-kit/search';
 
-	/** @type {import('./$types').Data} */
+	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
 
