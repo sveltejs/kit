1b2e1e07171a4a8e9f56e6970f7de75b548fe482
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2023-04-03 10:12:11 -0600

    Revert "feat: Typings for page routes"
    This reverts commit 05c7d4e1686ccd8b522c48dbda6cf380a3337297.

--- packages/kit/src/core/sync/write_types/index.js
+++ packages/kit/src/core/sync/write_types/index.js
@@ -442,15 +442,6 @@ function process_node(node, outdir, is_page, proxies, all_pages_have_load = true
 
 	exports.push(`export type ${prefix}Data = ${data};`);
 
-	if (
-		proxies?.server?.exports.includes('entries') ||
-		proxies?.universal?.exports.includes('entries')
-	) {
-		exports.push(
-			`export type EntryGenerator = () => Promise<Array<RouteParams>> | Array<RouteParams>;`
-		);
-	}
-
 	return { declarations, exports, proxies };
 
 	/**
@@ -569,7 +560,7 @@ function replace_ext_with_js(file_path) {
  * @returns {Omit<NonNullable<Proxy>, 'file_name'> | null}
  */
 export function tweak_types(content, is_server) {
-	const names = new Set(is_server ? ['load', 'actions', 'entries'] : ['load', 'entries']);
+	const names = new Set(is_server ? ['load', 'actions'] : ['load']);
 
 	try {
 		let modified = false;
