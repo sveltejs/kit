sveltejs/kit

commit 5aa3315e44610ea7c527d2b5c6450590476fcb6c

feat: add `OPTIONS` server method (#8731)
* allow options server export

* disable vite cors for preview server

* add test

* add changeset

* add mention of OPTIONS in routing docs

* revert disabling vite cors

* add OPTIONS method to postbuild analyse

* docs

* docs

---------

Co-authored-by: Rich Harris <hello@rich-harris.dev>

packages/kit/src/runtime/server/utils.js
@@ -41,7 +41,7 @@ export function method_not_allowed(mod, method) {
 export function allowed_methods(mod) {
 	const allowed = [];
 
-	for (const method of ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
+	for (const method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']) {
 		if (method in mod) allowed.push(method);
 	}
 

packages/kit/test/apps/basics/test/server.test.js
@@ -163,6 +163,17 @@ test.describe('Endpoints', () => {
 		const response = await request.put('/endpoint-input/sha256', { data });
 		expect(await response.text()).toEqual(digest);
 	});
+
+	test('OPTIONS handler', async ({ request }) => {
+		const url = '/endpoint-output/options';
+
+		var response = await request.fetch(url, {
+			method: 'OPTIONS'
+		});
+
+		expect(response.status()).toBe(200);
+		expect(await response.text()).toBe('ok');
+	});
 });
 
 test.describe('Errors', () => {

packages/kit/src/exports/vite/build/utils.js
@@ -87,7 +87,7 @@ export function assets_base(config) {
 	return (config.paths.assets || config.paths.base || '.') + '/';
 }
 
-const method_names = new Set(['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'PATCH']);
+const method_names = new Set(['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS']);
 
 // If we'd written this in TypeScript, it could be easy...
 /**

packages/kit/src/core/postbuild/analyse.js
@@ -89,6 +89,7 @@ async function analyse({ manifest_path, env }) {
 			if (mod.PUT) methods.add('PUT');
 			if (mod.PATCH) methods.add('PATCH');
 			if (mod.DELETE) methods.add('DELETE');
+			if (mod.OPTIONS) methods.add('OPTIONS');
 
 			config = mod.config;
 		}

packages/kit/src/exports/vite/index.js
@@ -240,7 +240,11 @@ function kit({ svelte_config }) {
 							// Ignore all siblings of config.kit.outDir/generated
 							`${posixify(kit.outDir)}/!(generated)`
 						]
-					}
+					},
+					cors: { preflightContinue: true }
+				},
+				preview: {
+					cors: { preflightContinue: true }
 				},
 				optimizeDeps: {
 					exclude: [

packages/kit/src/runtime/server/endpoint.js
@@ -75,7 +75,7 @@ export async function render_endpoint(event, route, mod, state) {
 export function is_endpoint_request(event) {
 	const { method, headers } = event.request;
 
-	if (method === 'PUT' || method === 'PATCH' || method === 'DELETE') {
+	if (method === 'PUT' || method === 'PATCH' || method === 'DELETE' || method === 'OPTIONS') {
 		// These methods exist exclusively for endpoints
 		return true;
 	}

packages/kit/src/utils/exports.js
@@ -51,6 +51,7 @@ export const validate_server_exports = validator([
 	'PATCH',
 	'PUT',
 	'DELETE',
+	'OPTIONS',
 	'prerender',
 	'trailingSlash',
 	'config'

packages/kit/src/utils/exports.spec.js
@@ -52,7 +52,7 @@ test('validates +server.js', () => {
 		validate_server_exports({
 			answer: 42
 		});
-	}, /Invalid export 'answer' \(valid exports are GET, POST, PATCH, PUT, DELETE, prerender, trailingSlash, config, or anything with a '_' prefix\)/);
+	}, /Invalid export 'answer' \(valid exports are GET, POST, PATCH, PUT, DELETE, OPTIONS, prerender, trailingSlash, config, or anything with a '_' prefix\)/);
 });
 
 test.run();

commit 208877d854c8ebcd06f253f90d2a34e3e31e930f

fix: allow header methods list for 405s (#9655)
see #8967 for explanation

regressed in #8731, probably because that PR started from an older branch

packages/kit/src/runtime/server/utils.js
@@ -34,13 +34,11 @@ export function method_not_allowed(mod, method) {
 
 /** @param {Partial<Record<import('types').HttpMethod, any>>} mod */
 export function allowed_methods(mod) {
-	const allowed = [];
+	const allowed = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'].filter(
+		(method) => method in mod
+	);
 
-	for (const method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']) {
-		if (method in mod) allowed.push(method);
-	}
-
-	if (mod.GET || mod.HEAD) allowed.push('HEAD');
+	if ('GET' in mod || 'HEAD' in mod) allowed.push('HEAD');
 
 	return allowed;
 }

packages/kit/test/apps/basics/test/server.test.js
@@ -125,7 +125,10 @@ test.describe('Endpoints', () => {
 		const response = await request.post('/endpoint-output/body');
 
 		expect(response.status()).toBe(405);
-		expect(response.headers()['allow'].includes('GET'));
+
+		const allow_header = response.headers()['allow'];
+		expect(allow_header).toMatch(/\bGET\b/);
+		expect(allow_header).toMatch(/\bHEAD\b/);
 	});
 
 	// TODO all the remaining tests in this section are really only testing