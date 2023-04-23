6e2f2066e68e34b57f3dd348db950afe6b292982
Yuichiro Yamashita <xydybaseball@gmail.com>
2023-04-23 15:03:38 +0900

    Update packages/create-svelte/shared/+eslint+prettier+typescript/.eslintrc.cjs
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>

--- packages/create-svelte/shared/+eslint+prettier+typescript/.eslintrc.cjs
+++ packages/create-svelte/shared/+eslint+prettier+typescript/.eslintrc.cjs
@@ -24,10 +24,7 @@ module.exports = {
 			files: ['*.svelte'],
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
-				parser: {
-					ts: '@typescript-eslint/parser',
-					js: 'espree'
-				}
+				parser: '@typescript-eslint/parser'
 			}
 		}
 	]
