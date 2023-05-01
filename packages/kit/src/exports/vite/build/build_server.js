4db5d56f449051f3a3aac87224533e0fd57ee87d
gtmnayan <gtmnayan@gmail.com>
2023-05-01 08:31:50 +0545

    dedupe deps

--- packages/kit/src/exports/vite/build/build_server.js
+++ packages/kit/src/exports/vite/build/build_server.js
@@ -49,18 +49,6 @@ export function build_server_nodes(out, kit, manifest_data, server_manifest, cli
 		const fonts = [];
 
 		if (node.component && client_manifest) {
-			const entry = find_deps(
-				client_manifest,
-				// client_manifest will be null for the call with the non-optimized nodes
-				// so it's fine to just use this path
-				`${normalizePath(kit.outDir)}/generated/client-optimized/nodes/${i}.js`,
-				true
-			);
-
-			imported.push(...entry.imports);
-			stylesheets.push(...entry.stylesheets);
-			fonts.push(...entry.fonts);
-
 			exports.push(
 				`export const component = async () => (await import('../${
 					resolve_symlinks(server_manifest, node.component).chunk.file
@@ -69,18 +57,6 @@ export function build_server_nodes(out, kit, manifest_data, server_manifest, cli
 		}
 
 		if (node.universal) {
-			if (client_manifest) {
-				const entry = find_deps(
-					client_manifest,
-					`${normalizePath(kit.outDir)}/generated/client-optimized/nodes/${i}.js`,
-					true
-				);
-
-				imported.push(...entry.imports);
-				stylesheets.push(...entry.stylesheets);
-				fonts.push(...entry.fonts);
-			}
-
 			imports.push(`import * as universal from '../${server_manifest[node.universal].file}';`);
 			exports.push(`export { universal };`);
 			exports.push(`export const universal_id = ${s(node.universal)};`);
@@ -90,6 +66,18 @@ export function build_server_nodes(out, kit, manifest_data, server_manifest, cli
 			imports.push(`import * as server from '../${server_manifest[node.server].file}';`);
 			exports.push(`export { server };`);
 			exports.push(`export const server_id = ${s(node.server)};`);
+		}
+
+		if (client_manifest && (node.universal || node.component)) {
+			const entry = find_deps(
+				client_manifest,
+				`${normalizePath(kit.outDir)}/generated/client-optimized/nodes/${i}.js`,
+				true
+			);
+
+			imported.push(...entry.imports);
+			stylesheets.push(...entry.stylesheets);
+			fonts.push(...entry.fonts);
 		}
 
 		exports.push(
