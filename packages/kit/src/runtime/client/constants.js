02579237050f514497cf5f3bca3564ed4e52e511
Rich Harris <git@rich-harris.dev>
2023-05-04 10:09:10 -0400

    undo temporary change

--- packages/kit/src/runtime/client/constants.js
+++ packages/kit/src/runtime/client/constants.js
@@ -1,9 +1,7 @@
 export const SNAPSHOT_KEY = 'sveltekit:snapshot';
 export const SCROLL_KEY = 'sveltekit:scroll';
 export const STATES_KEY = 'sveltekit:states';
-
-export const HISTORY_INDEX = 'sveltekit:history';
-export const NAVIGATION_INDEX = 'sveltekit:navigation';
+export const INDEX_KEY = 'sveltekit:index';
 
 export const PRELOAD_PRIORITIES = /** @type {const} */ ({
 	tap: 1,
