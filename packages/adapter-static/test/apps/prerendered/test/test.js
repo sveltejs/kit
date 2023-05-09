a95eaa05f4d370920d469b5aa74f263f98dc62c2
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-05-09 09:59:33 -0700

    chore: use playwright test runner for `adapter-static` tests (#9873)
    * chore: use playwright test runner for adapter-static tests
    
    * Update packages/adapter-static/test/apps/prerendered/test/test.js
    
    Co-authored-by: gtmnayan <50981692+gtm-nayan@users.noreply.github.com>
    
    * simplify setup
    
    ---------
    
    Co-authored-by: gtmnayan <50981692+gtm-nayan@users.noreply.github.com>

---
+++ packages/adapter-static/test/apps/prerendered/test/test.js
@@ -0,0 +1,27 @@
+import * as fs from 'node:fs';
+import { expect, test } from '@playwright/test';
+
+const cwd = process.cwd();
+
+test('generates HTML files', () => {
+	expect(fs.existsSync(`${cwd}/build/index.html`)).toBeTruthy();
+});
+
+test('prerenders a page', async ({ page }) => {
+	await page.goto('/');
+	expect(await page.textContent('h1')).toEqual('This page was prerendered');
+	expect(await page.textContent('p')).toEqual('answer: 42');
+});
+
+test('prerenders an unreferenced endpoint with explicit `prerender` setting', async () => {
+	expect(fs.existsSync(`${cwd}/build/endpoint/explicit.json`)).toBeTruthy();
+});
+
+test('prerenders a referenced endpoint with implicit `prerender` setting', async () => {
+	expect(fs.existsSync(`${cwd}/build/endpoint/implicit.json`)).toBeTruthy();
+});
+
+test('exposes public env vars to the client', async ({ page }) => {
+	await page.goto('/public-env');
+	expect(await page.textContent('h1')).toEqual('The answer is 42');
+});
