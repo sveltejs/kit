a34be844f9cb98e74e474add014b799ba04bfe60
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-05-09 08:22:07 -0700

    simplify setup

--- packages/adapter-static/test/utils.js
+++ packages/adapter-static/test/utils.js
@@ -1,23 +1,4 @@
-import { test as base, devices } from '@playwright/test';
-
-const known_devices = {
-	chromium: devices['Desktop Chrome'],
-	firefox: devices['Desktop Firefox'],
-	webkit: devices['Desktop Safari']
-};
-const test_browser = /** @type {keyof typeof known_devices} */ (
-	process.env.KIT_E2E_BROWSER ?? 'chromium'
-);
-
-const test_browser_device = known_devices[test_browser];
-
-if (!test_browser_device) {
-	throw new Error(
-		`invalid test browser specified: KIT_E2E_BROWSER=${
-			process.env.KIT_E2E_BROWSER
-		}. Allowed values: ${Object.keys(known_devices).join(', ')}`
-	);
-}
+import { devices } from '@playwright/test';
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 export const config = {
@@ -31,11 +12,11 @@ export const config = {
 	retries: process.env.CI ? 2 : 0,
 	projects: [
 		{
-			name: `${test_browser}`
+			name: 'chromium'
 		}
 	],
 	use: {
-		...test_browser_device,
+		...devices['Desktop Chrome'],
 		screenshot: 'only-on-failure',
 		trace: 'retain-on-failure'
 	},
