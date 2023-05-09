a80de8cad7311673be258e36ae65d67987f217b9
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-05-09 12:23:26 -0700

    format

--- packages/kit/test/build-errors/prerender.spec.js
+++ packages/kit/test/build-errors/prerender.spec.js
@@ -23,7 +23,9 @@ test('entry generators should match their own route', () => {
 				stdio: 'pipe',
 				timeout: 60000
 			}),
-		new Error('The entries export from /[slug]/[notSpecific] generated entry /whatever/specific, which was matched by /[slug]/specific - see the `handleEntryGeneratorMismatch` option in https://kit.svelte.dev/docs/configuration#prerender for more info.')
+		new Error(
+			'The entries export from /[slug]/[notSpecific] generated entry /whatever/specific, which was matched by /[slug]/specific - see the `handleEntryGeneratorMismatch` option in https://kit.svelte.dev/docs/configuration#prerender for more info.'
+		)
 	);
 });
 
