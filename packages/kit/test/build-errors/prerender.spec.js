84c4b53a35fbb2e636879335b02c95d401f65f1e
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-05-09 12:37:53 -0700

    chore: fix another test that wasn't actually running (#9886)

--- packages/kit/test/build-errors/prerender.spec.js
+++ packages/kit/test/build-errors/prerender.spec.js
@@ -19,11 +19,13 @@ test('entry generators should match their own route', () => {
 	assert.throws(
 		() =>
 			execSync('pnpm build', {
-				cwd: path.join(process.cwd(), 'apps/entry-generator-mismatch'),
+				cwd: path.join(process.cwd(), 'apps/preprender-entry-generator-mismatch'),
 				stdio: 'pipe',
 				timeout: 60000
 			}),
-		`The entries export from /[slug]/[notSpecific] generated entry /whatever/specific, which was matched by /[slug]/specific - see the \`handleEntryGeneratorMismatch\` option in https://kit.svelte.dev/docs/configuration#prerender for more info.`
+		new Error(
+			'The entries export from /[slug]/[notSpecific] generated entry /whatever/specific, which was matched by /[slug]/specific - see the `handleEntryGeneratorMismatch` option in https://kit.svelte.dev/docs/configuration#prerender for more info.'
+		)
 	);
 });
 
