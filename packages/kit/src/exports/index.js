b99894b6b86f68ef91bf9cbad06e9b6f83322729
Simon H <5968653+dummdidumm@users.noreply.github.com>
2023-05-11 10:40:26 +0200

    Update packages/kit/src/exports/index.js

--- packages/kit/src/exports/index.js
+++ packages/kit/src/exports/index.js
@@ -94,7 +94,7 @@ export function text(body, init) {
 
 /**
  * Create an `ActionFailure` object.
- * @template {Record<string, any> | undefined} T
+ * @template {Record<string, any> | undefined} [T=undefined]
  * @param {number} status The [HTTP status code](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses). Must be in the range 400-599.
  * @param {T} [data] Data associated with the failure (e.g. validation errors)
  * @returns {ActionFailure<T>}
