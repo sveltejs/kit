1c4951552efecd2ecbcf8a85d3ed3ea29f89cf79
gtmnayan <gtmnayan@gmail.com>
2023-05-15 07:48:42 +0545

    fix: use transformRequest for CSS modules

--- packages/kit/src/exports/vite/dev/index.js
+++ packages/kit/src/exports/vite/dev/index.js
@@ -176,21 +176,33 @@ export async function dev(vite, vite_config, svelte_config) {
 							const styles = {};
 
 							for (const dep of deps) {
-								const url = new URL(dep.url, 'http://localhost/');
+								const url = new URL(dep.url, 'dummy:/');
 								const query = url.searchParams;
 
 								if (
 									isCSSRequest(dep.file) ||
 									(query.has('svelte') && query.get('type') === 'style')
 								) {
-									// setting `?inline` to load CSS modules as css string
-									query.set('inline', '');
-
 									try {
-										const mod = await loud_ssr_load_module(
-											`${url.pathname}${url.search}${url.hash}`
-										);
-										styles[dep.url] = mod.default;
+										if (query.has('svelte')) {
+											// Hashes are different when using transformRequest
+											// which causes FOUC
+											const mod = await loud_ssr_load_module(
+												`${url.pathname}${url.search}${url.hash}`
+											);
+											styles[dep.url] = mod.default;
+										} else {
+											// Pretend it's a client request so that it gets what the client would get
+											query.set('direct', '')
+
+											const result = await vite.transformRequest(
+												`${url.pathname}${url.search}${url.hash}`
+											);
+
+											if (result?.code) {
+												styles[dep.url] = result.code;
+											}
+										}
 									} catch {
 										// this can happen with dynamically imported modules, I think
 										// because the Vite module graph doesn't distinguish between
