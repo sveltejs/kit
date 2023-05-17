8973f3052ab3682529f16be1d609a61d16da089b
Rich Harris <git@rich-harris.dev>
2023-05-17 11:19:31 -0400

    remove expect-error

--- packages/kit/test/apps/basics/src/routes/load/cache-control/default/+page.svelte
+++ packages/kit/test/apps/basics/src/routes/load/cache-control/default/+page.svelte
@@ -5,11 +5,9 @@
 	export let data;
 
 	async function update() {
-		// @ts-expect-error
 		window.update_state += 1;
 		await fetch('/load/cache-control/default/increment');
 		await invalidate('/load/cache-control/default/count');
-		// @ts-expect-error
 		window.update_state += 1;
 	}
 </script>
