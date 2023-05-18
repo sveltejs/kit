a4155c865d6408e4a2f2e9f51458e42b73a2ec01
Ivan Hofer <ivan.hofer@outlook.com>
2023-05-18 12:13:09 +0200

    auto update version in CI

--- packages/kit/scripts/update_version.js
+++ packages/kit/scripts/update_version.js
@@ -1,7 +1,15 @@
-import { readFile, writeFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-const pkg = JSON.parse(await readFile("./package.json", { encoding: "utf-8" }));
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const pathToPackageJson = resolve(__dirname, '..', 'package.json')
+const pathToVersionModule = resolve(__dirname, '..', 'src', 'version.js')
 
-await writeFile('./src/version.js', `// This file get's auto-updated on each release. Please don't edit it manually.
+const pkg = JSON.parse(await readFile(pathToPackageJson, { encoding: 'utf-8' }));
+
+await writeFile(
+	pathToVersionModule,
+	`// This file get's auto-updated on each release. Please don't edit it manually.
 export const VERSION = '${pkg.version}';
 `)
