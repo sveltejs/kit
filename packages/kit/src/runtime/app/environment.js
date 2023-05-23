2c4b8ab2232cd835f24408c6d760ce99aa680223
Rich Harris <git@rich-harris.dev>
2023-05-23 18:13:50 -0400

    juggle some stuff

--- packages/kit/src/runtime/app/environment.js
+++ packages/kit/src/runtime/app/environment.js
@@ -1,5 +1,5 @@
 import { BROWSER, DEV } from 'esm-env';
-import * as environment from '__sveltekit/environment';
+export { building, version } from '__sveltekit/environment';
 
 /**
  * `true` if the app is running in the browser.
@@ -10,13 +10,3 @@ export const browser = BROWSER;
  * Whether the dev server is running. This is not guaranteed to correspond to `NODE_ENV` or `MODE`.
  */
 export const dev = DEV;
-
-/**
- * SvelteKit analyses your app during the `build` step by running it. During this process, `building` is `true`. This also applies during prerendering.
- */
-export const building = environment.building;
-
-/**
- * The value of `config.kit.version.name`.
- */
-export const version = environment.version;
