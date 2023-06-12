0a48dd168a4ee4d0724c40abb5d683b87b1d9034
gtmnayan <gtmnayan@gmail.com>
2023-06-12 14:54:42 +0545

    fix: batch synchronous invalidate invocations

---
+++ packages/kit/test/apps/basics/src/routes/load/invalidation/multiple-batched/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { invalidate } from '$app/navigation';
+
+	export let data;
+</script>
+
+<div>
+	<button
+		id="multiple-batched"
+		on:click={(event) => {
+			invalidate('multiple:invalidations-go-brr');
+			invalidate('multiple:invalidations-go-brr');
+
+			Promise.resolve()
+				.then(() => invalidate('multiple:invalidations-go-brr'))
+				.then(() => {
+					event.currentTarget.dataset.done = 'true';
+				});
+		}}>{data.count}</button
+	>
+</div>
