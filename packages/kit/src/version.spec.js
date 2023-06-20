cc0f567751230c4085fd1cf3fe70330e6191bbae
Ivan Hofer <ivan.hofer@outlok.com>
2023-06-20 12:20:20 +0200

    import version after updating it

--- packages/kit/src/version.spec.js
+++ packages/kit/src/version.spec.js
@@ -1,13 +1,13 @@
 import { fileURLToPath } from 'node:url';
 import { readFileSync } from 'node:fs';
 import { assert, describe, it } from 'vitest';
-import { VERSION } from './version.js';
 
 // runs the version generation as a side-effect of importing
 import '../scripts/generate-version.js';
 
-describe('@sveltejs/kit VERSION', () => {
+describe('@sveltejs/kit VERSION', async () => {
 	it('should be the exact version from package.json');
+	const { VERSION } = await import('./version.js');
 	const pkg = JSON.parse(
 		readFileSync(fileURLToPath(new URL('../package.json', import.meta.url)), 'utf-8')
 	);
