882e75ff48c1c7431108960e7de291934ae5a216
gtmnayan <50981692+gtm-nayan@users.noreply.github.com>
2023-06-28 20:11:01 +0545

    perf: cache dynamic imports of nodes (#10080)
    it seems that Node spends a considerable amount of time inside its own module cache, so memoizing those ourselves is faster

--- packages/kit/src/exports/index.js
+++ packages/kit/src/exports/index.js
@@ -86,7 +86,12 @@ const encoder = new TextEncoder();
 export function text(body, init) {
 	const headers = new Headers(init?.headers);
 	if (!headers.has('content-length')) {
-		headers.set('content-length', encoder.encode(body).byteLength.toString());
+		const encoded = encoder.encode(body);
+		headers.set('content-length', encoded.byteLength.toString());
+		return new Response(encoded, {
+			...init,
+			headers
+		});
 	}
 
 	return new Response(body, {
