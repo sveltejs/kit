ef5d76df27bc4e70f6149d1167add48251c16fff
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2023-07-22 16:28:06 +0530

    Update sites/kit.svelte.dev/src/lib/server/docs/types.d.ts
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- sites/kit.svelte.dev/src/lib/server/docs/types.d.ts
+++ sites/kit.svelte.dev/src/lib/server/docs/types.d.ts
@@ -3,7 +3,7 @@ export type DocsData = Category[];
 export interface Section {
 	title: string;
 	slug: string;
-	// Currently, we are only going with 2 level headings, so this will be undefined. In future, we may want to support 3 levels, in which case this will be a list of sections
+	/** Currently, we are only going with 2 level headings, so this will be undefined. In future, we may want to support 3 levels, in which case this will be a list of sections */
 	sections?: Section[];
 }
 
