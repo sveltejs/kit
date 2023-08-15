ff87c06919f795e67905e4f471fda8f71d449155
Tee Ming <chewteeming01@gmail.com>
2023-08-16 05:22:33 +0800

    remove line break
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- packages/kit/src/core/sync/create_manifest_data/index.js
+++ packages/kit/src/core/sync/create_manifest_data/index.js
@@ -218,7 +218,7 @@ function create_routes_and_nodes(cwd, config, fallback) {
 							colors
 								.bold()
 								.yellow(
-									`Missing route file prefix. Did you mean +${file.name}?\n` +
+									`Missing route file prefix. Did you mean +${file.name}?` +
 										`  at ${path.join(dir, file.name)}`
 								)
 						);
