3807bcfecde2a3824f9d2787887be9435851ea9b
Tee Ming <chewteeming01@gmail.com>
2023-09-27 03:34:10 +0800

    use filter when copying client files

--- packages/kit/src/core/adapt/builder.js
+++ packages/kit/src/core/adapt/builder.js
@@ -181,9 +181,10 @@ export function create_builder({
 		},
 
 		writeClient(dest) {
-			const files = copy(`${config.kit.outDir}/output/client`, dest);
-			// avoid making vite files public
-			rimraf(`${dest}/.vite`);
+			const files = copy(`${config.kit.outDir}/output/client`, dest, {
+				// avoid making vite files public
+				filter: (basename) => basename === '.vite'
+			});
 			return filter_vite_files(files);
 		},
 
