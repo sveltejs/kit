475e256ed04c247f6ed766947d24b2e8b86399d3
Sai Srikar Dumpeti <80447788+the-r3aper7@users.noreply.github.com>
2023-10-05 01:23:19 +0530

    feat: make output reflect user's choice of package manager (#10811)
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- packages/create-svelte/utils.js
+++ packages/create-svelte/utils.js
@@ -49,3 +49,17 @@ export function copy(from, to, rename = identity) {
 export function dist(path) {
 	return fileURLToPath(new URL(`./dist/${path}`, import.meta.url).href);
 }
+
+export const package_manager = get_package_manager() || 'npm';
+
+// Thanks to https://github.com/zkochan/packages/tree/main/which-pm-runs for this code!
+function get_package_manager() {
+	if (!process.env.npm_config_user_agent) {
+		return undefined;
+	}
+	const user_agent = process.env.npm_config_user_agent;
+	const pm_spec = user_agent.split(' ')[0];
+	const separator_pos = pm_spec.lastIndexOf('/');
+	const name = pm_spec.substring(0, separator_pos);
+	return name === 'npminstall' ? 'cnpm' : name;
+}
