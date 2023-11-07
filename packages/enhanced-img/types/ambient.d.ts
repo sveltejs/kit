e810ee8a5a76c470ff5b41cc4fd65fa4433839d4
Simon Holthausen <simon.holthausen@vercel.com>
2023-11-07 15:12:05 +0100

    move image types into its package - move ambient declarations into its own file and make sure they are loaded when anything from the package is loaded by importing it in the root types file - move element augmentation into root types file. Needs to be there because it can't be in an ambient module, the augmentations wouldn't work in such a module - add fix to SvelteKit to ensure vite.config.js is also loaded, so that when someone uses svelte:image with a vite.config.js/ts its types are loaded

--- packages/enhanced-img/types/ambient.d.ts
+++ packages/enhanced-img/types/ambient.d.ts
@@ -1,4 +1,3 @@
-// TODO: get this from @sveltejs/enhanced-img
 declare module '*?enhanced' {
 	const value: string;
 	export default value;
--- sites/kit.svelte.dev/src/app.d.ts
+++ sites/kit.svelte.dev/src/app.d.ts
@@ -1,11 +1,3 @@
-// TODO: get this from @sveltejs/enhanced-img
-// https://svelte.dev/docs/typescript#enhancing-built-in-dom-types
-declare module 'svelte/elements' {
-	export interface SvelteHTMLElements {
-		'enhanced:img': HTMLImgAttributes;
-	}
-}
-
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
