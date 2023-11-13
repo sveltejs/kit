fc8a03dcf28b3cde898fdf46da679ba6234676a4
Tee Ming <chewteeming01@gmail.com>
2023-11-13 19:38:39 +0800

    fix eslint

--- packages/kit/src/core/postbuild/analyse.js
+++ packages/kit/src/core/postbuild/analyse.js
@@ -94,9 +94,9 @@ async function analyse({ manifest_path, env }) {
 			}
 
 			Object.keys(mod).forEach((key) => {
-				// @ts-ignore
-				if (mod[key] && ENDPOINT_METHODS.has(key)) {
-					api_methods.push(/** @type {import('types').HttpMethod} */ (key));
+				const method = /** @type {import('types').HttpMethod} */ (key);
+				if (mod[method] && ENDPOINT_METHODS.has(method)) {
+					api_methods.push(method);
 				} else if (mod.fallback && key === 'fallback') {
 					api_methods.push('*');
 				}
