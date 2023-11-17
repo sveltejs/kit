a6fe421779aedadb99f126bd3bd6bf26b9eab327
Simon Holthausen <simon.holthausen@vercel.com>
2023-11-17 20:51:03 +0100

    add loader options interface for the possibility of types LoaderOptions

--- packages/adapter-vercel/image-loader.d.ts
+++ packages/adapter-vercel/image-loader.d.ts
@@ -4,6 +4,28 @@
 export default function loader(
 	src: string,
 	width: number,
-	loader_options: { domain: string[] },
-	image_options?: { quality: number }
+	loader_options: { domains: string[] },
+	image_options?: { quality?: number }
 ): string;
+
+/**
+ * https://vercel.com/docs/concepts/image-optimization
+ */
+export interface LoaderOptions {
+	domains?: string[];
+	remotePatterns?: RemotePattern[];
+	minimumCacheTTL?: number;
+	formats?: ImageFormat[];
+	dangerouslyAllowSVG?: boolean;
+	contentSecurityPolicy?: string;
+	contentDispositionType?: string;
+}
+
+type ImageFormat = 'image/avif' | 'image/webp';
+
+type RemotePattern = {
+	protocol?: 'http' | 'https';
+	hostname: string;
+	port?: string;
+	pathname?: string;
+};
