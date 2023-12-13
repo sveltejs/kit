a132100fd605b7a22e77c2060ffe3c5dbf05dcb9
Rich Harris <rich.harris@vercel.com>
2023-12-12 19:35:29 -0500

    add test

--- packages/kit/test/apps/basics/playwright.config.js
+++ packages/kit/test/apps/basics/playwright.config.js
@@ -1,1 +1,11 @@
-export { config as default } from '../../utils.js';
+import { config } from '../../utils.js';
+
+export default {
+	...config,
+	webServer: {
+		command: process.env.DEV
+			? 'PUBLIC_PRERENDERING=false pnpm dev'
+			: 'PUBLIC_PRERENDERING=true pnpm build && pnpm preview',
+		port: process.env.DEV ? 5173 : 4173
+	}
+};
