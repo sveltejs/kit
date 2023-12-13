5b1d1ab3d33697016bcec41f2cd55d6473b4b24c
Tee Ming <chewteeming01@gmail.com>
2023-12-13 16:57:53 +0800

    fix: prevent incorrect trailing slash redirect for prerendered root page when `paths.base` is set (#10763)
    fixes #9982

---
+++ .changeset/rich-pandas-provide.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/kit': patch
+---
+
+fix: prerendered root page with `paths.base` config uses correct trailing slash option
--- packages/kit/src/exports/vite/preview/index.js
+++ packages/kit/src/exports/vite/preview/index.js
@@ -71,7 +71,19 @@ export async function preview(vite, vite_config, svelte_config) {
 
 		vite.middlewares.use((req, res, next) => {
 			const original_url = /** @type {string} */ (req.url);
-			const { pathname } = new URL(original_url, 'http://dummy');
+			const { pathname, search } = new URL(original_url, 'http://dummy');
+
+			// if `paths.base === '/a/b/c`, then the root route is `/a/b/c/`,
+			// regardless of the `trailingSlash` route option
+			if (base.length > 1 && pathname === base) {
+				let location = base + '/';
+				if (search) location += search;
+				res.writeHead(307, {
+					location
+				});
+				res.end();
+				return;
+			}
 
 			if (pathname.startsWith(base)) {
 				next();
