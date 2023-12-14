b3abca5f351893a39c5837fa46bd718c054c06ce
Rich Harris <rich.harris@vercel.com>
2023-12-13 22:21:37 -0500

    tests

---
+++ packages/kit/test/apps/basics/src/routes/shallow-routing/push-state/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { pushState } from '$app/navigation';
+	import { page } from '$app/stores';
+
+	function one() {
+		pushState('', { active: true });
+	}
+
+	function two() {
+		pushState('/shallow-routing/push-state/a', { active: true });
+	}
+</script>
+
+<h1>parent</h1>
+
+<button data-id="one" on:click={one}>push state on current page</button>
+<button data-id="two" on:click={two}>push state on child page</button>
+
+<p>active: {$page.state.active ?? false}</p>
