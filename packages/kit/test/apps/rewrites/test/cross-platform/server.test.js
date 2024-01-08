83727dde1dfb2c3fa8fc3eb5d3e2c6fef673f22b
Loris Sigrist <sigrist@hasch.io>
2024-01-08 11:30:53 +0100

    Remove empty test files

--- packages/kit/test/apps/rewrites/test/cross-platform/server.test.js
+++
@@ -1,7 +0,0 @@
-import { test } from '../../../../utils.js';
-
-/** @typedef {import('@playwright/test').Response} Response */
-
-test.skip(({ javaScriptEnabled }) => !!javaScriptEnabled);
-
-test.describe.configure({ mode: 'parallel' });
