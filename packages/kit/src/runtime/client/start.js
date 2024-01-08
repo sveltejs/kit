aabe33c4f6097503c7dabf8a5b9a8a566981da2a
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-01-09 00:57:31 +0100

    feat: make client router treeshakeable (#11340)
    * feat: make client router treeshakeable
    
    This removes the `create_client` method in `client.js` in favor of having everything as exported functions at the top level. Slight rearrangements to make some things lazy or put it behind a `BROWSER ?` condition are necessary, but otherwise the code is almost completely untouched. This makes it even less likely we can ever unit test the client router, but I think that ship has sailed a long time ago and e2e tests are a much more robust and an outright better way to test this, so it's a non-issue.
    
    * changeset
    
    * fix PURE annotation positions
    
    * POC for potential solution: force deduplication of modules by appending a query string
    
    * refine embedded approach
    
    use an increasing id -> things are still cached, but dynamically, and Vite plays ball, too
    
    * hacking my way to victory
    
    * tweak
    
    * move whole startup logic into create_client
    
    * these can be private functions now
    
    * fix
    
    * Update packages/kit/src/runtime/client/start_embedded.js
    
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>
    
    * chore: apply state to history.state directly (#11521)
    
    * chore: apply state to history.state directly
    
    The serialization capabilities of history.state are basically identical to those of devalue.stringify . As such, the indirection of saving the data in the session storage isn't necessary, which also saves 1kb compressed / 3kb uncompressed because of the no longer needed devalue functions
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Rich Harris <richard.a.harris@gmail.com>
    
    * Reduce indirection in treeshakeable client (#11544)
    
    * reduce indirection
    
    * fix
    
    * tweak error messages
    
    * sigh
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>
    
    * remove all the embedded hackery (#11545)
    
    * remove all the embedded hackery
    
    * ugh
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>
    
    * reduce diff size
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>
    Co-authored-by: Rich Harris <richard.a.harris@gmail.com>

--- packages/kit/src/runtime/client/start.js
+++ packages/kit/src/runtime/client/start.js
@@ -1,28 +1,17 @@
 import { DEV } from 'esm-env';
 import { create_client } from './client.js';
-import { init } from './singletons.js';
 
 /**
  * @param {import('./types.js').SvelteKitApp} app
  * @param {HTMLElement} target
- * @param {Parameters<import('./types.js').Client['_hydrate']>[0]} [hydrate]
+ * @param {any} [hydrate] (see _hydrate in client.js for the type)
  */
-export async function start(app, target, hydrate) {
+export function start(app, target, hydrate) {
 	if (DEV && target === document.body) {
 		console.warn(
 			'Placing %sveltekit.body% directly inside <body> is not recommended, as your app may break for users who have certain browser extensions installed.\n\nConsider wrapping it in an element:\n\n<div style="display: contents">\n  %sveltekit.body%\n</div>'
 		);
 	}
 
-	const client = create_client(app, target);
-
-	init({ client });
-
-	if (hydrate) {
-		await client._hydrate(hydrate);
-	} else {
-		client.goto(location.href, { replaceState: true });
-	}
-
-	client._start_router();
+	create_client(app, target, hydrate);
 }
--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -1935,10 +1935,38 @@ declare module '$app/forms' {
 
 declare module '$app/navigation' {
 	/**
+	 * A lifecycle function that runs the supplied `callback` when the current component mounts, and also whenever we navigate to a new URL.
+	 *
+	 * `afterNavigate` must be called during a component initialization. It remains active as long as the component is mounted.
+	 * */
+	export function afterNavigate(callback: (navigation: import('@sveltejs/kit').AfterNavigate) => void): void;
+	/**
+	 * A navigation interceptor that triggers before we navigate to a new URL, whether by clicking a link, calling `goto(...)`, or using the browser back/forward controls.
+	 *
+	 * Calling `cancel()` will prevent the navigation from completing. If `navigation.type === 'leave'` — meaning the user is navigating away from the app (or closing the tab) — calling `cancel` will trigger the native browser unload confirmation dialog. In this case, the navigation may or may not be cancelled depending on the user's response.
+	 *
+	 * When a navigation isn't to a SvelteKit-owned route (and therefore controlled by SvelteKit's client-side router), `navigation.to.route.id` will be `null`.
+	 *
+	 * If the navigation will (if not cancelled) cause the document to unload — in other words `'leave'` navigations and `'link'` navigations where `navigation.to.route === null` — `navigation.willUnload` is `true`.
+	 *
+	 * `beforeNavigate` must be called during a component initialization. It remains active as long as the component is mounted.
+	 * */
+	export function beforeNavigate(callback: (navigation: import('@sveltejs/kit').BeforeNavigate) => void): void;
+	/**
+	 * A lifecycle function that runs the supplied `callback` immediately before we navigate to a new URL except during full-page navigations.
+	 *
+	 * If you return a `Promise`, SvelteKit will wait for it to resolve before completing the navigation. This allows you to — for example — use `document.startViewTransition`. Avoid promises that are slow to resolve, since navigation will appear stalled to the user.
+	 *
+	 * If a function (or a `Promise` that resolves to a function) is returned from the callback, it will be called once the DOM has updated.
+	 *
+	 * `onNavigate` must be called during a component initialization. It remains active as long as the component is mounted.
+	 * */
+	export function onNavigate(callback: (navigation: import('@sveltejs/kit').OnNavigate) => MaybePromise<void>): void;
+	/**
 	 * If called when the page is being updated following a navigation (in `onMount` or `afterNavigate` or an action, for example), this disables SvelteKit's built-in scroll handling.
 	 * This is generally discouraged, since it breaks user expectations.
 	 * */
-	export const disableScrollHandling: () => void;
+	export function disableScrollHandling(): void;
 	/**
 	 * Returns a Promise that resolves when SvelteKit navigates (or fails to navigate, in which case the promise rejects) to the specified `url`.
 	 * For external URLs, use `window.location = url` instead of calling `goto(url)`.
@@ -1946,13 +1974,13 @@ declare module '$app/navigation' {
 	 * @param url Where to navigate to. Note that if you've set [`config.kit.paths.base`](https://kit.svelte.dev/docs/configuration#paths) and the URL is root-relative, you need to prepend the base path if you want to navigate within the app.
 	 * @param {Object} opts Options related to the navigation
 	 * */
-	export const goto: (url: string | URL, opts?: {
-		replaceState?: boolean;
-		noScroll?: boolean;
-		keepFocus?: boolean;
-		invalidateAll?: boolean;
-		state?: App.PageState;
-	}) => Promise<void>;
+	export function goto(url: string | URL, opts?: {
+		replaceState?: boolean | undefined;
+		noScroll?: boolean | undefined;
+		keepFocus?: boolean | undefined;
+		invalidateAll?: boolean | undefined;
+		state?: App.PageState | undefined;
+	} | undefined): Promise<void>;
 	/**
 	 * Causes any `load` functions belonging to the currently active page to re-run if they depend on the `url` in question, via `fetch` or `depends`. Returns a `Promise` that resolves when the page is subsequently updated.
 	 *
@@ -1968,13 +1996,13 @@ declare module '$app/navigation' {
 	 *
 	 * invalidate((url) => url.pathname === '/path');
 	 * ```
-	 * @param url The invalidated URL
+	 * @param resource The invalidated URL
 	 * */
-	export const invalidate: (url: string | URL | ((url: URL) => boolean)) => Promise<void>;
+	export function invalidate(resource: string | URL | ((url: URL) => boolean)): Promise<void>;
 	/**
 	 * Causes all `load` functions belonging to the currently active page to re-run. Returns a `Promise` that resolves when the page is subsequently updated.
 	 * */
-	export const invalidateAll: () => Promise<void>;
+	export function invalidateAll(): Promise<void>;
 	/**
 	 * Programmatically preloads the given page, which means
 	 *  1. ensuring that the code for the page is loaded, and
@@ -1986,7 +2014,14 @@ declare module '$app/navigation' {
 	 *
 	 * @param href Page to preload
 	 * */
-	export const preloadData: (href: string) => Promise<Record<string, any>>;
+	export function preloadData(href: string): Promise<{
+		type: 'loaded';
+		status: number;
+		data: Record<string, any>;
+	} | {
+		type: 'redirect';
+		location: string;
+	}>;
 	/**
 	 * Programmatically imports the code for routes that haven't yet been fetched.
 	 * Typically, you might call this to speed up subsequent navigation.
@@ -1997,45 +2032,17 @@ declare module '$app/navigation' {
 	 * Returns a Promise that resolves when the modules have been imported.
 	 *
 	 * */
-	export const preloadCode: (url: string) => Promise<void>;
-	/**
-	 * A navigation interceptor that triggers before we navigate to a new URL, whether by clicking a link, calling `goto(...)`, or using the browser back/forward controls.
-	 *
-	 * Calling `cancel()` will prevent the navigation from completing. If `navigation.type === 'leave'` — meaning the user is navigating away from the app (or closing the tab) — calling `cancel` will trigger the native browser unload confirmation dialog. In this case, the navigation may or may not be cancelled depending on the user's response.
-	 *
-	 * When a navigation isn't to a SvelteKit-owned route (and therefore controlled by SvelteKit's client-side router), `navigation.to.route.id` will be `null`.
-	 *
-	 * If the navigation will (if not cancelled) cause the document to unload — in other words `'leave'` navigations and `'link'` navigations where `navigation.to.route === null` — `navigation.willUnload` is `true`.
-	 *
-	 * `beforeNavigate` must be called during a component initialization. It remains active as long as the component is mounted.
-	 * */
-	export const beforeNavigate: (callback: (navigation: import('@sveltejs/kit').BeforeNavigate) => void) => void;
-	/**
-	 * A lifecycle function that runs the supplied `callback` immediately before we navigate to a new URL except during full-page navigations.
-	 *
-	 * If you return a `Promise`, SvelteKit will wait for it to resolve before completing the navigation. This allows you to — for example — use `document.startViewTransition`. Avoid promises that are slow to resolve, since navigation will appear stalled to the user.
-	 *
-	 * If a function (or a `Promise` that resolves to a function) is returned from the callback, it will be called once the DOM has updated.
-	 *
-	 * `onNavigate` must be called during a component initialization. It remains active as long as the component is mounted.
-	 * */
-	export const onNavigate: (callback: (navigation: import('@sveltejs/kit').OnNavigate) => MaybePromise<(() => void) | void>) => void;
-	/**
-	 * A lifecycle function that runs the supplied `callback` when the current component mounts, and also whenever we navigate to a new URL.
-	 *
-	 * `afterNavigate` must be called during a component initialization. It remains active as long as the component is mounted.
-	 * */
-	export const afterNavigate: (callback: (navigation: import('@sveltejs/kit').AfterNavigate) => void) => void;
+	export function preloadCode(pathname: string): Promise<void>;
 	/**
 	 * Programmatically create a new history entry with the given `$page.state`. To use the current URL, you can pass `''` as the first argument. Used for [shallow routing](https://kit.svelte.dev/docs/shallow-routing).
 	 *
 	 * */
-	export const pushState: (url: string | URL, state: App.PageState) => void;
+	export function pushState(url: string | URL, state: App.PageState): void;
 	/**
 	 * Programmatically replace the current history entry with the given `$page.state`. To use the current URL, you can pass `''` as the first argument. Used for [shallow routing](https://kit.svelte.dev/docs/shallow-routing).
 	 *
 	 * */
-	export const replaceState: (url: string | URL, state: App.PageState) => void;
+	export function replaceState(url: string | URL, state: App.PageState): void;
 	type MaybePromise<T> = T | Promise<T>;
 }
 
