689811e3b3912c53c9b1e9d3327b19809e105bd5
Rich Harris <hello@rich-harris.dev>
2024-01-10 11:56:05 -0500

    Update packages/kit/test/apps/basics/src/routes/reroute/error-handling/+page.svelte

--- packages/kit/test/apps/basics/src/routes/reroute/error-handling/+page.svelte
+++ packages/kit/test/apps/basics/src/routes/reroute/error-handling/+page.svelte
@@ -1,8 +1,2 @@
-<script>
-	import { setup } from '../../../../../../setup';
-
-	setup();
-</script>
-
 <a href="/reroute/error-handling/client-error" id="client-error">Url with client error</a>
 <a href="/reroute/error-handling/server-error" id="server-error">Url with server error</a>
