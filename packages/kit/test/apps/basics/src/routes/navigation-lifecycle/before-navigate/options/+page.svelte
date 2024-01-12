4d73d916a364b9784c75156a59fec328561386d3
Gautier Ben Aim <benaim.gautier@gmail.com>
2024-01-12 18:25:33 +0100

    sort

--- packages/kit/test/apps/basics/src/routes/navigation-lifecycle/before-navigate/options/+page.svelte
+++ packages/kit/test/apps/basics/src/routes/navigation-lifecycle/before-navigate/options/+page.svelte
@@ -12,9 +12,9 @@
 
 <p>
 	{type}
-	{options.replaceState}
 	{options.noScroll}
 	{options.keepFocus}
+	{options.replaceState}
 	{options.invalidateAll}
 	{JSON.stringify(options.state)}
 </p>
@@ -22,16 +22,16 @@
 <button
 	on:click={() =>
 		goto('/somewhere', {
-			invalidateAll: true,
 			noScroll: true,
 			keepFocus: true,
 			replaceState: true,
+			invalidateAll: true,
 			state: { active: true }
 		})}
 >
 	goto
 </button>
 
-<a href="/somewhere" data-sveltekit-keepfocus data-sveltekit-noscroll data-sveltekit-replacestate>
+<a href="/somewhere" data-sveltekit-noscroll data-sveltekit-keepfocus data-sveltekit-replacestate>
 	link
 </a>
