3b9bae63679c799848effe3621241ce499fbc656
Gautier Ben Aim <benaim.gautier@gmail.com>
2024-01-12 18:23:04 +0100

    added tests

---
+++ packages/kit/test/apps/basics/src/routes/navigation-lifecycle/before-navigate/options/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { beforeNavigate, goto } from '$app/navigation';
+
+	let type;
+	let options = {};
+	beforeNavigate((nav) => {
+		type = nav.type;
+		options = nav.options;
+		nav.cancel();
+	});
+</script>
+
+<p>
+	{type}
+	{options.replaceState}
+	{options.noScroll}
+	{options.keepFocus}
+	{options.invalidateAll}
+	{JSON.stringify(options.state)}
+</p>
+
+<button
+	on:click={() =>
+		goto('/somewhere', {
+			invalidateAll: true,
+			noScroll: true,
+			keepFocus: true,
+			replaceState: true,
+			state: { active: true }
+		})}
+>
+	goto
+</button>
+
+<a href="/somewhere" data-sveltekit-keepfocus data-sveltekit-noscroll data-sveltekit-replacestate>
+	link
+</a>
