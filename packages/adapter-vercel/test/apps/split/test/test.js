88b68009fe63b9e16c06ea6ab2eccaf525112db2
Tee Ming <chewteeming01@gmail.com>
2024-01-14 01:49:36 +0800

    fix tests?

--- packages/adapter-vercel/test/apps/split/test/test.js
+++ packages/adapter-vercel/test/apps/split/test/test.js
@@ -6,19 +6,23 @@ const vercel_fn_dir = `${cwd}/.vercel/output/functions`;
 const serverless_functions = ['fn-0', 'fn-1', 'fn-2', 'fn'];
 const assets_dir = '_app/immutable/assets';
 
-const server_assets = Object.fromEntries(
-	serverless_functions.map((fn) => [
-		fn,
-		fs.readdirSync(`${vercel_fn_dir}/${fn}.func/${assets_dir}`)
-	])
-);
+function scan_build_output() {
+	return Object.fromEntries(
+		serverless_functions.map((fn) => [
+			fn,
+			fs.readdirSync(`${vercel_fn_dir}/${fn}.func/${assets_dir}`)
+		])
+	);
+}
 
 test('includes server assets from page server load functions', () => {
+	const server_assets = scan_build_output();
 	const server_asset = server_assets['fn-2'].find((file) => file.startsWith('_page.server.js'));
 	expect(server_asset).toBeTruthy();
 });
 
 test('includes server assets from layout server load functions', () => {
+	const server_assets = scan_build_output();
 	for (const fn of serverless_functions) {
 		const server_asset = server_assets[fn].find((file) => file.startsWith('_layout.server.js'));
 		expect(server_asset).toBeTruthy();
@@ -26,11 +30,13 @@ test('includes server assets from layout server load functions', () => {
 });
 
 test('includes server assets from endpoints', () => {
+	const server_assets = scan_build_output();
 	const server_asset = server_assets['fn-1'].find((file) => file.startsWith('_server.js'));
 	expect(server_asset).toBeTruthy();
 });
 
 test('includes server assets from server hooks', () => {
+	const server_assets = scan_build_output();
 	for (const fn of serverless_functions) {
 		const server_asset = server_assets[fn].find((file) => file.startsWith('hooks.server.js'));
 		expect(server_asset).toBeTruthy();
@@ -38,11 +44,13 @@ test('includes server assets from server hooks', () => {
 });
 
 test('includes server assets from transitive imports', () => {
+	const server_assets = scan_build_output();
 	const server_asset = server_assets['fn-0'].find((file) => file.startsWith('transitive'));
 	expect(server_asset).toBeTruthy();
 });
 
 test('includes server assets of the default error page for every function', () => {
+	const server_assets = scan_build_output();
 	for (const fn of serverless_functions) {
 		const server_asset = server_assets[fn].find((file) => file.startsWith('_layout.server.js'));
 		expect(server_asset).toBeTruthy();
@@ -50,6 +58,8 @@ test('includes server assets of the default error page for every function', () =
 });
 
 test('only includes relevant server assets for split functions', () => {
+	const server_assets = scan_build_output();
+
 	for (const fn of serverless_functions) {
 		const server_asset = server_assets[fn].find((file) => file.startsWith('_server.js'));
 		if (fn === 'fn-1') {
@@ -70,6 +80,8 @@ test('only includes relevant server assets for split functions', () => {
 });
 
 test('excludes server assets from universal load functions', () => {
+	const server_assets = scan_build_output();
+
 	const layout_load = server_assets['fn-2'].find((file) => file.startsWith('_layout.js'));
 	const page_load = server_assets['fn-2'].find((file) => file.startsWith('_page.js'));
 
@@ -78,8 +90,9 @@ test('excludes server assets from universal load functions', () => {
 });
 
 test('excludes server assets from components', () => {
-	const components = ['_layout.svelte', '_page.svelte', '_error.svelte'];
-	for (const component_name of components) {
+	const server_assets = scan_build_output();
+
+	for (const component_name of ['_layout.svelte', '_page.svelte', '_error.svelte']) {
 		const server_asset = server_assets['fn-2'].find((f) => f.startsWith(component_name));
 		expect(server_asset).toBeFalsy();
 	}
