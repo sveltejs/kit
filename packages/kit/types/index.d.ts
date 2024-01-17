38e1419218cdaf78b01709ada13a6fe9199cfacb
Rich Harris <richard.a.harris@gmail.com>
2024-01-17 11:34:36 -0500

    Apply suggestions from code review
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -80,7 +80,7 @@ declare module '@sveltejs/kit' {
 
 		/**
 		 * Find all the assets imported by server files belonging to `routes`
-		 * */
+		 */
 		findServerAssets(routes: RouteDefinition[]): string[];
 
 		/**
@@ -1886,7 +1886,7 @@ declare module '@sveltejs/kit/node' {
 	export function setResponse(res: import('http').ServerResponse, response: Response): Promise<void>;
 	/**
 	 * Converts a file on disk to a readable stream
-	 * */
+	 */
 	export function createReadable(file: string): ReadableStream;
 }
 
@@ -2133,7 +2133,7 @@ declare module '$app/server' {
 	 * const asset = read(somefile);
 	 * const text = await asset.text();
 	 * ```
-	 * */
+	 */
 	export function read(asset: string): Response;
 }
 
