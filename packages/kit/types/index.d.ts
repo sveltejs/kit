e7046f66e2ec6403e56583946746c98c4b087a6d
Rich Harris <rich.harris@vercel.com>
2024-01-18 20:02:24 -0500

    regenerate types

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -1145,7 +1145,7 @@ declare module '@sveltejs/kit' {
 		/** A map of environment variables */
 		env: Record<string, string>;
 		/** A function that turns an asset filename into a `ReadableStream`. Required for the `read` export from `$app/server` to work */
-		read?: (file: string) => MaybePromise<ReadableStream>;
+		read?: (file: string) => ReadableStream;
 	}
 
 	export interface SSRManifest {
