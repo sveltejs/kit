e9706383b86ca10c7903edb5b1d0c336678752b8
niassuh1 <galenwaros@gmail.com>
2024-01-25 23:07:00 +0300

    refactor: postcss config and tailwind config in tailwind template

--- packages/create-svelte/templates/skeleton-tailwind/tailwind.config.js
+++ packages/create-svelte/templates/skeleton-tailwind/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
