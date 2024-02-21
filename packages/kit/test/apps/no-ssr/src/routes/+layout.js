cf60aeba4866a204496dbb8999010417399c70e9
manhhungpc <phamhung2002@gmail.com>
2024-02-21 14:45:40 +0700

    fix: add test load root layout

--- packages/kit/test/apps/no-ssr/src/routes/+layout.js
+++ packages/kit/test/apps/no-ssr/src/routes/+layout.js
@@ -1,1 +1,10 @@
+import { redirect } from '@sveltejs/kit';
+
 export const ssr = false;
+
+export async function load({ route }) {
+	//@ts-ignore
+	if (route.id == '/bad-route' || route.id == '/good-route') {
+		throw redirect(302, '/');
+	}
+}
--- packages/kit/test/apps/no-ssr/test/test.js
+++ packages/kit/test/apps/no-ssr/test/test.js
@@ -13,3 +13,12 @@ test('navigating to a non-existent route renders the default error page', async 
 	await page.waitForLoadState('networkidle');
 	expect(await page.textContent('h1')).toBe('404');
 });
+
+test('error occur in load function at root layout renders the default error page', async ({
+	page
+}) => {
+	test.setTimeout(3000);
+	await page.goto('/bad-route');
+	await page.waitForLoadState('networkidle');
+	expect(await page.textContent('h1')).toBe('404');
+});
