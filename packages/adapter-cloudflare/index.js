491cc23d98a678967489f75403f5ff9350650c87
Tee Ming <chewteeming01@gmail.com>
2024-04-03 21:03:05 +0800

    Update packages/adapter-cloudflare/index.js
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- packages/adapter-cloudflare/index.js
+++ packages/adapter-cloudflare/index.js
@@ -74,6 +74,7 @@ export default function (options = {}) {
 			try {
 				const result = await esbuild.build({
 					platform: 'browser',
+					// https://github.com/cloudflare/workers-sdk/blob/a12b2786ce745f24475174bcec994ad691e65b0f/packages/wrangler/src/deployment-bundle/bundle.ts#L35-L36
 					conditions: ['workerd', 'worker', 'browser'],
 					sourcemap: 'linked',
 					target: 'es2022',
