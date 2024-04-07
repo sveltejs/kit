04fc07a27d16edb73d4390aac01b909fa7beb2de
Jeremiasz Major <jrh.mjr@gmail.com>
2024-04-07 18:46:15 +0200

    bump eslint and plugin versions

---
+++ packages/create-svelte/shared/+eslint+typescript/eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+
+/** @type { import("eslint").Linter.FlatConfig[] } */
+export default [
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	{
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: {
+				...globals.browser,
+				...globals.es2017,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser
+			}
+		}
+	}
+];
---
+++ packages/create-svelte/shared/+eslint-typescript/eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+
+/** @type { import("eslint").Linter.FlatConfig[] } */
+export default [
+	js.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	{
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: {
+				...globals.browser,
+				...globals.es2017,
+				...globals.node
+			}
+		}
+	}
+];
