e0f77360957f8f5601f53b9710dfce0042f6e952
Albert <albert@lumina.earth>
2024-05-13 15:42:51 +0930

    global counter

--- playgrounds/basic/src/routes/+page.ts
+++ playgrounds/basic/src/routes/+page.ts
@@ -1,9 +1,6 @@
 import { global_counter } from '$lib/global_store';
-import { get } from 'svelte/store';
 
 export async function load() {
+    // increment the counter on every page load, but it will always stay at 1 because requests are isolated
     global_counter.update((count) => count + 1);
-    const v = get(global_counter);
-
-    console.log('load', v);
 }
