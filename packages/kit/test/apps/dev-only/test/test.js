b18eb2b05e4c78ae8dee4a8b71abf27358511c83
Nayan Gautam <gtmnayan@gmail.com>
2024-05-21 14:26:25 +0545

    Revert "fix glob to exclude +server.js and fix tests"
    This reverts commit c778f781dd93bfafb6fe793f194e21d71da271c8.

--- packages/kit/test/apps/dev-only/test/test.js
+++ packages/kit/test/apps/dev-only/test/test.js
@@ -107,6 +107,16 @@ test.describe('Vite', () => {
 		expect(manifest).toHaveProperty('optimized.e2e-test-dep-page-universal');
 	});
 
+	test('optimizes +page.server.js dependencies', async ({ page }) => {
+		await page.goto('/');
+		await page.getByText('hello world!').waitFor();
+
+		const manifest_path = path.join(__dirname, '../node_modules/.vite/deps/_metadata.json');
+		const manifest = JSON.parse(fs.readFileSync(manifest_path, 'utf-8'));
+
+		expect(manifest).toHaveProperty('optimized.e2e-test-dep-page-server');
+	});
+
 	test('optimizes +layout.svelte dependencies', async ({ page }) => {
 		await page.goto('/');
 		await page.getByText('hello world!').waitFor();
@@ -125,6 +135,16 @@ test.describe('Vite', () => {
 		const manifest = JSON.parse(fs.readFileSync(manifest_path, 'utf-8'));
 
 		expect(manifest).toHaveProperty('optimized.e2e-test-dep-layout-universal');
+	});
+
+	test('optimizes +layout.server.js dependencies', async ({ page }) => {
+		await page.goto('/');
+		await page.getByText('hello world!').waitFor();
+
+		const manifest_path = path.join(__dirname, '../node_modules/.vite/deps/_metadata.json');
+		const manifest = JSON.parse(fs.readFileSync(manifest_path, 'utf-8'));
+
+		expect(manifest).toHaveProperty('optimized.e2e-test-dep-layout-server');
 	});
 
 	test('optimizes +error.svelte dependencies', async ({ page }) => {
