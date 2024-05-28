e56c6c97f8c66b1ba72b8015b43e9a4e24581e7e
K-Sato1995 <Katsuki_Sato0130@icloud.com>
2024-05-28 14:39:20 +0900

    chroe: run format

--- packages/create-svelte/shared/+playwright+skeletonlib/tests/test.ts
+++ packages/create-svelte/shared/+playwright+skeletonlib/tests/test.ts
@@ -2,5 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to your library project' })).toBeVisible();
+	await expect(
+		page.getByRole('heading', { name: 'Welcome to your library project' })
+	).toBeVisible();
 });
