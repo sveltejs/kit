8a6075fb445ff3ca943fac7a61c55f0be8cdffc5
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-05-28 09:41:14 -0700

    simplify

--- packages/create-svelte/shared/+playwright+skeletonlib/tests/test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(
-		page.getByRole('heading', { name: 'Welcome to your library project' })
-	).toBeVisible();
-});
