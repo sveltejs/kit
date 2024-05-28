275b603c50496cd75116510fc35ce8702641fc1c
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-05-28 12:14:39 -0700

    feat: upgrade eslint to v9

---
+++ .changeset/young-monkeys-learn.md
@@ -0,0 +1,5 @@
+---
+"create-svelte": minor
+---
+
+feat: upgrade eslint to v9
---
+++ packages/create-svelte/shared/+eslint+typescript/eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser
+			}
+		}
+	},
+	{
+		ignores: ['build/', '.svelte-kit/', 'dist/']
+	}
+];
