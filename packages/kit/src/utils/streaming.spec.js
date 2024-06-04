24fc70118405ce43bd82b4ce1b70546c7f9032db
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-06-04 10:29:12 -0700

    format

--- packages/kit/src/utils/streaming.spec.js
+++ packages/kit/src/utils/streaming.spec.js
@@ -5,9 +5,15 @@ import { fail } from 'node:assert';
 test('works with fast consecutive promise resolutions', async () => {
 	const iterator = create_async_iterator();
 
-	Promise.resolve(1).then((n) => iterator.push(n)).catch(fail);
-	Promise.resolve(2).then((n) => iterator.push(n)).catch(fail);
-	Promise.resolve().then(() => iterator.done()).catch(fail);
+	Promise.resolve(1)
+		.then((n) => iterator.push(n))
+		.catch(fail);
+	Promise.resolve(2)
+		.then((n) => iterator.push(n))
+		.catch(fail);
+	Promise.resolve()
+		.then(() => iterator.done())
+		.catch(fail);
 
 	const actual = [];
 	for await (const value of iterator.iterator) {
