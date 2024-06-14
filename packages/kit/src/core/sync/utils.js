81136dd46b97d6ab116ae47b0ffa64786a26cd51
Rich Harris <rich.harris@vercel.com>
2024-06-14 18:33:10 -0400

    gah

--- packages/kit/src/core/sync/utils.js
+++ packages/kit/src/core/sync/utils.js
@@ -3,7 +3,14 @@ import path from 'node:path';
 import { mkdirp } from '../../utils/filesystem.js';
 import { resolve_peer_dependency } from '../../utils/import.js';
 
-const { VERSION } = await resolve_peer_dependency('svelte/compiler');
+/** @type {string} */
+let VERSION;
+
+try {
+	({ VERSION } = await resolve_peer_dependency('svelte/compiler'));
+} catch {
+	({ VERSION } = await import('svelte/compiler'));
+}
 
 /** @type {Map<string, string>} */
 const previous_contents = new Map();
