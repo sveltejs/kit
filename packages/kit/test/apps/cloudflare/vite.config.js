adc8a88bd4eb337b5c6bf8b29e66e26b5279195d
Wooseop Kim <wooseop@protonmail.com>
2024-07-10 22:56:30 +0900

    chore: run `pnpm run format`

--- packages/kit/test/apps/cloudflare/vite.config.js
+++ packages/kit/test/apps/cloudflare/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}'],
+		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	server: {
 		fs: {
