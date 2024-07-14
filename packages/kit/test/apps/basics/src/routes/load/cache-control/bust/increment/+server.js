b437da571a94163b69044c1cf89a6648f0544279
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-07-14 07:50:10 -0700

    sveltekit 2 migration fix

--- packages/kit/test/apps/basics/src/routes/load/cache-control/bust/increment/+server.js
+++ packages/kit/test/apps/basics/src/routes/load/cache-control/bust/increment/+server.js
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 
 export function GET({ cookies }) {
-	cookies.set('cache-control-bust-count', +(cookies.get('cache-control-bust-count') ?? 0) + 1 + '');
+	cookies.set('cache-control-bust-count', +(cookies.get('cache-control-bust-count') ?? 0) + 1 + '', { path: '/' });
 
 	return json({});
 }
