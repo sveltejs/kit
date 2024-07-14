72a71efe00e10f40333f668952808cc506f54b9d
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-07-14 08:15:04 -0700

    two more

--- packages/kit/test/apps/basics/src/routes/load/cache-control/default/increment/+server.js
+++ packages/kit/test/apps/basics/src/routes/load/cache-control/default/increment/+server.js
@@ -3,7 +3,8 @@ import { json } from '@sveltejs/kit';
 export function GET({ cookies }) {
 	cookies.set(
 		'cache-control-default-count',
-		+(cookies.get('cache-control-default-count') ?? 0) + 1 + ''
+		+(cookies.get('cache-control-default-count') ?? 0) + 1 + '',
+		{ path: '/' }
 	);
 
 	return json({});
--- packages/kit/test/apps/basics/src/routes/load/cache-control/force/increment/+server.js
+++ packages/kit/test/apps/basics/src/routes/load/cache-control/force/increment/+server.js
@@ -3,7 +3,8 @@ import { json } from '@sveltejs/kit';
 export function GET({ cookies }) {
 	cookies.set(
 		'cache-control-force-count',
-		+(cookies.get('cache-control-force-count') ?? 0) + 1 + ''
+		+(cookies.get('cache-control-force-count') ?? 0) + 1 + '',
+		{ path: '/' }
 	);
 
 	return json({});
