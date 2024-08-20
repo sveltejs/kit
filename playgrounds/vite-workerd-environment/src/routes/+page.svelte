8edc12a0ece3c571a6dffc55c4a66b5d6c133c42
James Opstad <jopstad@cloudflare.com>
2024-08-20 17:57:05 +0100

    Created vite-workerd-environment playground

---
+++ playgrounds/vite-workerd-environment/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+	export let data: PageData
+</script>
+
+<div>
+	<h1>Svelte Kit with Vite Workerd Environment</h1>
+	<hr />
+	<h2>{data.userAgentText}</h2>
+	<form method="POST">
+		<button>Click me</button>
+	</form>
+</div>
---
+++ playgrounds/vite-workerd-environment/vite.config.ts
@@ -0,0 +1,11 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: ['../../packages/kit']
+		}
+	}
+});
