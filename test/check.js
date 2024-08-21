2e0a797ab3163b3c221fdb417fbda57314304dd0
Kian McKenna <kian@mckenna.cloud>
2024-08-21 16:48:42 -0500

    update test

--- test/check.js
+++ test/check.js
@@ -16,26 +16,12 @@ const resolve_path = (path) => fileURLToPath(new URL(path, import.meta.url));
 // use a directory outside of packages to ensure it isn't added to the pnpm workspace
 const test_workspace_dir = resolve_path('../../../.test-tmp/create-svelte/');
 
-const existing_workspace_overrides = JSON.parse(
-	fs.readFileSync(resolve_path('../../../package.json'), 'utf-8')
-).pnpm?.overrides;
-
-const overrides = { ...existing_workspace_overrides };
+const overrides = { };
 
 for (const pkg_path of glob(resolve_path('../../../packages/*/package.json'))) {
 	const name = JSON.parse(fs.readFileSync(pkg_path, 'utf-8')).name;
 	// use `file:` protocol for opting into stricter resolve logic which catches more bugs
 	overrides[name] = `file:${path.dirname(path.resolve(pkg_path))}`;
-}
-
-try {
-	const kit_dir = resolve_path('../../../packages/kit');
-	const ls_vite_result = execSync('pnpm ls --json vite', { cwd: kit_dir });
-	const vite_version = JSON.parse(ls_vite_result.toString())[0].devDependencies.vite.version;
-	overrides.vite = vite_version;
-} catch (e) {
-	console.error('failed to parse installed vite version from packages/kit');
-	throw e;
 }
 
 // prepare test pnpm workspace
