d5ccb563beca0ca6985c620002ee3dd0dfb394fa
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-09-13 11:50:27 -0700

    chore: simplify output of hoisted declarations

--- packages/enhanced-img/test/Output.svelte
+++ packages/enhanced-img/test/Output.svelte
@@ -1,11 +1,11 @@
-<script context="module">
+<script context="module">	
+	import __IMPORTED_ASSET_0__ from "./foo.svg";
 	const __DECLARED_ASSET_0__ = "__VITE_ASSET__2AM7_y_a__ 1440w, __VITE_ASSET__2AM7_y_b__ 960w";
 	const __DECLARED_ASSET_1__ = "__VITE_ASSET__2AM7_y_c__ 1440w, __VITE_ASSET__2AM7_y_d__ 960w";
 	const __DECLARED_ASSET_2__ = "__VITE_ASSET__2AM7_y_e__ 1440w, __VITE_ASSET__2AM7_y_f__ 960w";
 	const __DECLARED_ASSET_3__ = "__VITE_ASSET__2AM7_y_g__";
 </script>
 <script lang="ts">
-	import __IMPORTED_ASSET_0__ from "./foo.svg";
 	
 	import manual_image1 from './no.png';
 	
