2b56f1f5a81d92070d28c30f2556c0e257f6606b
mastermakrela <mastermakrela@gmail.com>
2024-10-10 21:56:25 +0200

    remove console.log

--- packages/kit/test/apps/basics/src/routes/endpoint-output/fetch-asset/relative/+server.js
+++ packages/kit/test/apps/basics/src/routes/endpoint-output/fetch-asset/relative/+server.js
@@ -2,8 +2,6 @@ import file_path from '../test.txt?url';
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET({ fetch }) {
-	console.log('🚀 ~ file_path:', file_path);
-
 	const response = await fetch(file_path);
 
 	return new Response(response.body, {
