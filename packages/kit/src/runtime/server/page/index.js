83388f89c7b3a9c190220846f865ff089059bc8c
Chew Tee Ming <chew.tee.ming@nindatech.com>
2024-10-11 14:13:22 +0800

    cleanup

--- packages/kit/src/runtime/server/page/index.js
+++ packages/kit/src/runtime/server/page/index.js
@@ -102,23 +102,16 @@ export async function render_page(event, page, options, manifest, state, resolve
 		if (get_option(nodes, 'ssr') === false && !(state.prerendering && should_prerender_data)) {
 			// if the user makes a request through a non-enhanced form, the returned value is lost
 			// because there is no SSR or client-side handling of the response
-			if (DEV) {
-				const no_enhance = !event.request.headers.has('x-sveltekit-action');
-
-				if (action_result && no_enhance) {
-					if (action_result.type === 'error') {
-						console.warn(
-							"The form action returned an error, but +error.svelte wasn't rendered because SSR is off. To get the error page with CSR, enhance your form with `use:enhance`. See https://kit.svelte.dev/docs/form-actions#progressive-enhancement-use-enhance"
-						);
-					}
-					// for type: 'success' and type: 'failure'
-					else if (action_result.data) {
-						/// case: lost data
-						console.warn(
-							"The form action returned a value, but it isn't available in `$page.form`, because SSR is off. To handle the returned value in CSR, enhance your form with `use:enhance`. See https://kit.svelte.dev/docs/form-actions#progressive-enhancement-use-enhance"
-						);
-					}
-					// type 'redirect' is returned immediately, so we don't need to handle it here
+			if (DEV && action_result && !event.request.headers.has('x-sveltekit-action')) {
+				if (action_result.type === 'error') {
+					console.warn(
+						"The form action returned an error, but +error.svelte wasn't rendered because SSR is off. To get the error page with CSR, enhance your form with `use:enhance`. See https://kit.svelte.dev/docs/form-actions#progressive-enhancement-use-enhance"
+					);
+				} else if (action_result.data) {
+					/// case: lost data
+					console.warn(
+						"The form action returned a value, but it isn't available in `$page.form`, because SSR is off. To handle the returned value in CSR, enhance your form with `use:enhance`. See https://kit.svelte.dev/docs/form-actions#progressive-enhancement-use-enhance"
+					);
 				}
 			}
 
