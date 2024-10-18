7fff4390d7f06cd764f9a4416dba074262c34667
Chew Tee Ming <chew.tee.ming@nindatech.com>
2024-10-18 14:13:59 +0800

    lazy load platform proxy

--- packages/adapter-cloudflare/index.js
+++ packages/adapter-cloudflare/index.js
@@ -144,29 +144,33 @@ export default function (options = {}) {
 				);
 			}
 		},
-		async emulate() {
-			const proxy = await getPlatformProxy(options.platformProxy);
-			const platform = /** @type {App.Platform} */ ({
-				env: proxy.env,
-				context: proxy.ctx,
-				caches: proxy.caches,
-				cf: proxy.cf
-			});
-
-			/** @type {Record<string, any>} */
-			const env = {};
-			const prerender_platform = /** @type {App.Platform} */ (/** @type {unknown} */ ({ env }));
-
-			for (const key in proxy.env) {
-				Object.defineProperty(env, key, {
-					get: () => {
-						throw new Error(`Cannot access platform.env.${key} in a prerenderable route`);
-					}
+		emulate() {
+			const getting_platform = (async () => {
+				const proxy = await getPlatformProxy(options.platformProxy);
+				const platform = /** @type {App.Platform} */ ({
+					env: proxy.env,
+					context: proxy.ctx,
+					caches: proxy.caches,
+					cf: proxy.cf
 				});
-			}
+
+				/** @type {Record<string, any>} */
+				const env = {};
+				const prerender_platform = /** @type {App.Platform} */ (/** @type {unknown} */ ({ env }));
+
+				for (const key in proxy.env) {
+					Object.defineProperty(env, key, {
+						get: () => {
+							throw new Error(`Cannot access platform.env.${key} in a prerenderable route`);
+						}
+					});
+				}
+				return { platform, prerender_platform };
+			})();
 
 			return {
-				platform: ({ prerender }) => {
+				platform: async ({ prerender }) => {
+					const { platform, prerender_platform } = await getting_platform;
 					return prerender ? prerender_platform : platform;
 				}
 			};
