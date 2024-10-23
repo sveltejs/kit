5e7cc9b677d1e0ee075097eaa5df80312af576bf
Chew Tee Ming <chew.tee.ming@nindatech.com>
2024-10-23 15:06:40 +0800

    add tests

--- packages/kit/test/apps/basics/test/cross-platform/client.test.js
+++ packages/kit/test/apps/basics/test/cross-platform/client.test.js
@@ -715,6 +715,23 @@ test.describe('Routing', () => {
 		expect(await page.textContent('#page-url-hash')).toBe('#target');
 	});
 
+	test('clicking on a hash link focuses the associated element', async ({ page }) => {
+		await page.goto('/routing/hashes/focus');
+		await page.locator('a[href="#example"]').click();
+		await expect(page.getByRole('textbox')).toBeFocused();
+		// check it still works when the hash is already present in the URL
+		await page.locator('a[href="#example"]').click();
+		await expect(page.getByRole('textbox')).toBeFocused();
+	});
+
+	test('backwards navigation works after clicking a hash link with data-sveltekit-reload', async ({ page, baseURL }) => {
+		await page.goto('/data-sveltekit/reload/hash');
+		await page.locator('a[href="#example"]').click();
+		await page.locator('a[href="/data-sveltekit/reload/hash/new-page"]').click();
+		await page.goBack();
+		expect(page.url()).toBe(`${baseURL}/data-sveltekit/reload/hash#example`);
+	});
+
 	test('back button returns to previous route when previous route has been navigated to via hash anchor', async ({
 		page,
 		clicknav
