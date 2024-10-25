9a2fe16c672666c642737839f38c984f53f3c8de
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-10-25 11:49:03 +0200

    check file existance instead

--- .changeset/eight-insects-live.md
+++ .changeset/eight-insects-live.md
@@ -2,4 +2,4 @@
 '@sveltejs/kit': patch
 ---
 
-fix: revert reading assets directly when using the load function's fetch
+fix: don't try reading assets directly that aren't present
--- packages/kit/src/runtime/server/fetch.js
+++ packages/kit/src/runtime/server/fetch.js
@@ -1,6 +1,7 @@
 import * as set_cookie_parser from 'set-cookie-parser';
 import { respond } from './respond.js';
 import * as paths from '__sveltekit/paths';
+import { read_implementation } from '__sveltekit/server';
 
 /**
  * @param {{
@@ -95,6 +96,16 @@ export function create_fetch({ event, options, manifest, state, get_cookie_heade
 
 						return new Response(state.read(file), {
 							headers: type ? { 'content-type': type } : {}
+						});
+					} else if (read_implementation && file in manifest._.server_assets) {
+						const length = manifest._.server_assets[file];
+						const type = manifest.mimeTypes[file.slice(file.lastIndexOf('.'))];
+
+						return new Response(read_implementation(file), {
+							headers: {
+								'Content-Length': '' + length,
+								'Content-Type': type
+							}
 						});
 					}
 
