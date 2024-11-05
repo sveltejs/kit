68f976ce38e5463b250ee9090a33993d9d4a87cc
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-11-04 16:20:16 -0800

    Update packages/kit/src/exports/vite/index.js

--- packages/kit/src/exports/vite/index.js
+++ packages/kit/src/exports/vite/index.js
@@ -353,7 +353,7 @@ async function kit({ svelte_config }) {
 			const vite_basic_ssl = config.plugins.find(({ name }) => name === 'vite:basic-ssl');
 
 			// by default, when enabling HTTPS in Vite, it also enables HTTP/2
-			// however, undici's Headers implementation does not like the HTTP/2 headers
+			// however, undici has not yet enabled HTTP/2 by default: https://github.com/nodejs/undici/issues/2750
 			// we set a no-op proxy config to force Vite to downgrade to TLS-only
 			// see https://vitejs.dev/config/#server-https
 			if ((config.server.https || vite_basic_ssl) && !config.server.proxy) {
