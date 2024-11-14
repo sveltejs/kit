7dbd2f6cebd15dd5ca070a819543952c6117fa13
Chew Tee Ming <chew.tee.ming@nindatech.com>
2024-11-14 14:24:25 +0800

    format

--- packages/kit/test/apps/basics/src/routes/before-navigate/preserve-search-params/+layout.svelte
+++ packages/kit/test/apps/basics/src/routes/before-navigate/preserve-search-params/+layout.svelte
@@ -1,21 +1,21 @@
 <script>
 	import { beforeNavigate } from '$app/navigation';
 
-    // do not change, used in test (/basics/test/cross-platform/client.test.js):
-    const preserved_param = 'preserved';
+	// do not change, used in test (/basics/test/cross-platform/client.test.js):
+	const preserved_param = 'preserved';
 
 	beforeNavigate(({ from, to }) => {
 		const previous_param_value = from?.url.searchParams.get(preserved_param);
-        if (typeof previous_param_value === 'string') {
-            to?.url.searchParams.set(preserved_param, previous_param_value);
-        }
+		if (typeof previous_param_value === 'string') {
+			to?.url.searchParams.set(preserved_param, previous_param_value);
+		}
 	});
 </script>
 
 <div data-sveltekit-preload-data="hover">
-    <h1>preserve search params</h1>
-    <a href="/before-navigate/preserve-search-params">main page</a>
-    <a href="/before-navigate/preserve-search-params/another-page">another page</a>
-    <a href="/before-navigate/preserve-search-params/yet-another-page">yet another page</a>
-    <slot />
+	<h1>preserve search params</h1>
+	<a href="/before-navigate/preserve-search-params">main page</a>
+	<a href="/before-navigate/preserve-search-params/another-page">another page</a>
+	<a href="/before-navigate/preserve-search-params/yet-another-page">yet another page</a>
+	<slot />
 </div>
