3414ce3e29e24583ca815fdd24cc7036f59c1f0b
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-11-26 08:33:09 -0800

    remove todo. issue has been fixed

--- packages/kit/test/apps/basics/src/routes/load/url-hash/+page.js
+++ packages/kit/test/apps/basics/src/routes/load/url-hash/+page.js
@@ -1,6 +1,3 @@
-// TODO: remove this when Svelte 5 hydration is fixed. See https://github.com/sveltejs/svelte/issues/14437
-export const ssr = false;
-
 export const load = ({ url }) => {
 	const hash = url.hash;
 	return { hash };
