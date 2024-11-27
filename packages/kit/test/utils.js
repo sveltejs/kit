b3c2e10763ca50f9aa3e68dcf0168fb16c06e033
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-11-27 10:13:24 -0800

    other function too

--- packages/kit/test/utils.js
+++ packages/kit/test/utils.js
@@ -80,7 +80,11 @@ export const test = base.extend({
 				/** @param {{ x: number, y: number }} opt */ (opt) => window.scrollTo(opt.x, opt.y),
 				{ x, y }
 			);
-			await page.waitForFunction(() => window.scrollY === y);
+			await page.waitForFunction(
+				/** @param {{ x: number, y: number }} opt */ (opt) =>
+					window.scrollX === opt.x && window.scrollY === opt.y,
+				{ x, y }
+			);
 		}
 
 		await use(scroll_to);
