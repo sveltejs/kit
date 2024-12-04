7ba6cd33f996a71301c66cb7d6acbc04ffd9faf9
paoloricciuti <ricciutipaolo@gmail.com>
2024-12-04 09:27:04 +0100

    chore: revert weird did_it_run thing

--- packages/kit/test/apps/basics/src/routes/init-hooks/+page.server.ts
+++ packages/kit/test/apps/basics/src/routes/init-hooks/+page.server.ts
@@ -1,12 +1,11 @@
-const did_init_run = {};
+let did_init_run = 0;
 
 export function _set_from_init() {
-	did_init_run[process.env.TEST_WORKER_INDEX] ??= 0;
-	did_init_run[process.env.TEST_WORKER_INDEX]++;
+	did_init_run++;
 }
 
 export function load() {
 	return {
-		did_init_run: did_init_run[process.env.TEST_WORKER_INDEX]
+		did_init_run
 	};
 }
