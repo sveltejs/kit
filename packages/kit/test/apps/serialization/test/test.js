79617ca019d1b56a6d23c36e0d578ebf300255b7
Dominic Gannaway <dg@domgan.com>
2024-12-09 16:52:10 +0000

    lint

--- packages/kit/test/apps/serialization/test/test.js
+++ packages/kit/test/apps/serialization/test/test.js
@@ -1,8 +1,4 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import process from 'node:process';
 import { expect } from '@playwright/test';
-import { fileURLToPath } from 'node:url';
 import { test } from '../../../utils.js';
 
 /** @typedef {import('@playwright/test').Response} Response */
