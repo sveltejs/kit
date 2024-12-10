65f63d1f29a533976e15c68366f583aa7205be5a
Rich Harris <rich.harris@vercel.com>
2024-12-10 09:26:20 -0500

    WIP

---
+++ packages/kit/src/runtime/app/state/client.js
@@ -0,0 +1,29 @@
+import { page as _page } from '../../client/state.svelte.js';
+
+/** @type {import('@sveltejs/kit').Page} */
+export const page = {
+	get data() {
+		return _page.data;
+	},
+	get error() {
+		return _page.error;
+	},
+	get form() {
+		return _page.form;
+	},
+	get params() {
+		return _page.params;
+	},
+	get route() {
+		return _page.route;
+	},
+	get state() {
+		return _page.state;
+	},
+	get status() {
+		return _page.status;
+	},
+	get url() {
+		return _page.url;
+	}
+}
---
+++ packages/kit/src/runtime/client/state.svelte.js
@@ -0,0 +1,14 @@
+/** @type {import('@sveltejs/kit').Page} */
+export const page = new class Page {
+	data = $state.raw({})
+	form = $state.raw(null)
+	error = $state.raw(null)
+	/** @type {Record<string, string>} */
+	params = $state.raw({})
+	route = $state.raw({
+		id: null
+	})
+	state = $state.raw({})
+	status = $state.raw(-1)
+	url = $state.raw(new URL(location.href))
+}
