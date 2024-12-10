0ce95fc4ac64f05bc35fb155a9e03d234e9fd10f
Rich Harris <rich.harris@vercel.com>
2024-12-10 17:30:57 -0500

    stopgap

--- packages/kit/src/runtime/client/state.svelte.js
+++ packages/kit/src/runtime/client/state.svelte.js
@@ -1,17 +1,33 @@
+import * as s from 'svelte';
+
 /** @type {import('@sveltejs/kit').Page} */
-export const page = new (class Page {
-	data = $state.raw({});
-	form = $state.raw(null);
-	error = $state.raw(null);
-	/** @type {Record<string, string>} */
-	params = $state.raw({});
-	route = $state.raw({
-		id: null
-	});
-	state = $state.raw({});
-	status = $state.raw(-1);
-	url = $state.raw(new URL('https://example.com'));
-})();
+export let page;
+
+// TODO come up with a better way to check version, this will
+// cause all of svelte to be imported
+if ('untrack' in s) {
+	page = new (class Page {
+		data = $state.raw({});
+		form = $state.raw(null);
+		error = $state.raw(null);
+		params = $state.raw({});
+		route = $state.raw({ id: null });
+		state = $state.raw({});
+		status = $state.raw(-1);
+		url = $state.raw(new URL('https://example.com'));
+	})();
+} else {
+	page = {
+		data: {},
+		form: null,
+		error: null,
+		params: {},
+		route: { id: null },
+		state: {},
+		status: -1,
+		url: new URL('https://example.com')
+	};
+}
 
 /**
  * @param {import('@sveltejs/kit').Page} new_page
