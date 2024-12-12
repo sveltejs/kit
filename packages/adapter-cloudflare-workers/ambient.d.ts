bd53117feb2502f3673817b79aa87174d374d9a2
Chew Tee Ming <chew.tee.ming@nindatech.com>
2024-12-12 12:39:21 +0800

    remove unneccesary export

--- packages/adapter-cloudflare-workers/ambient.d.ts
+++ packages/adapter-cloudflare-workers/ambient.d.ts
@@ -11,5 +11,3 @@ declare global {
 		}
 	}
 }
-
-export {};
--- packages/adapter-node/ambient.d.ts
+++ packages/adapter-node/ambient.d.ts
@@ -1,12 +1,12 @@
+import http from 'node:http';
+
 declare global {
 	namespace App {
 		export interface Platform {
 			/**
 			 * The original Node request object (https://nodejs.org/api/http.html#class-httpincomingmessage)
 			 */
-			req: import('http').IncomingMessage;
+			req: http.IncomingMessage;
 		}
 	}
 }
-
-export {};
