a42e9eef3f242cca2f0764a280f0524a1f9bf7d1
Simon Holthausen <simon.holthausen@vercel.com>
2024-12-13 15:19:29 +0100

    better dev time errors

--- packages/kit/src/runtime/app/state/server.js
+++ packages/kit/src/runtime/app/state/server.js
@@ -4,36 +4,49 @@ function context() {
 	return getContext('__request__');
 }
 
+/** @param {string} name */
+function context_dev(name) {
+	try {
+		return context();
+	} catch {
+		throw new Error(
+			`Cannot retrieve '${name}' on the server outside of a Svelte component, as it is bound to the current request via component context. This prevents state from leaking between users.` +
+				'For more information, see https://svelte.dev/docs/kit/state-management#avoid-shared-state-on-the-server'
+		);
+	}
+}
+
+// TODO we're using DEV in some places and __SVELTEKIT_DEV__ in others - why? Can we consolidate?
 export const page = {
 	get data() {
-		return context().page.data;
+		return (__SVELTEKIT_DEV__ ? context_dev('page.data') : context()).page.data;
 	},
 	get error() {
-		return context().page.error;
+		return (__SVELTEKIT_DEV__ ? context_dev('page.error') : context()).page.error;
 	},
 	get form() {
-		return context().page.form;
+		return (__SVELTEKIT_DEV__ ? context_dev('page.form') : context()).page.form;
 	},
 	get params() {
-		return context().page.params;
+		return (__SVELTEKIT_DEV__ ? context_dev('page.params') : context()).page.params;
 	},
 	get route() {
-		return context().page.route;
+		return (__SVELTEKIT_DEV__ ? context_dev('page.route') : context()).page.route;
 	},
 	get state() {
-		return context().page.state;
+		return (__SVELTEKIT_DEV__ ? context_dev('page.state') : context()).page.state;
 	},
 	get status() {
-		return context().page.status;
+		return (__SVELTEKIT_DEV__ ? context_dev('page.status') : context()).page.status;
 	},
 	get url() {
-		return context().page.url;
+		return (__SVELTEKIT_DEV__ ? context_dev('page.url') : context()).page.url;
 	}
 };
 
 export const navigating = {
 	get current() {
-		return context().navigating;
+		return (__SVELTEKIT_DEV__ ? context_dev('navigating.current') : context()).navigating;
 	}
 };
 
