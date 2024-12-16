6882d058c8eb2933b86d57dbe592d29a37136041
paoloricciuti <ricciutipaolo@gmail.com>
2024-12-16 18:18:24 +0100

    fix: only use `manualChunks` when `!codeSplitJS`

--- packages/kit/src/exports/vite/index.js
+++ packages/kit/src/exports/vite/index.js
@@ -650,7 +650,7 @@ async function kit({ svelte_config }) {
 								assetFileNames: `${prefix}/assets/[name].[hash][extname]`,
 								hoistTransitiveImports: false,
 								sourcemapIgnoreList,
-								manualChunks: () => 'bundle.js'
+								manualChunks: svelte_config.kit.output.codeSplitJs ? undefined : () => 'bundle.js'
 							},
 							preserveEntrySignatures: 'strict'
 						},
@@ -680,7 +680,7 @@ async function kit({ svelte_config }) {
 							input: `${runtime_directory}/client/entry.js`,
 							output: {
 								format: 'esm',
-								manualChunks: () => 'bundle.js'
+								manualChunks: svelte_config.kit.output.codeSplitJs ? undefined : () => 'bundle.js'
 							}
 						}
 					},
