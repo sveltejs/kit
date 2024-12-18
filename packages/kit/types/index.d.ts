27c78bd69451b8f95da7fa1f61efea108b152249
Rich Harris <rich.harris@vercel.com>
2024-12-18 17:43:58 -0500

    regenerate

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -488,7 +488,7 @@ declare module '@sveltejs/kit' {
 			 * @default 'split'
 			 * @since 2.13.0
 			 */
-			bundleStrategy?: 'split' | 'single';
+			bundleStrategy?: 'split' | 'single' | 'inline';
 		};
 		paths?: {
 			/**
@@ -1655,6 +1655,10 @@ declare module '@sveltejs/kit' {
 			stylesheets: string[];
 			fonts: string[];
 			uses_env_dynamic_public: boolean;
+			inline?: {
+				script: string;
+				style: string;
+			};
 		} | null;
 		server_manifest: import('vite').Manifest;
 	}
