a0c654eecf3ed6c1e9444497cfbf0a0a717ca552
Rich Harris <rich.harris@vercel.com>
2024-12-18 08:55:08 -0500

    types

--- packages/kit/src/runtime/client/bundled-entry.js
+++ packages/kit/src/runtime/client/bundled-entry.js
@@ -3,6 +3,11 @@ import * as kit from './entry.js';
 // @ts-expect-error
 import * as app from '__sveltekit/APP';
 
+/**
+ *
+ * @param {HTMLElement} element
+ * @param {import('./types.js').HydrateOptions} options
+ */
 export function start(element, options) {
 	kit.start(app, element, options);
 }
