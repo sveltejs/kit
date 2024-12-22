ba80ea7224b8c5749b517a1f7bc7a3c824f1fcb8
Rich Harris <rich.harris@vercel.com>
2024-12-22 12:17:07 -0500

    make self-contained apps possible

--- packages/kit/src/runtime/client/utils.js
+++ packages/kit/src/runtime/client/utils.js
@@ -311,10 +311,14 @@ export function is_external_url(url, base, hash_routing) {
 	}
 
 	if (hash_routing) {
-		if (url.pathname === base + '/') return false;
+		if (url.pathname === base + '/') {
+			return false;
+		}
 
 		// be lenient if serving from filesystem
-		if (url.protocol === 'file:' && url.pathname === base + '/index.html') return false;
+		if (url.protocol === 'file:' && url.pathname.replace(/\/[^/]+\.html?$/, '') === base) {
+			return false;
+		}
 
 		return true;
 	}
