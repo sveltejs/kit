80a9e835f1b00332160894aa1ab7cb0aa6bd356d
Tee Ming <chewteeming01@gmail.com>
2025-01-06 18:42:52 +0800

    fix: prerender data when there's no server load but `trailingSlash` option is set from the server (#13262)
    fixes #13255
    
    trailingSlash is evaluated on the client by making a server call, so we need to prerender it

--- packages/kit/src/runtime/server/page/index.js
+++ packages/kit/src/runtime/server/page/index.js
@@ -70,7 +70,10 @@ export async function render_page(event, page, options, manifest, state, resolve
 			}
 		}
 
-		const should_prerender_data = nodes.some((node) => node?.server?.load);
+		const should_prerender_data = nodes.some(
+			// prerender in case of trailingSlash because the client retrieves that value from the server
+			(node) => node?.server?.load || node?.server?.trailingSlash !== undefined
+		);
 		const data_pathname = add_data_suffix(event.url.pathname);
 
 		// it's crucial that we do this before returning the non-SSR response, otherwise
