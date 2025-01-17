b2c6c8fb0e59eb1a131515b74d985c1168375b05
Simon H <5968653+dummdidumm@users.noreply.github.com>
2025-01-17 10:04:28 +0100

    Update packages/kit/src/utils/import.js

--- packages/kit/src/utils/import.js
+++ packages/kit/src/utils/import.js
@@ -14,6 +14,7 @@ export async function resolve_peer_dependency(dependency) {
 		return await import(resolved);
 	} catch {
 		try {
+			// both imr.resolve and await import above can throw, which is why we can't just do import(resolved).catch(...) above
 			return await import(dependency);
 		} catch {
 			throw new Error(
