fd1869540a319f68ca3be9e7507b17c09fc193c3
Black1358 <122863429+Black1358@users.noreply.github.com>
2025-01-20 20:03:32 +0300

    chore: formating

--- packages/package/test/fixtures/resolve-alias/expected/sub/bar.js
+++ packages/package/test/fixtures/resolve-alias/expected/sub/bar.js
@@ -1,5 +1,5 @@
 import { baz } from '../baz';
 import { foo } from './foo';
-export const dynamic = () => import("../Test.svelte");
+export const dynamic = () => import('../Test.svelte');
 export const bar1 = foo;
 export const bar2 = baz;
