a8bdcf800b1e96a969ce69547d61b703b0999d87
Tee Ming <chewteeming01@gmail.com>
2025-01-21 17:29:03 +0800

    fix: correctly handle relative anchors when hash router is enabled (#13356)
    fixes #13320
    
    This PR enables relative hash links to be used with the hash router so that links such as #example get recognised as a link to the current page. We do this by assuming links that have a hash value starting with #/ links to a route while anything else is probably linking to an element with an id on the current page.

---
+++ .changeset/forty-houses-collect.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/kit': patch
+---
+
+fix: correctly handle relative anchors when using the hash router
--- packages/kit/test/apps/hash-based-routing/test/test.js
+++ packages/kit/test/apps/hash-based-routing/test/test.js
@@ -89,4 +89,13 @@ test.describe('hash based navigation', () => {
 		url = new URL(page.url());
 		expect(url.hash).toBe('#/reroute-b');
 	});
+
+	test('relative anchor works', async ({ page }) => {
+		await page.goto('/#/anchor');
+
+		await page.locator('a[href="#test"]').click();
+		await expect(page.locator('#test')).toHaveText('#test');
+		const url = new URL(page.url());
+		expect(url.hash).toBe('#/anchor#test');
+	});
 });
