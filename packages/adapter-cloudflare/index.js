e452fe763e372421357e8a53b9757f6bdb591713
Tee Ming <chewteeming01@gmail.com>
2025-01-24 16:45:34 +0800

    Update packages/adapter-cloudflare/index.js

--- packages/adapter-cloudflare/index.js
+++ packages/adapter-cloudflare/index.js
@@ -78,7 +78,6 @@ export default function (options = {}) {
 					caches: proxy.caches,
 					cf: proxy.cf
 				});
-		
 				/** @type {Record<string, any>} */
 				const env = {};
 				const prerender_platform = /** @type {App.Platform} */ (/** @type {unknown} */ ({ env }));
