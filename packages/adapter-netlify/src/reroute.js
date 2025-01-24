e38a660252687376b3c8aff473f99c4671674a85
Chew Tee Ming <chew.tee.ming@nindatech.com>
2025-01-24 12:43:54 +0800

    fix endless loop on Netlify

--- packages/adapter-netlify/src/reroute.js
+++ packages/adapter-netlify/src/reroute.js
@@ -6,8 +6,13 @@ import { applyReroute } from '@sveltejs/kit/adapter';
  * @returns {URL | undefined}
  */
 export default function middleware(request) {
-	const rerouted_path = applyReroute(new URL(request.url), reroute);
-	if (rerouted_path) {
+	const url = new URL(request.url);
+	const rerouted_path = applyReroute(url, reroute);
+
+	// a rewrite on Netlify will cause this function to run again with the new URL
+	// instead of moving onto the route function, so we only return a URL if
+	// the reroute path is different from the original to avoid an endless loop
+	if (rerouted_path && url.pathname !== rerouted_path.pathname) {
 		return rerouted_path;
 	}
 }
