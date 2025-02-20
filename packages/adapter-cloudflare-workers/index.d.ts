ba421dc8d12e0c99745c9edaea751eed9a0ef745
Chew Tee Ming <chew.tee.ming@nindatech.com>
2025-02-20 18:29:23 +0800

    add adapter option description for config

--- packages/adapter-cloudflare-workers/index.d.ts
+++ packages/adapter-cloudflare-workers/index.d.ts
@@ -5,6 +5,9 @@ import { GetPlatformProxyOptions } from 'wrangler';
 export default function plugin(options?: AdapterOptions): Adapter;
 
 export interface AdapterOptions {
+	/**
+	 * Path to your {@link https://developers.cloudflare.com/workers/wrangler/configuration/ | Wrangler configuration file}.
+	 */
 	config?: string;
 	/**
 	 * Config object passed to {@link https://developers.cloudflare.com/workers/wrangler/api/#getplatformproxy | getPlatformProxy}
