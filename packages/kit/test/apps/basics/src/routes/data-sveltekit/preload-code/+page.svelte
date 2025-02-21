acbaa2512791edc7efd4f24ffe2f12b3f9967f32
Chew Tee Ming <chew.tee.ming@nindatech.com>
2025-02-21 15:14:15 +0800

    format

--- packages/kit/test/apps/basics/src/routes/data-sveltekit/preload-code/+page.svelte
+++ packages/kit/test/apps/basics/src/routes/data-sveltekit/preload-code/+page.svelte
@@ -1,4 +1,6 @@
-<a id="eager" href="/data-sveltekit/preload-code/target/eager" data-sveltekit-preload-code="eager">eager</a>
+<a id="eager" href="/data-sveltekit/preload-code/target/eager" data-sveltekit-preload-code="eager"
+	>eager</a
+>
 
 <div style="height: 200vh"></div>
 
