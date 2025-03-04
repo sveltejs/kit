76996c98785aa3543baff41a8b1e7e9b07aa05cc
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-03-03 22:24:39 -0700

    lint

--- packages/kit/test/apps/prerendered-app-error-pages/test/test.js
+++ packages/kit/test/apps/prerendered-app-error-pages/test/test.js
@@ -4,6 +4,6 @@ import { test } from '../../../utils.js';
 test.describe.configure({ mode: 'parallel' });
 
 test('renders error page on nonexistent route', async ({ page }) => {
-	await page.goto('/nonexistent', { wait_for_started: false});
+	await page.goto('/nonexistent', { wait_for_started: false });
 	expect(await page.textContent('p')).toBe('This is your custom error page.');
 });
