0967a24cc8c66f779d9a27b8eb621c845f5754fa
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-03-03 22:22:54 -0700

    remove

--- packages/kit/test/apps/prerendered-app-error-pages/test/test.js
+++ packages/kit/test/apps/prerendered-app-error-pages/test/test.js
@@ -4,11 +4,6 @@ import { test } from '../../../utils.js';
 test.describe.configure({ mode: 'parallel' });
 
 test('renders error page on nonexistent route', async ({ page }) => {
-	await page.goto('/nonexistent');
-	expect(await page.textContent('p')).toBe('This is your custom error page.');
-});
-
-test('renders error page on manually 404ing route', async ({ page }) => {
-	await page.goto('/thrown-error');
+	await page.goto('/nonexistent', { wait_for_started: false});
 	expect(await page.textContent('p')).toBe('This is your custom error page.');
 });
