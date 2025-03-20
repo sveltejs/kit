ccb02423086212741267199ad7073daf473c8203
Chew Tee Ming <chew.tee.ming@nindatech.com>
2025-03-20 15:56:39 +0800

    serve immutable assets from worker with cache control

--- packages/adapter-cloudflare-workers/src/worker.js
+++ packages/adapter-cloudflare-workers/src/worker.js
@@ -7,7 +7,6 @@ const server = new Server(manifest);
 const app_path = `/${manifest.appPath}`;
 
 const immutable = `${app_path}/immutable/`;
-const version_file = `${app_path}/version.json`;
 
 export default {
 	/**
@@ -34,55 +33,71 @@ export default {
 			// ignore invalid URI
 		}
 
-		const stripped_pathname = pathname.replace(/\/$/, '');
-
-		// prerendered pages and /static files
-		let is_static_asset = false;
-		const filename = stripped_pathname.slice(base_path.length + 1);
-		if (filename) {
-			is_static_asset =
-				manifest.assets.has(filename) ||
-				manifest.assets.has(filename + '/index.html') ||
-				filename in manifest._.server_assets ||
-				filename + '/index.html' in manifest._.server_assets;
-		}
-
-		let location = pathname.at(-1) === '/' ? stripped_pathname : pathname + '/';
-
-		if (
-			is_static_asset ||
-			prerendered.has(pathname) ||
-			pathname === version_file ||
-			pathname.startsWith(immutable)
-		) {
+		// immutable assets and version.json
+		if (pathname.startsWith(app_path)) {
 			res = await env.ASSETS.fetch(req);
-		} else if (location && prerendered.has(location)) {
-			if (search) location += search;
-			res = new Response('', {
-				status: 308,
-				headers: {
-					location
-				}
-			});
+			if (is_error(res.status)) return res;
+
+			const cache_control = pathname.startsWith(immutable)
+				? 'public, immutable, max-age=31536000'
+				: 'no-cache';
+
+			res.headers.set('cache-control', cache_control);
 		} else {
-			// dynamically-generated pages
-			res = await server.respond(req, {
-				platform: {
-					env,
-					context,
-					// @ts-expect-error webworker types from worktop are not compatible with Cloudflare Workers types
-					caches,
-					cf: req.cf
-				},
-				getClientAddress() {
-					return req.headers.get('cf-connecting-ip');
-				}
-			});
+			const stripped_pathname = pathname.replace(/\/$/, '');
+
+			// /static files, the service worker, and Vite imported server assets
+			let is_static_asset = false;
+			const filename = stripped_pathname.slice(base_path.length + 1);
+			if (filename) {
+				is_static_asset =
+					manifest.assets.has(filename) ||
+					manifest.assets.has(filename + '/index.html') ||
+					filename in manifest._.server_assets ||
+					filename + '/index.html' in manifest._.server_assets;
+			}
+
+			let location = pathname.at(-1) === '/' ? stripped_pathname : pathname + '/';
+
+			if (is_static_asset || prerendered.has(pathname)) {
+				res = await env.ASSETS.fetch(req);
+			} else if (location && prerendered.has(location)) {
+				// trailing slash redirect for prerendered pages
+				if (search) location += search;
+				res = new Response('', {
+					status: 308,
+					headers: {
+						location
+					}
+				});
+			} else {
+				// dynamically-generated pages
+				res = await server.respond(req, {
+					platform: {
+						env,
+						context,
+						// @ts-expect-error webworker types from worktop are not compatible with Cloudflare Workers types
+						caches,
+						cf: req.cf
+					},
+					getClientAddress() {
+						return req.headers.get('cf-connecting-ip');
+					}
+				});
+			}
 		}
 
 		// write to `Cache` only if response is not an error,
 		// let `Cache.save` handle the Cache-Control and Vary headers
 		pragma = res.headers.get('cache-control') || '';
-		return pragma && res.status < 400 ? Cache.save(req, res, context) : res;
+		return pragma && !is_error(res.status) ? Cache.save(req, res, context) : res;
 	}
 };
+
+/**
+ * @param {number} status
+ * @returns {boolean}
+ */
+function is_error(status) {
+	return status > 399;
+}
