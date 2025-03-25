912acff03afbd2a90204fd6e542ba970435b3a4e
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-03-25 07:21:34 +0100

    Skip tests when JS is not enabled

--- packages/kit/test/apps/embedded-widget/test/test.js
+++ packages/kit/test/apps/embedded-widget/test/test.js
@@ -3,6 +3,8 @@ import { test } from '../../../utils.js';
 
 /** @typedef {import('@playwright/test').Response} Response */
 
+test.skip(({ javaScriptEnabled }) => !javaScriptEnabled);
+
 test.describe.configure({ mode: 'parallel' });
 
 test.describe('embedded-widget', () => {
