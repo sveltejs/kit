5ebd41848ac9a554ce5d24c070b502e9a28b1d9b
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-03-25 07:27:23 +0100

    Use same port as vite server for embed script

--- packages/kit/test/apps/embedded-widget/src/hooks.server.ts
+++ packages/kit/test/apps/embedded-widget/src/hooks.server.ts
@@ -5,7 +5,8 @@ export const handle: Handle = async ({ event, resolve }) => {
 	if (event.url.pathname.startsWith('/third-party')) {
 		// simulate a third party page
 		const filePath = 'src/third-party.html';
-		const content = fs.readFileSync(filePath, 'utf-8');
+		// replace the embed script port to use the same port as the current request
+		const content = fs.readFileSync(filePath, 'utf-8').replace('__PORT__', event.url.port);
 		return new Response(content, {
 			headers: {
 				'Content-Type': 'text/html',
