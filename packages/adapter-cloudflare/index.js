ebec1e95694644ba49dc74baeb906f853a30ce41
Tee Ming <chewteeming01@gmail.com>
2025-04-02 10:26:24 +0800

    feat: deprecate `adapter-cloudflare-workers` in favour of `adapter-cloudflare` (#13634)
    * merge adapters
    
    * changeset
    
    * it's valid to not specify a main entry if the user is only deploying static assets
    
    * format
    
    * adjust docs
    
    * also use cf pages env var
    
    * fix migration guide link
    
    * try to fix doc build error
    
    * Update index.d.ts
    
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>
    
    * Update kind-carrots-deliver.md
    
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>
    
    * validate assets key if main key is set
    
    * delete adapter-cloudflare-workers
    
    * adjust docs and workspace for removing cloudflare workers
    
    * fix docs title
    
    * clarify _routes.json belongs to cloudflare pages
    
    * Update packages/adapter-cloudflare/src/worker.js
    
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>
    
    * changes according to feedback
    
    * revert type change
    
    * fix lock file
    
    * fixes
    
    * tests
    
    * fix lockfile
    
    * format
    
    ---------
    
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- packages/adapter-cloudflare/index.js
+++ packages/adapter-cloudflare/index.js
@@ -1,7 +1,8 @@
 import { copyFileSync, existsSync, writeFileSync } from 'node:fs';
-import * as path from 'node:path';
+import path from 'node:path';
+import process from 'node:process';
 import { fileURLToPath } from 'node:url';
-import { getPlatformProxy } from 'wrangler';
+import { getPlatformProxy, unstable_readConfig } from 'wrangler';
 
 /** @type {import('./index.js').default} */
 export default function (options = {}) {
@@ -10,7 +11,7 @@ export default function (options = {}) {
 		async adapt(builder) {
 			if (existsSync('_routes.json')) {
 				throw new Error(
-					"Cloudflare's _routes.json should be configured in svelte.config.js. See https://svelte.dev/docs/kit/adapter-cloudflare#Options-routes"
+					"Cloudflare Pages' _routes.json should be configured in svelte.config.js. See https://svelte.dev/docs/kit/adapter-cloudflare#Options-routes"
 				);
 			}
 
@@ -26,56 +27,87 @@ export default function (options = {}) {
 				);
 			}
 
+			const wrangler_config = validate_config(options.config);
+			const building_for_cloudflare_pages = is_building_for_cloudflare_pages(wrangler_config);
+
+			let dest = builder.getBuildDirectory('cloudflare');
+			let worker_dest = `${dest}/_worker.js`;
+			let assets_binding = 'ASSETS';
+
+			if (building_for_cloudflare_pages) {
+				if (wrangler_config.pages_build_output_dir) {
+					dest = wrangler_config.pages_build_output_dir;
+				}
+			} else {
+				if (wrangler_config.main) {
+					worker_dest = wrangler_config.main;
+				}
+				if (wrangler_config.assets?.directory) {
+					dest = wrangler_config.assets.directory;
+				}
+				if (wrangler_config.assets?.binding) {
+					assets_binding = wrangler_config.assets.binding;
+				}
+			}
+
 			const files = fileURLToPath(new URL('./files', import.meta.url).href);
-			const dest = builder.getBuildDirectory('cloudflare');
 			const tmp = builder.getBuildDirectory('cloudflare-tmp');
 
 			builder.rimraf(dest);
+			builder.rimraf(worker_dest);
 
 			builder.mkdirp(dest);
 			builder.mkdirp(tmp);
 
-			// generate plaintext 404.html first which can then be overridden by prerendering, if the user defined such a page
-			const fallback = path.join(dest, '404.html');
-			if (options.fallback === 'spa') {
-				await builder.generateFallback(fallback);
-			} else {
-				writeFileSync(fallback, 'Not Found');
+			// client assets and prerendered pages
+			const assets_dest = `${dest}${builder.config.kit.paths.base}`;
+			if (
+				building_for_cloudflare_pages ||
+				wrangler_config.assets?.not_found_handling === '404-page'
+			) {
+				// generate plaintext 404.html first which can then be overridden by prerendering, if the user defined such a page
+				const fallback = path.join(assets_dest, '404.html');
+				if (options.fallback === 'spa') {
+					await builder.generateFallback(fallback);
+				} else {
+					writeFileSync(fallback, 'Not Found');
+				}
+			}
+			const client_assets = builder.writeClient(assets_dest);
+			builder.writePrerendered(assets_dest);
+			if (
+				!building_for_cloudflare_pages &&
+				wrangler_config.assets?.not_found_handling === 'single-page-application'
+			) {
+				await builder.generateFallback(path.join(assets_dest, 'index.html'));
 			}
 
-			const dest_dir = `${dest}${builder.config.kit.paths.base}`;
-			const written_files = builder.writeClient(dest_dir);
-			builder.writePrerendered(dest_dir);
-
-			const relativePath = path.posix.relative(dest, builder.getServerDirectory());
+			// worker
+			const worker_dest_dir = path.dirname(worker_dest);
 			writeFileSync(
 				`${tmp}/manifest.js`,
-				`export const manifest = ${builder.generateManifest({ relativePath })};\n\n` +
+				`export const manifest = ${builder.generateManifest({ relativePath: path.posix.relative(tmp, builder.getServerDirectory()) })};\n\n` +
 					`export const prerendered = new Set(${JSON.stringify(builder.prerendered.paths)});\n\n` +
 					`export const base_path = ${JSON.stringify(builder.config.kit.paths.base)};\n`
 			);
-			builder.copy(`${files}/worker.js`, `${dest}/_worker.js`, {
+			builder.copy(`${files}/worker.js`, worker_dest, {
 				replace: {
-					SERVER: `${relativePath}/index.js`,
-					MANIFEST: `${path.posix.relative(dest, tmp)}/manifest.js`
+					SERVER: `${path.posix.relative(worker_dest_dir, builder.getServerDirectory())}/index.js`,
+					MANIFEST: `${path.posix.relative(worker_dest_dir, tmp)}/manifest.js`,
+					ASSETS: assets_binding
 				}
 			});
 
-			writeFileSync(
-				`${dest}/_routes.json`,
-				JSON.stringify(get_routes_json(builder, written_files, options.routes ?? {}), null, '\t')
-			);
-
+			// _headers
 			if (existsSync('_headers')) {
 				copyFileSync('_headers', `${dest}/_headers`);
 			}
-
 			writeFileSync(`${dest}/_headers`, generate_headers(builder.getAppPath()), { flag: 'a' });
 
+			// _redirects
 			if (existsSync('_redirects')) {
 				copyFileSync('_redirects', `${dest}/_redirects`);
 			}
-
 			if (builder.prerendered.redirects.size > 0) {
 				writeFileSync(`${dest}/_redirects`, generate_redirects(builder.prerendered.redirects), {
 					flag: 'a'
@@ -83,6 +115,13 @@ export default function (options = {}) {
 			}
 
 			writeFileSync(`${dest}/.assetsignore`, generate_assetsignore(), { flag: 'a' });
+
+			if (building_for_cloudflare_pages) {
+				writeFileSync(
+					`${dest}/_routes.json`,
+					JSON.stringify(get_routes_json(builder, client_assets, options.routes ?? {}), null, '\t')
+				);
+			}
 		},
 		emulate() {
 			// we want to invoke `getPlatformProxy` only once, but await it only when it is accessed.
@@ -169,7 +208,7 @@ function get_routes_json(builder, assets, { include = ['/*'], exclude = ['<all>'
 
 	const excess = include.length + exclude.length - 100;
 	if (excess > 0) {
-		const message = `Function includes/excludes exceeds _routes.json limits (see https://developers.cloudflare.com/pages/platform/functions/routing/#limits). Dropping ${excess} exclude rules — this will cause unnecessary function invocations.`;
+		const message = `Cloudflare Pages Functions' includes/excludes exceeds _routes.json limits (see https://developers.cloudflare.com/pages/platform/functions/routing/#limits). Dropping ${excess} exclude rules — this will cause unnecessary function invocations.`;
 		builder.log.warn(message);
 
 		exclude.length -= excess;
@@ -219,4 +258,54 @@ _routes.json
 _headers
 _redirects
 `;
+}
+
+/**
+ * @param {string} config_file
+ * @returns {import('wrangler').Unstable_Config}
+ */
+function validate_config(config_file = undefined) {
+	const wrangler_config = unstable_readConfig({ config: config_file });
+
+	// we don't support workers sites
+	if (wrangler_config.site) {
+		throw new Error(
+			`You must remove all \`site\` keys in ${wrangler_config.configPath}. Consult https://svelte.dev/docs/kit/adapter-cloudflare#Migrating-from-Workers-Sites-to-Workers-Static-Assets`
+		);
+	}
+
+	if (is_building_for_cloudflare_pages(wrangler_config)) {
+		return wrangler_config;
+	}
+
+	// probably deploying to Cloudflare Workers
+	if (wrangler_config.main || wrangler_config.assets) {
+		if (!wrangler_config.assets?.directory) {
+			throw new Error(
+				`You must specify the \`assets.directory\` key in ${wrangler_config.configPath}. Consult https://developers.cloudflare.com/workers/static-assets/binding/#directory`
+			);
+		}
+
+		if (!wrangler_config.assets?.binding) {
+			throw new Error(
+				`You must specify the \`assets.binding\` key in ${wrangler_config.configPath}. Consult https://developers.cloudflare.com/workers/static-assets/binding/#binding`
+			);
+		}
+	}
+
+	return wrangler_config;
+}
+
+/**
+ * @param {import('wrangler').Unstable_Config} wrangler_config
+ * @returns {boolean}
+ */
+function is_building_for_cloudflare_pages(wrangler_config) {
+	return (
+		!!process.env.CF_PAGES ||
+		!wrangler_config.configPath ||
+		!!wrangler_config.pages_build_output_dir ||
+		!wrangler_config.main ||
+		!wrangler_config.assets
+	);
 }
