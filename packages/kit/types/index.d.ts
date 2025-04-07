99b8079bfb8e7b403ef3d03db653302b20571bab
George Daskalakis <george.dask97@gmail.com>
2025-04-07 07:31:15 +0300

    fix: allow `HandleServerError` hook to access the ` getRequestEvent` (#13666)
    * fix: allow usage of `getRequestEvent` inside `handleError`
    
    * chore: format and add changeset
    
    * fix: add test for `getReqeuestEvent` in `handleError`
    
    * Update packages/kit/types/index.d.ts
    
    * Update packages/kit/src/runtime/app/server/event.js
    
    * Update packages/kit/src/runtime/app/server/event.js
    
    ---------
    
    Co-authored-by: Tee Ming <chewteeming01@gmail.com>

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -2417,7 +2417,7 @@ declare module '$app/server' {
 	 */
 	export function read(asset: string): Response;
 	/**
-	 * Returns the current `RequestEvent`. Can be used inside `handle`, `load` and actions (and functions called by them).
+	 * Returns the current `RequestEvent`. Can be used inside server hooks, server `load` functions, actions, and endpoints (and functions called by them).
 	 *
 	 * In environments without [`AsyncLocalStorage`](https://nodejs.org/api/async_context.html#class-asynclocalstorage), this must be called synchronously (i.e. not after an `await`).
 	 * @since 2.20.0
