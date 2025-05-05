08a3fcc3ad81c3aac8dd209d0c655f98b8622c12
Tee Ming <chewteeming01@gmail.com>
2025-05-05 11:00:50 +0200

    format

--- packages/adapter-node/test/apps/basic/test/test.js
+++ packages/adapter-node/test/apps/basic/test/test.js
@@ -6,15 +6,15 @@ test('WebSockets work', async ({ page }) => {
 });
 
 test('WebSockets getPeers helper', async ({ page }) => {
-  await page.goto('/ws/helpers');
-  await expect(page.getByText('connected')).toBeVisible();
-  await page.locator('button', { hasText: 'message all peers' }).click();
-  await expect(page.getByText('sent to each peer')).toBeVisible();
+	await page.goto('/ws/helpers');
+	await expect(page.getByText('connected')).toBeVisible();
+	await page.locator('button', { hasText: 'message all peers' }).click();
+	await expect(page.getByText('sent to each peer')).toBeVisible();
 });
 
 test('WebSockets publish helper', async ({ page }) => {
-  await page.goto('/ws/helpers');
-  await expect(page.getByText('connected')).toBeVisible();
-  await page.locator('button', { hasText: 'create user' }).click();
-  await expect(page.getByText('created a new user')).toBeVisible();
+	await page.goto('/ws/helpers');
+	await expect(page.getByText('connected')).toBeVisible();
+	await page.locator('button', { hasText: 'create user' }).click();
+	await expect(page.getByText('created a new user')).toBeVisible();
 });
