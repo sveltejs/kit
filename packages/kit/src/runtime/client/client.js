sveltejs/kit

commit 6261a877ae97ff85d07277c51391d925ed1bd096

fix: hash link to new page focuses the correct element (#10856)
* use window.location to scroll to deep linked element

* changeset

* prettier

* fix

* fix scroll position being changed

* prettier

* found a fix that works!

* improve some old related tests

* forgot to restore scroll pos

packages/kit/src/runtime/client/client.js
@@ -2800,22 +2800,42 @@ function reset_focus() {
 		autofocus.focus();
 	} else {
 		// Reset page selection and focus
-		// We try to mimic browsers' behaviour as closely as possible by targeting the
-		// first scrollable region, but unfortunately it's not a perfect match — e.g.
-		// shift-tabbing won't immediately cycle up from the end of the page on Chromium
-		// See https://html.spec.whatwg.org/multipage/interaction.html#get-the-focusable-area
-		const root = document.body;
-		const tabindex = root.getAttribute('tabindex');
+		if (location.hash && document.querySelector(location.hash)) {
+			const { x, y } = scroll_state();
 
-		root.tabIndex = -1;
-		// @ts-expect-error
-		root.focus({ preventScroll: true, focusVisible: false });
+			setTimeout(() => {
+				const history_state = history.state;
+				// Mimic the browsers' behaviour and set the sequential focus navigation
+				// starting point to the fragment identifier
+				location.replace(location.hash);
+				// but Firefox has a bug that sets the history state as null so we
+				// need to restore the history state
+				// See https://bugzilla.mozilla.org/show_bug.cgi?id=1199924
+				history.replaceState(history_state, '', location.hash);
 
-		// restore `tabindex` as to prevent `root` from stealing input from elements
-		if (tabindex !== null) {
-			root.setAttribute('tabindex', tabindex);
+				// Scroll management has already happened earlier so we need to restore
+				// the scroll position after setting the sequential focus navigation starting point
+				scrollTo(x, y);
+			});
 		} else {
-			root.removeAttribute('tabindex');
+			// We try to mimic browsers' behaviour as closely as possible by targeting the
+			// first scrollable region, but unfortunately it's not a perfect match — e.g.
+			// shift-tabbing won't immediately cycle up from the end of the page on Chromium
+			// See https://html.spec.whatwg.org/multipage/interaction.html#get-the-focusable-area
+			const root = document.body;
+			const tabindex = root.getAttribute('tabindex');
+
+			root.tabIndex = -1;
+			// @ts-expect-error options.focusVisible is only supported in Firefox
+			// See https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus#browser_compatibility
+			root.focus({ preventScroll: true, focusVisible: false });
+
+			// restore `tabindex` as to prevent `root` from stealing input from elements
+			if (tabindex !== null) {
+				root.setAttribute('tabindex', tabindex);
+			} else {
+				root.removeAttribute('tabindex');
+			}
 		}
 
 		// capture current selection, so we can compare the state after

packages/kit/test/apps/basics/src/routes/routing/focus/+page.svelte
@@ -1,0 +1,1 @@
+<a href="/routing/focus/a#p">click me!</a>

packages/kit/test/apps/basics/src/routes/routing/focus/a/+page.svelte
@@ -1,0 +1,4 @@
+<button>button 1</button>
+<button>button 2</button>
+<p id="p">cannot be focused</p>
+<button id="button3">button 3</button>

packages/kit/test/apps/basics/test/cross-platform/client.test.js
@@ -806,14 +806,31 @@ test.describe('Routing', () => {
 		await expect(page.getByRole('textbox')).toBeVisible();
 	});
 
-	test('back button returns to previous route when previous route has been navigated to via hash anchor', async ({
+	test('sequential focus navigation starting point is set correctly on navigation', async ({
 		page,
-		clicknav
+		browserName
+	}) => {
+		const tab = browserName === 'webkit' ? 'Alt+Tab' : 'Tab';
+		await page.goto('/routing/focus');
+		await page.locator('[href="/routing/focus/a#p"]').click();
+		await page.waitForURL('**/routing/focus/a#p');
+		expect(await page.evaluate(() => (document.activeElement || {}).nodeName)).toBe('BODY');
+		await page.keyboard.press(tab);
+		await expect(page.locator('#button3')).toBeFocused();
+	});
+
+	test('back button returns to previous route when previous route was navigated to via hash anchor', async ({
+		page,
+		clicknav,
+		baseURL
 	}) => {
 		await page.goto('/routing/hashes/a');
 
 		await page.locator('[href="#hash-target"]').click();
+		expect(page.url()).toBe(`${baseURL}/routing/hashes/a#hash-target`);
+
 		await clicknav('[href="/routing/hashes/b"]');
+		expect(await page.textContent('h1')).toBe('b');
 
 		await expect(page.locator('h1')).toHaveText('b');
 		await page.goBack();
@@ -828,10 +845,13 @@ test.describe('Routing', () => {
 		await page.goto('/routing/hashes/a');
 
 		await clicknav('[href="#hash-target"]');
+		expect(page.url()).toBe(`${baseURL}/routing/hashes/a#hash-target`);
+
 		await clicknav('[href="#replace-state"]');
+		expect(page.url()).toBe(`${baseURL}/routing/hashes/a#replace-state`);
 
 		await page.goBack();
-		expect(await page.url()).toBe(`${baseURL}/routing/hashes/a`);
+		expect(page.url()).toBe(`${baseURL}/routing/hashes/a`);
 	});
 
 	test('does not normalize external path', async ({ page, start_server }) => {

commit ab6787498c7c1d6a3431c516dabc5ae487fcf1e4

fix: avoid setting sequential focus navigation starting point when using hash router (#13861)
This PR fixes a regression caused by #10856 which broke hash router navigation.

We can't apply the same fix for hash routing because location.replace doesn't work since the browser interprets the entire hash as the element it should focus but we're using it as a pathname replacement. element.focus() doesn't work on Safari and Ubuntu Firefox so that's moot too.

packages/kit/src/runtime/client/client.js
@@ -2800,7 +2800,8 @@ function reset_focus() {
 		autofocus.focus();
 	} else {
 		// Reset page selection and focus
-		if (location.hash && document.querySelector(location.hash)) {
+		// TODO: find a fix that works with hash routing too
+		if (!app.hash && location.hash && document.querySelector(location.hash)) {
 			const { x, y } = scroll_state();
 
 			setTimeout(() => {