d70e99bcd09d2740b7c6b8f281f6f624e776f178
jycouet <jycouet@gmail.com>
2025-06-11 23:07:33 +0200

    let's be nice and format things

--- packages/kit/test/apps/options/source/pages/resolve-route/site/[id]/+page.server.ts
+++ packages/kit/test/apps/options/source/pages/resolve-route/site/[id]/+page.server.ts
@@ -1,6 +1,6 @@
-import type { Actions } from '../$types.js'
+import type { Actions } from '../$types.js';
 
 export const actions = {
 	update: async () => {},
-	delete: async () => {},
-} satisfies Actions
+	delete: async () => {}
+} satisfies Actions;
