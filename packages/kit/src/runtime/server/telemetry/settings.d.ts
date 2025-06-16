8827f073aa8a2213400b2f7d23ff91eaabfea2e7
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-06-16 11:56:15 -0600

    format

--- packages/kit/src/runtime/server/telemetry/settings.d.ts
+++ packages/kit/src/runtime/server/telemetry/settings.d.ts
@@ -4,8 +4,8 @@
 // This is meant to be both flexible for custom app requirements (metadata)
 // and extensible for standardization (example: functionId, more to come).
 export type TelemetrySettings = {
-  /**
-   * Enable or disable telemetry. Disabled by default while experimental.
-   */
-  enabled?: boolean;
+	/**
+	 * Enable or disable telemetry. Disabled by default while experimental.
+	 */
+	enabled?: boolean;
 };
