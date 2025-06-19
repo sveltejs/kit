fef086e6f8907ae3f03af25b1101f1fc29c43a97
Chew Tee Ming <chew.tee.ming@nindatech.com>
2025-06-19 12:12:17 +0800

    last one

--- packages/kit/src/exports/vite/build/build_service_worker.js
+++ packages/kit/src/exports/vite/build/build_service_worker.js
@@ -103,6 +103,7 @@ export async function build_service_worker(
 				},
 				output: {
 					// .mjs so that esbuild doesn't incorrectly inject `export` https://github.com/vitejs/vite/issues/15379
+					// @ts-expect-error `vite.rolldownVersion` only exists in `rolldown-vite`
 					entryFileNames: `service-worker.${vite.rolldownVersion ? 'js' : 'mjs'}`,
 					assetFileNames: `${kit.appDir}/immutable/assets/[name].[hash][extname]`,
 					inlineDynamicImports: true
