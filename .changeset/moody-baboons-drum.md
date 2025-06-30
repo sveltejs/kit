ab83d46bba17423d645f30b053dcd20f3fc853ea
Chew Tee Ming <chew.tee.ming@nindatech.com>
2025-06-30 11:12:10 +0800

    add test

--- .changeset/moody-baboons-drum.md
+++ .changeset/moody-baboons-drum.md
@@ -2,4 +2,4 @@
 '@sveltejs/adapter-cloudflare': patch
 ---
 
-fix: deprecate `platform.context` in favor of `platform.ctx` to align with Cloudflare naming convention
+fix: deprecate `platform.context` in favor of `platform.ctx` to align with Cloudflare's naming convention
--- packages/adapter-cloudflare/test/apps/workers/test/test.js
+++ packages/adapter-cloudflare/test/apps/workers/test/test.js
@@ -1,6 +1,11 @@
 import { expect, test } from '@playwright/test';
 
-test('worker works', async ({ page }) => {
+test('worker', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.locator('h1')).toContainText('Sum: 3');
+});
+
+test('ctx', async ({ request }) => {
+	const res = await request.get('/ctx');
+	expect(await res.text()).toBe('ctx works');
 });
