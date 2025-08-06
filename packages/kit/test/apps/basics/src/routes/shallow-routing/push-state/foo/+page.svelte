205c678ab9464f54349fcc89a87b36941502f43b
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-08-06 22:57:23 +0200

    feat: use devalue for pushState/replaceState

---
+++ packages/kit/test/apps/basics/src/routes/shallow-routing/push-state/foo/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { pushState } from '$app/navigation';
+	import { page } from '$app/state';
+	import { Foo } from '$lib';
+</script>
+
+<button onclick={() => pushState('', { foo: new Foo('it works?') })}>do the thing</button>
+
+<p>foo: {page.state.foo?.bar() ?? 'nope'}</p>
