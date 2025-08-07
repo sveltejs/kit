4aa769e2efdf7c0d49ebc81b89d1c0b095080964
Ihar Dziamidau <id5346216@gmail.com>
2025-08-07 13:09:47 +0300

    feat: adapter-node bundle sourcemap option

--- packages/adapter-node/index.d.ts
+++ packages/adapter-node/index.d.ts
@@ -9,6 +9,11 @@ interface AdapterOptions {
 	out?: string;
 	precompress?: boolean;
 	envPrefix?: string;
+	/**
+	 * If set to `false`, sourcemaps for bundled files will not be generated.
+	 * @default true
+	 */
+	bundleSourcemap?: boolean;
 }
 
 export default function plugin(options?: AdapterOptions): Adapter;
--- packages/adapter-node/index.js
+++ packages/adapter-node/index.js
@@ -9,7 +9,7 @@ const files = fileURLToPath(new URL('./files', import.meta.url).href);
 
 /** @type {import('./index.js').default} */
 export default function (opts = {}) {
-	const { out = 'build', precompress = true, envPrefix = '' } = opts;
+	const { out = 'build', precompress = true, envPrefix = '', bundleSourcemap = true } = opts;
 
 	return {
 		name: '@sveltejs/adapter-node',
@@ -75,7 +75,7 @@ export default function (opts = {}) {
 			await bundle.write({
 				dir: `${out}/server`,
 				format: 'esm',
-				sourcemap: true,
+				sourcemap: bundleSourcemap,
 				chunkFileNames: 'chunks/[name]-[hash].js'
 			});
 
