50f3f1c6d0a29f3cc4e0c9665594234eb31d21c0
Rich Harris <rich.harris@vercel.com>
2025-08-11 15:58:48 -0400

    typo

--- packages/kit/src/runtime/server/cookie.js
+++ packages/kit/src/runtime/server/cookie.js
@@ -41,7 +41,7 @@ export function get_cookies(request, url) {
 	/** @type {Record<string, import('./page/types.js').Cookie>} */
 	const new_cookies = {};
 
-	/** @type {import('cookie').CookieSerializeOptions}*/
+	/** @type {import('cookie').CookieSerializeOptions} */
 	const defaults = {
 		httpOnly: true,
 		sameSite: 'lax',
