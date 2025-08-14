faeea6b4ec93c700f37e5e21c37c1c61cae0892a
Tee Ming <chewteeming01@gmail.com>
2025-08-14 10:45:23 +0800

    Apply suggestion from @eltigerchino

--- packages/kit/test/apps/basics/unit-test/client.spec.js
+++ packages/kit/test/apps/basics/unit-test/client.spec.js
@@ -2,6 +2,6 @@ import { expect, test } from 'vitest';
 
 // some users will import these modules to mock them in Vitest so we need to test
 // that importing them works
-test('$app/navigation exports work', async () => {
+test('$app/navigation can be imported', async () => {
 	await expect(import('$app/navigation')).resolves.not.toThrow();
 });
