5c4ec5fa8214a14d18f6077bfe2a7eb9d4061c63
Elliott Johnson <elliott.johnson@vercel.com>
2025-08-18 13:45:58 -0600

    fix: Use correct flag for server tracing (#14250)
    * fix: Use correct flag for server tracing
    
    * changeset
    
    * docs-and-error
    
    * changeset
    
    * unit tests
    
    * typo

---
+++ .changeset/ready-points-clap.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/kit': patch
+---
+
+fix: use correct flag for server tracing
--- packages/kit/src/runtime/telemetry/otel.js
+++ packages/kit/src/runtime/telemetry/otel.js
@@ -15,7 +15,7 @@ if (__SVELTEKIT_SERVER_TRACING_ENABLED__) {
 		})
 		.catch(() => {
 			throw new Error(
-				'Tracing is enabled (see `config.kit.experimental.instrumentation.server` in your svelte.config.js), but `@opentelemetry/api` is not available. Have you installed it?'
+				'Tracing is enabled (see `config.kit.experimental.instrumentation.server` in your svelte.config.js), but `@opentelemetry/api` is not available. This error will likely resolve itself when you set up your tracing instrumentation in `instrumentation.server.js`. For more information, see https://svelte.dev/docs/kit/observability#opentelemetry-api'
 			);
 		});
 }
