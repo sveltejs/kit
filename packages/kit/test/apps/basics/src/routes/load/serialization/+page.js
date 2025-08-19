5aab6bf434ff08ee57c86aa3f80cee3d455c21df
Rich Harris <rich.harris@vercel.com>
2025-08-19 13:52:40 -0400

    ugh

--- packages/kit/test/apps/basics/src/routes/load/serialization/+page.js
+++ packages/kit/test/apps/basics/src/routes/load/serialization/+page.js
@@ -7,7 +7,9 @@ export async function load({ fetch, data, url }) {
 
 	// check that this doesn't mutate the original object
 	// and make the server data unserializable
+	// @ts-expect-error
 	data.sum = () => a + b;
 
+	// @ts-expect-error
 	return { a, b, c: data.sum() };
 }
