1aa8865641309c3eaaf494628d07bcad89001595
Rich Harris <rich.harris@vercel.com>
2025-08-19 13:17:35 -0400

    failing test

--- packages/kit/test/apps/basics/src/routes/load/serialization/+page.js
+++ packages/kit/test/apps/basics/src/routes/load/serialization/+page.js
@@ -5,5 +5,9 @@ export async function load({ fetch, data, url }) {
 	const res = await fetch(new URL('/load/serialization/fetched-from-shared.json', url.origin));
 	const { b } = await res.json();
 
-	return { a, b, c: a + b };
+	// check that this doesn't mutate the original object
+	// and make the server data unserializable
+	data.sum = () => a + b;
+
+	return { a, b, c: data.sum() };
 }
