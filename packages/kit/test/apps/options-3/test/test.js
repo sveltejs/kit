926a55ecab7e209007bcbb461aa765a38f00eec7
Chew Tee Ming <chew.tee.ming@nindatech.com>
2025-08-20 14:07:39 +0800

    add test for inlined apps

---
+++ packages/kit/test/apps/options-3/test/test.js
@@ -0,0 +1,18 @@
+import path from 'node:path';
+import process from 'node:process';
+import { fileURLToPath } from 'node:url';
+import { expect } from '@playwright/test';
+import { test } from '../../../utils.js';
+
+/** @typedef {import('@playwright/test').Response} Response */
+
+test.describe.configure({ mode: 'parallel' });
+
+test.describe("bundleStrategy: 'inline'", () => {
+	test.skip(({ javaScriptEnabled }) => !javaScriptEnabled || !!process.env.DEV);
+
+	test('serialization works with streaming', async ({ page }) => {
+		await page.goto('/serialization-stream');
+		await expect(page.locator('h1', { hasText: 'It works!' })).toBeVisible();
+	});
+});
