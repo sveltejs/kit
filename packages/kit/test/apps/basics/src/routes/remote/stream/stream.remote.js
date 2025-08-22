96ea81cb592b6e25a74ad50d10bf3f17485743a1
Simon Holthausen <simon.holthausen@vercel.com>
2025-08-22 15:39:51 +0200

    oops

--- packages/kit/test/apps/basics/src/routes/remote/stream/stream.remote.js
+++ packages/kit/test/apps/basics/src/routes/remote/stream/stream.remote.js
@@ -1,12 +1,23 @@
 import { command, query } from '$app/server';
 
+// TODO 3.0 remove this once we support a high enough version of Node.js
+function withResolvers() {
+	/** @type {any} */
+	let resolve, reject;
+	const promise = new Promise((res, rej) => {
+		resolve = res;
+		reject = rej;
+	});
+	return { promise, resolve, reject };
+}
+
 let i = 0;
-let p = Promise.withResolvers();
+let p = withResolvers();
 
 export const next = command(() => {
 	i++;
 	p.resolve();
-	p = Promise.withResolvers();
+	p = withResolvers();
 });
 
 export const time = query.stream(async function* () {
