fe10ccb309ca42f98bb4e62f317aa58b225c21c7
Patrick <me@patrick.id>
2025-08-26 18:37:30 +0200

    use `DEV` from `esm-env`

--- packages/kit/src/runtime/server/utils.js
+++ packages/kit/src/runtime/server/utils.js
@@ -48,7 +48,7 @@ export function allowed_methods(mod) {
  * @param {import('types').SSROptions} options
  */
 export function get_global_name(options) {
-	return __SVELTEKIT_DEV__ ? '__sveltekit_dev' : `__sveltekit_${options.version_hash}`;
+	return DEV ? '__sveltekit_dev' : `__sveltekit_${options.version_hash}`;
 }
 
 /**
