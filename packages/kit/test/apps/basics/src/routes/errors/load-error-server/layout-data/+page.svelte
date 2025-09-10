7c25f525159c00cf5d7471cc2dc41e6d1ae2d8d3
Patrick <Patrick@ShowYou.us>
2025-09-10 04:23:32 +0200

    better test without needing to change others

---
+++ packages/kit/test/apps/basics/src/routes/errors/load-error-server/layout-data/+page.svelte
@@ -0,0 +1,5 @@
+
+	test('server-side error from load() still has layout data', async ({ page }) => {
+		await page.goto('/errors/load-error-server/layout-data');
+		expect(await page.textContent('#error-layout-data')).toBe('42');
+	});
