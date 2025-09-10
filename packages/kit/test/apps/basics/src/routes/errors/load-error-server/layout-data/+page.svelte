d0f6a59d1911ac20fdbe35cea98775137ca3ef35
Patrick <Patrick@ShowYou.us>
2025-09-10 04:26:42 +0200

    move test to correct file

--- packages/kit/test/apps/basics/src/routes/errors/load-error-server/layout-data/+page.svelte
+++
@@ -1,5 +0,0 @@
-
-	test('server-side error from load() still has layout data', async ({ page }) => {
-		await page.goto('/errors/load-error-server/layout-data');
-		expect(await page.textContent('#error-layout-data')).toBe('42');
-	});
