e5ac4a3085e5fee84e0c4c24b058788fdef9f278
Simon H <5968653+dummdidumm@users.noreply.github.com>
2025-09-11 18:10:39 +0200

    fix: fail prerendering when remote function fails (#14365)
    * fix: fail prerendering when remote function fails
    
    fixes #14242
    
    * new test app
    
    * add test
    
    * repair lockfile
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>

--- packages/kit/src/runtime/server/remote.js
+++ packages/kit/src/runtime/server/remote.js
@@ -176,13 +176,19 @@ async function handle_remote_call_internal(event, state, options, manifest, id) 
 			});
 		}
 
+		const status =
+			error instanceof HttpError || error instanceof SvelteKitError ? error.status : 500;
+
 		return json(
 			/** @type {RemoteFunctionResponse} */ ({
 				type: 'error',
 				error: await handle_error_and_jsonify(event, state, options, error),
-				status: error instanceof HttpError || error instanceof SvelteKitError ? error.status : 500
+				status
 			}),
 			{
+				// By setting a non-200 during prerendering we fail the prerender process (unless handleHttpError handles it).
+				// Errors at runtime will be passed to the client and are handled there
+				status: state.prerendering ? status : undefined,
 				headers: {
 					'cache-control': 'private, no-store'
 				}
