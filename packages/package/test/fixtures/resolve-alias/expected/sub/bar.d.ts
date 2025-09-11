b6cb2e678c3a2815ad1fc3a21d8aea7f1c297851
Simon Holthausen <simon.holthausen@vercel.com>
2025-09-12 00:48:59 +0200

    fix: better alias resolution
    The fix in #13351 solved some issues but introduced others. This reworks the aliasing further to better check against the patterns that come up.
    
    Fixes #13532
    Fixes #13436

--- packages/package/test/fixtures/resolve-alias/expected/sub/bar.d.ts
+++ packages/package/test/fixtures/resolve-alias/expected/sub/bar.d.ts
@@ -1,3 +1,6 @@
+import type { Baz } from '../baz';
 export declare const dynamic: () => Promise<typeof import('../Test.svelte')>;
 export declare const bar1: import('./foo').Foo;
-export declare const bar2: import('../baz').Baz;
+export declare const bar2: Baz;
+export declare const bar3: Baz;
+export declare const bar4: import('./foo').Foo;
