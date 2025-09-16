d2ada93973086254ebc1650701e7a4aad31ea6da
Simon Holthausen <simon.holthausen@vercel.com>
2025-09-16 20:41:46 +0200

    fix test

--- packages/kit/test/apps/basics/src/routes/remote/+page.js
+++ packages/kit/test/apps/basics/src/routes/remote/+page.js
@@ -1,8 +1,6 @@
-import { q } from './accessing-env.remote';
 import { echo } from './query-command.remote';
 
 export async function load() {
-	q();
 	return {
 		echo_result: await echo('Hello world')
 	};
