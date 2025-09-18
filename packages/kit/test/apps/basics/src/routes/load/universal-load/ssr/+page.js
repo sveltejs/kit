c1b4d66c04102c50419d502b28df2fe9577333a9
seorii <me@seorii.page>
2025-09-18 14:44:06 +0000

    fix: fix test case

--- packages/kit/test/apps/basics/src/routes/load/universal-load/ssr/+page.js
+++ packages/kit/test/apps/basics/src/routes/load/universal-load/ssr/+page.js
@@ -2,5 +2,7 @@ import { browser } from '$app/environment';
 
 /** @type {import('@sveltejs/kit').Load} */
 export async function load({ server }) {
-	return { correct: !browser && !!server };
+	return {
+		correct: browser === !server && (!server || ((await server.request.arrayBuffer()) && true))
+	};
 }
