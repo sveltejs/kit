9790e4086f7a08399de3d8f7c13f89a7c6a14861
Rich Harris <rich.harris@vercel.com>
2025-09-20 10:46:41 -0400

    fix

--- packages/kit/src/runtime/app/paths/internal/server.js
+++ packages/kit/src/runtime/app/paths/internal/server.js
@@ -1,5 +1,5 @@
 export let base = __SVELTEKIT_PATHS_BASE__;
-export let assets = __SVELTEKIT_PATHS_ASSETS__;
+export let assets = __SVELTEKIT_PATHS_ASSETS__ || base;
 export const app_dir = __SVELTEKIT_APP_DIR__;
 export const relative = __SVELTEKIT_PATHS_RELATIVE__;
 
