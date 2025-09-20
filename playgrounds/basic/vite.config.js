a4d4e2bfd230320caacc17c98c9e294292c3e80b
Rich Harris <rich.harris@vercel.com>
2025-09-20 09:03:34 -0400

    revert

--- playgrounds/basic/vite.config.js
+++ playgrounds/basic/vite.config.js
@@ -1,9 +1,8 @@
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
-import inspect from 'vite-plugin-inspect';
 
 export default {
-	plugins: [inspect(), enhancedImages(), sveltekit()],
+	plugins: [enhancedImages(), sveltekit()],
 	server: {
 		fs: {
 			allow: ['../../packages/kit']
