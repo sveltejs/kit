d68689e3f8f2cd86e059d09df71087ed65d008c7
Rich Harris <rich.harris@vercel.com>
2025-09-21 20:01:52 -0400

    remove

--- packages/kit/src/types/internal.d.ts
+++ packages/kit/src/types/internal.d.ts
@@ -598,7 +598,6 @@ export interface RequestState {
 	};
 	form_instances?: Map<any, any>;
 	remote_cache?: Map<RemoteInfo, Record<string, MaybePromise<any>>>;
-	remote_data?: Record<string, MaybePromise<any>>;
 	refreshes?: Record<string, Promise<any>>;
 	is_endpoint_request?: boolean;
 }
