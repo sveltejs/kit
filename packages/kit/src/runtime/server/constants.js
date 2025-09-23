13462681227bc84eaccea743c45eb9567fa448b7
Patrick <Patrick@ShowYou.us>
2025-09-23 22:06:24 +0200

    Revert "hardening webcontainer detection based on @webcontainer/env"
    This reverts commit 0f5e744c5a0f76f0df086171cfc48f98df9cd0f7.

--- packages/kit/src/runtime/server/constants.js
+++ packages/kit/src/runtime/server/constants.js
@@ -2,12 +2,4 @@ import { DEV } from 'esm-env';
 import process from 'node:process';
 
 export const NULL_BODY_STATUS = [101, 103, 204, 205, 304];
-
-export const IN_WEBCONTAINER =
-	DEV &&
-	!!process.versions.webcontainer &&
-	// @ts-expect-error
-	(await import('@blitz/internal/env')).then(
-		(/** @type {any} */ mod) => !!mod,
-		() => false
-	);
+export const IN_WEBCONTAINER = DEV && !!process.versions.webcontainer;
