e5784e66d7a38da62bca15126236555125da2dc0
Rich Harris <rich.harris@vercel.com>
2025-09-24 08:45:33 -0400

    try this

--- packages/kit/src/runtime/server/constants.js
+++ packages/kit/src/runtime/server/constants.js
@@ -1,5 +1,4 @@
-import { DEV } from 'esm-env';
-import process from 'node:process';
-
 export const NULL_BODY_STATUS = [101, 103, 204, 205, 304];
-export const IN_WEBCONTAINER = !!process.versions.webcontainer;
+
+// eslint-disable-next-line n/prefer-global/process
+export const IN_WEBCONTAINER = !!globalThis.process?.versions?.webcontainer;
