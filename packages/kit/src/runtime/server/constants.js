812596c02ef1da2e5af222f42d61697a31ebc063
Rich Harris <hello@rich-harris.dev>
2025-09-24 08:38:56 -0400

    Update packages/kit/src/runtime/server/constants.js
    Co-authored-by: Tee Ming <chewteeming01@gmail.com>

--- packages/kit/src/runtime/server/constants.js
+++ packages/kit/src/runtime/server/constants.js
@@ -2,4 +2,4 @@ import { DEV } from 'esm-env';
 import process from 'node:process';
 
 export const NULL_BODY_STATUS = [101, 103, 204, 205, 304];
-export const IN_WEBCONTAINER = DEV && !!process.versions.webcontainer;
+export const IN_WEBCONTAINER = !!process.versions.webcontainer;
