53422562ef309111097ef087736e84268004c004
Tee Ming Chew <chew.tee.ming@nindatech.com>
2025-09-30 11:59:24 +0800

    add test

--- packages/kit/test/apps/basics/src/routes/endpoint-output/head-handler/+server.js
+++ packages/kit/test/apps/basics/src/routes/endpoint-output/head-handler/+server.js
@@ -1,3 +1,9 @@
+// The GET handler is included alongside the HEAD handler to test that HEAD
+// is not included twice in the `allow` header list of allowed methods
+export function GET() {
+	return new Response('Hello world!');
+}
+
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export function HEAD() {
 	return new Response('', {
