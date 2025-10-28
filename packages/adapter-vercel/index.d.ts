79ad8866a6c5de57b6009b006135c653c8e5dce5
Elliott Johnson <elliott.johnson@vercel.com>
2025-10-28 14:35:14 -0600

    chore: improve runtime validation for adapter-vercel (#14838)
    * chore: improve runtime validation for adapter-vercel
    
    * Update packages/adapter-vercel/utils.js
    
    Co-authored-by: Simon H <5968653+dummdidumm@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Simon H <5968653+dummdidumm@users.noreply.github.com>

--- packages/adapter-vercel/index.d.ts
+++ packages/adapter-vercel/index.d.ts
@@ -1,5 +1,6 @@
 import { Adapter } from '@sveltejs/kit';
 import './ambient.js';
+import { RuntimeConfigKey } from './utils.js';
 
 export default function plugin(config?: Config): Adapter;
 
@@ -8,7 +9,7 @@ export interface ServerlessConfig {
 	 * Whether to use [Edge Functions](https://vercel.com/docs/concepts/functions/edge-functions) (`'edge'`) or [Serverless Functions](https://vercel.com/docs/concepts/functions/serverless-functions) (`'nodejs18.x'`, `'nodejs20.x'` etc).
 	 * @default Same as the build environment
 	 */
-	runtime?: `nodejs${number}.x` | `experimental_bun1.x`;
+	runtime?: Exclude<RuntimeConfigKey, 'edge'>;
 	/**
 	 * To which regions to deploy the app. A list of regions.
 	 * More info: https://vercel.com/docs/concepts/edge-network/regions
