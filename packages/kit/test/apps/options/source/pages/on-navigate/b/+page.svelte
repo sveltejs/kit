08d06a8ec98a137e7321b4499e50535e8cb14e16
Tee Ming Chew <chew.tee.ming@nindatech.com>
2025-10-28 14:07:42 +0800

    add test

---
+++ packages/kit/test/apps/options/source/pages/on-navigate/b/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { onMount } from 'svelte';
+
+	await new Promise((resolve) => setTimeout(resolve, 500));
+
+	onMount(() => {
+		console.log('mounted');
+	});
+</script>
+
+<h1>Page B</h1>
