49a6e561d9d3de0b3fd4055cb7c8c73fb3453355
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-10-28 20:26:11 -0700

    chore: enable commented out test

--- packages/enhanced-img/test/apps/basics/src/routes/+page.svelte
+++ packages/enhanced-img/test/apps/basics/src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script>
-	import logo from './logo.png';
+	import logo from './logo.png?enhanced';
 </script>
 
 <enhanced:img id="birds" src="./birds.jpg" alt="birds" />
-<!--
 <enhanced:img id="logo" src={logo} alt="Svelte logo" />
--->
