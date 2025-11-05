fe8d72fffdc6b3d5759cdef874484dcd87f0e349
Elliott Johnson <hello@ell.iott.dev>
2025-11-05 10:10:25 -0700

    fix

--- playgrounds/basic/src/routes/+page.svelte
+++ playgrounds/basic/src/routes/+page.svelte
@@ -1,17 +1,4 @@
 <script lang="ts">
-	import { get_delayed, increment } from './test.remote';
-
-	const delayed = get_delayed();
-
-	$inspect(delayed === get_delayed());
 </script>
 
-<svelte:boundary><p>awaited: {await delayed}</p></svelte:boundary>
-<p>current: {delayed.current}</p>
-<p>loading: {delayed.loading}</p>
-<p>ready: {delayed.ready}</p>
-<p>error: {delayed.error}</p>
-<button onclick={() => increment().updates(get_delayed().withOverride((num) => num + 1))}
-	>Increment</button
->
-{delayed === get_delayed()}
+<h1>hello!</h1>
