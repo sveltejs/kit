390ea98e00958e737b25c878c2e34f61fdb02aa6
Simon Holthausen <simon.holthausen@vercel.com>
2025-11-20 13:25:48 +0100

    no test necessary on this one

--- packages/kit/test/apps/basics/src/routes/headers/server-timing/+layout.server.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('./$types').LayoutServerLoad} */
-export function load({ setHeaders }) {
-	setHeaders({
-		'server-timing': 'db;dur=53'
-	});
-}
--- packages/kit/test/apps/basics/src/routes/headers/server-timing/sub/+page.server.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('./$types').PageServerLoad} */
-export function load({ setHeaders }) {
-	setHeaders({
-		'server-timing': 'external-api;dur=250'
-	});
-}
