c805b36b2824b3f1c515a9f8188a6095d09682b8
Matt DeKok <matt@dekok.dev>
2025-11-23 16:27:15 -0600

    fix generated types

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -2050,15 +2050,15 @@ declare module '@sveltejs/kit' {
 		get pending(): number;
 		/** Access form fields using object notation */
 		fields: IsAny<Input> extends true
-		? RecursiveFormFields
-		: Input extends void
-			? {
-					/** Validation issues, if any */
-					issues(): RemoteFormIssue[] | undefined;
-					/** Validation issues belonging to this or any of the fields that belong to it, if any */
-					allIssues(): RemoteFormIssue[] | undefined;
-				}
-			: RemoteFormFields<Input>;
+			? RecursiveFormFields
+			: Input extends void
+				? {
+						/** Validation issues, if any */
+						issues(): RemoteFormIssue[] | undefined;
+						/** Validation issues belonging to this or any of the fields that belong to it, if any */
+						allIssues(): RemoteFormIssue[] | undefined;
+					}
+				: RemoteFormFields<Input>;
 		/** Spread this onto a `<button>` or `<input type="submit">` */
 		buttonProps: {
 			type: 'submit';
@@ -2401,7 +2401,7 @@ declare module '@sveltejs/kit' {
 
 	type TrailingSlash = 'never' | 'always' | 'ignore';
 
-	type IsAny<T> = 0 extends (1 & T) ? true : false;
+	type IsAny<T> = 0 extends 1 & T ? true : false;
 	interface Asset {
 		file: string;
 		size: number;
