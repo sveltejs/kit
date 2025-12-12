a3442ea27c0c4188535c3f76f47dcec6d5497d04
Elliott Johnson <hello@ell.iott.dev>
2025-12-12 16:56:42 -0700

    polyfill withResolvers

--- packages/kit/test/apps/async/src/hooks.server.js
+++ packages/kit/test/apps/async/src/hooks.server.js
@@ -9,3 +9,13 @@ export const handleError = ({ error: e, status, message }) => {
 
 	return { message: `${error.message} (${status} ${message})` };
 };
+
+// @ts-ignore this doesn't exist in old Node TODO remove SvelteKit 3 (same in test-basics)
+Promise.withResolvers ??= () => {
+	const d = {};
+	d.promise = new Promise((resolve, reject) => {
+		d.resolve = resolve;
+		d.reject = reject;
+	});
+	return d;
+};
