3c4e6b1619e2940379913ba48d86ef951db127ef
Tee Ming <chewteeming01@gmail.com>
2026-01-02 15:53:07 +0800

    format

--- packages/kit/test/apps/basics/vite.config.js
+++ packages/kit/test/apps/basics/vite.config.js
@@ -1,6 +1,6 @@
 import * as path from 'node:path';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { playwright } from '@vitest/browser-playwright'
+import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
