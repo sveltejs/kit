8ca47691f2d841cd88c3f91efccf3dc72fd64331
Tee Ming <chewteeming01@gmail.com>
2026-01-15 18:58:48 +0800

    pass port in through npm script

--- packages/adapter-netlify/test/preview.js
+++ packages/adapter-netlify/test/preview.js
@@ -2,18 +2,21 @@
 // Don't include this file in type checking because @netlify/dev has transitive imports with type errors
 import { NetlifyDev } from '@netlify/dev';
 import http from 'node:http';
+import process from 'node:process';
 import { getRequest, setResponse } from '@sveltejs/kit/node';
 
 const netlifyDev = new NetlifyDev({});
 
 const serverReady = netlifyDev.start();
 
+const port = process.env.PORT ? +process.env.PORT : 8888;
+
 http
 	.createServer(async (req, res) => {
 		await serverReady;
-		const request = await getRequest({ request: req, base: 'http://localhost:8888' });
+		const request = await getRequest({ request: req, base: `http://localhost:${port}` });
 		const response =
 			(await netlifyDev.handle(request)) ?? new Response('Not Found', { status: 404 });
 		await setResponse(res, response);
 	})
-	.listen(8888);
+	.listen(port);
