767093cb2044e60db3eddd70fdc1a34c182dfc80
Tee Ming <chewteeming01@gmail.com>
2026-01-15 17:26:15 +0800

    remove srvx because it doesn't work with node 18

--- packages/adapter-netlify/test/preview.js
+++ packages/adapter-netlify/test/preview.js
@@ -1,19 +1,19 @@
-// This allows to run `netlify dev` without installing the Netlify CLI
-
+// This allows us to run `netlify dev` without installing the Netlify CLI
+// Don't include this file in type checking because @netlify/dev has transitive imports with type errors
 import { NetlifyDev } from '@netlify/dev';
+import http from 'node:http';
+import { getRequest, setResponse } from '@sveltejs/kit/node';
 
 const netlifyDev = new NetlifyDev({});
 
 const serverReady = netlifyDev.start();
 
-export default {
-	/**
-	 * @param {Request} req
-	 * @returns {Promise<Response>}
-	 */
-	async fetch(req) {
+http
+	.createServer(async (req, res) => {
 		await serverReady;
-		const res = await netlifyDev.handle(req);
-		return res ?? new Response('Not Found', { status: 404 });
-	}
-};
+		const request = await getRequest({ request: req, base: 'http://localhost:8888' });
+		const response =
+			(await netlifyDev.handle(request)) ?? new Response('Not Found', { status: 404 });
+		await setResponse(res, response);
+	})
+	.listen(8888);
