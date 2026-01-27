1e6e37d84d06815db4bb681613ff4875be3d6921
dominikg <dominik.goepel@gmx.de>
2026-01-27 20:46:28 +0100

    fix: account for build using _ instead of + in +server.js files

--- packages/kit/test/apps/basics/src/routes/load/state.js
+++ packages/kit/test/apps/basics/src/routes/load/state.js
@@ -16,8 +16,8 @@ export function increment(import_meta_url) {
  * @param {string} import_meta_url
  */
 export function reset_states(import_meta_url) {
-	if (!import_meta_url.endsWith('reset-states/+server.js')) {
-		throw new Error('must be called from reset-states/+server.js');
+	if (!import_meta_url.match(/reset-states\/[+_]server\.js$/)) { // allow _server for build entrypoint
+		throw new Error('must be called from reset-states/+server.js but was called from '+import_meta_url);
 	}
 	const parent_dir = new URL(`${import_meta_url}/../..`).toString();
 	Object.keys(counters)
