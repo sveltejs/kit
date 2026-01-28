dbe2945278248710eaa1ebc76e32fcd352cae8ae
dominikg <dominik.goepel@gmx.de>
2026-01-28 14:19:20 +0100

    format

--- packages/enhanced-img/test/utils.js
+++ packages/enhanced-img/test/utils.js
@@ -6,10 +6,13 @@ export const config = {
 	forbidOnly: !!process.env.CI,
 	// generous timeouts on CI
 	timeout: process.env.CI ? 45000 : 15000,
-	webServer: is_node18 ? undefined : { // do not try to build on node18
-		command: 'pnpm build && pnpm preview',
-		port: 4173
-	},
+	webServer: is_node18
+		? undefined
+		: {
+				// do not try to build on node18
+				command: 'pnpm build && pnpm preview',
+				port: 4173
+			},
 	retries: process.env.CI ? 2 : 0,
 	projects: [
 		{
