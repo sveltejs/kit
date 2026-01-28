53f748a530d6e7a30fe05433f57dfa4634920ce4
dominikg <dominik.goepel@gmx.de>
2026-01-28 14:18:52 +0100

    fix: don't call build on playwright webServer when using node18, we skip the test anyways and it would fail

--- packages/enhanced-img/test/utils.js
+++ packages/enhanced-img/test/utils.js
@@ -1,12 +1,12 @@
 import { devices } from '@playwright/test';
 import process from 'node:process';
-
+const is_node18 = process.versions.node.startsWith('18.');
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 export const config = {
 	forbidOnly: !!process.env.CI,
 	// generous timeouts on CI
 	timeout: process.env.CI ? 45000 : 15000,
-	webServer: {
+	webServer: is_node18 ? undefined : { // do not try to build on node18
 		command: 'pnpm build && pnpm preview',
 		port: 4173
 	},
