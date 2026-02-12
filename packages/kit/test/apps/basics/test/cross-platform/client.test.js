f0a13b6fb1873d22064f404cd0123583d8535219
Ben McCann <322311+benmccann@users.noreply.github.com>
2026-02-12 14:23:46 -0800

    chore: fix type checking in client test (#15303)

--- packages/kit/test/apps/basics/test/cross-platform/client.test.js
+++ packages/kit/test/apps/basics/test/cross-platform/client.test.js
@@ -326,9 +326,9 @@ test.describe('Navigation lifecycle functions', () => {
 		await page.goto('/navigation-lifecycle/scroll-state/a');
 		await scroll_to(0, 500);
 
-		/** @type {any} */
-		let beforeNav, onNav, afterNav;
 		const navPromise = new Promise((resolve) => {
+			/** @type {any} */
+			let beforeNav, onNav, afterNav;
 			page.on('console', (msg) => {
 				const text = msg.text();
 				if (text.startsWith('beforeNavigate:')) {
@@ -339,12 +339,12 @@ test.describe('Navigation lifecycle functions', () => {
 					afterNav = JSON.parse(text.slice('afterNavigate:'.length));
 				}
 
-				if (beforeNav && onNav && afterNav) resolve(undefined);
+				if (beforeNav && onNav && afterNav) resolve({ beforeNav, onNav, afterNav });
 			});
 		});
 
 		await clicknav('#to-b');
-		await navPromise;
+		const { beforeNav, onNav, afterNav } = await navPromise;
 
 		expect(beforeNav.fromScroll).toEqual({ x: 0, y: 500 });
 		expect(beforeNav.toScroll).toBe(null);
@@ -380,9 +380,9 @@ test.describe('Navigation lifecycle functions', () => {
 
 		const savedScrollY = afterNav.fromScroll.y;
 
-		/** @type {any} */
-		let beforeNav, onNav;
 		navPromise = new Promise((resolve) => {
+			/** @type {any} */
+			let beforeNav, onNav, afterNav;
 			page.on('console', (msg) => {
 				const text = msg.text();
 				if (text.startsWith('beforeNavigate:')) {
@@ -393,13 +393,15 @@ test.describe('Navigation lifecycle functions', () => {
 					afterNav = JSON.parse(text.slice('afterNavigate:'.length));
 				}
 
-				if (beforeNav && onNav && afterNav) resolve(undefined);
+				if (beforeNav && onNav && afterNav) resolve({ beforeNav, onNav, afterNav });
 			});
 		});
 
 		await page.goBack();
 		await page.waitForURL('/navigation-lifecycle/scroll-state/a');
-		await navPromise;
+		/** @type {any} */
+		let beforeNav, onNav;
+		({ beforeNav, onNav, afterNav } = await navPromise);
 
 		expect(beforeNav.fromScroll).toEqual({ x: 0, y: 0 });
 		expect(beforeNav.toScroll).toEqual({ x: 0, y: savedScrollY });
