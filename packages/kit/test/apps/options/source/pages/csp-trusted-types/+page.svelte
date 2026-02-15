5512fdc5aac97b0fa920d2ca0e921a1662ad5122
Tee Ming <chewteeming01@gmail.com>
2026-02-16 00:20:16 +0800

    add failing test

---
+++ packages/kit/test/apps/options/source/pages/csp-trusted-types/+page.svelte
@@ -0,0 +1,4 @@
+<p>
+	this page uses csr only and will error when Svelte tries to set the innerHTML without a trusted
+	type
+</p>
---
+++ packages/kit/test/apps/options/source/pages/csp-trusted-types/+page.ts
@@ -0,0 +1,2 @@
+// we want only CSR to test Svelte setting innerHTML succesfully
+export const ssr = false;
