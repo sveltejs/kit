372819229b1f21f562182a025bfed4f490159e80
Tee Ming <chewteeming01@gmail.com>
2026-02-16 14:29:02 +0800

    Apply suggestion from @teemingc

--- packages/kit/src/exports/vite/build/utils.spec.js
+++ packages/kit/src/exports/vite/build/utils.spec.js
@@ -6,7 +6,7 @@ test('create_dynamic_string escapes backslashes', () => {
 	const code = create_dynamic_string('css', [], input);
 	assert.equal(code, "function css() { return `div:after { content: '\\\\s'; }`; }");
 
-	const css; = eval(`(${code})()`);
+	const css = eval(`(${code})()`);
 
 	assert.equal(css, input);
 });
