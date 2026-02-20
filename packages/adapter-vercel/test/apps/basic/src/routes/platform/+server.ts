66bc1699e1b32d99aac2682e87737cf1e2b0501f
Elliott Johnson <hello@ell.iott.dev>
2026-02-20 12:47:48 -0700

    chore: misc platform test improvements

--- packages/adapter-vercel/test/apps/basic/src/routes/platform/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = (event) => {
-	return json({
-		has_platform: !!event.platform
-	});
-};
