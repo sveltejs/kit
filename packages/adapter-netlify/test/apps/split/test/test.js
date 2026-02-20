1e7c0f572d9ebef056daae84242797ab245d8d88
Tee Ming <chewteeming01@gmail.com>
2026-02-21 04:17:52 +0800

    fix and test

--- packages/adapter-netlify/test/apps/split/test/test.js
+++ packages/adapter-netlify/test/apps/split/test/test.js
@@ -2,5 +2,11 @@ import { expect, test } from '@playwright/test';
 
 test('routes to routes with dynamic params', async ({ page }) => {
 	await page.goto('/dynamic/123');
-	await expect(page.locator('p')).toHaveText('Hello world!');
+	await expect(page.locator('p')).toHaveText('id: 123');
+});
+
+test('client-side navigation fetches server load function data', async ({ page }) => {
+	await page.goto('/dynamic');
+	await page.click('a');
+	await expect(page.locator('p')).toHaveText('id: 1');
 });
