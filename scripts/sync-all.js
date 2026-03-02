1d9d811071506e61e1b7a923ee5cc19dcd692120
Tee Ming <chewteeming01@gmail.com>
2026-03-03 05:27:11 +0800

    fix sync-all

--- scripts/sync-all.js
+++ scripts/sync-all.js
@@ -14,12 +14,12 @@ for (const directories of [
 	for (const dir of fs.readdirSync(directories)) {
 		const cwd = path.join(directories, dir);
 
-		if (!fs.existsSync(path.join(cwd,'svelte.config.js'))) {
+		if (!fs.existsSync(path.join(cwd, 'svelte.config.js'))) {
 			continue;
 		}
 
 		chdir(cwd);
 
-		syncAll(await load_config({ cwd }), 'development');
+		syncAll(await load_config({ cwd }), 'development', cwd);
 	}
 }
