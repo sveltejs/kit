e8a36070ad33f2f3432636e4e7b4bef8b797ea1f
Tee Ming <chewteeming01@gmail.com>
2026-03-06 11:08:29 +0800

    maybe this?

--- packages/kit/src/runtime/client/client.js
+++ packages/kit/src/runtime/client/client.js
@@ -1740,7 +1740,7 @@ async function navigate({
 
 	const { activeElement } = document;
 
-	await commit_promise;
+	await (commit_promise ?? svelte.tick());
 
 	// we reset scroll before dealing with focus, to avoid a flash of unscrolled content
 	/** @type {Element | null | ''} */
