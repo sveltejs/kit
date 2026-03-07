4da760d24745591377b524408f510275fab55b18
Tee Ming <chewteeming01@gmail.com>
2026-03-08 00:45:50 +0800

    remove with_resolvers

--- packages/kit/src/runtime/server/index.js
+++ packages/kit/src/runtime/server/index.js
@@ -1,5 +1,3 @@
-/** @import { PromiseWithResolvers } from '../../utils/promise.js' */
-import { with_resolvers } from '../../utils/promise.js';
 import { IN_WEBCONTAINER } from './constants.js';
 import { respond } from './respond.js';
 import { set_private_env, set_public_env } from '../shared-server.js';
@@ -36,7 +34,9 @@ export class Server {
 
 			/** @type {typeof respond} */
 			this.respond = async (...args) => {
-				const { promise, resolve } = /** @type {PromiseWithResolvers<void>} */ (with_resolvers());
+				const { promise, resolve } = /** @type {PromiseWithResolvers<void>} */ (
+					Promise.withResolvers()
+				);
 
 				const previous = current;
 				current = promise;
