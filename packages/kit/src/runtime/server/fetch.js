50903673cdda3a5f035cf4c1c8d9194578c08d5a
Tee Ming <chewteeming01@gmail.com>
2026-03-11 07:47:24 +0800

    workaround vite 8 quirkiness

--- packages/kit/src/runtime/server/fetch.js
+++ packages/kit/src/runtime/server/fetch.js
@@ -4,6 +4,8 @@ import * as paths from '$app/paths/internal/server';
 import { read_implementation } from '__sveltekit/server';
 import { has_prerendered_path } from './utils.js';
 
+const prefix = paths.get_assets_prefix();
+
 /**
  * @param {{
  *   event: import('@sveltejs/kit').RequestEvent;
@@ -20,6 +22,16 @@ export function create_fetch({ event, options, manifest, state, get_cookie_heade
 	 * @type {typeof fetch}
 	 */
 	const server_fetch = async (info, init) => {
+		// Vite 8 mysteriously prefixes a relative base to imported asset paths only in production
+		// so we need to correct it back to an absolute path before normalising the request
+		if (
+			prefix === '.' &&
+			typeof info === 'string' &&
+			decodeURIComponent(info).slice(prefix.length + 1) in manifest._.server_assets
+		) {
+			info = info.slice(prefix.length);
+		}
+
 		const original_request = normalize_fetch_input(info, init, event.url);
 
 		// some runtimes (e.g. Cloudflare) error if you access `request.mode`,
@@ -81,7 +93,6 @@ export function create_fetch({ event, options, manifest, state, get_cookie_heade
 
 				// handle fetch requests for static assets. e.g. prebaked data, etc.
 				// we need to support everything the browser's fetch supports
-				const prefix = paths.get_assets_prefix();
 				const filename = (
 					decoded.startsWith(prefix) ? decoded.slice(prefix.length) : decoded
 				).slice(1);
