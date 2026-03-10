9a3f657b5b1ac6d468eb7cf2519487505d06451e
Tee Ming <chewteeming01@gmail.com>
2026-03-11 07:48:36 +0800

    format

--- packages/kit/test/apps/options/source/pages/performance/+page.svelte
+++ packages/kit/test/apps/options/source/pages/performance/+page.svelte
@@ -2,6 +2,9 @@
 	import Image from './favicon.png?no-inline';
 </script>
 
-<p>this app has paths.assets set so it should not use relative paths for imported assets in the client code</p>
+<p>
+	this app has paths.assets set so it should not use relative paths for imported assets in the
+	client code
+</p>
 
 <img src={Image} alt="svelte logo" />
