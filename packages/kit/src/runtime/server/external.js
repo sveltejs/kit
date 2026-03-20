fa879bab87aabab0bf02d306cdc0d8f7a1d717e7
Tee Ming <chewteeming01@gmail.com>
2026-03-20 20:16:23 +0800

    fix type

--- packages/kit/src/runtime/server/external.js
+++ packages/kit/src/runtime/server/external.js
@@ -8,9 +8,11 @@
 export let read_implementation = null;
 
 /**
- * @type {SSRManifest | null}
+ * The manifest will be set when the server starts. Exporting it this way allows
+ * us to access its value without having to pass it around as an argument.
+ * This is especially useful for public APIs such as `read` and `match`
  */
-export let manifest = null;
+export let manifest = /** @type {SSRManifest} */ (/** @type {unknown} */ (null));
 
 /**
  * @param {ReadImplementation} fn
