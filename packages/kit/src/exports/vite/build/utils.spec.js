2ad84530d042a5bea739a0ae44a7104cc241f5b4
Tee Ming <chewteeming01@gmail.com>
2026-03-24 03:26:16 +0800

    fix and test

--- packages/kit/src/exports/vite/build/utils.spec.js
+++ packages/kit/src/exports/vite/build/utils.spec.js
@@ -1,12 +1,58 @@
-import { assert, test } from 'vitest';
+import { assert, describe, test } from 'vitest';
 import { create_function_as_string } from './utils.js';
+import { fix_css_urls } from '../../../utils/css.js';
+import { escape_for_interpolation } from '../../../utils/escape.js';
 
-test('create_dynamic_string escapes backslashes', () => {
-	const input = "div:after { content: '\\s'; }";
-	const code = create_function_as_string('css', [], input);
-	assert.equal(code, "function css() { return `div:after { content: '\\\\s'; }`; }");
+describe('dynamic URL paths in CSS', () => {
+	const assets_path = './_app/immutable/assets';
+	const base_path = '.';
 
-	const css = eval(`(${code})()`);
-
-	assert.equal(css, input);
+	test.each([
+		{
+			name: 'escapes backslashes',
+			input: "div:after { content: '\\s'; } div { background: url(./image.png); }",
+			expected:
+				"div:after { content: '\\s'; } div { background: url(./_app/immutable/assets/image.png); }"
+		},
+		{
+			name: 'escapes backticks',
+			input: '.prose :where(code)::before { content: "`"; } div { background: url(./image.png); }',
+			expected:
+				'.prose :where(code)::before { content: "`"; } div { background: url(./_app/immutable/assets/image.png); }'
+		},
+		{
+			name: 'escapes dollar expressions',
+			input: 'div { content: "${not_a_placeholder}"; } div { background: url(./image.png); }',
+			expected:
+				'div { content: "${not_a_placeholder}"; } div { background: url(./_app/immutable/assets/image.png); }'
+		},
+		{
+			name: 'does not escape injected placeholder expressions',
+			input: '.prose code::before { content: "`"; } .bg { background: url(./image.png); }',
+			expected:
+				'.prose code::before { content: "`"; } .bg { background: url(./_app/immutable/assets/image.png); }'
+		}
+	])('$name', ({ input, expected }) => {
+		const transformed_css = fix_css_urls({
+			css: input,
+			vite_assets: new Set(['image.png']),
+			static_assets: new Set(['image_2.png']),
+			paths_assets: '__SVELTEKIT_ASSETS__',
+			base: '__SVELTEKIT_BASE__',
+			static_asset_prefix: '../../../'
+		});
+		const escaped = escape_for_interpolation(transformed_css, [
+			{
+				placeholder: '__SVELTEKIT_ASSETS__',
+				replacement: '${assets}'
+			},
+			{
+				placeholder: '__SVELTEKIT_BASE__',
+				replacement: '${base}'
+			}
+		]);
+		const code = create_function_as_string('css', ['assets', 'base'], escaped);
+		const output = eval(`(${code})('${assets_path}', '${base_path}')`);
+		assert.equal(output, expected);
+	});
 });
