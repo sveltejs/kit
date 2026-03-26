d692148afd037dac0ae1227e8713a3704d421460
Tee Ming <chewteeming01@gmail.com>
2026-03-26 09:16:16 +0800

    better treeshaking

--- packages/kit/src/runtime/app/environment/internal.js
+++ packages/kit/src/runtime/app/environment/internal.js
@@ -1,13 +1,16 @@
+// taken from https://github.com/benmccann/esm-env/blob/main/packages/esm-env/dev-fallback.js
+
 export const BROWSER =
 	typeof __SVELTEKIT_BROWSER__ === 'boolean'
 		? __SVELTEKIT_BROWSER__
 		: typeof window !== 'undefined';
 
-// taken from https://github.com/benmccann/esm-env/blob/main/packages/esm-env/dev-fallback.js
-// eslint-disable-next-line n/prefer-global/process
-const node_env = globalThis.process?.env?.NODE_ENV;
-
 export const DEV =
 	typeof __SVELTEKIT_DEV__ === 'boolean'
 		? __SVELTEKIT_DEV__
-		: node_env && !node_env.toLowerCase().startsWith('prod');
+		: // inlining the globalThis.process access instead of declaring a variable for it helps
+			// Rolldown treeshake it if it's unused
+			// eslint-disable-next-line n/prefer-global/process
+			globalThis.process?.env?.NODE_ENV &&
+			// eslint-disable-next-line n/prefer-global/process
+			!globalThis.process?.env?.NODE_ENV.toLowerCase().startsWith('prod');
