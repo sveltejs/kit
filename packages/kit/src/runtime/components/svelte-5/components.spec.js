bd878d05ba92cd209f4c41bf00e5e35cc1948dd4
Tee Ming <chewteeming01@gmail.com>
2026-03-28 01:24:20 +0800

    use static list

--- packages/kit/src/runtime/components/svelte-5/components.spec.js
+++ packages/kit/src/runtime/components/svelte-5/components.spec.js
@@ -1,14 +1,13 @@
-import { readdirSync, readFileSync } from 'node:fs';
+import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { compile } from 'svelte/compiler';
 import { assert, test } from 'vitest';
 
+// TODO: use import.meta.dirname when merging into the version-3 branch
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const components = readdirSync(__dirname).filter((f) => f.endsWith('.svelte'));
-
-test.each(components)('%s compiles in runes mode', (component) => {
+test.each(['layout.svelte', 'error.svelte'])('%s compiles in runes mode', (component) => {
 	const source = readFileSync(join(__dirname, component), 'utf-8');
 
 	const { metadata } = compile(source, { generate: false });
