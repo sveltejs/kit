204542b982e2f78864c91a3b9f8875bcf9310c2c
Max G. <max.gottschalk@mailbox.org>
2026-03-29 19:20:19 +0200

    fix: update image plugin to handle id filters and add image.svelte for testing

--- packages/enhanced-img/test/apps/basics/src/routes/image.svelte
+++ packages/enhanced-img/test/apps/basics/src/routes/image.svelte
@@ -1,9 +1,5 @@
 <script>
 	import logo from './logo.png?enhanced';
-
-	// see https://github.com/sveltejs/kit/issues/15616
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const rawImports = import.meta.glob('./image.svelte', { eager: true, query: '?raw' });
 </script>
 
 <!-- standard image -->
