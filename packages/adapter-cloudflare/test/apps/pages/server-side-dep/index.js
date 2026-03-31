f3ee12483732e84df6768ba6262e3a755f61ff82
Tee Ming <chewteeming01@gmail.com>
2026-04-01 05:12:19 +0800

    wip

--- packages/adapter-cloudflare/test/apps/pages/server-side-dep/index.js
+++
@@ -1,4 +0,0 @@
-/** @type {import('./index.js').sum} */
-export function sum(a, b) {
-	return a + b;
-}
--- packages/adapter-cloudflare/test/apps/workers/vite.config.js
+++ packages/adapter-cloudflare/test/apps/workers/vite.config.js
@@ -1,11 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '../../../index.js';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	build: {
 		minify: false
 	},
-	plugins: [sveltekit()]
+	plugins: [sveltekit({ adapter: adapter() })],
+	server: {
+		fs: {
+			allow: ['../../../../kit']
+		}
+	}
 };
 
 export default config;
