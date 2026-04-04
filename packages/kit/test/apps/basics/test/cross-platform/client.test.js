23f46cf450cd0f61ab57e1d2acd76c5d2da25171
Tee Ming <chewteeming01@gmail.com>
2026-04-04 23:02:50 +0800

    maybe this?

--- packages/kit/test/apps/basics/test/cross-platform/client.test.js
+++ packages/kit/test/apps/basics/test/cross-platform/client.test.js
@@ -1100,32 +1100,34 @@ test.describe('Routing', () => {
 	});
 
 	test('responds to <form target="_blank"> submission with new tab', async ({ page }) => {
-		test.slow();
-
 		await page.goto('/routing/form-target-blank');
 
 		let tabs = page.context().pages();
 		expect(tabs.length === 1);
 
-		const new_tab = page.waitForEvent('popup');
-		await page.locator('button', { hasText: 'Inside form' }).click();
-		await new_tab;
+		await page.waitForEvent('popup', {
+			async predicate(page) {
+				await page.locator('button', { hasText: 'Inside form' }).click();
+				return true;
+			}
+		});
 
 		tabs = page.context().pages();
 		expect(tabs.length > 1);
 	});
 
 	test('responds to <button formtarget="_blank" submission with new tab', async ({ page }) => {
-		test.slow();
-
 		await page.goto('/routing/form-target-blank');
 
 		let tabs = page.context().pages();
 		expect(tabs.length === 1);
 
-		const new_tab = page.waitForEvent('popup');
-		await page.locator('button', { hasText: 'Outside form' }).click();
-		await new_tab;
+		await page.waitForEvent('popup', {
+			async predicate(page) {
+				await page.locator('button', { hasText: 'Outside form' }).click();
+				return true;
+			}
+		});
 
 		tabs = page.context().pages();
 		expect(tabs.length > 1);
