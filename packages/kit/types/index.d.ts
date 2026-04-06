8b41fbccf7bab289160515eeb82e1ff91803259e
Tee Ming <chewteeming01@gmail.com>
2026-04-06 19:07:11 +0800

    generate type

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -3636,4 +3636,17 @@ declare module '$app/types' {
 	export type Asset = ReturnType<AppTypes['Asset']>;
 }
 
+declare module '@sveltejs/kit/vite/environment' {
+	import { SSRManifest } from '@sveltejs/kit';
+
+	export const manifest: SSRManifest;
+	export const env: Record<string, string>;
+}
+
+declare module '@sveltejs/kit/vite/environment/server' {
+	import { InternalServer } from 'types';
+
+	export { InternalServer as Server };
+}
+
 //# sourceMappingURL=index.d.ts.map
