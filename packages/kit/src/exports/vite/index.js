204c7b596e4b9699c7e9578b3cc14b92a4b21468
Rich Harris <rich.harris@vercel.com>
2026-04-09 19:25:57 -0400

    lint

--- packages/kit/src/exports/vite/index.js
+++ packages/kit/src/exports/vite/index.js
@@ -1,6 +1,6 @@
 /** @import { Options } from '@sveltejs/vite-plugin-svelte' */
 /** @import { PreprocessorGroup } from 'svelte/compiler' */
-/** @import { ConfigEnv, Manifest, Plugin, ResolvedConfig, UserConfig, ViteDevServer } from 'vite' */
+/** @import { ConfigEnv, Plugin, ResolvedConfig, UserConfig, ViteDevServer } from 'vite' */
 import fs from 'node:fs';
 import path from 'node:path';
 import process from 'node:process';
