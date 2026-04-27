8237c6434edd35960259a1cb8f1a62b88dcdc2bb
Simon Holthausen <simon.holthausen@vercel.com>
2026-04-27 17:21:35 +0200

    move/expose

--- packages/kit/src/exports/vite/dev/index.js
+++ packages/kit/src/exports/vite/dev/index.js
@@ -521,9 +521,7 @@ export async function dev(vite, vite_config, svelte_config, get_remotes) {
 				const { set_assets } = await vite.ssrLoadModule('$app/paths/internal/server');
 				set_assets(assets);
 
-				const memory_cache = await vite.ssrLoadModule(
-					`${runtime_base}/server/in-memory-query-cache.js`
-				);
+				const memory_cache = await vite.ssrLoadModule('@sveltejs/kit/node/in-memory-cache');
 
 				const server = new Server(manifest);
 
