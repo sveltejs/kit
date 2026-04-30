010cb7b8ca50c9253c56a06d92c4c9290d5e20f8
Tee Ming <chewteeming01@gmail.com>
2026-04-30 23:05:08 +0800

    Update packages/kit/src/exports/vite/options.js
    Co-authored-by: Rich Harris <richard.a.harris@gmail.com>

--- packages/kit/src/exports/vite/options.js
+++ packages/kit/src/exports/vite/options.js
@@ -7,12 +7,6 @@ const options = object({
 	adapter: validate(null, (input, keypath) => {
 		if (typeof input !== 'object' || !input.adapt) {
 			let message = `${keypath} should be an object with an "adapt" method`;
-
-			if (Array.isArray(input) || typeof input === 'string') {
-				// for the early adapter adopters
-				message += ', rather than the name of an adapter';
-			}
-
 			throw new Error(`${message}. See https://svelte.dev/docs/kit/adapters`);
 		}
 
