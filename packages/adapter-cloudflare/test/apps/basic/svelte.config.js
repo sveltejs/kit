9215ba470f442176fc91f515e4d5b648e0b78b2b
Tee Ming <chewteeming01@gmail.com>
2026-05-01 03:38:49 +0800

    fix test app

--- packages/adapter-cloudflare/test/apps/basic/svelte.config.js
+++ packages/adapter-cloudflare/test/apps/basic/svelte.config.js
@@ -1,16 +1,6 @@
-import adapter from '../../../index.js';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			vitePluginOptions: {
-				config(user_config) {
-					user_config.compatibility_date = '2026-04-22';
-				}
-			}
-		})
-	}
+	kit: {}
 };
 
 export default config;
--- packages/adapter-cloudflare/test/apps/basic/vite.config.js
+++ packages/adapter-cloudflare/test/apps/basic/vite.config.js
@@ -1,12 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import svelteConfig from './svelte.config.js';
+import adapter from '../../../index.js';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	build: {
 		minify: false
 	},
-	plugins: [sveltekit({ adapter: svelteConfig.kit?.adapter })],
+	plugins: [
+		sveltekit({
+			adapter: adapter({
+				vitePluginOptions: {
+					config(user_config) {
+						user_config.compatibility_date = '2026-04-22';
+					}
+				}
+			})
+		})
+	],
 	server: {
 		fs: {
 			allow: ['../../../../kit']
