d7bad874778f721c60357e1bc43a63dda7ef3e2f
Tee Ming <chewteeming01@gmail.com>
2026-05-04 23:37:59 +0800

    improve error message

--- packages/kit/src/exports/vite/options.js
+++ packages/kit/src/exports/vite/options.js
@@ -1,12 +1,11 @@
 /** @import { Validator } from '../../core/config/types.js' */
-
 import { object, validate } from '../../core/config/options.js';
 
 /** @type {Validator} */
 const options = object({
-	adapter: validate(null, (input, keypath) => {
+	adapter: validate(undefined, (input, keypath) => {
 		if (typeof input !== 'object' || !input.adapt) {
-			const message = `${keypath} should be an object with an "adapt" method`;
+			const message = `The SvelteKit Vite plugin ${keypath} should be an object with an "adapt" method`;
 			throw new Error(`${message}. See https://svelte.dev/docs/kit/adapters`);
 		}
 
