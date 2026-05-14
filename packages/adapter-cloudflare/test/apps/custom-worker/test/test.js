0665a78d74c8b635fb7be39fbe52809379b5bcf1
Tee Ming <chewteeming01@gmail.com>
2026-05-14 21:31:01 +0800

    don't need this now that we have an assets only test app

--- packages/adapter-cloudflare/test/apps/custom-worker/test/test.js
+++ packages/adapter-cloudflare/test/apps/custom-worker/test/test.js
@@ -5,5 +5,3 @@ test('custom worker entry point', async ({ request }) => {
 	expect(await response.text()).toContain('hello world!');
 	expect(response.headers()).toHaveProperty('x-custom-worker', 'true');
 });
-
-// TODO: test generating a fallback page
