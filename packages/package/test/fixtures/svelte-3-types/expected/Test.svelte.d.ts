d81e2b516ef61ff04116980b90d1c433d5224114
Tee Ming <chewteeming01@gmail.com>
2026-05-23 05:17:20 +0800

    update typescript fixtures

--- packages/package/test/fixtures/svelte-3-types/expected/Test.svelte.d.ts
+++ packages/package/test/fixtures/svelte-3-types/expected/Test.svelte.d.ts
@@ -1,7 +1,7 @@
 import { SvelteComponentTyped } from 'svelte';
 declare const __propDef: {
 	props: {
-		astring?: string;
+		astring?: string | undefined;
 	};
 	events: {
 		event: CustomEvent<boolean>;
