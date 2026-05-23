sveltejs/kit

commit 844f17d31ae21e9da2739925c0fca4705cd89a97

fix: avoid conflict for routes ending with optional parameters (#15861)
closes https://github.com/sveltejs/kit/issues/13704

This PR wraps the optional parameters permutations computation in a
guard so that routes ending with an optional parameter are treated
specially. Routes such as `/` and `/[[optional]]` should be legal with
`/` matching greedily.

---

### Please don't delete this checklist! Before submitting the PR, please
make sure you do the following:
- [ ] It's really useful if your PR references an issue where it is
discussed ahead of time. In many cases, features are absent for a
reason. For large changes, please create an RFC:
https://github.com/sveltejs/rfcs
- [ ] This message body should clearly illustrate what problems it
solves.
- [ ] Ideally, include a test that fails without this PR but passes with
it.

### Tests
- [ ] Run the tests with `pnpm test` and lint the project with `pnpm
lint` and `pnpm check`

### Changesets
- [ ] If your PR makes a change that should be noted in one or more
packages' changelogs, generate a changeset by running `pnpm changeset`
and following the prompts. Changesets that add features should be
`minor` and those that fix bugs should be `patch`. Please prefix
changeset messages with `feat:`, `fix:`, or `chore:`.

### Edits

- [ ] Please ensure that 'Allow edits from maintainers' is checked. PRs
without this option may be closed.

packages/kit/src/utils/routing.js
@@ -3,6 +3,8 @@ import { decode_params } from './url.js';
 
 const param_pattern = /^(\[)?(\.\.\.)?(\w+)(?:=(\w+))?(\])?$/;
 
+const root_group_pattern = /^\/\((?:.+)\)$/;
+
 /**
  * Creates the regex pattern, extracts parameter names, and generates types for a route
  * @param {string} id
@@ -12,7 +14,7 @@ export function parse_route_id(id) {
 	const params = [];
 
 	const pattern =
-		id === '/'
+		id === '/' || root_group_pattern.test(id)
 			? /^\/$/
 			: new RegExp(
 					`^${get_route_segments(id)

packages/kit/src/core/sync/create_manifest_data/conflict.js
@@ -1,0 +1,72 @@
+/** @param {import('types').RouteData[]} routes */
+export function prevent_conflicts(routes) {
+	/** @type {Map<string, string>} */
+	const lookup = new Map();
+
+	for (const route of routes) {
+		if (!route.leaf && !route.endpoint) continue;
+
+		/** @type {string[]} */
+		let permutations;
+
+		const normalized = normalize_route_id(route.id);
+
+		const ends_with_optional = /<\?.+?>$/.test(normalized);
+		if (!ends_with_optional && Array.from(normalized.matchAll(/<\?.+?>/g)).length) {
+			// find all permutations created by optional parameters
+			const split = normalized.split(/<\?(.+?)>/g);
+
+			permutations = [/** @type {string} */ (split[0])];
+
+			// turn `x/[[optional]]/y` into `x/y` and `x/[required]/y`
+			for (let i = 1; i < split.length; i += 2) {
+				const matcher = split[i];
+				const next = split[i + 1];
+
+				permutations = permutations.reduce((a, b) => {
+					a.push(b + next);
+					if (!(matcher === '*' && b.endsWith('//'))) a.push(b + `<${matcher}>${next}`);
+					return a;
+				}, /** @type {string[]} */ ([]));
+			}
+		} else {
+			permutations = [normalized];
+		}
+
+		for (const permutation of permutations) {
+			// remove leading/trailing/duplicated slashes caused by prior
+			// manipulation of optional parameters and (groups)
+			const key = permutation
+				.replace(/\/{2,}/, '/')
+				.replace(/^\//, '')
+				.replace(/\/$/, '');
+
+			if (lookup.has(key)) {
+				throw new Error(
+					`The "${lookup.get(key)}" and "${route.id}" routes conflict with each other`
+				);
+			}
+
+			lookup.set(key, route.id);
+		}
+	}
+}
+
+/** @param {string} id */
+function normalize_route_id(id) {
+	return (
+		id
+			// remove groups
+			.replace(/(?<=^|\/)\(.+?\)(?=$|\/)/g, '')
+
+			.replace(/\[[ux]\+([0-9a-f]+)\]/g, (_, x) =>
+				String.fromCharCode(parseInt(x, 16)).replace(/\//g, '%2f')
+			)
+
+			// replace `[param]` with `<*>`, `[param=x]` with `<x>`, and `[[param]]` with `<?*>`
+			.replace(
+				/\[(?:(\[)|(\.\.\.))?.+?(=.+?)?\]\]?/g,
+				(_, optional, rest, matcher) => `<${optional ? '?' : ''}${rest ?? ''}${matcher ?? '*'}>`
+			)
+	);
+}

packages/kit/src/core/sync/create_manifest_data/index.js
@@ -6,6 +6,7 @@ import { lookup } from 'mrmime';
 import { list_files, runtime_directory } from '../../utils.js';
 import { posixify, resolve_entry } from '../../../utils/filesystem.js';
 import { parse_route_id } from '../../../utils/routing.js';
+import { prevent_conflicts } from './conflict.js';
 import { sort_routes } from './sort.js';
 import { isSvelte5Plus } from '../utils.js';
 import {
@@ -545,69 +546,4 @@ function count_occurrences(needle, haystack) {
 		if (haystack[i] === needle) count += 1;
 	}
 	return count;
-}
-
-/** @param {import('types').RouteData[]} routes */
-function prevent_conflicts(routes) {
-	/** @type {Map<string, string>} */
-	const lookup = new Map();
-
-	for (const route of routes) {
-		if (!route.leaf && !route.endpoint) continue;
-
-		const normalized = normalize_route_id(route.id);
-
-		// find all permutations created by optional parameters
-		const split = normalized.split(/<\?(.+?)>/g);
-
-		let permutations = [/** @type {string} */ (split[0])];
-
-		// turn `x/[[optional]]/y` into `x/y` and `x/[required]/y`
-		for (let i = 1; i < split.length; i += 2) {
-			const matcher = split[i];
-			const next = split[i + 1];
-
-			permutations = permutations.reduce((a, b) => {
-				a.push(b + next);
-				if (!(matcher === '*' && b.endsWith('//'))) a.push(b + `<${matcher}>${next}`);
-				return a;
-			}, /** @type {string[]} */ ([]));
-		}
-
-		for (const permutation of permutations) {
-			// remove leading/trailing/duplicated slashes caused by prior
-			// manipulation of optional parameters and (groups)
-			const key = permutation
-				.replace(/\/{2,}/, '/')
-				.replace(/^\//, '')
-				.replace(/\/$/, '');
-
-			if (lookup.has(key)) {
-				throw new Error(
-					`The "${lookup.get(key)}" and "${route.id}" routes conflict with each other`
-				);
-			}
-
-			lookup.set(key, route.id);
-		}
-	}
-}
-
-/** @param {string} id */
-function normalize_route_id(id) {
-	return (
-		id
-			// remove groups
-			.replace(/(?<=^|\/)\(.+?\)(?=$|\/)/g, '')
-
-			.replace(/\[[ux]\+([0-9a-f]+)\]/g, (_, x) =>
-				String.fromCharCode(parseInt(x, 16)).replace(/\//g, '%2f')
-			)
-
-			// replace `[param]` with `<*>`, `[param=x]` with `<x>`, and `[[param]]` with `<?*>`
-			.replace(
-				/\[(?:(\[)|(\.\.\.))?.+?(=.+?)?\]\]?/g,
-				(_, optional, rest, matcher) => `<${optional ? '?' : ''}${rest ?? ''}${matcher ?? '*'}>`
-			)
-	);
 }

packages/kit/src/core/sync/create_manifest_data/index.spec.js
@@ -456,6 +456,82 @@ test('group preceding optional parameters', () => {
 	]);
 });
 
+test('optional parameters adjacent to another route', () => {
+	const { nodes, routes } = create('samples/optional-adjacent');
+
+	expect(nodes.map(simplify_node)).toEqual([
+		default_layout,
+		default_error,
+		{
+			component: 'samples/optional-adjacent/+page.svelte'
+		},
+		{
+			component: 'samples/optional-adjacent/[[optional]]/+page.svelte'
+		}
+	]);
+
+	expect(routes.map(simplify_route)).toEqual([
+		{
+			id: '/',
+			pattern: '/^/$/',
+			page: {
+				layouts: [0],
+				errors: [1],
+				leaf: nodes.findIndex((node) => node.component?.includes('/optional-adjacent/+page.svelte'))
+			}
+		},
+		{
+			id: '/[[optional]]',
+			pattern: '/^(?:/([^/]+))?/?$/',
+			page: {
+				layouts: [0],
+				errors: [1],
+				leaf: nodes.findIndex((node) => node.component?.includes('/[[optional]]'))
+			}
+		}
+	]);
+});
+
+test('optional parameters inside a group adjacent to another route', () => {
+	const { nodes, routes } = create('samples/group-optional');
+
+	expect(nodes.map(simplify_node)).toEqual([
+		default_layout,
+		default_error,
+		{
+			component: 'samples/group-optional/+page.svelte'
+		},
+		{
+			component: 'samples/group-optional/(group)/[[optional]]/+page.svelte'
+		}
+	]);
+
+	expect(routes.map(simplify_route)).toEqual([
+		{
+			id: '/(group)',
+			pattern: '/^/$/'
+		},
+		{
+			id: '/',
+			pattern: '/^/$/',
+			page: {
+				layouts: [0],
+				errors: [1],
+				leaf: nodes.findIndex((node) => node.component?.includes('/group-optional/+page.svelte'))
+			}
+		},
+		{
+			id: '/(group)/[[optional]]',
+			pattern: '/^(?:/([^/]+))?/?$/',
+			page: {
+				layouts: [0],
+				errors: [1],
+				leaf: nodes.findIndex((node) => node.component?.includes('/(group)/[[optional]]'))
+			}
+		}
+	]);
+});
+
 test('ignores files and directories with leading underscores', () => {
 	const { routes } = create('samples/hidden-underscore');
 

commit 131fdc7f86fa8e6c65eacde3d9092321154a9d85

fix: regression where routes starting and ending with a route group are not found (#15903)
closes #15901 

Regression was caused by
https://github.com/sveltejs/kit/pull/15861/changes#diff-a1a15b06db23e414ab4202232607550bcac6cfe2b0809777468e8dabb922b276R17
which greedily matches routes such as `/(group1)/anything/(group2)`
rather than just `/(group)` routes. Added a unit test to ensure this
doesn't regress

---

### Please don't delete this checklist! Before submitting the PR, please
make sure you do the following:
- [x] It's really useful if your PR references an issue where it is
discussed ahead of time. In many cases, features are absent for a
reason. For large changes, please create an RFC:
https://github.com/sveltejs/rfcs
- [x] This message body should clearly illustrate what problems it
solves.
- [x] Ideally, include a test that fails without this PR but passes with
it.

### Tests
- [ ] Run the tests with `pnpm test` and lint the project with `pnpm
lint` and `pnpm check`

### Changesets
- [x] If your PR makes a change that should be noted in one or more
packages' changelogs, generate a changeset by running `pnpm changeset`
and following the prompts. Changesets that add features should be
`minor` and those that fix bugs should be `patch`. Please prefix
changeset messages with `feat:`, `fix:`, or `chore:`.

### Edits

- [x] Please ensure that 'Allow edits from maintainers' is checked. PRs
without this option may be closed.

packages/kit/src/utils/routing.js
@@ -3,7 +3,7 @@ import { decode_params } from './url.js';
 
 const param_pattern = /^(\[)?(\.\.\.)?(\w+)(?:=(\w+))?(\])?$/;
 
-const root_group_pattern = /^\/\((?:.+)\)$/;
+const root_group_pattern = /^\/\((?:[^)]+)\)$/;
 
 /**
  * Creates the regex pattern, extracts parameter names, and generates types for a route

packages/kit/src/utils/routing.spec.js
@@ -274,6 +274,16 @@ describe('exec', () => {
 			route: '/[[...catchall]]',
 			path: '/\n',
 			expected: { catchall: '\n' }
+		},
+		{
+			route: '/(group)/[[optional]]',
+			path: '/',
+			expected: {}
+		},
+		{
+			route: '/(group1)/[slug]/(group2)',
+			path: '/123',
+			expected: { slug: '123' }
 		}
 	];
 