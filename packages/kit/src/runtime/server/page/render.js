4dc66a54d02e98d32eab743c91e2753efbad8381
Rich Harris <rich.harris@vercel.com>
2026-05-30 16:09:22 -0400

    fix

--- packages/kit/src/runtime/server/page/render.js
+++ packages/kit/src/runtime/server/page/render.js
@@ -413,7 +413,7 @@ export async function render_response({
 			properties.push(`assets: ${s(paths.assets)}`);
 		}
 
-		if (Object.keys(env).length > 0) { // TODO gate this on experimental.explicitEnvironmentVariables
+		if (__SVELTEKIT_EXPERIMENTAL_EXPLICIT_ENVIRONMENT_VARIABLES__) {
 			properties.push(`env: ${load_env_eagerly ? 'null' : devalue.uneval(env.rendered_env)}`);
 		} else if (client.uses_env_dynamic_public) {
 			properties.push(`env: ${load_env_eagerly ? 'null' : s(public_env)}`);
