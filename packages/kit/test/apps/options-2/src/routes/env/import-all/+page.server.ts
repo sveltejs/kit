4400b00b7fc14b26c81b8ea725881c4b6ec3dbd7
Rich Harris <rich.harris@vercel.com>
2026-06-03 16:31:04 -0400

    god i hate computers

--- packages/kit/test/apps/options-2/src/routes/env/import-all/+page.server.ts
+++ packages/kit/test/apps/options-2/src/routes/env/import-all/+page.server.ts
@@ -2,6 +2,7 @@ import * as env from '$app/env/private';
 
 export function load() {
 	return {
-		env
+		// we need to do this dance to strip out Symbol keys in Node 18, apparently?
+		env: Object.fromEntries(Object.entries(env))
 	};
 }
