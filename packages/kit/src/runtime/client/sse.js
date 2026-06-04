caf16246ba645d259512edf01812122875b0741d
Elliott Johnson <hello@ell.iott.dev>
2026-06-04 15:56:04 -0600

    reduce duplication

--- packages/kit/src/runtime/client/sse.js
+++ packages/kit/src/runtime/client/sse.js
@@ -1,3 +1,5 @@
+import { read_stream } from './stream.js';
+
 /**
  * @param {string} block
  * @returns {string | undefined}
@@ -21,43 +23,10 @@ function parse_sse_event_data(block) {
  * @param {ReadableStreamDefaultReader<Uint8Array>} reader
  */
 export async function* read_sse(reader) {
-	let done = false;
-	let buffer = '';
-	const decoder = new TextDecoder();
-
-	while (true) {
-		let split = buffer.indexOf('\n\n');
-		while (split !== -1) {
-			const block = buffer.slice(0, split);
-			buffer = buffer.slice(split + 2);
-
-			const data = parse_sse_event_data(block);
-			if (data) {
-				yield JSON.parse(data);
-			}
-
-			split = buffer.indexOf('\n\n');
-		}
-
-		if (done) {
-			const block = buffer.trim();
-			if (block) {
-				const data = parse_sse_event_data(block);
-				if (data) {
-					yield JSON.parse(data);
-				}
-			}
-			return;
-		}
-
-		const chunk = await reader.read();
-		done = chunk.done;
-		if (chunk.value) {
-			buffer += decoder.decode(chunk.value, { stream: true });
-		}
-
-		if (done) {
-			buffer += decoder.decode();
+	for await (const block of read_stream(reader, '\n\n')) {
+		const data = parse_sse_event_data(block);
+		if (data) {
+			yield JSON.parse(data);
 		}
 	}
 }
