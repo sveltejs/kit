61bf336848ae875866efec281b5cfc334275e9f1
Rich Harris <rich.harris@vercel.com>
2026-06-04 12:54:45 -0400

    fix

--- scripts/sync-all.js
+++ scripts/sync-all.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { chdir } from 'node:process';
-import { load_config } from '../packages/kit/src/core/config/index.js';
+import { extract_svelte_config, load_vite_config } from '../packages/kit/src/core/config/index.js';
 import { all as syncAll } from '../packages/kit/src/core/sync/sync.js';
 
 // This isn't strictly necessary, but it eliminates some annoying warnings in CI
@@ -23,6 +23,9 @@ for (const directories of [
 
 		chdir(cwd);
 
-		syncAll(await load_config(), cwd);
+		const vite_config = await load_vite_config();
+		const sveltekit_config = extract_svelte_config(vite_config);
+
+		syncAll(sveltekit_config, cwd);
 	}
 }
