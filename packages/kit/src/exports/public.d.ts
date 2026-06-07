a31fdc689cd759592d7541b85703241e5b986f72
Antoine Lethimonnier <43064022+WarningImHack3r@users.noreply.github.com>
2026-06-08 01:01:11 +0200

    docs: fix explicit env vars docs version (#15968)
    <!-- Explain the goal of the PR, why it is needed, and what has been
    changed to achieve that goal -->
    
    #15934 was created before Kit 2.62 got released, thus its docs updates
    naturally indicated 2.62 as a target version. However, 2.62 got released
    before merging this PR (or not including it). As a result, the "since
    version" is erroneous.
    
    This PR fixes this by bumping the numbers everywhere the original PR set
    them to 2.63, which actually includes the change.
    
    ---
    
    ### Please don't delete this checklist! Before submitting the PR, please
    make sure you do the following:
    - [ ] It's really useful if your PR references an issue where it is
    discussed ahead of time. In many cases, features are absent for a
    reason. For large changes, please create an RFC:
    https://github.com/sveltejs/rfcs
    - [x] This message body should clearly illustrate what problems it
    solves.
    - [ ] Ideally, include a test that fails without this PR but passes with
    it.
    
    ### Tests
    - [ ] Run the tests with `pnpm test` and lint the project with `pnpm
    lint` and `pnpm check`
    
    ### Changesets
    - [ ] If your PR makes a change that should be noted in one or more
    packages' changelogs, generate a changeset by running `pnpm changeset`
    and following the prompts. Changesets that add features should be
    `minor` and those that fix bugs should be `patch`. Please prefix
    changeset messages with `feat:`, `fix:`, or `chore:`.
    
    ### Edits
    
    - [x] Please ensure that 'Allow edits from maintainers' is checked. PRs
    without this option may be closed.

--- packages/kit/src/exports/public.d.ts
+++ packages/kit/src/exports/public.d.ts
@@ -478,7 +478,7 @@ export interface KitConfig {
 	experimental?: {
 		/**
 		 * Whether to enable explicit environment variables using `src/env.js` or `src/env.ts`.
-		 * @since 2.62.0
+		 * @since 2.63.0
 		 * @default false
 		 */
 		explicitEnvironmentVariables?: boolean;
