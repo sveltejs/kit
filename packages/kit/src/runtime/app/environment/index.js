27e0fe1e0af35a14dda32ec72f1f78eae1a3f336
Rich Harris <richard.a.harris@gmail.com>
2026-06-07 21:37:30 -0400

    Update packages/kit/src/runtime/app/environment/index.js
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- packages/kit/src/runtime/app/environment/index.js
+++ packages/kit/src/runtime/app/environment/index.js
@@ -3,6 +3,6 @@ export * from '../env/index.js';
 
 if (dev && __SVELTEKIT_EXPERIMENTAL_EXPLICIT_ENVIRONMENT_VARIABLES__) {
 	console.warn(
-		'Use `$app/env` instead of `$app.environment when `experimental.explicitEnvironmentVariables` is enabled'
+		'Use `$app/env` instead of `$app/environment` when `experimental.explicitEnvironmentVariables` is enabled'
 	);
 }
