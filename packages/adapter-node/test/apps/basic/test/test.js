37282f544a89ead8e9d3d89e20bb5f042c120d1f
Tee Ming Chew <chewteeming01@gmail.com>
2026-06-20 22:01:49 +1000

    Update test.js

--- packages/adapter-node/test/apps/basic/test/test.js
+++ packages/adapter-node/test/apps/basic/test/test.js
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('worker', async ({ page }) => {
+test('SSR-ed page', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.locator('h1')).toContainText('Hello world!');
 });
