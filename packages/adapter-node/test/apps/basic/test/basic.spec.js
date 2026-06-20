4def6702da1760b583086eccb38ec9b4646fa098
Tee Ming Chew <chewteeming01@gmail.com>
2026-06-20 22:15:14 +1000

    switch to vitest

---
+++ packages/adapter-node/test/apps/basic/test/basic.spec.js
@@ -0,0 +1,11 @@
+import { execSync } from 'node:child_process';
+import { beforeAll, expect, test } from 'vitest';
+
+beforeAll(() => {
+	execSync('pnpm build && pnpm preview', { stdio: 'inherit' });
+});
+
+test('server starts', async () => {
+	const response = await fetch('http://localhost:3000');
+	expect(response.ok).toBe(true);
+});
--- packages/adapter-node/test/apps/basic/test/test.js
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('SSR-ed page', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toContainText('Hello world!');
-});
