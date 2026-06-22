dceff15a52087b37617709b873eb958b6950351d
Tee Ming Chew <chewteeming01@gmail.com>
2026-06-22 15:57:47 +1000

    add csr test

--- packages/adapter-node/test/apps/basic/test/test.js
+++ packages/adapter-node/test/apps/basic/test/test.js
@@ -1,6 +1,13 @@
 import { expect, test } from '@playwright/test';
 
-test('SSR-ed page', async ({ page }) => {
+test('SSR', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.locator('h1')).toContainText('Hello world!');
+});
+
+test('CSR', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.locator('button')).toContainText('Toggle: false');
+	await page.locator('button').click();
+	await expect(page.locator('button')).toContainText('Toggle: true');
 });
