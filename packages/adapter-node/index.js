2286f5fb27b35260f9116e6c73cdd0a81408200b
Rich Harris <rich.harris@vercel.com>
2026-06-22 17:47:22 -0400

    lint

--- packages/adapter-node/index.js
+++ packages/adapter-node/index.js
@@ -1,5 +1,4 @@
 import { readFileSync } from 'node:fs';
-import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { rollup } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
