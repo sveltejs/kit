580cc2085d4ea22fdb3fa3fbde0b6341dca4ca71
Tee Ming Chew <chewteeming01@gmail.com>
2026-06-24 14:32:26 +1000

    add error to help with migration

---
+++ packages/kit/src/runtime/app/stores.js
@@ -0,0 +1,4 @@
+// TODO: remove this when enough people have migrated to Kit 3
+throw new Error(
+	'`$app/stores` has been removed in favour of `$app/state`. See https://svelte.dev/docs/kit/migrating-to-sveltekit-2#SvelteKit-2.12:-$app-stores-deprecated'
+);
