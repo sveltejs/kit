5c5acc22bd28cc20ce596f5aab76ee2fc3ae6673
Rich Harris <rich.harris@vercel.com>
2026-06-25 21:49:34 -0400

    consistency

--- packages/kit/src/runtime/form-utils.js
+++ packages/kit/src/runtime/form-utils.js
@@ -616,11 +616,11 @@ function get_type_prefix(field_type, is_array, input_value) {
  * @returns {any}
  */
 function deep_clone(value) {
-	if (value instanceof Date) {
-		return new Date(value.getTime());
-	}
-
 	if (value !== null && typeof value === 'object') {
+		if (value instanceof Date) {
+			return new Date(value.getTime());
+		}
+
 		if (value instanceof File) {
 			return value;
 		}
