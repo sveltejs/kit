9629cfc2ca30288b45d9f7c72e93096244b766a1
Tee Ming Chew <chewteeming01@gmail.com>
2026-06-30 22:15:25 +0800

    generate types

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -1668,6 +1668,7 @@ declare module '@sveltejs/kit' {
 		appPath: string;
 		/** Static files from `config.files.assets` and the service worker (if any). */
 		assets: Set<string>;
+		base: string;
 		mimeTypes: Record<string, string>;
 
 		/** @internal private fields */
