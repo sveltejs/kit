4f2eb125ea9e9e93b8a67b2afc5339e20c9f55e0
Tee Ming Chew <chewteeming01@gmail.com>
2026-07-03 19:29:36 +0800

    Update internal.d.ts

--- packages/kit/src/types/internal.d.ts
+++ packages/kit/src/types/internal.d.ts
@@ -247,7 +247,7 @@ export type RecursiveRequired<T> = {
 	// Recursive implementation of TypeScript's Required utility type.
 	// Will recursively continue until it reaches a primitive or Function
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-	[K in keyof T]-?: Extract<T[K], Function> extends never // If it does not have a Function type
+	[K in keyof T]-?: Extract<T[K], Function | (`${string}:` & {})> extends never // If it does not have a Function type
 		? RecursiveRequired<T[K]> // recursively continue through.
 		: T[K]; // Use the exact type for everything else
 };
