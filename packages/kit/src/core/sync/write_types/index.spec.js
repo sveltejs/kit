6067b3a96161b9ceec13693f9c91aeaed6f3ff6f
Simon Holthausen <simon.holthausen@vercel.com>
2026-07-03 22:37:48 +0200

    gah

--- packages/kit/src/core/sync/write_types/index.spec.js
+++ packages/kit/src/core/sync/write_types/index.spec.js
@@ -38,7 +38,7 @@ function run_test(dir) {
 	console.log('done', dir);
 }
 
-test.only('Creates correct $types', { timeout: 60000 }, () => {
+test('Creates correct $types', { timeout: 60000 }, () => {
 	// To save us from creating a real SvelteKit project for each of the tests,
 	// we first run the type generation directly for each test case, and then
 	// call `tsc` to check that the generated types are valid.
