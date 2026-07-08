fa432ff20a8e4c72673143933ae4f6baff3ac03c
Rich Harris <rich.harris@vercel.com>
2026-07-08 14:20:04 -0400

    note to self

--- packages/kit/src/runtime/server/remote.js
+++ packages/kit/src/runtime/server/remote.js
@@ -361,6 +361,7 @@ export async function collect_remote_data(data, event, state, options) {
 	 * @returns {Promise<App.Error>}
 	 */
 	function convert_error(error) {
+		// TODO 4.0 remove the `Promise.resolve(...)`
 		return Promise.resolve(handle_error_and_jsonify(event, state, options, error));
 	}
 
