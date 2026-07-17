d3d806d7196c5d63f97e54e3adb7bce382e8011c
Rich Harris <rich.harris@vercel.com>
2026-07-16 22:13:15 -0400

    deprecate instead of erroring, for now

--- packages/kit/src/exports/hooks/index.js
+++ packages/kit/src/exports/hooks/index.js
@@ -1,6 +1,15 @@
+/** @import { EnvVarConfig } from '@sveltejs/kit' */
+
 export { sequence } from './sequence.js';
 
-/** @internal */
-export function defineEnvVars() {
-	throw new Error(`\`defineEnvVars\` has moved — it should be imported from @sveltejs/kit/env`);
+/**
+ * Utility for defining [environment variables](https://svelte.dev/docs/kit/environment-variables),
+ * which are made available via `$app/env/public` and `$app/env/private`.
+ * @template {Record<string, EnvVarConfig<any>>} T
+ * @param {T} variables
+ * @returns {T}
+ * @deprecated Import `defineEnvVars` from `@sveltejs/kit/env` instead
+ */
+export function defineEnvVars(variables) {
+	return variables;
 }
