2198a09e09d3674600a9c74b91f733770ea798f0
Rich Harris <rich.harris@vercel.com>
2026-07-18 14:56:00 -0400

    WIP production sourcemaps

--- packages/kit/src/runtime/server/internal.js
+++ packages/kit/src/runtime/server/internal.js
@@ -1,10 +1,5 @@
+/* eslint-disable n/prefer-global/process */
 /** @import { SSRManifest } from '@sveltejs/kit'; */
-
-/**
- * @param {Error} _error
- * @returns void
- */
-export let fix_stack_trace = (_error) => {};
 
 /**
  * @type {((path: string) => ReadableStream<any>) | null}
@@ -12,13 +7,6 @@ export let fix_stack_trace = (_error) => {};
 export let read_implementation = null;
 
 export let manifest = /** @type {SSRManifest} */ (/** @type {unknown} */ (null));
-
-/**
- * @param {(error: Error) => void} fn
- */
-export function set_fix_stack_trace(fn) {
-	fix_stack_trace = fn;
-}
 
 /**
  * @param {(path: string) => ReadableStream<any>} fn
@@ -34,3 +22,5 @@ export function set_read_implementation(fn) {
 export function set_manifest(value) {
 	manifest = value;
 }
+
+export { fix_stack_trace, set_fix_stack_trace } from './sourcemaps.js';
