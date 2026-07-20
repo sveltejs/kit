64a98e75c9494621f501795ead7769cc3561999a
Tee Ming Chew <chewteeming01@gmail.com>
2026-07-21 02:33:30 +0800

    format

--- packages/kit/test/apps/async/vite.config.js
+++ packages/kit/test/apps/async/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
 			},
 			typescript: {
 				config(config) {
-					config.include.push('../unit-test/*.js', '../test/*.js', '../playwright.config.js')
+					config.include.push('../unit-test/*.js', '../test/*.js', '../playwright.config.js');
 				}
 			}
 		})
