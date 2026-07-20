0420ce778e441ba5605b968c26cd8b984a4a2d99
Rich Harris <rich.harris@vercel.com>
2026-07-19 20:52:36 -0400

    make AssetPath type stricter

--- packages/kit/src/core/sync/write_non_ambient.js
+++ packages/kit/src/core/sync/write_non_ambient.js
@@ -246,7 +246,7 @@ function generate_app_types(manifest_data, config) {
 		`\t\tLayoutParams(): {\n\t\t\t${layouts.join(';\n\t\t\t')}\n\t\t};`,
 		`\t\tPath(): ${Array.from(pathnames).join(' | ')};`,
 		'\t\tResolvedPathname(): `${"/" | `/${string}/`}${ReturnType<AppTypes[\'Path\']>}`;',
-		`\t\tAssetPath(): ${assets.concat('string & {}').join(' | ')};`,
+		`\t\tAssetPath(): ${assets.join(' | ')};`,
 		'\t}',
 		'}'
 	].join('\n');
