0ba765ade38e1ee0e56887d78386ff9b0409d660
Tee Ming Chew <chewteeming01@gmail.com>
2026-07-22 23:59:38 +0800

    prettier

--- packages/kit/src/exports/vite/dev/context.js
+++ packages/kit/src/exports/vite/dev/context.js
@@ -1,38 +1,42 @@
 /** @import { ValidatedConfig } from 'types' */
 /** @import { ViteDevServer } from 'vite' */
 
-/** @returns {Array<{ hash: string, file: string }>} */
-export function get_remotes() {
-	return import.meta.hot?.data.remotes;
-}
+/**
+ * @typedef {object} Context
+ * @property {ViteDevServer} dev_server
+ * @property {Array<{ hash: string, file: string }>} remotes
+ * @property {ValidatedConfig} svelte_config
+ */
 
-/** @param {Array<{ hash: string, file: string }>} r */
-export function set_remotes(r) {
-	if (import.meta.hot) {
-		import.meta.hot.data.remotes = r;
-	}
-}
-
-/** @returns {ValidatedConfig} */
-export function get_svelte_config() {
-	return import.meta.hot?.data.svelte_config;
-}
-
-/** @param {ValidatedConfig} config */
-export function set_svelte_config(config) {
-	if (import.meta.hot) {
-		import.meta.hot.data.svelte_config = config;
-	}
-}
+/** @type {Context} */
+const context = import.meta.hot?.data;
 
 /** @returns {ViteDevServer} */
 export function get_dev_server() {
-	return import.meta.hot?.data.dev_server;
+	return context.dev_server;
 }
 
 /** @param {ViteDevServer} server */
 export function set_dev_server(server) {
-	if (import.meta.hot) {
-		import.meta.hot.data.dev_server = server;
-	}
+	context.dev_server = server;
+}
+
+/** @returns {ValidatedConfig} */
+export function get_svelte_config() {
+	return context.svelte_config;
+}
+
+/** @param {ValidatedConfig} config */
+export function set_svelte_config(config) {
+	context.svelte_config = config;
+}
+
+/** @returns {Array<{ hash: string, file: string }>} */
+export function get_remotes() {
+	return context.remotes;
+}
+
+/** @param {Array<{ hash: string, file: string }>} r */
+export function set_remotes(r) {
+	context.remotes = r;
 }
