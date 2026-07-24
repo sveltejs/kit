114507eeec357df68d59ff4a9856362628808a58
Tee Ming Chew <chewteeming01@gmail.com>
2026-07-25 06:09:25 +0800

    prerender behaviour during development

--- packages/kit/src/runtime/server/page/load_data.js
+++ packages/kit/src/runtime/server/page/load_data.js
@@ -67,7 +67,7 @@ export async function load_server_data({ event, event_state, state, node, parent
 		}
 	);
 
-	if (state.prerendering) {
+	if (state.prerender_default) {
 		disable_search(url);
 	}
 
