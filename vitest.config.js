9720621d8bb756b0a410a761d9ca839e8f57eaec
Tee Ming Chew <chewteeming01@gmail.com>
2026-07-25 06:22:43 +0800

    revert

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vitest/config';
 
-// used by the Vitest IDE extension
 export default defineConfig({
 	test: {
 		projects: [
 			'packages/*',
-			// Kit app has a custom name for the vitest config
+			// prevent Vitest from crawling nested Vite apps in the kit test directory
+			// which do not use Vitest but have a vite.config.js file
 			'!packages/kit',
 			{
-				extends: import.meta.dirname + '/packages/kit/kit.vitest.config.js'
+				extends: 'packages/kit/kit.vitest.config.js',
+				root: 'packages/kit'
+			},
+			{
+				extends: 'packages/kit/test/apps/basics/vite.config.js',
+				root: 'packages/kit/test/apps/basics'
+			},
+			'packages/kit/test/build-errors',
+			{
+				extends: 'packages/kit/test/prerendering/basics/vite.config.js',
+				root: 'packages/kit/test/prerendering/basics',
+				test: {
+					name: 'kit-prerendering-basics'
+				}
 			}
 		]
 	}
