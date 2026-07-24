d7a3fdced4cde7566a94e4ede356ac9918d459b4
Rich Harris <rich.harris@vercel.com>
2026-07-24 10:47:10 -0400

    fix

--- packages/kit/test/apps/options-2/test/service-worker.test.js
+++ packages/kit/test/apps/options-2/test/service-worker.test.js
@@ -28,7 +28,7 @@ test('build /basepath/service-worker.js', async ({ baseURL, request }) => {
 	const self = {
 		addEventListener: () => {},
 		base: null,
-		build: null,
+		immutable: null,
 		image_src: undefined
 	};
 
@@ -40,7 +40,7 @@ test('build /basepath/service-worker.js', async ({ baseURL, request }) => {
 	});
 
 	expect(self.base).toBe('/basepath/');
-	expect(self.build?.[0]).toMatch(/\/basepath\/_app\/immutable\/bundle\.[\w-]+\.js/);
+	expect(self.immutable?.[0].path).toMatch(/^_app\/immutable\/bundle\.[\w-]+\.js/);
 	expect(self.image_src).toMatch(/\/basepath\/_app\/immutable\/assets\/image\.[\w-]+\.jpg/);
 });
 
