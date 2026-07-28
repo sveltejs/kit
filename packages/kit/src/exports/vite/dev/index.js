e5d69b225ed6e98adb8e3f9fe9d04c8852614df8
Tee Ming Chew <chewteeming01@gmail.com>
2026-07-28 17:55:05 +0800

    split out change

--- packages/kit/src/exports/vite/dev/index.js
+++ packages/kit/src/exports/vite/dev/index.js
@@ -244,12 +244,6 @@ export function dev(vite, vite_config, svelte_config, remotes, root, set_manifes
 				);
 
 				if (resolved_instrumentation) {
-					if (svelte_config.kit.adapter && !adapter_supports_instrumentation) {
-						throw new Error(
-							`${resolved_instrumentation} is unsupported in ${svelte_config.kit.adapter.name}.`
-						);
-					}
-
 					await runner.import(resolved_instrumentation);
 				}
 
