9eaa3f1e5003c8a76a99f1df86841e828c447afa
Nic Polumeyv <162764842+Nic-Polumeyv@users.noreply.github.com>
2026-07-29 21:29:55 -0400

    chore: only generate each route resolution module once when prerendering

--- packages/kit/src/core/postbuild/fallback.js
+++ packages/kit/src/core/postbuild/fallback.js
@@ -39,7 +39,8 @@ async function generate_fallback({ manifest_path, env, out_dir, origin, assets }
 		prerendering: {
 			fallback: true,
 			dependencies: new Map(),
-			remote_responses: new Map()
+			remote_responses: new Map(),
+			resolved_route_ids: new Set()
 		},
 		read: (file) => readFileSync(join(assets, file))
 	});
