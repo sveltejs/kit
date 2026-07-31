c6c2796db425be7c1a6a2ac277cb6f23edbc45d8
Tee Ming Chew <chewteeming01@gmail.com>
2026-07-31 22:37:03 +0800

    move into different branch

--- packages/adapter-vercel/test/apps/split/test/test.ts
+++ packages/adapter-vercel/test/apps/split/test/test.ts
@@ -6,10 +6,3 @@ test('split functions work', async ({ page }) => {
 	await page.goto('/a');
 	await expect(page.locator('p')).toContainText(/split config/);
 });
-
-test('edge middleware runs reroute before split function', async ({ page }) => {
-	await page.goto('/reroute');
-	await expect(page.locator('p')).toContainText('/reroute');
-	await page.goto('/en/reroute');
-	await expect(page.locator('p')).toContainText('/en/reroute');
-});
