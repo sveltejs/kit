21ba8e98f09968b16441032c446f9bfb5ffdfb54
Elliott Johnson <hello+git@ell.iott.dev>
2026-08-06 13:11:04 -0600

    ugh

--- packages/kit/src/runtime/server/index.js
+++ packages/kit/src/runtime/server/index.js
@@ -5,11 +5,9 @@ import { create_request_state } from './state.js';
 import { options, get_hooks } from '__SERVER__/internal.js';
 import { set_read_implementation, set_manifest, fix_stack_trace } from './internal.js';
 import { set_env } from '__sveltekit/env';
-import { SvelteKitError } from '@sveltejs/kit/internal';
 import { init_tracing } from '@sveltejs/kit/internal/server';
 import { DEV } from 'esm-env';
 import { init_transport } from '#app/internal/transport';
-import { set_app } from './app.js';
 
 /** @type {Promise<any>} */
 let init_promise;
