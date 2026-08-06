9b285d3096b1248c821b8ffc5a0996e01c8962e9
Tee Ming Chew <chewteeming01@gmail.com>
2026-08-06 19:25:46 +0800

    Update load_data.spec.js

--- packages/kit/src/runtime/server/page/load_data.spec.js
+++ packages/kit/src/runtime/server/page/load_data.spec.js
@@ -18,7 +18,7 @@ function create_fetch(event) {
 		/** @type {Pick<import('@sveltejs/kit').RequestEvent, 'fetch' | 'url' | 'request' | 'route'>} */ (
 			event
 		),
-		{ getClientAddress: () => '', error: false, depth: 0 },
+		{ getClientAddress: () => '', error: false, depth: 0, rerouted_path: null },
 		[],
 		true,
 		{
