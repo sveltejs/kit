1a1486fadc3d6b64a358f46e6db3c309099751c4
Rich Harris <rich.harris@vercel.com>
2026-08-10 16:07:17 -0400

    review

--- packages/kit/src/runtime/app/paths/public.js
+++
@@ -1,1 +0,0 @@
-export { asset, resolve, match } from '#app/paths';
--- packages/kit/src/runtime/app/paths/server.js
+++ packages/kit/src/runtime/app/paths/server.js
@@ -1,7 +1,7 @@
 import { base, assets, relative } from './internal/server.js';
 import { resolve_route, find_route } from '../../../utils/routing.js';
 import { decode_pathname } from '../../../utils/url.js';
-import { add_data_suffix } from '../../pathname.js';
+import { add_data_suffix } from '../../../pathname.js';
 import { try_get_request_store } from '@sveltejs/kit/internal/server';
 import { manifest } from '../../server/internal.js';
 import { get_hooks } from '__SERVER__/internal.js';
