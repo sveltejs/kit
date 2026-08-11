af70873892c32a032013f643adef162502c23391
Rich Harris <rich.harris@vercel.com>
2026-08-11 12:21:20 -0400

    WIP

--- packages/kit/scripts/generate-dts.js
+++ packages/kit/scripts/generate-dts.js
@@ -14,7 +14,7 @@ await createBundle({
 		'$app/forms': 'src/runtime/app/forms/public.d.ts',
 		'$app/navigation': 'src/runtime/app/navigation/public.d.ts',
 		'$app/paths': 'src/runtime/app/paths/public.d.ts',
-		'$app/server': 'src/runtime/app/server/index.js',
+		'$app/server': 'src/runtime/app/server/public.d.ts',
 		'$app/service-worker': 'src/runtime/app/service-worker/index.js',
 		'$app/state': 'src/runtime/app/state/public.d.ts'
 	},
