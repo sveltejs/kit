778c0f31451a1478e07d88bddbd6c8034d641d68
Tee Ming Chew <chewteeming01@gmail.com>
2026-08-12 01:35:44 +0800

    import from new subpath in test apps

--- packages/kit/test/apps/dev-only/src/hooks.server.js
+++ packages/kit/test/apps/dev-only/src/hooks.server.js
@@ -1,4 +1,4 @@
-/** @type {import("@sveltejs/kit").HandleServerError} */
+/** @type {import("@sveltejs/kit/hooks").HandleServerError} */
 export function handleError({ kind, error }) {
 	if (kind !== 'unknown') return error;
 	return { message: /**@type{any}*/ (error).message };
