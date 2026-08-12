7786c6c871fd25eccf8e65c11fbe02d76b47078b
Ottomated <otto@ottomated.net>
2026-08-11 20:39:26 -0700

    optional chaining

--- packages/kit/src/exports/vite/preview/index.js
+++ packages/kit/src/exports/vite/preview/index.js
@@ -205,13 +205,13 @@ export async function preview(vite, svelte_config) {
 		vite.middlewares.use(async (req, res) => {
 			const host = req.headers[':authority'] || req.headers.host;
 
-			const request = (svelte_config.adapter.getRequest ?? getRequest)({
+			const request = (svelte_config.adapter?.getRequest ?? getRequest)({
 				base: `${protocol}://${host}`,
 				request: req,
 				response: res
 			});
 
-			(svelte_config.adapter.setResponse ?? setResponse)(
+			(svelte_config.adapter?.setResponse ?? setResponse)(
 				res,
 				await server.respond(request, {
 					getClientAddress: () => {
