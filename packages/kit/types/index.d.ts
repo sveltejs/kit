f00e7235e18481a7bca0a364bef5f6111f089d6c
Ottomated <otto@ottomated.net>
2026-08-13 14:36:42 -0700

    regen types

--- packages/kit/types/index.d.ts
+++ packages/kit/types/index.d.ts
@@ -4,8 +4,8 @@
 declare module '@sveltejs/kit' {
 	import type { Plugin } from 'vite';
 	import type { RouteId as AppRouteId, LayoutParams as AppLayoutParams } from '$app/types';
-	import type { getRequest, setResponse } from '@sveltejs/kit/node';
 	import type { StandardSchemaV1 } from '@standard-schema/spec';
+	import type { getRequest, setResponse } from '@sveltejs/kit/node';
 	import type { Component } from 'svelte';
 	import type { Config } from '@sveltejs/kit/vite';
 	import type { ParamMatcher } from '@sveltejs/kit/params';
