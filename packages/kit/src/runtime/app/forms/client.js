b222c78921c571a7578a131e493b51644370579d
Rich Harris <rich.harris@vercel.com>
2026-08-13 21:58:50 -0400

    fix

--- packages/kit/src/runtime/app/forms/client.js
+++ packages/kit/src/runtime/app/forms/client.js
@@ -9,41 +9,9 @@ import {
 	is_current_location
 } from '../../client/client.js';
 import { notify_version } from '../../client/state.svelte.js';
-import { parse } from '#app/internal/transport';
 
 export { applyAction };
-
-/**
- * Use this function to deserialize the response from a form submission.
- * Usage:
- *
- * ```js
- * import { deserialize } from '$app/forms';
- *
- * async function handleSubmit(event) {
- *   const response = await fetch('/form?/action', {
- *     method: 'POST',
- *     body: new FormData(event.target)
- *   });
- *
- *   const result = deserialize(await response.text());
- *   // ...
- * }
- * ```
- * @template {Record<string, unknown> | undefined} Success
- * @template {Record<string, unknown> | undefined} Failure
- * @param {string} result
- * @returns {ActionResult<Success, Failure>}
- */
-export function deserialize(result) {
-	const parsed = JSON.parse(result);
-
-	if (parsed.data) {
-		parsed.data = parse(parsed.data);
-	}
-
-	return parsed;
-}
+export { deserialize } from './shared.js';
 
 /**
  * Shallow clone an element, so that we can access e.g. `form.action` without worrying
--- packages/kit/src/runtime/app/forms/server.js
+++ packages/kit/src/runtime/app/forms/server.js
@@ -1,5 +1,6 @@
 import { noop, disallow_on_server } from '../../../utils/functions.js';
 
+export { deserialize } from './shared.js';
+
 export const applyAction = disallow_on_server('applyAction');
-export const deserialize = disallow_on_server('deserialize');
 export const enhance = noop;
