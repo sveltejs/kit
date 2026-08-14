7500a7c208f1698b26528d6b569078070532a72e
Rich Harris <rich.harris@vercel.com>
2026-08-13 20:43:14 -0400

    fix

--- packages/kit/src/core/env.js
+++ packages/kit/src/core/env.js
@@ -39,8 +39,10 @@ export async function load_explicit_env(vite, kit, file, root, mode) {
 		logLevel: 'silent',
 		mode,
 		define: {
-			__SVELTEKIT_PAYLOAD__: 'undefined', // coming in through static import in env/internal.js but will end up unused
-			__SVELTEKIT_APP_VERSION__: JSON.stringify(kit.version.name) // needed by $app/env
+			// these are needed by $app/env
+			__SVELTEKIT_APP_VERSION__: JSON.stringify(kit.version.name),
+			__SVELTEKIT_DEV__: mode === 'development',
+			__SVELTEKIT_PAYLOAD__: 'undefined' // coming in through static import in env/internal.js but will end up unused
 		},
 		resolve: {
 			alias: [
