ff755eb7c853ba7dca34a98cd4b6b6e854575d77
Rich Harris <rich.harris@vercel.com>
2026-08-14 11:38:39 -0400

    fix unit tests

--- packages/kit/vitest.kit.config.js
+++ packages/kit/vitest.kit.config.js
@@ -15,7 +15,9 @@ const exclude = [
 export default /** @satisfies {import('vitest/config').ViteUserConfig} */ ({
 	plugins: [svelte({ compilerOptions: { hmr: false, experimental: { async: true } } })],
 	define: {
-		__SVELTEKIT_SERVER_TRACING_ENABLED__: false
+		__SVELTEKIT_SERVER_TRACING_ENABLED__: false,
+		__SVELTEKIT_APP_VERSION_POLL_INTERVAL__: 0,
+		__SVELTEKIT_APP_VERSION_CHECKS_ENABLED__: false
 	},
 	server: {
 		watch: {
