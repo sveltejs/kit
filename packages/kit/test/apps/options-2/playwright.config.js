b74b6f0e5811e9c41c9fc6786a09888c900ebc62
Nic Polumeyv <162764842+Nic-Polumeyv@users.noreply.github.com>
2026-08-19 11:04:34 -0400

    declare the options-2 variants in its playwright config

--- packages/kit/test/apps/options-2/playwright.config.js
+++ packages/kit/test/apps/options-2/playwright.config.js
@@ -1,3 +1,6 @@
 import { configure } from '../../utils.js';
 
-export default configure();
+export default configure({
+	'service-worker': { REGISTER_SERVICE_WORKER: 'true' },
+	'dynamic-env': { NODE_ENV: 'custom', DYNAMIC_PUBLIC_ENV: 'true' }
+});
