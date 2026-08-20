07566386e157b4b3435bff19f0ba9e0114be5378
Ottomated <otto@ottomated.net>
2026-08-20 16:39:20 -0400

    netlify

--- packages/adapter-netlify/index.js
+++ packages/adapter-netlify/index.js
@@ -264,12 +264,11 @@ function write_frameworks_config({ builder }) {
  * }} opts
  */
 function generate_serverless_function({ builder, routes, patterns, name, type, exclude }) {
-	const manifest = builder.generateManifest({
-		relativePath: '../server',
+	builder.writeServerEntrypoint(`.netlify/v1/server-${name}.js`, {
 		routes
 	});
 
-	const fn = generate_serverless_function_module(manifest, type);
+	const fn = generate_serverless_function_module(name, type);
 	const config = generate_config_export(name, patterns, exclude);
 
 	if (builder.hasServerInstrumentationFile()) {
@@ -288,11 +287,11 @@ function generate_serverless_function({ builder, routes, patterns, name, type, e
 }
 
 /**
- * @param {string} manifest
+ * @param {string} name
  * @param {ServerlessFunctionType} type
  * @returns {string}
  */
-function generate_serverless_function_module(manifest, type) {
+function generate_serverless_function_module(name, type) {
 	if (type === 'catch-all') {
 		// Netlify encodes the response body but `fetch` automatically decodes it.
 		// So, we need to remove the `content-encoding` header to allow Netlify
@@ -300,10 +299,11 @@ function generate_serverless_function_module(manifest, type) {
 		return `\
 import { applyReroute } from '@sveltejs/kit/adapter';
 import { init } from '../serverless.js';
+import { server } from '../server-${name}.js';
 
 const original_url_header = \`x-sveltekit-original-url-\${process.env.NETLIFY_FUNCTIONS_TOKEN}\`
 
-const respond = init(${manifest});
+const respond = init(server);
 
 export default async (request, context) => {
 	const catch_all_response = await respond(request, context);
@@ -329,10 +329,11 @@ export default async (request, context) => {
 	if (type === 'split') {
 		return `\
 import { init } from '../serverless.js';
+import { server } from '../server-${name}.js';
 
 const original_url_header = \`x-sveltekit-original-url-\${process.env.NETLIFY_FUNCTIONS_TOKEN}\`
 
-const respond = init(${manifest});
+const respond = init(server);
 
 export default async (request, context) => {
 	if (request.headers.has(original_url_header)) {
@@ -348,8 +349,9 @@ export default async (request, context) => {
 
 	return `\
 import { init } from '../serverless.js';
+import { server } from '../server-${name}.js';
 
-export default init(${manifest});
+export default init(server);
 `;
 }
 
@@ -426,19 +428,11 @@ async function generate_edge_functions({ builder }) {
 
 	builder.copy(`${files}/edge.js`, `${tmp}/entry.js`, {
 		replace: {
-			'0SERVER': `${relativePath}/index.js`,
-			MANIFEST: './manifest.js'
+			'0SERVER': `./server.js`
 		}
 	});
 
-	const manifest = builder.generateManifest({
-		relativePath
-	});
-
-	writeFileSync(`${tmp}/manifest.js`, `export const manifest = ${manifest};\n`);
-
-	/** @type {{ assets: Set<string> }} */
-	const { assets } = (await import(pathToFileURL(`${tmp}/manifest.js`).href)).manifest;
+	builder.writeServerEntrypoint(`${tmp}/server.js`);
 
 	const path = '/*';
 	// We only need to specify paths without the trailing slash because
@@ -449,7 +443,7 @@ async function generate_edge_functions({ builder }) {
 		`/${builder.getAppPath()}/version.json`,
 		// the base root and `trailingSlash: 'always'` pages are recorded with a trailing slash
 		...builder.prerendered.paths.map((path) => (path === '/' ? path : path.replace(/\/$/, ''))),
-		...Array.from(assets).flatMap((asset) => {
+		...Array.from(builder.manifest.assets).flatMap((asset) => {
 			if (asset.endsWith('/index.html')) {
 				const dir = asset.replace(/\/index\.html$/, '');
 				return [`${builder.config.paths.base}/${asset}`, `${builder.config.paths.base}/${dir}`];
