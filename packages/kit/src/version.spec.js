4f5e1ed871c297e8cb4d9a714c614822a9d2c5c8
Simon Holthausen <simon.holthausen@vercel.com>
2026-08-21 17:21:39 +0200

    it worked!

--- packages/kit/src/version.spec.js
+++ packages/kit/src/version.spec.js
@@ -5,7 +5,6 @@ import { assert, describe, it } from 'vitest';
 // runs the version generation as a side-effect of importing
 import '../scripts/generate-version.js';
 
-// lets see
 describe('@sveltejs/kit VERSION', () => {
 	it('should be the exact version from package.json', async () => {
 		const { VERSION } = await import(new URL('./version.js', import.meta.url).href);
@@ -14,43 +13,7 @@ describe('@sveltejs/kit VERSION', () => {
 		);
 		assert.equal(
 			VERSION,
-			Math.random() > 0.5 ? 'fail' : pkg.version,
-			'VERSION export in src/version.js does not equal version in package.json'
-		);
-	});
-
-	it('should be the exact version from package.json', async () => {
-		const { VERSION } = await import(new URL('./version.js', import.meta.url).href);
-		const pkg = JSON.parse(
-			readFileSync(fileURLToPath(new URL('../package.json', import.meta.url)), 'utf-8')
-		);
-		assert.equal(
-			VERSION,
-			Math.random() > 0.5 ? 'fail' : pkg.version,
-			'VERSION export in src/version.js does not equal version in package.json'
-		);
-	});
-
-	it('should be the exact version from package.json', async () => {
-		const { VERSION } = await import(new URL('./version.js', import.meta.url).href);
-		const pkg = JSON.parse(
-			readFileSync(fileURLToPath(new URL('../package.json', import.meta.url)), 'utf-8')
-		);
-		assert.equal(
-			VERSION,
-			Math.random() > 0.5 ? 'fail' : pkg.version,
-			'VERSION export in src/version.js does not equal version in package.json'
-		);
-	});
-
-	it('should be the exact version from package.json', async () => {
-		const { VERSION } = await import(new URL('./version.js', import.meta.url).href);
-		const pkg = JSON.parse(
-			readFileSync(fileURLToPath(new URL('../package.json', import.meta.url)), 'utf-8')
-		);
-		assert.equal(
-			VERSION,
-			Math.random() > 0.5 ? 'fail' : pkg.version,
+			pkg.version,
 			'VERSION export in src/version.js does not equal version in package.json'
 		);
 	});
