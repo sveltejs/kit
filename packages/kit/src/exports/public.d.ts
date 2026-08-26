be3e2ded6160851081106a015b5299951bced488
Ottomated <otto@ottomated.net>
2026-08-26 13:25:55 -0700

    move to vite property

--- packages/kit/src/exports/public.d.ts
+++ packages/kit/src/exports/public.d.ts
@@ -53,23 +53,25 @@ export interface Adapter {
 		instrumentation?: () => boolean;
 	};
 	/**
-	 * This function overrides the default behavior to convert an `http.IncomingMessage` to a `Request` object.
-	 * To call the original setRequest function, import it from `@sveltejs/kit/node`.
-	 * @since 3.0.0
-	 */
-	getRequest?: typeof getRequest;
-	/**
-	 * This function overrides the default behavior to write a `Response` object to an `http.ServerResponse`.
-	 * To call the original setResponse function, import it from `@sveltejs/kit/node`.
-	 * @since 3.0.0
-	 */
-	setResponse?: typeof setResponse;
-	/**
 	 * Creates an `Emulator`, which allows the adapter to influence the environment
 	 * during dev, build and prerendering.
 	 */
 	emulate?: () => MaybePromise<Emulator>;
 	vite?: {
+		/**
+		 * This function overrides the default behavior in vite dev and preview mode
+		 * to convert an `http.IncomingMessage` to a `Request` object.
+		 * To call the original setRequest function, import it from `@sveltejs/kit/node`.
+		 * @since 3.0.0
+		 */
+		getRequest?: typeof getRequest;
+		/**
+		 * This function overrides the default behavior in vite dev and preview mode
+		 * to write a `Response` object to an `http.ServerResponse`.
+		 * To call the original setResponse function, import it from `@sveltejs/kit/node`.
+		 * @since 3.0.0
+		 */
+		setResponse?: typeof setResponse;
 		plugins?: {
 			/**
 			 * Vite plugins placed before any of SvelteKit's own plugins.
