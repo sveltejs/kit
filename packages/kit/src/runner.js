77bee6c88f413f5271237ac433b3352544f28dec
Tee Ming Chew <chewteeming01@gmail.com>
2026-09-01 08:27:58 +0800

    Update runner.js

--- packages/kit/src/runner.js
+++ packages/kit/src/runner.js
@@ -1,13 +1,14 @@
-/** @import * as vite_types from 'vite' */
+/** @import * as vite from 'vite' */
+/** @import { ViteDevServer } from 'vite' */
 
 /**
- * @param {typeof vite_types} vite the vite module that created the server
- * @param {vite_types.ViteDevServer} server
+ * @param {typeof vite} vite_mod the vite module that created the server
+ * @param {ViteDevServer} server
  */
-export function get_runner(vite, server) {
+export function get_runner({ isRunnableDevEnvironment }, server) {
 	// `isRunnableDevEnvironment` does an `instanceof` check and will fail if
 	// we're using different instances of Vite
-	if (!vite.isRunnableDevEnvironment(server.environments.ssr)) {
+	if (!isRunnableDevEnvironment(server.environments.ssr)) {
 		throw new Error('The configured Vite SSR environment must be a RunnableDevEnvironment');
 	}
 
