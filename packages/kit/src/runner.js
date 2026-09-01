d0152a07c6d7ad51e310e1f3d1b85e4fd1818120
Tee Ming Chew <chewteeming01@gmail.com>
2026-09-01 08:25:12 +0800

    Update runner.js

--- packages/kit/src/runner.js
+++ packages/kit/src/runner.js
@@ -1,8 +1,8 @@
-/** @import * as vite from 'vite' */
+/** @import * as vite_types from 'vite' */
 
 /**
- * @param {typeof vite} vite the vite module that created the server
- * @param {vite.ViteDevServer} server
+ * @param {typeof vite_types} vite the vite module that created the server
+ * @param {vite_types.ViteDevServer} server
  */
 export function get_runner(vite, server) {
 	// `isRunnableDevEnvironment` does an `instanceof` check and will fail if
