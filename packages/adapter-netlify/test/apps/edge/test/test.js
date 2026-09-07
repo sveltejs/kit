cdf7222905d72e0bfd2f61f33a15afed7e4d7032
Tee Ming Chew <chewteeming01@gmail.com>
2026-09-07 19:44:17 +0800

    add test

--- packages/adapter-netlify/test/apps/edge/test/test.js
+++ packages/adapter-netlify/test/apps/edge/test/test.js
@@ -29,6 +29,14 @@ test('read from $app/server works', async ({ request }) => {
 	expect(await response.text()).toBe(content);
 });
 
+test('_redirects are copied to publish directory', () => {
+	const redirects = fs.readFileSync(
+		path.resolve(import.meta.dirname, '../build/_redirects'),
+		'utf-8'
+	);
+	expect(redirects).toContain('/redirect-me /greeting/redirected 301');
+});
+
 test('_headers are copied to publish directory', () => {
 	const headers = fs.readFileSync(path.resolve(import.meta.dirname, '../build/_headers'), 'utf-8');
 	expect(headers).toContain('X-Custom-Header: test-value');
