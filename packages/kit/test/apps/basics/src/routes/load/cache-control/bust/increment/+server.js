398441328e4a4617a5e7ed2f87489eb8cf15ca46
Tee Ming Chew <chewteeming01@gmail.com>
2026-09-24 23:33:41 +0800

    path: '/' is the default

--- packages/kit/test/apps/basics/src/routes/load/cache-control/bust/increment/+server.js
+++ packages/kit/test/apps/basics/src/routes/load/cache-control/bust/increment/+server.js
@@ -4,8 +4,7 @@ import { json } from '@sveltejs/kit';
 export function GET({ cookies }) {
 	cookies.set(
 		'cache-control-bust-count',
-		+(cookies.get('cache-control-bust-count') ?? 0) + 1 + '',
-		{ path: '/' }
+		+(cookies.get('cache-control-bust-count') ?? 0) + 1 + ''
 	);
 
 	return json({});
--- packages/kit/test/apps/options-2/src/hooks.server.js
+++ packages/kit/test/apps/options-2/src/hooks.server.js
@@ -1,9 +1,5 @@
 /** @type {import("@sveltejs/kit/hooks").Handle} */
 export async function handle({ event, resolve }) {
-	// isolates the in-memory count in count.remote.js per browser session
-	if (!event.cookies.get('session')) {
-		event.cookies.set('session', crypto.randomUUID(), { path: '/' });
-	}
 
 	return resolve(event, {
 		// needed for link header preload tests
